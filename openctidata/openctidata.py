053ae4f48919f29c4a1b2f0f75edead87a497c2f
Samuel Hassine <samuel@hassine.fr>
2019-07-10 18:57:15 +0200

    [all] Refactor all connectors to use the new integration architecture (#11)

--- openctidata/openctidata.py
+++ openctidata/openctidata.py
@@ -1,60 +1,77 @@
 # coding: utf-8
 
 import os
-import json
+import yaml
+import logging
+import time
 import urllib.request
-from pycti import OpenCTI
+
+from pycti import OpenCTIConnectorHelper
+
+CONNECTOR_IDENTIFIER = 'openctidata'
 
 
-class Openctidata:
-    def __init__(self, config, scheduler):
-        # Initialize config
-        self.config = config
-        self.scheduler = scheduler
+class OpenCTIData:
+    def __init__(self):
+        # Get configuration
+        config_file_path = os.path.dirname(os.path.abspath(__file__)) + '/config.yml'
+        self.config = dict()
+        if os.path.isfile(config_file_path):
+            config = yaml.load(open(config_file_path), Loader=yaml.FullLoader)
+            self.rabbitmq_hostname = config['rabbitmq']['hostname']
+            self.rabbitmq_port = config['rabbitmq']['port']
+            self.rabbitmq_username = config['rabbitmq']['username']
+            self.rabbitmq_password = config['rabbitmq']['password']
+            self.config['sectors_file_url'] = config['openctidata']['sectors_file_url']
+            self.config['entities'] = config['openctidata']['entities'].split(',')
+            self.config['interval'] = config['openctidata']['interval']
+            self.config['log_level'] = config['openctidata']['log_level']
+        else:
+            self.rabbitmq_hostname = os.getenv('RABBITMQ_HOSTNAME', 'localhost')
+            self.rabbitmq_port = os.getenv('RABBITMQ_PORT', 5672)
+            self.rabbitmq_username = os.getenv('RABBITMQ_USERNAME', 'guest')
+            self.rabbitmq_password = os.getenv('RABBITMQ_PASSWORD', 'guest')
+            self.config['sectors_file_url'] = os.getenv('OPENCTIDATA_SECTORS_FILE_URL', 'https://raw.githubusercontent.com/OpenCTI-Platform/datasets/master/data/sectors.json')
+            self.config['entities'] = os.getenv('OPENCTIDATA_ENTITIES', 'sector,region,country,city')
+            self.config['interval'] = os.getenv('OPENCTIDATA_INTERVAL', 1)
+            self.config['log_level'] = os.getenv('OPENCTIDATA_LOG_LEVEL', 'info')
 
-        # Initialize OpenCTI client
-        self.opencti = OpenCTI(
-            self.config['opencti']['api_url'],
-            self.config['opencti']['api_key'],
-            os.path.dirname(os.path.abspath(__file__)) + '/openctidata.log',
-            True
+        # Initialize OpenCTI Connector
+        self.opencti_connector = OpenCTIConnectorHelper(
+            CONNECTOR_IDENTIFIER,
+            self.config,
+            self.rabbitmq_hostname,
+            self.rabbitmq_port,
+            self.rabbitmq_username,
+            self.rabbitmq_password
         )
 
-    def set_config(self, config):
-        self.config = config
+    def get_log_level(self):
+        return self.config['log_level']
 
-    def get_config(self):
-        return self.config['openctidata']
+    def get_interval(self):
+        return int(self.config['interval']) * 60 * 60 * 24
 
     def run(self):
-        if 'sector' in self.config['openctidata']['entities']:
-            sectors_data = urllib.request.urlopen(self.config['openctidata']['sectors_file_url']).read()
-            sectors = json.loads(sectors_data)
-            for sector in sectors:
-                sector_id = self.opencti.create_identity_if_not_exists('Sector', sector['name'], sector['description'], None, sector['stix_id'])['id']
-                self.opencti.update_stix_domain_entity_field(sector_id, 'name', sector['name'])
-                self.opencti.update_stix_domain_entity_field(sector_id, 'description', sector['description'])
-                self.opencti.update_stix_domain_entity_field(sector_id, 'stix_id', sector['stix_id'])
+        enterprise_data = urllib.request.urlopen(self.config['sectors_file_url']).read()
+        self.opencti_connector.send_stix2_bundle(enterprise_data.decode('utf-8'))
 
-                for subsector in sector['subsectors']:
-                    subsector_id = self.opencti.create_identity_if_not_exists('Sector', subsector['name'], subsector['description'], None, subsector['stix_id'])['id']
-                    self.opencti.update_stix_domain_entity_field(subsector_id, 'name', subsector['name'])
-                    self.opencti.update_stix_domain_entity_field(subsector_id, 'description', subsector['description'])
-                    self.opencti.update_stix_domain_entity_field(subsector_id, 'stix_id', subsector['stix_id'])
 
-                    # Temporary for fixing multiple relations of previous version
-                    old_relations = self.opencti.get_stix_relations(sector_id, subsector_id)
-                    for old_relation in old_relations:
-                        self.opencti.delete_relation(old_relation['id'])
+if __name__ == '__main__':
+    openctidata = OpenCTIData()
 
-                    self.opencti.create_relation_if_not_exists(
-                        sector_id,
-                        'sector',
-                        subsector_id,
-                        'sector',
-                        'gathering',
-                        'Subsector from OpenCTI connector',
-                        '1900-01-01T00:00:00.000Z',
-                        '1900-01-01T00:00:00.000Z',
-                        5
-                    )
+    # Configure logger
+    numeric_level = getattr(logging, openctidata.get_log_level().upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: ' + openctidata.get_log_level())
+    logging.basicConfig(level=numeric_level)
+
+    logging.info('Starting the OpenCTIData connector...')
+    while True:
+        try:
+            logging.info('Fetching new OpenCTIData events...')
+            openctidata.run()
+            time.sleep(openctidata.get_interval())
+        except Exception as e:
+            logging.error(e)
+            time.sleep(30)
