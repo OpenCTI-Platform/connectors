880b619a4585d02597b6bac8e3d3c1270a2ae212
Samuel Hassine <samuel.hassine@luatix.org>
2019-08-04 20:42:16 +0200

    [cve] Update the connector

--- cve/src/cvetostix2.py
+++ cve/src/cvetostix2.py
@@ -5,36 +5,62 @@ import json
 from stix2 import MemoryStore
 from stix2 import Vulnerability
 from stix2 import Bundle
+from stix2 import Identity
+from stix2 import ExternalReference
+
 
 def convert(filename, output='output.json'):
+    # Create the default author
+    author = Identity(name='The MITRE Corporation', identity_class='organization')
     count = 0
     with open(filename) as json_file:
-        vList = []
+        vulnerabilities_bundle = [author]
         data = json.load(json_file)
 
         print("Loaded the file")
         for cves in data['CVE_Items']:
-                count += 1
-                # Getting the different fields
-                name = cves['cve']['CVE_data_meta']['ID']
-                description = cves['cve']['description']['description_data'][0]["value"]
-                cdate = cves['publishedDate']
-                mdate = cves['lastModifiedDate']
-                creator = cves['cve']['CVE_data_meta']['ASSIGNER']
+            count += 1
+            # Get the name
+            name = cves['cve']['CVE_data_meta']['ID']
 
-                # Creating the vulnerability with the extracted fields
-                vuln = Vulnerability(name=name, created=cdate, modified=mdate, description=description)
+            # Create external references
+            external_reference = ExternalReference(
+                source_name='NIST NVD',
+                url='https://nvd.nist.gov/vuln/detail/' + name
+            )
+            external_references = [external_reference]
+            for reference in cves['cve']['references']['reference_data']:
+                external_reference = ExternalReference(
+                    source_name=reference['refsource'],
+                    url=reference['url']
+                )
+                external_references.append(external_reference)
 
-                # Adding the vulnerability to the list of vulnerabilities    
-                vList.append(vuln)
+            # Getting the different fields
+            description = cves['cve']['description']['description_data'][0]["value"]
+            cdate = cves['publishedDate']
+            mdate = cves['lastModifiedDate']
+
+            # Creating the vulnerability with the extracted fields
+            vuln = Vulnerability(
+                name=name,
+                created=cdate,
+                modified=mdate,
+                description=description,
+                created_by_ref=author,
+                external_references=external_references
+            )
+            # Adding the vulnerability to the list of vulnerabilities
+            vulnerabilities_bundle.append(vuln)
     # Creating the bundle from the list of vulnerabilities
-    bundle = Bundle(vList)
+    bundle = Bundle(vulnerabilities_bundle)
     # Creating a MemoryStore object from the bundle
     memorystore = MemoryStore(bundle)
     # Dumping this object to a file
     memorystore.save_to_file(output)
 
     print("Successfully converted " + str(count) + " vulnerabilities")
+
 
 if __name__ == '__main__':
     convert(sys.argv[1], sys.argv[2])
