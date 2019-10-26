ed07d189bb45616f92c77cdb756a3ebab0b93362
Samuel Hassine <samuel@hassine.fr>
2019-10-26 14:32:52 +0200

    [mitre/cve/opencti] Store last_run date to avoid re-send data when connectors are restarted (#30)

--- mitre/src/mitre.py
+++ mitre/src/mitre.py
@@ -3,6 +3,7 @@ import yaml
 import time
 import urllib.request
 
+from datetime import datetime
 from pycti import OpenCTIConnectorHelper
 
 
@@ -13,8 +14,10 @@ class Mitre:
         config = yaml.load(open(config_file_path), Loader=yaml.FullLoader) if os.path.isfile(config_file_path) else {}
         self.helper = OpenCTIConnectorHelper(config)
         # Extra config
-        self.mitre_enterprise_file_url = os.getenv('MITRE_ENTERPRISE_FILE_URL') or config['mitre']['enterprise_file_url']
-        self.mitre_pre_attack_file_url = os.getenv('MITRE_PRE_ATTACK_FILE_URL') or config['mitre']['pre_attack_file_url']
+        self.mitre_enterprise_file_url = os.getenv('MITRE_ENTERPRISE_FILE_URL') or config['mitre'][
+            'enterprise_file_url']
+        self.mitre_pre_attack_file_url = os.getenv('MITRE_PRE_ATTACK_FILE_URL') or config['mitre'][
+            'pre_attack_file_url']
         self.mitre_interval = os.getenv('MITRE_INTERVAL') or config['mitre']['interval']
 
     def get_interval(self):
@@ -24,11 +27,36 @@ class Mitre:
         self.helper.log_info('Fetching MITRE datasets...')
         while True:
             try:
-                enterprise_data = urllib.request.urlopen(self.mitre_enterprise_file_url).read()
-                self.helper.send_stix2_bundle(enterprise_data.decode('utf-8'), self.helper.connect_scope)
-                pre_attack_data = urllib.request.urlopen(self.mitre_pre_attack_file_url).read()
-                self.helper.send_stix2_bundle(pre_attack_data.decode('utf-8'), self.helper.connect_scope)
-                time.sleep(self.get_interval())
+                # Get the current timestamp and check
+                timestamp = int(time.time())
+                current_state = self.helper.get_state()
+                if current_state is not None and 'last_run' in current_state:
+                    last_run = current_state['last_run']
+                    self.helper.log_info(
+                        'Connector last run: ' + datetime.utcfromtimestamp(last_run).strftime('%Y-%m-%d %H:%M:%S'))
+                else:
+                    last_run = None
+                    self.helper.log_info('Connector has never run')
+                # If the last_run is more than interval-1 day
+                if last_run is None or ((timestamp - last_run) > ((int(self.mitre_interval) - 1) * 60 * 60 * 24)):
+                    self.helper.log_info('Connector will run!')
+                    enterprise_data = urllib.request.urlopen(self.mitre_enterprise_file_url).read().decode('utf-8')
+                    self.helper.send_stix2_bundle(enterprise_data, self.helper.connect_scope)
+                    pre_attack_data = urllib.request.urlopen(self.mitre_pre_attack_file_url).read()
+                    self.helper.send_stix2_bundle(pre_attack_data.decode('utf-8'), self.helper.connect_scope)
+                    # Store the current timestamp as a last run
+                    self.helper.log_info('Connector successfully run, storing last_run as ' + str(timestamp))
+                    self.helper.set_state({'last_run': timestamp})
+                    # Sleep all interval
+                    self.helper.log_info(
+                        'Last_run stored, sleeping for: ' + str(round(self.get_interval() / 60 / 60 / 24, 2)) + ' days')
+                    time.sleep(self.get_interval())
+                else:
+                    new_interval = self.get_interval() - (timestamp - last_run)
+                    self.helper.log_info(
+                        'Connector will not run, sleeping for: ' + str(round(new_interval / 60 / 60 / 24, 2)) + ' days')
+                    # Sleep only remaining time
+                    time.sleep(new_interval)
             except (KeyboardInterrupt, SystemExit):
                 self.helper.log_info('Connector stop')
                 exit(0)
