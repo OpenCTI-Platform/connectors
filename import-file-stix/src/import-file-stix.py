daa3966d41c7ec0cbd3ed3230899285c7509c040
Samuel Hassine <samuel.hassine@luatix.org>
2019-12-07 00:23:48 +0100

    [import-file-stix] Update is a param

--- import-file-stix/src/import-file-stix.py
+++ import-file-stix/src/import-file-stix.py
@@ -13,10 +13,11 @@ class ImportFileStix:
 
     def _process_message(self, data):
         file_path = data['file_path']
+        update = data['update']
         file_uri = self.helper.opencti_url + file_path
         self.helper.log_info('Importing the file ' + file_uri)
         file_content = self.helper.api.fetch_opencti_file(file_uri)
-        bundles_sent = self.helper.send_stix2_bundle(file_content)
+        bundles_sent = self.helper.send_stix2_bundle(file_content, None, update)
         return ['Sent ' + str(len(bundles_sent)) + ' stix bundle(s) for worker import']
 
     # Start the main loop
