198387b72700d9b46b882b79f62945635fee7d71
Samuel Hassine <samuel@hassine.fr>
2020-12-22 21:36:29 +0100

    [sighting] More work on automatic sightings

--- sighting/src/sighting.py
+++ sighting/src/sighting.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 
-from pycti import OpenCTIConnectorHelper
+from pycti import OpenCTIConnectorHelper, get_config_variable
 
 
 class SightingConnector:
@@ -14,11 +14,58 @@ class SightingConnector:
             else {}
         )
         self.helper = OpenCTIConnectorHelper(config)
+        self.organization = get_config_variable(
+            "ORGANIZATION", ["sighting", "organization"], config
+        )
+        self.labels = get_config_variable(
+            "SIGHTING_LABELS", ["sighting", "labels"], config
+        ).split(",")
+
+        self.identity = self.helper.api.identity.create(
+            type="Organization",
+            name=self.organization,
+            description=self.organization + " created by the Sighting connector",
+        )
 
     def _process_message(self, data):
         entity_id = data["entity_id"]
         observable = self.helper.api.stix_cyber_observable.read(id=entity_id)
-        print(observable)
+        indicator = self.helper.api.indicator.read(
+            customAttributes="""
+                id
+                observables {
+                    edges {
+                        node {
+                            id
+                        }
+                    }
+                }
+            """,
+            filters=[
+                {
+                    "key": "pattern",
+                    "values": [observable["observable_value"]],
+                    "operator": "wildcard",
+                }
+            ],
+        )
+
+        # If no indicator, return
+        if indicator is None:
+            return
+
+        # Check if indicator and observable are already linked, if not, create the "based-on" relationship
+        current_observable_is_present = False
+        for indicator_observable in indicator["observables"]:
+            if indicator_observable["id"] == observable["id"]:
+                current_observable_is_present = True
+        if not current_observable_is_present:
+            # Create a relationship "based-on"
+            self.helper.api.stix_core_relationship.create(
+                fromId=indicator["id"],
+                toId=observable["id"],
+                relationship_type="based-on",
+            )
 
     # Start the main loop
     def start(self):
