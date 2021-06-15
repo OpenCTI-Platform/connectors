784ec7b14793a2def037571dc64152a72276051b
nor3th <>
2021-06-15 22:41:07 +0200

    [import-file-stix] Added stix knowledge to existing report (#1402)

--- import-file-stix/src/import-file-stix.py
+++ import-file-stix/src/import-file-stix.py
@@ -1,7 +1,8 @@
 import os
 import yaml
 import time
-
+from typing import List, Dict
+from stix2 import Report, Bundle, parse
 from stix2elevator import elevate
 from stix2elevator.options import initialize_options
 from pycti import OpenCTIConnectorHelper
@@ -18,20 +19,57 @@ class ImportFileStix:
         )
         self.helper = OpenCTIConnectorHelper(config)
 
-    def _process_message(self, data):
+    def _process_message(self, data: Dict) -> str:
         file_fetch = data["file_fetch"]
         file_uri = self.helper.opencti_url + file_fetch
-        self.helper.log_info("Importing the file " + file_uri)
+        self.helper.log_info(f"Importing the file {file_uri}")
+
         file_content = self.helper.api.fetch_opencti_file(file_uri)
         if data["file_mime"] == "text/xml":
+            self.helper.log_debug("Stix1 file. Attempting conversion")
             initialize_options()
             file_content = elevate(file_content)
+
+        entity_id = data.get("entity_id", None)
+        if entity_id:
+            self.helper.log_debug("Contextual import.")
+
+            bundle = parse(file_content)["objects"]
+
+            if self._contains_report(bundle):
+                self.helper.log_debug("Bundle contains report.")
+            else:
+                self.helper.log_debug("No Report in Stix file. Updating current report")
+                bundle = self._update_report(bundle, entity_id)
+
+            file_content = Bundle(objects=bundle).serialize()
+
         bundles_sent = self.helper.send_stix2_bundle(file_content)
         return "Sent " + str(len(bundles_sent)) + " stix bundle(s) for worker import"
 
     # Start the main loop
-    def start(self):
+    def start(self) -> None:
         self.helper.listen(self._process_message)
+
+    @staticmethod
+    def _contains_report(bundle: List) -> bool:
+        for elem in bundle:
+            if type(elem) == Report:
+                return True
+        return False
+
+    def _update_report(self, bundle: List, entity_id: int) -> List:
+        report = self.helper.api.report.read(id=entity_id)
+        report = Report(
+            id=report["standard_id"],
+            name=report["name"],
+            description=report["description"],
+            published=self.helper.api.stix2.format_date(report["created"]),
+            report_types=report["report_types"],
+            object_refs=bundle,
+        )
+        bundle.append(report)
+        return bundle
 
 
 if __name__ == "__main__":
