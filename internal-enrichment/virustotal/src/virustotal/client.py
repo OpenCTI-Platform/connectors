ee8e03f4d971e09630de0138b8c6523fa9d4efad
Axel Fahy <axelfahy@users.noreply.github.com>
2021-09-30 09:05:19 +0200

    [virustotal] add yara ruleset (#497)

---
+++ internal-enrichment/virustotal/src/virustotal/client.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""Virustotal client module."""
+import json
+import logging
+from typing import Any, Optional
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+logger = logging.getLogger(__name__)
+
+# Custom type to simulate a JSON format.
+JSONType = dict[str, Any]
+
+
+class VirusTotalClient:
+    """VirusTotal client."""
+
+    def __init__(self, base_url: str, token: str) -> None:
+        """Initialize Virustotal client."""
+        # Drop the ending slash if present.
+        self.url = base_url[:-1] if base_url[-1] == "/" else base_url
+        logger.info(f"[VirusTotal] URL: {self.url}")
+        self.headers = {
+            "x-apikey": token,
+            "accept": "application/json",
+            "content-type": "application/json",
+        }
+
+    def _query(self, url: str) -> Optional[JSONType]:
+        """
+        Execute a query to the Virustotal api.
+
+        The authentication is done using the headers with the token given
+        during the creation of the client.
+
+        Retries are done if the query fails.
+
+        Parameters
+        ----------
+        url : str
+            Url to query.
+
+        Returns
+        -------
+        JSON or None
+            The result of the query, as JSON or None in case of failure.
+        """
+        # Configure the adapter for the retry strategy.
+        retry_strategy = Retry(
+            total=3,
+            status_forcelist=[429, 500, 502, 503, 504],
+            method_whitelist=["HEAD", "GET", "OPTIONS"],
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http = requests.Session()
+        http.mount("https://", adapter)
+        try:
+            response = http.get(url, headers=self.headers)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            logger.error(f"[VirusTotal] Http error: {errh}")
+        except requests.exceptions.ConnectionError as errc:
+            logger.error(f"[VirusTotal] Error connecting: {errc}")
+        except requests.exceptions.Timeout as errt:
+            logger.error(f"[VirusTotal] Timeout error: {errt}")
+        except requests.exceptions.RequestException as err:
+            logger.error(f"[VirusTotal] Something else happened: {err}")
+        else:
+            try:
+                return response.json()
+            except json.JSONDecodeError as err:
+                logger.error(
+                    f"[VirusTotal] Error decoding the json: {err} - {response.text}"
+                )
+        return None
+
+    def get_file_info(self, hash_256: str) -> Optional[JSONType]:
+        """
+        Retrieve file information based on the given hash-256.
+
+        Parameters
+        ----------
+        hash_256 : str
+            Hash of the file to retrieve.
+
+        Returns
+        -------
+        JSON
+            File information, as JSON.
+        """
+        url = f"{self.url}/files/{hash_256}"
+        return self._query(url)
+
+    def get_yara_ruleset(self, ruleset_id: str) -> Optional[JSONType]:
+        """
+        Retrieve the YARA rules based on the given ruleset id.
+
+        Parameters
+        ----------
+        ruleset_id : str
+            Ruleset id to retrieve.
+
+        Returns
+        -------
+        JSON
+            YARA ruleset objects, as JSON.
+        """
+        url = f"{self.url}/yara_rulesets/{ruleset_id}"
+        return self._query(url)
--- internal-enrichment/virustotal/src/virustotal/virustotal.py
+++ internal-enrichment/virustotal/src/virustotal/virustotal.py
@@ -5,20 +5,25 @@ import json
 from pathlib import Path
 from time import sleep
 from typing import Optional
-import requests
+
+import plyara
+import plyara.utils
 from pycti import OpenCTIConnectorHelper, get_config_variable
 from stix2 import Indicator
 import yaml
+
+from .client import VirusTotalClient
 
 
 class VirusTotalConnector:
     """VirusTotal connector."""
 
     _CONNECTOR_RUN_INTERVAL_SEC = 60 * 60
+    _API_URL = "https://www.virustotal.com/api/v3"
 
     def __init__(self):
         # Instantiate the connector helper from config
-        config_file_path = Path(__file__).parent.resolve() / "config.yml"
+        config_file_path = Path(__file__).parent.parent.resolve() / "config.yml"
 
         config = (
             yaml.load(open(config_file_path, encoding="utf-8"), Loader=yaml.FullLoader)
@@ -26,30 +31,46 @@ class VirusTotalConnector:
             else {}
         )
         self.helper = OpenCTIConnectorHelper(config)
-        self.token = get_config_variable(
-            "VIRUSTOTAL_TOKEN", ["virustotal", "token"], config
-        )
+        token = get_config_variable("VIRUSTOTAL_TOKEN", ["virustotal", "token"], config)
         self.max_tlp = get_config_variable(
             "VIRUSTOTAL_MAX_TLP", ["virustotal", "max_tlp"], config
         )
-        self.api_url = "https://www.virustotal.com/api/v3"
-        self.headers = {
-            "x-apikey": self.token,
-            "accept": "application/json",
-            "content-type": "application/json",
-        }
+
+        self.client = VirusTotalClient(self._API_URL, token)
+
+        # Cache to store YARA rulesets.
+        self.yara_cache = {}
 
     def _create_yara_indicator(
         self, yara: dict, valid_from: Optional[int] = None
-    ) -> Indicator:
-        """Create an indicator containing the YARA results from VirusTotal."""
+    ) -> Optional[Indicator]:
+        """Create an indicator containing the YARA rule from VirusTotal."""
         valid_from_date = (
             datetime.datetime.min
             if valid_from is None
             else datetime.datetime.utcfromtimestamp(valid_from)
         )
-        source = yara.get("source", "No source provided")
         ruleset_id = yara.get("ruleset_id", "No ruleset id provided")
+        self.helper.log_info(f"[VirusTotal] Retrieving ruleset {ruleset_id}")
+
+        # Lookup in the cache for the ruleset id, otherwise, request VirusTotal API.
+        if ruleset_id in self.yara_cache:
+            self.helper.log_debug(f"Retrieving YARA ruleset {ruleset_id} from cache.")
+            ruleset = self.yara_cache[ruleset_id]
+        else:
+            self.helper.log_debug(f"Retrieving YARA ruleset {ruleset_id} from API.")
+            ruleset = self.client.get_yara_ruleset(ruleset_id)
+            self.yara_cache[ruleset_id] = ruleset
+
+        # Parse the rules to find the correct one.
+        parser = plyara.Plyara()
+        rules = parser.parse_string(ruleset["data"]["attributes"]["rules"])
+        rule_name = yara.get("rule_name", "No ruleset name provided")
+        rule = [r for r in rules if r["rule_name"] == rule_name]
+        if len(rule) == 0:
+            self.helper.log_warning(f"No YARA rule for rule name {rule_name}")
+            return None
+
         return self.helper.api.indicator.create(
             name=yara.get("rule_name", "No rulename provided"),
             description=json.dumps(
@@ -63,19 +84,14 @@ class VirusTotalConnector:
                     ),
                 }
             ),
-            pattern=f"[YARA] {ruleset_id} full rule available on {source}",
-            pattern_type="virustotal-yara",
+            pattern=plyara.utils.rebuild_yara_rule(rule[0]),
+            pattern_type="yara",
             valid_from=self.helper.api.stix2.format_date(valid_from_date),
             x_opencti_main_observable_type="StixFile",
         )
 
     def _process_file(self, observable):
-        response = requests.request(
-            "GET",
-            self.api_url + "/files/" + observable["observable_value"],
-            headers=self.headers,
-        )
-        json_data = json.loads(response.text)
+        json_data = self.client.get_file_info(observable["observable_value"])
         if "error" in json_data:
             if json_data["error"]["message"] == "Quota exceeded":
                 self.helper.log_info("Quota reached, waiting 1 hour.")
@@ -144,13 +160,18 @@ class VirusTotalConnector:
             if "crowdsourced_yara_results" in attributes:
                 self.helper.log_info("[VirusTotal] adding yara results to file.")
 
-                # Add YARA results
-                yaras = [
-                    self._create_yara_indicator(
-                        yara, attributes.get("creation_date", None)
+                # Add YARA rules (only if a rule is given).
+                yaras = list(
+                    filter(
+                        None,
+                        [
+                            self._create_yara_indicator(
+                                yara, attributes.get("creation_date", None)
+                            )
+                            for yara in attributes["crowdsourced_yara_results"]
+                        ],
                     )
-                    for yara in attributes["crowdsourced_yara_results"]
-                ]
+                )
 
                 self.helper.log_debug(f"[VirusTotal] Indicators created: {yaras}")
 
@@ -181,8 +202,3 @@ class VirusTotalConnector:
     def start(self):
         """Start the main loop."""
         self.helper.listen(self._process_message)
-
-
-if __name__ == "__main__":
-    virusTotalInstance = VirusTotalConnector()
-    virusTotalInstance.start()
