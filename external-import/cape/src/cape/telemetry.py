086fbd990e6dc952fd53ac23db21bd1a79396644
Max Watermolen <max.watermolen@infogressive.com>
2021-10-29 12:12:19 -0500

    inital

---
+++ external-import/cape/src/cape/telemetry.py
@@ -0,0 +1,571 @@
+from stix2.v21.bundle import Bundle
+from stix2.v21.common import ExternalReference
+from stix2.v21.observables import File, NetworkTraffic, WindowsRegistryKey
+from stix2.v21.sdo import Indicator, Report
+from cape.cape import (
+    cuckooPayload,
+    cuckooReport,
+    cuckooReportDomain,
+    cuckooReportHost,
+    cuckooReportICMP,
+    cuckooReportNetwork,
+    cuckooReportProcess,
+    cuckooReportTCPUDP,
+    cuckooReportTarget,
+    cuckooReportTTP,
+)
+from datetime import datetime
+from pycti.connector.opencti_connector_helper import OpenCTIConnectorHelper
+from stix2.v21 import (
+    IPv4Address,
+    DomainName,
+    Process,
+    Relationship,
+)
+
+
+class openCTIInterface:
+    def __init__(
+        self,
+        report: cuckooReport,
+        helper: OpenCTIConnectorHelper,
+        update,
+        labels=[],
+        CreateIndicator=False,
+        CuckooURL="",
+        EnableNetTraffic=False,
+        EnableRegKeys=False,
+        ReportScore=0,
+    ):
+        self.API = helper.api
+        self.helper = helper
+        self.CreateIndicator = CreateIndicator
+        self.report = report
+        self.labels = labels
+        self.update = update
+        self.cuckoo_url = CuckooURL
+        self.EnableNetTraffic = EnableNetTraffic
+        self.EnableRegKeys = EnableRegKeys
+        self.ReportScore = ReportScore
+        try:
+            self.octiLabels = self.API.label.list()  # Get labels Once ;)
+        except:
+            self.octiLabels = self.API.label.list()
+
+        self.processAndSubmit()  # This is where the magic happens
+
+    def get_or_create_label(self, label):
+        labels = self.octiLabels
+        for labelX in labels:
+            if label.lower() == labelX["value"].lower():
+                return labelX["id"]
+        try:
+            label = self.API.label.create(value=label)
+        except:
+            return None
+        return label["id"]
+
+    # Get and Return STIX Patterning
+    def getStixPattern(self, IOC, TYPE):
+        IOCTypes = {
+            "MD5": {
+                "prefix": "file:hashes.'MD5'",
+            },
+            "SHA1": {
+                "prefix": "file:hashes.'SHA-1'",
+            },
+            "SHA256": {
+                "prefix": "file:hashes.'SHA-256'",
+            },
+            "SHA512": {
+                "prefix": "file:hashes.'SHA-512'",
+            },
+            "IPV4": {
+                "prefix": "ipv4-addr:value",
+            },
+            "IPV6": {
+                "prefix": "ipv6-addr:value",
+            },
+            "FQDN": {
+                "prefix": "domain-name:value",
+            },
+            "URL": {"prefix": "url:value"},
+            "EMAIL": {
+                "prefix": "email-addr:value",
+            },
+            "MAC": {
+                "prefix": "mac-addr:value",
+            },
+            "PROCESS": {
+                "prefix": "process:command_line",
+            },
+            "NETWORK-TRAFFIC": {
+                "prefix": "network-traffic:dst_ref.type = 'ipv4-addr' AND network-traffic:dst_ref.value"
+            },
+        }
+        return f"[{IOCTypes[TYPE.upper()]['prefix']}='{IOC.upper()}']"
+
+    # STIX-erize IP info
+    def createIPObs(self, hosts) -> list(IPv4Address):
+        IPObs = []
+        for host in hosts:
+            host: cuckooReportHost
+            IPObs.append(IPv4Address(value=host.ip))
+            if self.CreateIndicator:
+                STIXPattern = self.getStixPattern(host.ip, "ipv4")
+                IPind = Indicator(
+                    name=host.ip, pattern=STIXPattern, pattern_type="stix"
+                )
+                IPObs.append(IPind)
+        return IPObs
+
+    # STIX-erize DNS info
+    def createDNSObs(
+        self, DNSOBJ
+    ):
+        DNSObs = []
+        DNSRel = []
+        for host in DNSOBJ:
+            host: cuckooReportDomain
+            IP = IPv4Address(value=host.ip)
+            DNS = DomainName(
+                value=host.domain
+            )  # , resolves_to_refs=IP.id) ref https://github.com/OpenCTI-Platform/client-python/issues/155
+            Rel = Relationship(
+                source_ref=DNS.id, target_ref=IP.id, relationship_type="resolves-to"
+            )
+
+            if self.CreateIndicator:
+                STIXPattern = self.getStixPattern(host.domain, "FQDN")
+                DNSind = Indicator(
+                    name=host.domain, pattern=STIXPattern, pattern_type="stix"
+                )
+                STIXPattern = self.getStixPattern(host["ip"], "ipv4")
+                IPind = Indicator(
+                    name=host.ip, pattern=STIXPattern, pattern_type="stix"
+                )
+                DNSObs.append(DNSind)
+                DNSObs.append(IPind)
+            DNSObs.append(IP)
+            DNSObs.append(DNS)
+            DNSRel.append(Rel)
+
+        return [DNSObs, DNSRel]
+
+    # Stix-erize Registry Keys
+    def createRegKeysObs(self, reg_keys):
+        regObs = []
+        for key in reg_keys:
+            regObs.append(WindowsRegistryKey(key=key))
+
+        return regObs
+
+    # Sacrifised a Chicken Nugget to the Abstract Programing entity and they provided
+    def createProcessObs(self, procs):
+        ProcessObjects = []
+
+        for proc in procs:
+            proc: cuckooReportProcess
+            ChildProcObjs = [[], []]
+
+            ext = self.extractChildren(
+                proc.children
+            )  # Get Child Procesees list [Child, Children of Child]
+
+            # This ID10T-ism is for not claiming grandchildren directly
+            ChildProcObjs[0].extend(ext[0])  # Add Child to Children array
+            ChildProcObjs[1].extend(
+                ext[1]
+            )  # Add Children of Child to ChildrenOfChildren array (For STIX)
+            IDs = [child.id for child in ChildProcObjs[0]]  # Find all Child_ref ID's
+
+            MainProc = Process(  # Create Parent Process
+                pid=proc.pid,
+                command_line=proc.environ.command_line,
+                child_refs=IDs,
+            )
+
+            # Flatten List Insanity
+            ProcessObjects.append(MainProc)  # Add Parent Process to Bundle
+            ProcessObjects.extend(ChildProcObjs[0])  # Add Direct Children to Bundle
+            ProcessObjects.extend(
+                ChildProcObjs[1]
+            )  # Add Children of Children (Main Process list) to bundle
+
+        return ProcessObjects  # list(Process)
+
+    def extractChildren(self, children):
+        grand = []
+        someObsecurechildren = []
+        for Child in children:
+            Child: cuckooReportProcess
+            childs = [[], []]
+
+            if not len(Child.children) > 0:  # No Granchildren this is easy ;)
+                ChildProc = Process(
+                    pid=Child.pid,
+                    command_line=Child.environ.command_line,
+                    child_refs=[],
+                )
+                grand.append(ChildProc)
+            else:
+                ext = self.extractChildren(Child.children)
+
+                # This ID10T-ism is for not claiming grandchildren directly
+                childs[0].extend(ext[0])  # Add Child to Children array
+                childs[1].extend(
+                    ext[1]
+                )  # Add Children of Child to ChildrenOfChildren array (For STIX)
+                IDs = [childX.id for childX in childs[0]]  # Find all Child_ref ID's
+
+                ChildProc = Process(  # Create Child-Parent Process iNcePtIon
+                    pid=Child.pid,
+                    command_line=Child.environ.command_line,
+                    child_refs=IDs,
+                )
+
+                grand.append(ChildProc)  # Add Main Child Process to Bundle
+                someObsecurechildren.extend(childs[1])  # Add Direct Children to Bundle
+                someObsecurechildren.extend(
+                    childs[0]
+                )  # Add Children of Children (Main Process list) to bundle
+
+        return [grand, someObsecurechildren]
+
+    def createNetTrafficBlock(
+        self, traffic: cuckooReportTCPUDP, protocol
+    ) -> NetworkTraffic:
+        srcIP = IPv4Address(value=traffic.src)
+        dstIP = IPv4Address(value=traffic.dst)
+        traffic = NetworkTraffic(
+            src_ref=srcIP.id,
+            dst_ref=dstIP.id,
+            src_port=traffic.sport,
+            dst_port=traffic.dport,
+            protocols=[protocol],
+        )
+        return traffic
+
+    def createNetICMPlock(self, traffic: cuckooReportICMP, protocol):
+        srcIP = IPv4Address(value=traffic.src)
+        dstIP = IPv4Address(value=traffic.dst)
+        traffic = NetworkTraffic(
+            src_ref=srcIP.id, dst_ref=dstIP.id, protocols=[protocol]
+        )
+        return traffic
+
+    def createNetTrafficObs(
+        self, traffic: cuckooReportNetwork
+    ):
+        TCPCons, UDPCons, ICMPCons = [], [], []
+        for packet in traffic.tcp:
+            TCPCons.append(self.createNetTrafficBlock(packet, "tcp"))
+
+        for packet in traffic.udp:
+            UDPCons.append(self.createNetTrafficBlock(packet, "udp"))
+
+        for packet in traffic.icmp:
+            ICMPCons.append(self.createNetICMPlock(packet, "icmp"))
+
+        return {"TCP": TCPCons, "UDP": UDPCons, "ICMP": ICMPCons}
+
+    def createPrimaryBinary(self, file: cuckooReportTarget, external_references):
+        hashes = {
+            "MD5": file.file.md5.upper(),
+            "SHA-1": file.file.sha1.upper(),
+            "SHA-256": file.file.sha256.upper(),
+            "SHA-512": file.file.sha512.upper(),
+            "SSDEEP": file.file.ssdeep.upper(),
+        }
+
+        STIXPattern = self.getStixPattern(file.sha256, "sha256")
+
+        size = 0
+        try:
+            if file.size:
+                size = file.size
+        except:
+            pass
+
+        Filex = File(hashes=hashes, size=size, name=file.name, mime_type=file.type)
+        ind = Indicator(
+            name=file.file.name,
+            pattern=STIXPattern,
+            pattern_type="stix",
+            external_references=external_references,
+        )
+
+        rel = Relationship(
+            source_ref=Filex.id, relationship_type="based-on", target_ref=ind.id
+        )
+
+        return [Filex, ind, rel]
+
+    def createBinarieObs(self, objects):
+        iocs = []
+
+        for file in objects:
+            file: cuckooPayload
+            hashes = {
+                "MD5": file.md5.upper(),
+                "SHA-1": file.sha1.upper(),
+                "SHA-256": file.sha256.upper(),
+                "SHA-512": file.sha512.upper(),
+                "SSDEEP": file.ssdeep.upper(),
+            }
+            iocs.append(
+                File(hashes=hashes, size=file.size, name=file.name, mime_type=file.type)
+            )
+            if self.CreateIndicator:
+                STIXPattern = self.getStixPattern(file.sha256.upper(), "sha256")
+                fileind = Indicator(
+                    name=file.name, pattern=STIXPattern, pattern_type="stix"
+                )
+                iocs.append(fileind)
+
+        return iocs
+
+    def createCuckooReport(
+        self, report: cuckooReport, object_refs=[], external_refs=[]
+    ):
+        if report.target.category == "url":
+            name = f"CAPE Sandbox Report {str(report.info.id)} - {report.target.url}"
+        else:
+            name = (
+                f"CAPE Sandbox Report {str(report.info.id)} - {report.target.file.name}"
+            )
+
+        if report.target.category == "url":
+            desc = f"CAPE Sandbox Report {str(report.info.id)} - {report.target.url}"
+        else:
+            desc = f"CAPE Sandbox Report {str(report.info.id)} - {report.target.file.name}\nAnalyzied File:\n  SHA256: {report.target.file.sha256}\n  SHA1:{report.target.file.sha1}\n  MD5:{report.target.file.md5}"
+
+        conf = int(report.malscore * 100)
+        reportLabels = ["sandbox"]
+
+        if conf > 100:
+            conf = 100
+
+        if conf > 70:
+            reportLabels.append("Malicious")
+
+        if report.detections:
+            reportLabels.append(report.detections)
+
+        labelIDs = []
+        for label in reportLabels:
+            labelIDs.append(self.get_or_create_label(label))
+
+        report = Report(
+            name=name,
+            report_types="sandbox-report",
+            published=datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            object_refs=object_refs,
+            description=desc,
+            external_references=external_refs,
+            confidence=conf,
+            labels=labelIDs,
+        )
+
+        return report
+
+    def Get_TTP(self, TTP: cuckooReportTTP):
+        try:
+            ATP = self.API.attack_pattern.read(
+                filters={
+                    "key": "x_mitre_id",
+                    "values": [TTP.ttp],
+                }
+            )
+        except:
+            return None
+
+        return ATP
+
+    def getTTPs(self, TTPs):
+        ATPs = []
+        for TTP in TTPs:
+            TTP: cuckooReportTTP
+            TTPX = self.Get_TTP(TTP)
+            if TTPX:
+                ATPs.append(self.Get_TTP(TTPX))
+
+        return ATPs
+
+    def Get_Malware(self, Detection: str):
+        try:
+            Malware = self.API.malware.read(
+                filters={
+                    "key": "name",
+                    "values": [Detection],
+                }
+            )
+        except:
+            return None
+
+        return Malware
+
+    def get_related(
+        self,
+        ips,
+        fqdns,
+        processes,
+        network_traffic,
+        dropped_binaries,
+        AttackPatterns,
+        reg_keys,
+        Malware,
+    ):
+        IDs = []
+
+        IDs.append(Malware)
+
+        for ip in ips:
+            IDs.append(ip)
+
+        for fqdn in fqdns:
+            IDs.append(fqdn)
+
+        for process in processes:
+            IDs.append(process)
+
+        for db in dropped_binaries:
+            IDs.append(db)
+
+        for ATP in AttackPatterns:
+            IDs.append(ATP)
+
+        if reg_keys:
+            for key in reg_keys:
+                IDs.append(key)
+
+        if network_traffic:
+            for type in ["TCP", "UDP", "ICMP"]:
+                for nt in network_traffic[type]:
+                    IDs.append(nt)
+
+        return IDs
+
+    def processAndSubmit(self):
+        # Create SDO's / Cyber Obs
+        ext_ref = ExternalReference(
+            source_name=f"Cape Sandbox Report {str(self.report.info.id)}",
+            url=f"{self.cuckoo_url}/analysis/{str(self.report.info.id)}/",
+            external_id=str(self.report.info.id),
+        )
+
+        if self.report.network.hosts:
+            ips = self.createIPObs(self.report.network.hosts)
+        else:
+            ips = []
+
+        if self.report.network.domains:
+            fqdns = self.createDNSObs(self.report.network.domains)
+        else:
+            fqdns = [[], []]
+
+        if self.report.process:
+            processes = self.createProcessObs(self.report.process)
+        else:
+            processes = []
+
+        if self.EnableRegKeys:
+            if self.report.behavior:
+                if self.report.behavior.write_keys:
+                    registry_keys = self.createRegKeysObs(
+                        self.report.behavior.write_keys
+                    )
+                else:
+                    registry_keys = None
+            else:
+                registry_keys = None
+        else:
+            registry_keys = None
+
+        if self.EnableNetTraffic:
+            if self.report.network:
+                network_traffic = self.createNetTrafficObs(self.report.network)
+            else:
+                network_traffic = None
+        else:
+            network_traffic = None
+
+        if self.report.payloads:
+            dropped_binaries = self.createBinarieObs(self.report.payloads)
+        else:
+            dropped_binaries = []
+
+        if self.report.signatures:
+            AttackPatterns = self.getTTPs(self.report.ttps)
+        else:
+            AttackPatterns = []
+
+        if self.report.detections:
+            Malware = self.Get_Malware(self.report.detections)
+        else:
+            Malware = None
+
+        # Get all IDs from ATPs/CyberObs
+        IDs = self.get_related(
+            ips,
+            fqdns[0],
+            processes,
+            network_traffic,
+            dropped_binaries,
+            AttackPatterns,
+            registry_keys,
+            Malware,
+        )
+
+        # Create Main binary and link All ATPs/Cyber Obs
+        payload = self.createPrimaryBinary(self.report.target.file, ext_ref)
+        payload_relations = []
+        bundle_ids = []
+        for ID in IDs:
+            try:
+                IDx = ID.id
+                bundle_ids.append(
+                    ID
+                )  # Get list for bundle w/o Attack Patterns that exisit
+            except:
+                IDx = ID
+            payload_relations.append(
+                Relationship(
+                    relationship_type="related-to",
+                    source_ref=payload[0].id,
+                    target_ref=IDx,
+                )
+            )
+        for ATP in AttackPatterns:
+            payload_relations.append(
+                Relationship(
+                    relationship_type="related-to",
+                    source_ref=payload[0].id,
+                    target_ref=ATP,
+                )
+            )
+        if Malware:
+            Relationship(
+                relationship_type="related-to",
+                source_ref=payload[0].id,
+                target_ref=Malware,
+            )
+
+        IDs.append(payload[0])  # Add Observeable
+        IDs.append(payload[1])  # Add Indicator
+        bundle_ids.append(payload[0])
+        bundle_ids.append(payload[1])
+        payload_relations.append(payload[2])
+
+        if int(self.report.info.score) >= self.ReportScore:
+            # Create Report and link All ATPs/Cyber Obs/Payload
+            report = self.createCuckooReport(self.report, IDs, ext_ref)
+            b = Bundle(
+                report, bundle_ids, payload_relations, fqdns[1]
+            )  # fqdns[1] is the Resolves-to relations
+        else:
+            b = Bundle(bundle_ids, payload_relations, fqdns[1])
+
+        self.helper.send_stix2_bundle(b.serialize())
+
+        return None
---
+++ external-import/cape/src/main.py
@@ -0,0 +1,189 @@
+import os
+import time
+from datetime import datetime
+import yaml
+from pycti import OpenCTIConnectorHelper, get_config_variable
+from cape.cape import cuckoo
+from cape.telemetry import openCTIInterface
+
+
+class capeConnector:
+    """Connector object"""
+
+    def __init__(self):
+        """Read in config variables"""
+
+        config_file_path = os.path.dirname(os.path.abspath(__file__))
+        config_file_path += "/config.yml"
+        config = (
+            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
+            if os.path.isfile(config_file_path)
+            else {}
+        )
+        self.helper = OpenCTIConnectorHelper(config)
+
+        self.cape_api_url = get_config_variable(
+            "CAPE_API_URL", ["cape", "api_url"], config
+        )
+
+        self.cape_url = get_config_variable(
+            "CAPE_BASE_URL", ["cape", "base_url"], config
+        )
+
+        self.EnableNetTraffic = get_config_variable(
+            "CAPE_ENABLE_NETWORK_TRAFFIC",
+            ["cape", "enable_network_traffic"],
+            config,
+            default=False,
+        )
+        self.EnableRegKeys = get_config_variable(
+            "CAPE_ENABLE_REGISTRY_KEYS",
+            ["cape", "enable_registry_keys"],
+            config,
+            default=False,
+        )
+
+        self.verify_ssl = get_config_variable(
+            "VERIFY_SSL", ["cape", "verify_ssl"], config, default=True
+        )
+
+        self.interval = get_config_variable(
+            "CAPE_INTERVAL", ["cape", "interval"], config, True, 30
+        )
+
+        self.start_id = get_config_variable(
+            "CAPE_START_TASK_ID", ["cape", "start_task_id"], config, True, 0
+        )
+
+        self.report_score = get_config_variable(
+            "CAPE_REPORT_SCORE", ["cape", "report_score"], config, True, 0
+        )
+
+        self.create_indicators = get_config_variable(
+            "CAPE_CREATE_INDICATORS", ["cape", "create_indicators"], config
+        )
+
+        self.update_existing_data = get_config_variable(
+            "CONNECTOR_UPDATE_EXISTING_DATA",
+            ["connector", "update_existing_data"],
+            config,
+        )
+
+        self.cape_api: cuckoo = cuckoo(self.helper, self.cape_api_url, self.verify_ssl)
+
+    def get_interval(self):
+        """Converts interval hours to seconds"""
+        return int(self.interval) * 60
+
+    @property
+    def first_run(self):
+        """Checks if connector has run before"""
+        current_state = self.helper.get_state()
+        return current_state is None or "last_run" not in current_state
+
+    def run(self):
+        """Run connector on a schedule"""
+        while True:
+            if self.first_run:
+                state = self.helper.get_state()
+                self.helper.log_info("Connector has never run")
+                self.helper.log_info(str(state))
+
+                # Get Last Cape Task Pulled
+                if not state:
+                    current_task = 0
+                else:
+                    if "task" in state:
+                        current_task = self.helper.get_state()["task"]
+                    else:
+                        current_task = 0
+
+                # Check If starting Task > last task
+                if self.start_id > current_task:
+                    current_task = self.start_id
+                    self.helper.set_state({"task": self.start_id})
+            else:
+                last_run = datetime.utcfromtimestamp(
+                    self.helper.get_state()["last_run"]
+                ).strftime("%Y-%m-%d %H:%M:%S")
+                self.helper.log_info("Connector last run: " + last_run)
+
+                # Get Last Cape Task Pulled
+                state = self.helper.get_state()
+                self.helper.log_info(str(state))
+                if not state:
+                    current_task = 0
+                    self.helper.log_info("Last Task ID (STATE): " + str(current_task))
+                if "task" in state:
+                    current_task = state["task"]
+                    self.helper.log_info("Last Task ID (STATE): " + str(current_task))
+                else:
+                    current_task = 0
+
+                # Check If starting Task > last task
+                if self.start_id > current_task:
+                    current_task = self.start_id
+                    self.helper.set_state({"task": self.start_id})
+
+            try:
+                CapeTasks = (
+                    self.cape_api.getCuckooTasks()
+                )  # Pull List of tasks from the Cape API
+            except Exception as err:
+                self.helper.log_error("Error connecting to Cape API")
+                self.helper.log_error(str(err))
+                raise (err)
+
+            for task in CapeTasks:
+                if not task["status"] == "reported":
+                    continue  # If task Has not reported Skip
+                if not task["completed_on"]:
+                    continue  # If task Has not completed Skip
+
+                try:
+                    if task["id"] > current_task:
+                        taskSummary = self.cape_api.getTaskReport(
+                            task["id"]
+                        )  # Pull Cape Report and Searilize
+                        if not taskSummary:
+                            continue  # If no report continue
+                        if not taskSummary.info:
+                            continue  # If no report.info continue - we really need this :)
+
+                        self.helper.log_info(f"Processing Task {taskSummary.info.id}")
+                        # Process and submit cape task as stix bundle
+                        openCTIInterface(
+                            taskSummary,
+                            self.helper,
+                            self.update_existing_data,
+                            [],
+                            self.create_indicators,
+                            self.cape_url,
+                            self.EnableNetTraffic,
+                            self.EnableRegKeys,
+                            self.report_score,
+                        )
+                        # Update last task pulled
+                        self.helper.set_state({"task": taskSummary.info.id})
+
+                        self.helper.log_info(f"Synced task {task['id']}")
+                except Exception as e:
+                    self.helper.log_error(
+                        f"An error Occured fetching task {task['id']}; {str(e)}"
+                    )
+
+            self.helper.log_info("Finished grabbing Cape Reports")
+
+            self.helper.log_info(
+                f"Run Complete. Sleeping until next run in " f"{self.interval} Minutes"
+            )
+
+            time.sleep(self.get_interval())
+
+
+if __name__ == "__main__":
+    try:
+        CONNECTOR = capeConnector()
+        CONNECTOR.run()
+    except Exception as e:
+        raise e
