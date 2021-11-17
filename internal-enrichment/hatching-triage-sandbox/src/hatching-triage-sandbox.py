1f840e490ddf0be9d9defeefeac10d964f6d8b74
YungBinary <93540406+YungBinary@users.noreply.github.com>
2021-11-17 09:47:30 -0600

    [hatching-triage-sandbox] add existing analysis config option and refactoring (#542)
    * Add use existing analysis.
    * Formatting
    * Bump version number
    * Check if signature key exists.

--- internal-enrichment/hatching-triage-sandbox/src/hatching-triage-sandbox.py
+++ internal-enrichment/hatching-triage-sandbox/src/hatching-triage-sandbox.py
@@ -9,6 +9,7 @@ import re
 import magic
 import ipaddress
 from triage import Client
+from hashlib import sha256
 
 from stix2 import (
     Bundle,
@@ -60,6 +61,11 @@ class HatchingTriageSandboxConnector:
         self.triage_client = Client(token, root_url=base_url)
 
         # Get other config values
+        self.use_existing_analysis = get_config_variable(
+            "HATCHING_TRIAGE_SANDBOX_USE_EXISTING_ANALYSIS",
+            ["hatching_triage_sandbox", "use_existing_analysis"],
+            config,
+        )
         self.family_color = get_config_variable(
             "HATCHING_TRIAGE_SANDBOX_FAMILY_COLOR",
             ["hatching_triage_sandbox", "family_color"],
@@ -102,28 +108,29 @@ class HatchingTriageSandboxConnector:
         )
 
         # Create labels from the tags
-        for tag in overview_dict["analysis"]["tags"]:
+        if "tags" in overview_dict["analysis"]:
+            for tag in overview_dict["analysis"]["tags"]:
 
-            # Set the label color depending on tag type
-            # Note: Only certain tags are separated by a colon
-            # Those are the tags we are colorizing
-            tag_split = tag.split(":")
-            tag_value = tag
-            label_color = self.default_tag_color
-            if len(tag_split) == 2:
-                if "family" == tag_split[0]:
-                    label_color = self.family_color
-                elif "botnet" == tag_split[0]:
-                    label_color = self.botnet_color
-                elif "campaign" == tag_split[0]:
-                    label_color = self.campaign_color
-                tag_value = tag_split[1]
+                # Set the label color depending on tag type
+                # Note: Only certain tags are separated by a colon
+                # Those are the tags we are colorizing
+                tag_split = tag.split(":")
+                tag_value = tag
+                label_color = self.default_tag_color
+                if len(tag_split) == 2:
+                    if "family" == tag_split[0]:
+                        label_color = self.family_color
+                    elif "botnet" == tag_split[0]:
+                        label_color = self.botnet_color
+                    elif "campaign" == tag_split[0]:
+                        label_color = self.campaign_color
+                    tag_value = tag_split[1]
 
-            # Create and add the label
-            label = self.helper.api.label.create(value=tag_value, color=label_color)
-            self.helper.api.stix_cyber_observable.add_label(
-                id=observable["id"], label_id=label["id"]
-            )
+                # Create and add the label
+                label = self.helper.api.label.create(value=tag_value, color=label_color)
+                self.helper.api.stix_cyber_observable.add_label(
+                    id=observable["id"], label_id=label["id"]
+                )
 
         # Create default labels
         self.helper.api.label.create(value="c2 server", color=self.default_tag_color)
@@ -337,7 +344,7 @@ class HatchingTriageSandboxConnector:
 
         # Attach domains
         domains = [
-            task_dict.get("iocs").get("domains", [])
+            task_dict.get("iocs", {}).get("domains", [])
             for task_dict in overview_dict["targets"]
         ]
         for domain in domains[0]:
@@ -362,7 +369,10 @@ class HatchingTriageSandboxConnector:
             bundle_objects.append(relationship)
 
         # Attach IP addresses
-        ips = [task_dict["iocs"]["ips"] for task_dict in overview_dict["targets"]]
+        ips = [
+            task_dict.get("iocs", {}).get("ips", [])
+            for task_dict in overview_dict["targets"]
+        ]
         for ip in ips[0]:
 
             # Filter out non-global and known DNS IPs
@@ -391,37 +401,38 @@ class HatchingTriageSandboxConnector:
             bundle_objects.append(relationship)
 
         # Attach the TTPs
-        for signature_dict in overview_dict["signatures"]:
+        if "signatures" in overview_dict:
+            for signature_dict in overview_dict["signatures"]:
 
-            # Skip any dicts without a ttps key
-            if "ttp" not in signature_dict:
-                continue
+                # Skip any dicts without a ttps key
+                if "ttp" not in signature_dict:
+                    continue
 
-            name = signature_dict["name"]
-            ttps = signature_dict["ttp"]
+                name = signature_dict["name"]
+                ttps = signature_dict["ttp"]
 
-            for ttp in ttps:
+                for ttp in ttps:
 
-                attack_pattern = AttackPattern(
-                    id=OpenCTIStix2Utils.generate_random_stix_id("attack-pattern"),
-                    created_by_ref=self.identity,
-                    name=name,
-                    custom_properties={
-                        "x_mitre_id": ttp,
-                    },
-                    object_marking_refs=[TLP_WHITE],
-                )
+                    attack_pattern = AttackPattern(
+                        id=OpenCTIStix2Utils.generate_random_stix_id("attack-pattern"),
+                        created_by_ref=self.identity,
+                        name=name,
+                        custom_properties={
+                            "x_mitre_id": ttp,
+                        },
+                        object_marking_refs=[TLP_WHITE],
+                    )
 
-                relationship = Relationship(
-                    id=OpenCTIStix2Utils.generate_random_stix_id("relationship"),
-                    relationship_type="uses",
-                    created_by_ref=self.identity,
-                    source_ref=observable["standard_id"],
-                    target_ref=attack_pattern.id,
-                    object_marking_refs=[TLP_WHITE],
-                )
-                bundle_objects.append(attack_pattern)
-                bundle_objects.append(relationship)
+                    relationship = Relationship(
+                        id=OpenCTIStix2Utils.generate_random_stix_id("relationship"),
+                        relationship_type="uses",
+                        created_by_ref=self.identity,
+                        source_ref=observable["standard_id"],
+                        target_ref=attack_pattern.id,
+                        object_marking_refs=[TLP_WHITE],
+                    )
+                    bundle_objects.append(attack_pattern)
+                    bundle_objects.append(relationship)
 
         # Serialize and send all bundles
         if bundle_objects:
@@ -442,29 +453,37 @@ class HatchingTriageSandboxConnector:
         file_id = observable["importFiles"][0]["id"]
         file_uri = f"{self.octi_api_url}/storage/get/{file_id}"
         file_content = self.helper.api.fetch_opencti_file(file_uri, True)
+        sha256 = self._get_sha256(file_content)
+        sample_id = None
 
-        # Create file object
-        file_obj = io.BytesIO(file_content)
-
-        # Submit sample for analysis
-        sample_json = self.triage_client.submit_sample_file(file_name, file_obj)
-        self.helper.log_info(sample_json)
-
-        sample_id = sample_json["id"]
-        sample_status = sample_json["status"]
-        self.helper.log_info(f'Analysis {sample_id} has status "{sample_status}".')
-
-        # Stream events for the sample
-        for events in self.triage_client.sample_events(sample_id):
-            if events["status"] == "pending":
-                self.helper.log_info(
-                    f"Analysis {sample_id} has status \"{events['status']}\"."
-                )
-            elif events["status"] == "reported":
-                self.helper.log_info(f"Analysis {sample_id} has finished.")
+        if self.use_existing_analysis:
+            # Perform a search of the sha256 to see if there's any existing analyses
+            search_paginator = self.triage_client.search(f"sha256:{sha256}", max=1)
+            for search in search_paginator:
+                existing_status = search["status"]
+                if existing_status == "reported":
+                    sample_id = search["id"]
+                    self.helper.log_info(
+                        f"Found existing analysis with id {sample_id} and status {existing_status}."
+                    )
+                elif existing_status == "pending":
+                    sample_id = search["id"]
+                    self.helper.log_info(
+                        f"Found existing analysis with id {sample_id} and status {existing_status}."
+                    )
+                    self._wait_for_analysis(sample_id)
+                # Don't paginate, just get the first result
                 break
-            elif events["status"] == "failed":
-                raise ValueError(f"Analysis {sample_id} failed.")
+
+        if sample_id is None:
+            file_obj = io.BytesIO(file_content)
+            sample_json = self.triage_client.submit_sample_file(file_name, file_obj)
+            sample_id = sample_json["id"]
+            sample_status = sample_json["status"]
+            self.helper.log_info(
+                f'Started new analysis {sample_id}, has status "{sample_status}".'
+            )
+            self._wait_for_analysis(sample_id)
 
         # Get the Overview report
         overview_dict = self.triage_client.overview_report(sample_id)
@@ -502,6 +521,36 @@ class HatchingTriageSandboxConnector:
                 "Do not send any data, TLP of the observable is greater than MAX TLP"
             )
         return self._process_observable(observable)
+
+    def _wait_for_analysis(self, sample_id):
+        """
+        Wait for an analysis to finish.
+
+        sample_id: a str representing the sample id
+        returns: none
+        """
+
+        for events in self.triage_client.sample_events(sample_id):
+            if events["status"] == "pending":
+                self.helper.log_info(
+                    f"Analysis {sample_id} has status \"{events['status']}\"."
+                )
+            elif events["status"] == "reported":
+                self.helper.log_info(f"Analysis {sample_id} has finished.")
+                break
+            elif events["status"] == "failed":
+                raise ValueError(f"Analysis {sample_id} failed.")
+
+    def _get_sha256(self, contents):
+        """
+        Return sha256 of bytes.
+
+        contents: a bytes object to get the sha256 for
+        returns: a str of the sha256
+        """
+        sha256obj = sha256()
+        sha256obj.update(contents)
+        return sha256obj.hexdigest()
 
     def _is_ipv4_address(self, ip):
         m = re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", ip)
