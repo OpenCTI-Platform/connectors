3fabfc82ea7209fd3e7641aa09ac6a851099ee16
Axel Fahy <axelfahy@users.noreply.github.com>
2021-11-19 18:03:03 +0100

    [mitre] add function to handle http requests and send the bundles (#550)

--- external-import/mitre/src/mitre.py
+++ external-import/mitre/src/mitre.py
@@ -1,15 +1,20 @@
-import os
-import yaml
-import time
-import urllib.request
-import certifi
-import ssl
-
+"""Mitre connector module."""
 from datetime import datetime
+import os
+import ssl
+import sys
+import time
+from typing import Optional
+import urllib
+
+import certifi
 from pycti import OpenCTIConnectorHelper, get_config_variable
+import yaml
 
 
 class Mitre:
+    """Mitre connector."""
+
     def __init__(self):
         # Instantiate the connector helper from config
         config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
@@ -44,8 +49,36 @@ class Mitre:
     def get_interval(self):
         return int(self.mitre_interval) * 60 * 60 * 24
 
-    def next_run(self, seconds):
-        return
+    def retrieve_data(self, url: str) -> Optional[str]:
+        """
+        Retrieve data from the given url.
+
+        Parameters
+        ----------
+        url : str
+            Url to retrieve.
+
+        Returns
+        -------
+        str
+            A string with the content or None in case of failure.
+        """
+        try:
+            return (
+                urllib.request.urlopen(
+                    url,
+                    context=ssl.create_default_context(cafile=certifi.where()),
+                )
+                .read()
+                .decode("utf-8")
+            )
+        except (
+            urllib.error.URLError,
+            urllib.error.HTTPError,
+            urllib.error.ContentTooShortError,
+        ) as urllib_error:
+            self.helper.log_error(f"Error retrieving url {url}: {urllib_error}")
+        return None
 
     def run(self):
         self.helper.log_info("Fetching MITRE datasets...")
@@ -71,87 +104,30 @@ class Mitre:
                     > ((int(self.mitre_interval) - 1) * 60 * 60 * 24)
                 ):
                     self.helper.log_info("Connector will run!")
+
                     now = datetime.utcfromtimestamp(timestamp)
                     friendly_name = "MITRE run @ " + now.strftime("%Y-%m-%d %H:%M:%S")
                     work_id = self.helper.api.work.initiate_work(
                         self.helper.connect_id, friendly_name
                     )
-                    try:
-                        enterprise_data = (
-                            urllib.request.urlopen(
-                                self.mitre_enterprise_file_url,
-                                context=ssl.create_default_context(
-                                    cafile=certifi.where()
-                                ),
-                            )
-                            .read()
-                            .decode("utf-8")
-                        )
-                        self.helper.send_stix2_bundle(
-                            enterprise_data,
-                            entities_types=self.helper.connect_scope,
-                            update=self.update_existing_data,
-                            work_id=work_id,
-                        )
-                    except Exception as e:
-                        self.helper.log_error(str(e))
-                    try:
-                        pre_attack_data = (
-                            urllib.request.urlopen(
-                                self.mitre_pre_attack_file_url,
-                                context=ssl.create_default_context(
-                                    cafile=certifi.where()
-                                ),
-                            )
-                            .read()
-                            .decode("utf-8")
-                        )
-                        self.helper.send_stix2_bundle(
-                            pre_attack_data,
-                            entities_types=self.helper.connect_scope,
-                            update=self.update_existing_data,
-                            work_id=work_id,
-                        )
-                    except Exception as e:
-                        self.helper.log_error(str(e))
-                    try:
-                        mobile_attack_data = (
-                            urllib.request.urlopen(
-                                self.mitre_mobile_attack_file_url,
-                                context=ssl.create_default_context(
-                                    cafile=certifi.where()
-                                ),
-                            )
-                            .read()
-                            .decode("utf-8")
-                        )
-                        self.helper.send_stix2_bundle(
-                            mobile_attack_data,
-                            entities_types=self.helper.connect_scope,
-                            update=self.update_existing_data,
-                            work_id=work_id,
-                        )
-                    except Exception as e:
-                        self.helper.log_error(str(e))
-                    try:
-                        ics_attack_data = (
-                            urllib.request.urlopen(
-                                self.mitre_ics_attack_file_url,
-                                context=ssl.create_default_context(
-                                    cafile=certifi.where()
-                                ),
-                            )
-                            .read()
-                            .decode("utf-8")
-                        )
-                        self.helper.send_stix2_bundle(
-                            ics_attack_data,
-                            entities_types=self.helper.connect_scope,
-                            update=self.update_existing_data,
-                            work_id=work_id,
-                        )
-                    except Exception as e:
-                        self.helper.log_error(str(e))
+                    # Mitre enterprise file url
+                    enterprise_data = self.retrieve_data(self.mitre_enterprise_file_url)
+                    self.send_bundle(work_id, enterprise_data)
+
+                    # Mitre pre attack file url
+                    pre_attack_data = self.retrieve_data(self.mitre_pre_attack_file_url)
+                    self.send_bundle(work_id, pre_attack_data)
+
+                    # Mitre mobile attack file url
+                    mobile_attack_data = self.retrieve_data(
+                        self.mitre_mobile_attack_file_url
+                    )
+                    self.send_bundle(work_id, mobile_attack_data)
+
+                    # Mitre ics attack file url
+                    ics_attack_data = self.retrieve_data(self.mitre_ics_attack_file_url)
+                    self.send_bundle(work_id, ics_attack_data)
+
                     # Store the current timestamp as a last run
                     message = "Connector successfully run, storing last_run as " + str(
                         timestamp
@@ -164,7 +140,6 @@ class Mitre:
                         + str(round(self.get_interval() / 60 / 60 / 24, 2))
                         + " days"
                     )
-                    time.sleep(60)
                 else:
                     new_interval = self.get_interval() - (timestamp - last_run)
                     self.helper.log_info(
@@ -172,13 +147,24 @@ class Mitre:
                         + str(round(new_interval / 60 / 60 / 24, 2))
                         + " days"
                     )
-                    time.sleep(60)
+                time.sleep(60)
             except (KeyboardInterrupt, SystemExit):
                 self.helper.log_info("Connector stop")
-                exit(0)
+                sys.exit(0)
             except Exception as e:
                 self.helper.log_error(str(e))
                 time.sleep(60)
+
+    def send_bundle(self, work_id: str, serialized_bundle: str) -> None:
+        try:
+            self.helper.send_stix2_bundle(
+                serialized_bundle,
+                entities_types=self.helper.connect_scope,
+                update=self.update_existing_data,
+                work_id=work_id,
+            )
+        except Exception as e:
+            self.helper.log_error(f"Error while sending bundle: {e}")
 
 
 if __name__ == "__main__":
@@ -188,4 +174,4 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
         time.sleep(10)
-        exit(0)
+        sys.exit(0)
