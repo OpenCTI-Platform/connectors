3bccc3c7f17d30cd212d4be2c1f38aa57e73cbe8
Samuel Hassine <samuel@hassine.fr>
2021-12-26 19:37:35 +0100

    [all] Bump Python library

--- stream/elastic/elastic/__init__.py
+++ stream/elastic/elastic/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "5.1.2"
+__version__ = "5.1.3"
 LOGGER_NAME = "elastic"
 RE_DATEMATH = (
     r"\{(?P<modulo>.*now[^{]*)(?:\{(?P<format>[^|]*)(?:\|(?P<offset>[^}]+))?\})?\}"
