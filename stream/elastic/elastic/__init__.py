9e885a15f9d6d600d0226b197ebaddad44848e16
Samuel Hassine <samuel@hassine.fr>
2022-03-30 23:33:49 +0200

    [all] Prepare new release

--- stream/elastic/elastic/__init__.py
+++ stream/elastic/elastic/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "5.2.3"
+__version__ = "5.2.4"
 LOGGER_NAME = "elastic"
 RE_DATEMATH = (
     r"\{(?P<modulo>.*now[^{]*)(?:\{(?P<format>[^|]*)(?:\|(?P<offset>[^}]+))?\})?\}"
