2e41336849a81b1f7cc34c277b0af4d069feb12f
Samuel Hassine <samuel.hassine@filigran.io>
2022-06-10 12:51:45 +0200

    [all] Bump the Python library

--- internal-enrichment/hybrid-analysis-sandbox/src/hybrid-analysis-sandbox.py
+++ internal-enrichment/hybrid-analysis-sandbox/src/hybrid-analysis-sandbox.py
@@ -41,7 +41,7 @@ class HybridAnalysis:
         self.api_url = "https://www.hybrid-analysis.com/api/v2"
         self.headers = {
             "api-key": self.api_key,
-            "user-agent": "OpenCTI Hybrid Analysis Connector - Version 5.3.3",
+            "user-agent": "OpenCTI Hybrid Analysis Connector - Version 5.3.4",
             "accept": "application/json",
         }
         self.identity = self.helper.api.identity.create(
