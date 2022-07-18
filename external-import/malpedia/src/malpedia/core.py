4401b1c090f50abb2d33287d0b00c4085a601071
Axel Fahy <axf@open.ch>
2022-07-18 12:00:43 +0200

    [malpedia] use log of OpenCTIConnectorHelper in MalpediaClient

--- external-import/malpedia/src/malpedia/core.py
+++ external-import/malpedia/src/malpedia/core.py
@@ -65,7 +65,7 @@ class Malpedia:
         self.helper.log_info(f"loaded malpedia config: {config}")
 
         # Create Malpedia client and importers
-        self.client = MalpediaClient(self.AUTH_KEY)
+        self.client = MalpediaClient(self.helper, self.AUTH_KEY)
 
         # If we run without API key we can assume all data is TLP:WHITE else we
         # default to TLP:AMBER to be safe.
