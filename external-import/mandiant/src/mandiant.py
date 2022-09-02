92d5f020e4cd99513c1ceabff66a2b796506badd
Samuel Hassine <samuel.hassine@filigran.io>
2022-09-02 18:27:12 +0200

    [all] Bump Python library

--- external-import/mandiant/src/mandiant.py
+++ external-import/mandiant/src/mandiant.py
@@ -81,7 +81,7 @@ class Mandiant:
     def _get_token(self):
         headers = {
             "accept": "application/json",
-            "x-app-name": "opencti-connector-5.3.8",
+            "x-app-name": "opencti-connector-5.3.9",
         }
         r = requests.post(
             self.mandiant_api_url + "/token",
