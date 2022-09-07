d36121293d2e9b143f5c0930ff621a42afd4f29d
Samuel Hassine <samuel.hassine@filigran.io>
2022-09-08 00:09:37 +0200

    [all] Release 5.3.11

--- external-import/maltiverse/README.md
+++ external-import/maltiverse/README.md
@@ -8,7 +8,7 @@ There is a [`docker-compose.yml`](docker-compose.yml) example you can use to ena
 
 ### Requirements
 
-- OpenCTI Platform >= 5.3.10
+- OpenCTI Platform >= 5.3.11
 
 ### Configuration
 
--- stream/elastic/elastic/__init__.py
+++ stream/elastic/elastic/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "5.3.10"
+__version__ = "5.3.11"
 LOGGER_NAME = "elastic"
 RE_DATEMATH = (
     r"\{(?P<modulo>.*now[^{]*)(?:\{(?P<format>[^|]*)(?:\|(?P<offset>[^}]+))?\})?\}"
