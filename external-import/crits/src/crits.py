90630adbe1b173aaf85df6697af080c3dfd5c8dd
Coleman Kane <ckane@colemankane.org>
2022-09-27 00:19:37 -0400

    [crits] Start ingesting IPs, Domains, and Events
    The Domains and IPs that are related to Events will be imported as
    contents of those reports (Events).

--- external-import/crits/src/crits.py
+++ external-import/crits/src/crits.py
@@ -2,12 +2,279 @@ import os
 import sys
 import time
 import requests
+from math import ceil
+from datetime import datetime
+from dateutil.parser import parse as dtparse
 
 import yaml
-from pycti import OpenCTIConnectorHelper, get_config_variable
+from pycti import OpenCTIConnectorHelper, get_config_variable, Identity, Report
+import stix2
+import validators
 
 
 class CRITsConnector:
+    def collect_srcs_refs(self, crits_obj):
+        # Grab all the source.*.instances.*.references, and import them as a list
+        # of External references
+        ext_refs = []
+        srcs = []
+        for src in crits_obj["source"]:
+            sname = src["name"]
+            author = stix2.Identity(
+                id=Identity.generate_id(sname, "organization"),
+                name=sname,
+                identity_class="organization",
+            )
+            srcs.append(author)
+
+            for inst in src["instances"]:
+                ref_params = {
+                    "source_name": sname,
+                    "description": inst["method"],
+                }
+                if inst["reference"]:
+                    if validators.url(inst["reference"]):
+                        ref_params["url"] = inst["reference"]
+                        ref_params["external_id"] = ""
+                    else:
+                        ref_params["url"] = ""
+                        ref_params["external_id"] = inst["reference"]
+
+                    ext_refs.append(stix2.ExternalReference(**ref_params))
+
+        return srcs, ext_refs
+
+    def domain_to_stix(self, crits_obj, custom_properties):
+        custom_properties["description"] = crits_obj.get("description", "")
+
+        return stix2.DomainName(
+            value=crits_obj["domain"],
+            object_marking_refs=[self.default_marking],
+            custom_properties=custom_properties,
+        )
+
+    def ip_to_stix(self, crits_obj, custom_properties):
+        custom_properties["description"] = crits_obj.get("description", "")
+
+        # IP Address observables have 4 subtype possibilities. However,
+        # STIX 2.x maps "Address" and "Subnet" to the same data types, for
+        # each kind of IP (4 or 6)
+        if crits_obj["type"] in ["IPv4 Address", "IPv4 Subnet"]:
+            return stix2.IPv4Address(
+                value=crits_obj["ip"],
+                object_marking_refs=[self.default_marking],
+                custom_properties=custom_properties,
+            )
+        elif crits_obj["type"] in ["IPv6 Address", "IPv6 Subnet"]:
+            return stix2.IPv6Address(
+                value=crits_obj["ip"],
+                object_marking_refs=[self.default_marking],
+                custom_properties=custom_properties,
+            )
+
+        return None
+
+    def process_objects(self, collection, since):
+        http_response = self.make_api_get(
+            collection,
+            query="?c-{t}__gt={v}&limit={l}".format(
+                t=self.crits_timestamp_field, v=since.isoformat(), l=self.chunk_size
+            ),
+        )
+
+        # Will be used to loop through the pages in the results
+        while http_response.ok:
+            new_objects = []
+            content = http_response.json()
+
+            self.helper.log_info(
+                "{c}: total={t} page={n}/{m})".format(
+                    c=collection,
+                    t=content["meta"]["total_count"],
+                    n=int(content["meta"]["offset"] / content["meta"]["total_count"])
+                    + 1,
+                    m=ceil(content["meta"]["total_count"] / self.chunk_size),
+                )
+            )
+
+            # Walk through each of the entities and build a stix object
+            for crits_obj in content["objects"]:
+                # Grab the first Source name from this list, and import it as the author
+                srcs, ext_refs = self.collect_srcs_refs(crits_obj)
+                new_objects.extend(srcs)
+
+                new_obj = None
+                custom_properties = {
+                    "x_opencti_score": self.default_score,
+                    "created_by_ref": srcs[0]["id"],
+                    "external_references": ext_refs,
+                }
+                if collection == "ips":
+                    new_obj = self.ip_to_stix(
+                        crits_obj=crits_obj, custom_properties=custom_properties
+                    )
+                elif collection == "domains":
+                    new_obj = self.domain_to_stix(
+                        crits_obj=crits_obj, custom_properties=custom_properties
+                    )
+
+                if new_obj:
+                    new_objects.append(new_obj)
+
+            work_id = self.helper.api.work.initiate_work(
+                self.helper.connect_id,
+                "test CRITs upload",
+            )
+
+            bundle = stix2.Bundle(objects=new_objects, allow_custom=True).serialize()
+            self.helper.send_stix2_bundle(bundle, update=True, work_id=work_id)
+
+            # If this is the last page of the collection, then break
+            if (
+                content["meta"]["offset"] + len(content["objects"])
+                >= content["meta"]["total_count"] - 1
+            ):
+                break
+
+            # Otherwise, load next page
+            http_response = self.make_api_get(
+                collection,
+                query="?c-{t}__gt={v}&limit={l}&offset={o}".format(
+                    t=self.crits_timestamp_field,
+                    v=since.isoformat(),
+                    l=self.chunk_size,
+                    o=content["meta"]["offset"] + self.chunk_size,
+                ),
+            )
+
+        else:
+            self.helper.log_warn(
+                'Unable to access "{c}" collection, HTTP code: {r}'.format(
+                    c=collection,
+                    r=http_response.status_code,
+                )
+            )
+
+    def process_events(self, since):
+        http_response = self.make_api_get(
+            "events",
+            query="?c-{t}__gt={v}&limit={l}".format(
+                t=self.crits_timestamp_field, v=since.isoformat(), l=self.chunk_size
+            ),
+        )
+
+        # Will be used to loop through the pages in the results
+        while http_response.ok:
+            new_objects = []
+            content = http_response.json()
+
+            self.helper.log_info(
+                "events: total={t} page={n}/{m})".format(
+                    t=content["meta"]["total_count"],
+                    n=int(content["meta"]["offset"] / content["meta"]["total_count"])
+                    + 1,
+                    m=ceil(content["meta"]["total_count"] / self.chunk_size),
+                )
+            )
+
+            # Walk through each of the entities and build a stix object
+            for crits_obj in content["objects"]:
+                # Grab the first Source name from this list, and import it as the author
+
+                srcs, ext_refs = self.collect_srcs_refs(crits_obj)
+                new_objects.extend(srcs)
+
+                custom_properties = {"x_opencti_report_status": 2}
+
+                ts = dtparse(crits_obj["created"])
+
+                allowed_types = ["IP", "Domain"]
+                report_contents_crits = list(
+                    filter(
+                        lambda x: x["type"] in allowed_types, crits_obj["relationships"]
+                    )
+                )
+
+                contained_objects = []
+                for contained in report_contents_crits:
+                    contained_stix = None
+                    contained_custom_properties = {
+                        "x_opencti_score": self.default_score,
+                        "created_by_ref": srcs[0]["id"],
+                    }
+                    if contained["type"] == "IP":
+                        contained_tlo = self.make_api_getobj(
+                            collection="ips", objid=contained["value"]
+                        )
+                        if contained_tlo.ok:
+                            contained_stix = self.ip_to_stix(
+                                crits_obj=contained_tlo.json(),
+                                custom_properties=contained_custom_properties,
+                            )
+                    elif contained["type"] == "Domain":
+                        contained_tlo = self.make_api_getobj(
+                            collection="domains", objid=contained["value"]
+                        )
+                        if contained_tlo.ok:
+                            contained_stix = self.domain_to_stix(
+                                crits_obj=contained_tlo.json(),
+                                custom_properties=contained_custom_properties,
+                            )
+
+                    if contained_stix:
+                        new_objects.append(contained_stix)
+                        contained_objects.append(contained_stix["id"])
+
+                report_entity = stix2.Report(
+                    id=Report.generate_id(crits_obj["title"], ts),
+                    name=crits_obj["title"],
+                    description=crits_obj.get("description", ""),
+                    object_marking_refs=[self.default_marking],
+                    published=ts,
+                    created=ts,
+                    modified=dtparse(crits_obj["modified"]),
+                    report_types=[self.crits_event_type],
+                    created_by_ref=srcs[0]["id"],
+                    external_references=ext_refs,
+                    allow_custom=True,
+                    custom_properties=custom_properties,
+                    object_refs=contained_objects,
+                )
+                new_objects.append(report_entity)
+
+            work_id = self.helper.api.work.initiate_work(
+                self.helper.connect_id,
+                "test CRITs upload",
+            )
+
+            bundle = stix2.Bundle(objects=new_objects, allow_custom=True).serialize()
+            self.helper.send_stix2_bundle(bundle, update=True, work_id=work_id)
+
+            # If this is the last page of the collection, then break
+            if (
+                content["meta"]["offset"] + len(content["objects"])
+                >= content["meta"]["total_count"] - 1
+            ):
+                break
+
+            # Otherwise, load next page
+            http_response = self.make_api_get(
+                "ips",
+                query="?c-{t}__gt={v}&limit={l}&offset={o}".format(
+                    t=self.crits_timestamp_field,
+                    v=since.isoformat(),
+                    l=self.chunk_size,
+                    o=content["meta"]["offset"] + self.chunk_size,
+                ),
+            )
+
+        else:
+            self.helper.log_warn(
+                'Unable to access "ips" collection, HTTP code: {c}'.format(
+                    c=http_response.status_code
+                )
+            )
+
     def __init__(self):
         # Instantiate the connector helper from config
         config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
@@ -51,6 +318,10 @@ class CRITsConnector:
         self.crits_interval = get_config_variable(
             "CRITS_INTERVAL", ["crits", "interval"], config, True
         )
+        self.crits_timestamp_field = "modified"
+        self.chunk_size = 100
+        self.default_marking = stix2.TLP_GREEN
+        self.default_score = 75
 
         # Test connection to <crits_url>/api/v1/events/?limit=1, which should give a JSON result
         # if the authentication is working, whether or not there's any Events in the database.
@@ -68,6 +339,15 @@ class CRITsConnector:
         http_response = requests.get(
             "{base}/api/v1/{collection}/{query}".format(
                 base=self.crits_url, collection=collection, query=query
+            ),
+            params={"username": self.crits_user, "api_key": self.crits_api_key},
+        )
+        return http_response
+
+    def make_api_getobj(self, collection, objid):
+        http_response = requests.get(
+            "{base}/api/v1/{collection}/{objid}/".format(
+                base=self.crits_url, collection=collection, objid=objid
             ),
             params={"username": self.crits_user, "api_key": self.crits_api_key},
         )
@@ -114,6 +394,15 @@ class CRITsConnector:
             #       both elements? Neither?
             #    2) Upload the relationships - walk through containing reports, if needed
             #
+            tmp_earliest = datetime(2010, 1, 1)
+
+            # First collect entities and observables and other objects that can be contained within reports
+            for collection in ["ips", "domains"]:
+                self.process_objects(collection=collection, since=tmp_earliest)
+
+            # Next, collect reports
+            self.process_events(since=tmp_earliest)
+
             time.sleep(60 * self.crits_interval)
 
 
