b82342995aeadcf34086fe0d3e16f9c579b7e27e
scott <>
2023-01-26 16:01:42 -0500

    [VirusTotal] Made connector async and added url upload

--- internal-enrichment/virustotal/src/virustotal/client.py
+++ internal-enrichment/virustotal/src/virustotal/client.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """Virustotal client module."""
-import base64
+import asyncio
+import hashlib
 import json
-from time import sleep
+import urllib.parse
 
 import requests
 from pycti import OpenCTIConnectorHelper
@@ -79,7 +80,7 @@ class VirusTotalClient:
             )
             return None
 
-    def _post(self, url, files):
+    def _post(self, url, data=None, files=None, additional_headers=None):
         """
         Execute a post to the Virustotal api.
 
@@ -87,8 +88,12 @@ class VirusTotalClient:
         ----------
         url : str
             Url to post to.
+        data : str
+            Data payload to be passed with the post.
         files : json
-            A JSON object with the files to be posted to VirusTotal
+            A JSON object with the files to be posted to VirusTotal.
+        additional_headers : dict
+            Headers to be added to the self.headers with the request.
 
         Returns
         -------
@@ -96,8 +101,13 @@ class VirusTotalClient:
             The result of the query, as JSON or None in case of failure.
         """
         response = None
+        headers = (
+            self.headers
+            if additional_headers == None
+            else self.headers | additional_headers
+        )
         try:
-            response = requests.post(url, files=files, headers=self.headers)
+            response = requests.post(url, data=data, files=files, headers=headers)
             response.raise_for_status()
         except requests.exceptions.HTTPError as errh:
             self.helper.log_error(f"[VirusTotal] Http error: {errh}")
@@ -153,42 +163,7 @@ class VirusTotalClient:
         """
         url = f"{self.url}/files"
         files = {"file": (artifact_name, artifact)}
-        return self._post(url, files)["data"]["id"]
-
-    def check_upload_status(self, name, analysis_id):
-        """
-        Wait for the uploaded queued artifact to finish being analyzed
-
-        Parameters
-        ----------
-        analysis_id : str
-            The ID returned by VirustTotal for the analysis job of the artifact
-        """
-        url = f"{self.url}/analyses/{analysis_id}"
-        retry_delay = 30  # in seconds
-        minimum_retry_delay = 60  # in seconds
-        maximum_retry_delay = 180  # in seconds
-        total_attempts = 10
-        i = 0
-        while i < total_attempts:
-            current_status = self._query(url)["data"]["attributes"]["status"]
-            current_retry_delay = min(
-                (i * retry_delay + minimum_retry_delay), maximum_retry_delay
-            )
-            i += 1
-            if not current_status == "completed":
-                self.helper.log_debug(
-                    f"[VirusTotal] Uploaded artifact {name} current VirusTotal "
-                    f"analysis status: {current_status}. Checking status update "
-                    f"attempt #{i} of {total_attempts} in {current_retry_delay} seconds."
-                )
-                sleep(current_retry_delay)
-            else:
-                return
-        raise ValueError(
-            f"The uploaded artifact {name} was not analyzed by VirusTotal before the "
-            "timeout was reached. Please try enriching the file again at a later time."
-        )
+        return self._post(url, files=files)["data"]["id"]
 
     def get_yara_ruleset(self, ruleset_id) -> dict:
         """
@@ -255,22 +230,65 @@ class VirusTotalClient:
         dict
             URL Object, see https://developers.virustotal.com/reference/url-object
         """
-        url = f"{self.url}/urls/{VirusTotalClient.base64_encode_no_padding(url)}"
-        return self._query(url)
+        endpoint_url = f"{self.url}/urls/{hashlib.sha256(url.encode()).hexdigest()}"
+        return self._query(endpoint_url)
 
-    @staticmethod
-    def base64_encode_no_padding(contents):
+    def upload_url(self, url) -> str:
         """
-        Base64 encode a string and remove padding.
+        Upload a URL to VirusTotal for analysis
 
         Parameters
         ----------
-        contents : str
-            String to encode.
+        url : str
+            The URL being uploaded
 
         Returns
         -------
         str
-            Base64 encoded string without padding
+            Analysis id, see https://developers.virustotal.com/reference/analysis
         """
-        return base64.b64encode(contents.encode()).decode().replace("=", "")
+        endpoint_url = f"{self.url}/urls"
+        payload = f"url={urllib.parse.quote(url, safe='')}"
+        headers = {"content-type": "application/x-www-form-urlencoded"}
+        return self._post(endpoint_url, data=payload, additional_headers=headers)[
+            "data"
+        ]["id"]
+
+    async def check_upload_status(self, upload_type, name, analysis_id):
+        """
+        Wait for the uploaded queued artifact or URL to finish being analyzed
+
+        Parameters
+        ----------
+        upload_type : str
+            The type of the upload (artifact || URL)
+        name : str
+            The name of the upload
+        analysis_id : str
+            The ID returned by VirusTotal for the analysis job of the upload
+        """
+        url = f"{self.url}/analyses/{analysis_id}"
+        retry_delay = 30 if upload_type == "artifact" else 2  # in seconds
+        minimum_retry_delay = 60 if upload_type == "artifact" else 1  # in seconds
+        maximum_retry_delay = 180 if upload_type == "artifact" else 10  # in seconds
+        total_attempts = 10
+        i = 0
+        while i < total_attempts:
+            current_status = self._query(url)["data"]["attributes"]["status"]
+            current_retry_delay = min(
+                (i * retry_delay + minimum_retry_delay), maximum_retry_delay
+            )
+            i += 1
+            if not current_status == "completed":
+                self.helper.log_debug(
+                    f"[VirusTotal] Uploaded {upload_type} {name} current VirusTotal "
+                    f"analysis status: {current_status}. Checking status update "
+                    f"attempt #{i} of {total_attempts} in {current_retry_delay} seconds."
+                )
+                await asyncio.sleep(current_retry_delay)
+            else:
+                return
+        raise ValueError(
+            f"The uploaded {upload_type} {name} was not analyzed by VirusTotal before the "
+            f"timeout was reached. Please try enriching the {upload_type} again at a later time."
+        )
