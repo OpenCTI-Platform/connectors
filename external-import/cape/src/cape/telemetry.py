e1bd950f71a004096311916e7017ba3b9a3a90aa
EC2 Default User <ec2-user@ip-10-1-1-10.ap-southeast-1.compute.internal>
2023-03-10 16:38:52 +0000

    apply isort to fix imports

--- external-import/cape/src/cape/telemetry.py
+++ external-import/cape/src/cape/telemetry.py
@@ -1,41 +1,18 @@
-from cape.cape import (
-    cuckooPayload,
-    cuckooReport,
-    cuckooReportDomain,
-    cuckooReportHost,
-    cuckooReportICMP,
-    cuckooReportNetwork,
-    cuckooReportProcess,
-    cuckooReportTCPUDP,
-    cuckooReportTTP,
-    cuckooTarget,
-)
 from datetime import datetime
-from pycti import (
-    Note as pyctiNote,
-    Report as pyctiReport,
-)
+
+from pycti import Note as pyctiNote
+from pycti import Report as pyctiReport
 from pycti.connector.opencti_connector_helper import OpenCTIConnectorHelper
 from stix2 import DomainName, IPv4Address
-from stix2.v21 import (
-    TLP_AMBER,
-    TLP_GREEN,
-    TLP_RED,
-    TLP_WHITE,
-    Bundle,
-    DomainName,
-    ExternalReference,
-    File,
-    Indicator,
-    IPv4Address,
-    Malware,
-    NetworkTraffic,
-    Note,
-    Process,
-    Relationship,
-    Report,
-    WindowsRegistryKey,
-)
+from stix2.v21 import (TLP_AMBER, TLP_GREEN, TLP_RED, TLP_WHITE, Bundle,
+                       DomainName, ExternalReference, File, Indicator,
+                       IPv4Address, Malware, NetworkTraffic, Note, Process,
+                       Relationship, Report, WindowsRegistryKey)
+
+from cape.cape import (cuckooPayload, cuckooReport, cuckooReportDomain,
+                       cuckooReportHost, cuckooReportICMP, cuckooReportNetwork,
+                       cuckooReportProcess, cuckooReportTCPUDP,
+                       cuckooReportTTP, cuckooTarget)
 
 
 class openCTIInterface:
