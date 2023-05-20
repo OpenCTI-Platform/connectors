0c307269a155157ab9a8d175246b2aa1b0727f07
Filigran Automation <automation@filigran.io>
2023-05-20 08:18:45 +0000

    [all] Release 5.7.6

--- internal-enrichment/yara/README.md
+++ internal-enrichment/yara/README.md
@@ -10,7 +10,7 @@ connector creates a relationship between the Artifact and Indicator.
 
 ### Requirements
 
-- OpenCTI Platform >= 5.7.5
+- OpenCTI Platform >= 5.7.6
 
 ### Configuration
 
--- stream/elastic/elastic/__init__.py
+++ stream/elastic/elastic/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "5.7.5"
+__version__ = "5.7.6"
 LOGGER_NAME = "elastic"
 RE_DATEMATH = (
     r"\{(?P<modulo>.*now[^{]*)(?:\{(?P<format>[^|]*)(?:\|(?P<offset>[^}]+))?\})?\}"
