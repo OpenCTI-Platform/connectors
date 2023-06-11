21666546cd5910bf8f0a060438fa703e8c02f384
Samuel Hassine <samuel.hassine@filigran.io>
2023-06-11 09:41:42 +0200

    [zerofox] Fix multiple errors

--- external-import/zerofox/src/zerofox/zerofox_connector.py
+++ external-import/zerofox/src/zerofox/zerofox_connector.py
@@ -2,31 +2,34 @@ import os
 
 import requests
 import yaml
-from pycti import OpenCTIApiClient
+from pycti import OpenCTIConnectorHelper, get_config_variable
 
-from .stix_converter import convert_to_stix_botnet
 from .zerofox_fetchers import fetch_data_from_zerofox_endpoint
 
 
 class ZeroFoxConnector:
     def __init__(self):
         # Instantiate the connector helper from config
-        print("Initializing ZeroFoxConnector...")
-        config_file_path = os.path.join(
-            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "config.yml"
-        )
+        print("Initializing ZeroFox connector...")
+        # Instantiate the connector helper from config
+        config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
         config = (
             yaml.load(open(config_file_path), Loader=yaml.FullLoader)
             if os.path.isfile(config_file_path)
             else {}
         )
+        self.helper = OpenCTIConnectorHelper(config)
 
         # ZeroFOX API credentials
-        self.zerofox_username = config["zerofox"]["username"]
-        self.zerofox_password = config["zerofox"]["password"]
-
-        self.helper = OpenCTIApiClient(
-            url=config["opencti"]["url"], token=config["opencti"]["token"]
+        self.zerofox_username = get_config_variable(
+            "ZEROFOX_USERNAME",
+            ["zerofox", "username"],
+            config,
+        )
+        self.zerofox_password = get_config_variable(
+            "ZEROFOX_PASSWORD",
+            ["zerofox", "password"],
+            config,
         )
 
     def get_access_token(self):
@@ -55,12 +58,6 @@ class ZeroFoxConnector:
                 f"Request failed with status code {response.status_code}, response: {response.text}"
             )
 
-    def upload_stix_bundle_to_opencti(self, stix_bundle):
-        # Import the STIX bundle to OpenCTI
-        self.helper.stix2.import_bundle_from_json(stix_bundle)
-
     def run(self, endpoint):
         access_token = self.get_access_token()
-        all_json_data = fetch_data_from_zerofox_endpoint(
-            access_token, endpoint, self.upload_stix_bundle_to_opencti
-        )
+        fetch_data_from_zerofox_endpoint(access_token, endpoint, self.helper)
