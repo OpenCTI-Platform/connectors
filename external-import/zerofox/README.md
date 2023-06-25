27600441de54ab4ada3ba44a003d7aaeda1a1072
Filigran Automation <automation@filigran.io>
2023-06-25 20:20:05 +0000

    [all] Release 5.8.4

--- external-import/zerofox/README.md
+++ external-import/zerofox/README.md
@@ -26,7 +26,7 @@ The connectors adds the following Entities:
 
 ### Requirements
 
-- OpenCTI Platform >= 5.8.3
+- OpenCTI Platform >= 5.8.4
 
 ### Configuration
 
--- stream/elastic/elastic/__init__.py
+++ stream/elastic/elastic/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "5.8.3"
+__version__ = "5.8.4"
 LOGGER_NAME = "elastic"
 RE_DATEMATH = (
     r"\{(?P<modulo>.*now[^{]*)(?:\{(?P<format>[^|]*)(?:\|(?P<offset>[^}]+))?\})?\}"
