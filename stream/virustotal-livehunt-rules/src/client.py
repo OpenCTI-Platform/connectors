9eb455b352dadbcbad15c4419bb1d5b76e414cd2
Axel Fahy <axelfahy@users.noreply.github.com>
2023-07-17 23:19:09 +0200

    [all] Replace `method_whitelist` by `allowed_methods` (#1293)

--- stream/virustotal-livehunt-rules/src/client.py
+++ stream/virustotal-livehunt-rules/src/client.py
@@ -49,7 +49,7 @@ class VirusTotalClient:
         retry_strategy = Retry(
             total=3,
             status_forcelist=[429, 500, 502, 503, 504],
-            method_whitelist=["HEAD", "GET", "OPTIONS", "POST", "DELETE"],
+            allowed_methods=["HEAD", "GET", "OPTIONS", "POST", "DELETE"],
         )
         adapter = HTTPAdapter(max_retries=retry_strategy)
         http = requests.Session()
