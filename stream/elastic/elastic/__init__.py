2b0e779336f427f93fda1eebc4b71052178d8933
Filigran Automation <automation@filigran.io>
2023-07-19 03:11:41 +0000

    [all] Release 5.9.4

--- stream/elastic/elastic/__init__.py
+++ stream/elastic/elastic/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "5.9.3"
+__version__ = "5.9.4"
 LOGGER_NAME = "elastic"
 RE_DATEMATH = (
     r"\{(?P<modulo>.*now[^{]*)(?:\{(?P<format>[^|]*)(?:\|(?P<offset>[^}]+))?\})?\}"
