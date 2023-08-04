9787b2e040d01ce64c095e6890cc2db29b67fb18
Jonah-RF <117381443+Jonah-RF@users.noreply.github.com>
2023-08-04 06:18:27 -0400

    [recordedfuture-enrichment] New refactored version

--- internal-enrichment/recordedfuture-enrichment/entrypoint.sh
+++ internal-enrichment/recordedfuture-enrichment/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Correct working directory
-cd /opt/opencti-connector-recordedfuture-enrichment
+cd /opt/opencti-connector-rf-enrichment
 
 # Start the connector
 python rf_enrichment.py
