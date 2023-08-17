e9c5e9b10770be91fcd078233c7bfdff8e2132dc
BocognanoSarah <sarah.bocognano@filigran.io>
2023-08-17 15:00:05 +0200

    Revert "[connectors] fix threat actor pdf export (#opencti/1305)"
    This reverts commit f1e1584cababe1d538132331c065c9939e186181.

--- internal-export-file/export-report-pdf/src/export-report-pdf.py
+++ internal-export-file/export-report-pdf/src/export-report-pdf.py
@@ -394,7 +394,7 @@ class ExportReportPdf:
         env = Environment(
             loader=FileSystemLoader(self.current_dir), finalize=self._finalize
         )
-        template = env.get_template("resources/threat-actor.html")
+        template = env.get_template("resources/threat-actor-group.html")
         html_string = template.render(context)
 
         # Generate pdf from html string
@@ -489,7 +489,7 @@ class ExportReportPdf:
         env = Environment(
             loader=FileSystemLoader(self.current_dir), finalize=self._finalize
         )
-        template = env.get_template("resources/threat-actor.html")
+        template = env.get_template("resources/threat-actor-individual.html")
         html_string = template.render(context)
 
         # Generate pdf from html string
