83e3db99e1e0e7a8ad9ea380b775365a1c614bc1
Atilla AK <aaak61@gmail.com>
2023-08-22 14:16:25 +0300

    Another fix

--- internal-import-file/import-url-to-report/entrypoint.sh
+++ internal-import-file/import-url-to-report/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Correct working directory
-cd /opt/opencti-connector-import-file-url
+cd /opt/opencti-connector-import-url-to-report
 
 # Start the connector
 python3 import-file-url.py
