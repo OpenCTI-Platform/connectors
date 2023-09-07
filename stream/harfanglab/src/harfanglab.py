b5a70f459bb1800b229b2cb6eef000962d674f3d
Frederic Basler <frederic.basler@filigran.io>
2023-09-07 10:57:52 +0200

    [HarfangLab] Update Yara and Sigma indicators

--- stream/harfanglab/src/harfanglab.py
+++ stream/harfanglab/src/harfanglab.py
@@ -152,7 +152,7 @@ class HarfangLabConnector:
                         json=sigma_indicator
                     )
                     if json.loads(response.content)['status'][0]['status'] is False:
-                        self.helper.log_error(f"{json.loads(response.content)['status'][0]['content']}")
+                        self.helper.log_error(f"[CREATE] Error = {json.loads(response.content)['status'][0]['content']}")
                     else:
                         self.helper.log_info(f'Indicator SIGMA created = {response}')
 
@@ -174,28 +174,86 @@ class HarfangLabConnector:
                         + "}"
                     )
 
-                    yara_indicator = {
-                        "name": data["name"],
-                        "content": data["pattern"],
-                        "enabled": True,
-                        "source_id": self.harfanglab_yara_list_id
-                    }
-
                     yara_name_indicator_reverse_patch = json.loads(msg.data)['context']['reverse_patch'][0]['value']
-                    response_get = requests.get(
+                    response_yara_name_indicator = requests.get(
                         self.api_url + f'/YaraFile/?search={yara_name_indicator_reverse_patch}',
+                        headers=self.headers,
                     )
-                    self.helper.log_info(f'Indicator old name = {response_get}')
-                    # response = requests.put(
-                    #     self.api_url + '/YaraFile/',
-                    #     headers=self.headers,
-                    #     json=yara_indicator
-                    # )
-                    # #TODO handle case where status contains many elements
-                    # if json.loads(response.content)['status'][0]['status'] is False:
-                    #     self.helper.log_error(f"Error = {json.loads(response.content)['status'][0]['content']}")
-                    # else:
-                    #     self.helper.log_info(f'Indicator YARA created = {response}')
+
+                    response_yara_content = response_yara_name_indicator.content
+                    response_yara_content_count = json.loads(response_yara_content)['count']
+                    response_yara_status_code = response_yara_name_indicator.status_code
+
+                    if response_yara_status_code != 200:
+                        msg_log = f'[UPDATE] The request returned code {response_yara_status_code}'
+                        self.helper.log_error(msg_log),
+                    elif response_yara_content_count == 0:
+                        msg_log = f'[UPDATE] The searched name of the Yara indicator ({yara_name_indicator_reverse_patch}) does not exist in HarfangLab'
+                        self.helper.log_error(msg_log),
+                    else:
+                        response_yara_content_id = json.loads(response_yara_content)['results'][0]['id']
+
+                        yara_indicator = {
+                            "name": data["name"],
+                            "content": data["pattern"],
+                            "source_id": self.harfanglab_yara_list_id
+                        }
+
+                        response = requests.put(
+                            self.api_url + f'/YaraFile/{response_yara_content_id}/',
+                            headers=self.headers,
+                            json=yara_indicator
+                        )
+                        # TODO handle case where status contains many elements
+                        if response.status_code != 200:
+                            msg_log = f"[UPDATE] Error {response.status_code} = {json.loads(response.content)['detail']}"
+                            self.helper.log_error(msg_log)
+                        else:
+                            self.helper.log_info(f'Indicator YARA updated')
+
+                if data["pattern_type"] == "sigma":
+                    self.helper.log_info(
+                        "[UPDATE] Processing indicator {"
+                        + OpenCTIConnectorHelper.get_attribute_in_extension("id", data)
+                        + "}"
+                    )
+
+                    sigma_name_indicator_reverse_patch = json.loads(msg.data)['context']['reverse_patch'][0]['value']
+                    response_sigma_name_indicator = requests.get(
+                        self.api_url + f'/SigmaRule/?search={sigma_name_indicator_reverse_patch}',
+                        headers=self.headers,
+                        )
+
+                    response_sigma_content = response_sigma_name_indicator.content
+                    response_sigma_content_count = json.loads(response_sigma_content)['count']
+                    response_sigma_status_code = response_sigma_name_indicator.status_code
+
+                    if response_sigma_status_code != 200:
+                        msg_log = f'[UPDATE] The request returned code {response_sigma_status_code}'
+                        self.helper.log_error(msg_log),
+                    elif response_sigma_content_count == 0:
+                        msg_log = f'[UPDATE] The searched name of the Sigma indicator ({sigma_name_indicator_reverse_patch}) does not exist in HarfangLab'
+                        self.helper.log_error(msg_log),
+                    else:
+                        response_sigma_content_id = json.loads(response_sigma_content)['results'][0]['id']
+
+                        sigma_indicator = {
+                            "name": data["name"],
+                            "content": data["pattern"],
+                            "source_id": self.harfanglab_sigma_list_id
+                        }
+
+                        response = requests.put(
+                            self.api_url + f'/SigmaRule/{response_sigma_content_id}/',
+                            headers=self.headers,
+                            json=sigma_indicator
+                        )
+                        # TODO handle case where status contains many elements
+                        if response.status_code != 200:
+                            msg = f"[UPDATE] Error {response.status_code} = {json.loads(response.content)['detail']}"
+                            self.helper.log_error(msg)
+                        else:
+                            self.helper.log_info(f'Indicator SIGMA updated')
 
             return
         if msg.event == "delete":
