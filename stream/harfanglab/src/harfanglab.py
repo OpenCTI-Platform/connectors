445d8c5157ebd941d0221f9e8ce7c599ee1d17e7
Frederic Basler <frederic.basler@filigran.io>
2023-09-20 16:18:39 +0200

    [HarfangLab] Refacto, delete Yara/Sigma ok, create observable okÃ

--- stream/harfanglab/src/harfanglab.py
+++ stream/harfanglab/src/harfanglab.py
@@ -51,6 +51,9 @@ class HarfangLabConnector:
         self.harfanglab_indicator_delete = get_config_variable(
             "HARFLANGLAB_INDICATOR_DELETE", ["harfanglab", "indicator_delete"], config
         )
+        self.harfanglab_rule_maturity = get_config_variable(
+            "HARFLANGLAB_RULE_MATURITY", ["harfanglab", "rule_maturity"], config
+        )
         self.source_list = {
             "name": self.harfanglab_source_list_name,
             "description": "Cyber Threat Intelligence knowledge imported from OpenCTI.",
@@ -84,9 +87,11 @@ class HarfangLabConnector:
             elif pattern == 'sigma':
                 self.sigma_list_id = create_source['id']
                 return self.helper.log_info(f'Sigma Source ID create = {self.sigma_list_id}')
-            else:
+            elif pattern == 'stix':
                 self.stix_list_id = create_source['id']
                 return self.helper.log_info(f'Stix Source ID create = {self.stix_list_id}')
+            else:
+                raise ValueError("Unsupported Pattern Type")
         else:
             if pattern == 'yara':
                 self.yara_list_id = source['id']
@@ -94,25 +99,31 @@ class HarfangLabConnector:
             elif pattern == 'sigma':
                 self.sigma_list_id = source['id']
                 return self.helper.log_info(f'Sigma Source ID existing = {self.sigma_list_id}')
-            else:
+            elif pattern == 'stix':
                 self.stix_list_id = source['id']
                 return self.helper.log_info(f'Stix Source ID existing = {self.stix_list_id}')
+            else:
+                raise ValueError("Unsupported Pattern Type")
 
     def _process_message(self, msg):
         try:
             data = json.loads(msg.data)["data"]
+            entity = self.helper.api.indicator.read(
+                id=OpenCTIConnectorHelper.get_attribute_in_extension("id", data)
+            )
+
         except:
             raise ValueError("Cannot process the message")
 
         # Handle create
         if msg.event == "create":
-            if data["type"] == "indicator" and data["revoked"] is False and OpenCTIConnectorHelper.get_attribute_in_extension("detection", data) is True:
+            if data["type"] == "indicator": #and data["revoked"] is False and OpenCTIConnectorHelper.get_attribute_in_extension("detection", data) is True:
                 if data['pattern_type'] == "yara":
-                    self.create_indicator(data, "yara", "YaraFile", self.yara_list_id)
+                    self.create_indicator(data, "yara", "YaraFile")
                 elif data['pattern_type'] == "sigma":
-                    self.create_indicator(data, "sigma", "SigmaRule", self.sigma_list_id)
-                # elif data['pattern_type'] == "stix":
-                    # self.create_indicator(data, "stix", "IOCRule", self.stix_list_id)
+                    self.create_indicator(data, "sigma", "SigmaRule")
+                elif data['pattern_type'] == "stix":
+                    self.create_observable(entity, "stix", "IOCRule")
                 else:
                     raise ValueError("Unsupported Pattern Type")
             return
@@ -124,8 +135,8 @@ class HarfangLabConnector:
                     self.update_indicator(data, msg, "yara", "YaraFile", self.yara_list_id)
                 elif data['pattern_type'] == "sigma":
                     self.update_indicator(data, msg, "sigma", "SigmaRule", self.sigma_list_id)
-                # elif data['pattern_type'] == "stix":
-                    # self.update_indicator(data, msg, "stix", "IOCRule", self.stix_list_id)
+                elif data['pattern_type'] == "stix":
+                    self.update_indicator(data, msg, "stix", "IOCRule", self.stix_list_id)
                 else:
                     raise ValueError("Unsupported Pattern Type")
             return
@@ -137,13 +148,48 @@ class HarfangLabConnector:
                     self.delete_indicator(data, "yara", "YaraFile")
                 elif data['pattern_type'] == "sigma":
                     self.delete_indicator(data, "sigma", "SigmaRule")
-                # elif data['pattern_type'] == "stix":
-                #     self.update_indicator(data, msg, "stix", "IOCRule")
+                elif data['pattern_type'] == "stix":
+                    self.delete_indicator(data, "stix", "IOCRule")
                 else:
                     raise ValueError("Unsupported Pattern")
             return
 
-    def create_indicator(self, data, pattern, uri, list_id):
+    def create_observable(self, entity, pattern, uri):
+        if entity["pattern_type"] == f"{pattern}":
+            self.helper.log_info(f"[CREATE] Processing {pattern} observable" + " {" + entity['id'] + "}")
+
+        observables = entity['observables']
+        if not observables:
+            return self.helper.log_error(f"[CREATE] The indicator has no observable, creation failure")
+        for observable in observables:
+            if observable['entity_type'] == "StixFile" or observable['entity_type'] == "Artifact":
+                observable['entity_type'] = 'hash'
+            elif observable['entity_type'] == "Domain-Name" or observable['entity_type'] == "Hostname":
+                observable['entity_type'] = 'domain_name'
+            elif observable['entity_type'] == "IPv4-Addr" or observable['entity_type'] == "IPv6-Addr":
+                observable['entity_type'] = 'ip_both'
+            elif observable['entity_type'] == "Url":
+                observable['entity_type'] = 'url'
+            else:
+                self.helper.log_error(f"The observable type {observable['entity_type']} has not been identified")
+
+            indicator_id = entity['standard_id']
+            indicator_score = entity['x_opencti_score']
+            observable["comment"] = {
+                "indicator_id": indicator_id,
+                "indicator_score": indicator_score
+            }
+
+            payload = self.pattern_payload(entity, True, observable)
+            response = self._query("post", f"/{uri}/", payload)
+
+            if response is None:
+                self.helper.log_error(f"[CREATE] The observable type not created")
+            elif response['status_code'] == 201:
+                self.helper.log_info(f"[CREATE] The observable type created = {response['response']['id']}")
+        return
+
+    def create_indicator(self, data, pattern, uri):
         if data["pattern_type"] == f"{pattern}":
             self.helper.log_info(
                 f"[CREATE] Processing {pattern} indicator" + " {"
@@ -151,7 +197,9 @@ class HarfangLabConnector:
                 + "}"
             )
 
-            payload = self.pattern_payload(data, pattern, list_id)
+            payload = self.pattern_payload(data)
+            if len(payload['name']) > 100:
+                payload['name'] = payload['name'][0:99]
             response = self._query("post", f"/{uri}/", payload)
 
             # TODO handle case where status contains many elements ?
@@ -161,7 +209,6 @@ class HarfangLabConnector:
             elif not response['status']:
                 return self.helper.log_error(f"Error missing value")
             elif response['status'][0]['status'] is False:
-                # {"ERROR", "message": "Error duplicate_rule = A rule with this ID already exists"}
                 return self.helper.log_error(f"Error {response['status'][0]['code']} = {response['status'][0]['content']}")
             else:
                 return self.helper.log_info(f"[CREATE] Indicator {pattern} created = {response['status'][0]['id']}")
@@ -177,24 +224,27 @@ class HarfangLabConnector:
             indicator_previous_name = json.loads(msg.data)['context']['reverse_patch'][0]['value']
 
             # TODO Problem
-            response_name_indicator = self._query("get", f"/{uri}/?search={indicator_previous_name}")
+            response_name_indicator = self._query("get", f"/{uri}/?search={indicator_previous_name}&source_id={list_id}")
             response_element = self.find_data_name_match(response_name_indicator['results'], indicator_previous_name)
 
             if response_element is None:
                 # UPSERT
                 if data["pattern_type"] == "yara":
-                    self.create_indicator(data, "yara", "YaraFile", self.yara_list_id)
+                    self.create_indicator(data, "yara", "YaraFile")
                 elif data["pattern_type"] == "sigma":
-                    self.create_indicator(data, "sigma", "SigmaRule", self.sigma_list_id)
+                    self.create_indicator(data, "sigma", "SigmaRule")
                 elif data["pattern_type"] == "stix":
-                    self.create_indicator(data, "stix", "IOCRule", self.stix_list_id)
+                    entity = self.helper.api.indicator.read(
+                        id=OpenCTIConnectorHelper.get_attribute_in_extension("id", data)
+                    )
+                    self.create_observable(entity, "stix", "IOCRule")
                 else:
                     raise ValueError("Unsupported Pattern")
                 return
             else:
                 response_id = response_element['id']
 
-                payload = self.pattern_payload(data, pattern, list_id)
+                payload = self.pattern_payload(data)
                 response = self._query("put", f"/{uri}/{response_id}/", payload)
 
                 # TODO handle case where status contains many elements
@@ -212,65 +262,82 @@ class HarfangLabConnector:
             )
 
         indicator_name = data['name']
+        if len(data['name']) > 100:
+            indicator_name = data['name'][0:99]
         response_name_indicator = self._query("get", f"/{uri}/?search={indicator_name}")
         response_element = self.find_data_name_match(response_name_indicator['results'], indicator_name)
 
-        if response_element is None:
-            msg_log = f'[DELETE] The searched name of the {pattern} indicator ({indicator_name}) does not exist in HarfangLab'
-            return self.helper.log_error(msg_log),
+        if self.harfanglab_indicator_delete is True:
+            if response_element is None:
+                msg_log = f'[DELETE] The searched name of the {pattern} indicator ({indicator_name}) does not exist in HarfangLab'
+                return self.helper.log_error(msg_log),
+            else:
+                response_id = response_element['id']
+                response = self._query("delete", f"/{uri}/{response_id}/")
+
+                if response is None:
+                    return self.helper.log_info(f"[DELETE] Indicator {pattern} deleted = {response_id}")
+                else:
+                    return self.helper.log_error(f"[DELETE] Indicator {pattern} not deleted = {response_id}")
         else:
-            response_id = response_element['id']
-            response = self._query("delete", f"/{uri}/{response_id}/")
+            if response_element is None:
+                msg_log = f'[DISABLE] The searched name of the {pattern} indicator ({indicator_name}) does not exist in HarfangLab'
+                return self.helper.log_error(msg_log),
+            else:
+                response_id = response_element['id']
+                payload = self.pattern_payload(data, self.harfanglab_indicator_delete)
+                response = self._query("put", f"/{uri}/{response_id}/", payload)
 
             if response is None:
-                return self.helper.log_error(f"[DELETE] Indicator {pattern} not delete = {response_id}")
+                return self.helper.log_error(f"[DISABLE] Indicator {pattern} not disabled = {response_id}")
             else:
-                return self.helper.log_info(f"[DELETE] Indicator {pattern} deleted = {response_id}")
+                return self.helper.log_info(f"[DISABLE] Indicator {pattern} disabled = {response_id}")
+
 
     def find_data_name_match(self, data, name):
         return next((x for x in data if x["name"] == name), None)
 
-    def pattern_payload(self, data, pattern, list_id):
-        if pattern == 'yara':
+    def pattern_payload(self, data, enabled=True, observable=None):
+        if data["pattern_type"] == 'yara':
             return {
                 "content": data["pattern"],
                 "name": data["name"],
-                "source_id": list_id,
+                "source_id": self.yara_list_id,
 
-                "enabled": True,
+                "enabled": enabled,
                 # Local testing status : [ in_progress, rejected, validated ]
                 "hl_local_testing_status": "in_progress",
                 # status : [ experimental, stable, testing ]
-                "hl_status": "stable"
+                "hl_status": self.harfanglab_rule_maturity
                 # "last_modifier": {"username": "string"},
             }
-        elif pattern == 'sigma':
+        elif data["pattern_type"] == 'sigma':
             return {
                 "content": data["pattern"],
                 "name": data["name"],
-                "source_id": list_id,
+                "source_id": self.sigma_list_id,
 
-                "enabled": True,
+                "enabled": enabled,
                 "hl_local_testing_status": "in_progress",
-                "hl_status": "stable",
+                "hl_status": self.harfanglab_rule_maturity,
                 # "block_on_agent": True,
                 # Rule level override: [ critical, high, informational, low, medium ]
                 # "rule_level_override": "critical"
                 # "last_modifier": {"username": "string"},
             }
-        elif pattern == "stix":
+        elif data["pattern_type"] == "stix":
             return {
-                "source_id": list_id,
+                "source_id": self.stix_list_id,
                 # "type": ["domain_name", "filename", "filepath", "hash", "ip_both", "ip_dst", "ip_src", "url"],
-                "type": "domain_name",
-                "value": data['pattern'],
+                "type": observable['entity_type'],
+                "value": observable['observable_value'],
 
-                "enabled": True,
+                "enabled": enabled,
+                "comment": json.dumps(observable['comment']),
+                "hl_status": self.harfanglab_rule_maturity,
                 # "hl_local_testing_status": "in_progress",
-                # "hl_status": "stable",
-                "description": data['description'],
+                # "description": data['description'],
                 # "last_modifier": {"username": "string"},
-                # "comment": "string",
                 # "category": "string",
                 # "info": "string",
                 # "reference": ["string"]
@@ -304,11 +371,18 @@ class HarfangLabConnector:
             )
         else:
             raise ValueError("Unsupported method")
-        if response.status_code == 200 or response.status_code == 201:
+        if response.status_code == 200:
             try:
                 return response.json()
             except:
                 return response.text
+        elif response.status_code == 201:
+            msg_log = f"Status code 201 : Resource was created"
+            self.helper.log_info(msg_log)
+            return {'response': response.json(), 'status_code': response.status_code}
+        elif response.status_code == 204:
+            msg_log = f"Status code 204 : Resource deleted successfully"
+            return self.helper.log_info(msg_log)
         elif response.status_code == 400:
             msg_log = f"Status code 400 : Bad Request = {json.loads(response.content)}"
             return self.helper.log_error(msg_log)
