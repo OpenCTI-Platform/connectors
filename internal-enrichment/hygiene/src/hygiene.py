749745dd1be4c58a31b30b6b2ec49c7217673030
Samuel Hassine <samuel.hassine@filigran.io>
2023-10-06 16:12:39 +0200

    Introduce first playbook compatible connectors

--- internal-enrichment/hygiene/src/hygiene.py
+++ internal-enrichment/hygiene/src/hygiene.py
@@ -101,7 +101,7 @@ class HygieneConnector:
             if os.path.isfile(config_file_path)
             else {}
         )
-        self.helper = OpenCTIConnectorHelper(config)
+        self.helper = OpenCTIConnectorHelper(config, True)
 
         warninglists_slow_search = bool(
             get_config_variable(
