50436d443a44019fcc0c29c13784e52f5354156c
Samuel Hassine <samuel.hassine@filigran.io>
2023-10-06 08:13:51 +0200

    [abuse-ssl] Fix the connector

--- external-import/abuse-ssl/src/abuse-ssl.py
+++ external-import/abuse-ssl/src/abuse-ssl.py
@@ -59,7 +59,9 @@ class AbuseSSLImportConnector:
             try:
                 current_state = self.helper.get_state()
                 now = datetime.now(tz=timezone.utc)
-                friendly_name = "Abuse.ch SSL run @ " + now.strftime("%Y-%m-%d %H:%M:%S")
+                friendly_name = "Abuse.ch SSL run @ " + now.strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                )
                 work_id = self.helper.api.work.initiate_work(
                     self.helper.connect_id, friendly_name
                 )
