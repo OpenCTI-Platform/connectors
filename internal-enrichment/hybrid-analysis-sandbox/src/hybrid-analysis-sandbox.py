a4a7a80fb2b12b9729c2938e3fbd7c70b1474ef7
Filigran Automation <automation@filigran.io>
2023-12-10 14:03:48 +0000

    [all] Release 5.12.5

--- internal-enrichment/hybrid-analysis-sandbox/src/hybrid-analysis-sandbox.py
+++ internal-enrichment/hybrid-analysis-sandbox/src/hybrid-analysis-sandbox.py
@@ -44,7 +44,7 @@ class HybridAnalysis:
         self.api_url = "https://www.hybrid-analysis.com/api/v2"
         self.headers = {
             "api-key": self.api_key,
-            "user-agent": "OpenCTI Hybrid Analysis Connector - Version 5.12.4",
+            "user-agent": "OpenCTI Hybrid Analysis Connector - Version 5.12.5",
             "accept": "application/json",
         }
         self.identity = self.helper.api.identity.create(
