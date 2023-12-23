62548c3e3c0c48a3ba43483b4b78899f742b990e
Chris Mandich <147428175+cmandich@users.noreply.github.com>
2023-12-23 05:20:57 -0800

    [recordedfuture-enrichment] Connector enhancements and bug resolution (#1642)

--- internal-enrichment/recordedfuture-enrichment/src/rflib/rf_client.py
+++ internal-enrichment/recordedfuture-enrichment/src/rflib/rf_client.py
@@ -8,22 +8,25 @@
 # using the foregoing.                                                         #
 ################################################################################
 """
-import urllib
+import logging
+import urllib.parse
 
 import requests
-import requests.exceptions
+
+from .rf_utils import extract_and_combine_links
 
 API_BASE = "https://api.recordedfuture.com"
-CONNECT_BASE = API_BASE + "/v2"
-LINKS_BASE = API_BASE + "/links"
-LINK_SEARCH = LINKS_BASE + "/search"
+CONNECT_BASE = urllib.parse.urljoin(API_BASE, "/v2")
+LINK_SEARCH = urllib.parse.urljoin(API_BASE, "/links/search")
+
+LOGGER = logging.getLogger("name")
 
 
 class RFClient:
     """class for talking to the RF API, specifically for enriching indicators"""
 
     def __init__(self, token, helper, header="OpenCTI-Enrichment/2.0"):
-        """Inits function"""
+        """Initialize the RFClient with API token and session settings."""
         self.token = token
         headers = {"X-RFToken": token, "User-Agent": header}
         self.session = requests.Session()
@@ -31,30 +34,61 @@ class RFClient:
         self.helper = helper
 
     def full_enrichment(self, entity, type_):
-        """Enrich an individual IOC"""
-        enrichment = self._enrich(entity, type_)
-        links = self._get_links(enrichment["entity"]["id"])
-        enrichment["links"] = links
-        return enrichment
+        """Enrich an individual IOC with additional links."""
+        reason, enrichment = self._enrich(entity, type_)
+        if enrichment:
+            links_reason, links = self._get_links(enrichment["entity"]["id"])
+            if links:
+                enrichment["links"] = links
+            else:
+                LOGGER.warning(f"Failed to return links: {links_reason}")
+                enrichment["links"] = None
+        return reason, enrichment
 
     def _enrich(self, entity, type_):
-        """Make enrichment call to get entity and risk score"""
+        """Enrich entity and get its risk score."""
         fields = "entity,risk"
         if type_.lower() == "hash":
             fields += ",hashAlgorithm"
-        url = "{}/{}/{}".format(
-            CONNECT_BASE, type_, urllib.parse.quote(entity, safe="")
-        )
-        res = self.session.get(url, params={"fields": fields})
-        res.raise_for_status()
-        return res.json()["data"]
+
+        url = f"{CONNECT_BASE}/{type_}/{urllib.parse.quote(entity, safe='')}"
+
+        try:
+            res = self.session.get(url, params={"fields": fields})
+            return self._handle_response(res)
+        except requests.exceptions.RequestException as e:
+            LOGGER.error(f"Error making request: {e}")
+            return None, None
 
     def _get_links(self, rfid):
-        """Get links for entity"""
+        """Retrieve links for a given entity ID."""
         query = {
-            "entities": ["{}".format(rfid)],
+            "entities": [f"{rfid}"],
             "limits": {"search_scope": "medium", "per_entity_type": 100},
         }
-        res = self.session.post(LINK_SEARCH, json=query)
-        res.raise_for_status()
-        return res.json()["data"][0]["links"]
+        try:
+            res = self.session.post(LINK_SEARCH, json=query)
+            LOGGER.debug(f"Response: {res.json()}")
+            return self._handle_response(res, expected_key="links")
+        except requests.exceptions.RequestException as e:
+            LOGGER.error(f"Error making request: {e}")
+            return "Error making Request", None
+
+    def _handle_response(self, response, expected_key=None):
+        """Handle API response, returning the relevant part if successful."""
+        if response.status_code == 200:
+            json_data = response.json().get("data", None)
+            if json_data:
+                if expected_key == "links":
+                    joined_lists = extract_and_combine_links(json_data)
+                    return response.reason, joined_lists
+                else:
+                    return response.reason, json_data
+            else:
+                LOGGER.warning('Response does not include the key: "data".')
+                return response.reason, None
+        else:
+            LOGGER.warning(
+                f"Status Code: {response.status_code}, Response: {response.reason}"
+            )
+            return response.reason, None
