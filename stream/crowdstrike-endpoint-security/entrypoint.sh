6b0aada3a9666e6b5c67a35f3a14bdbb655b5a8d
Samuel Hassine <samuel.hassine@filigran.io>
2024-01-25 14:49:51 +0100

    [crowdstrike-endpoint-security] Integrate to CI/CD

--- stream/crowdstrike-endpoint-security/entrypoint.sh
+++ stream/crowdstrike-endpoint-security/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Go to the right directory
-cd /opt/opencti-connector-crowdstrike
+cd /opt/opencti-connector-crowdstrike-endpoint-security
 
 # Launch the worker
 python3 crowdstrike.py
