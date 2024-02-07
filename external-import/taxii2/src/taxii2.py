44ea4dfa16baf8cbda36de6d5dcec822c01f0798
Michael D <you@example.com>
2024-02-07 19:19:12 +1300

    [taxii2] Add API key as an auth option for url

--- external-import/taxii2/src/taxii2.py
+++ external-import/taxii2/src/taxii2.py
@@ -13,7 +13,7 @@ import taxii2client.v20 as tx20
 import taxii2client.v21 as tx21
 import yaml
 from pycti import OpenCTIConnectorHelper, StixCyberObservableTypes, get_config_variable
-from requests.auth import HTTPBasicAuth, AuthBase
+from requests.auth import AuthBase, HTTPBasicAuth
 from requests.exceptions import HTTPError
 from taxii2client.common import TokenAuth
 from taxii2client.exceptions import TAXIIServiceException
