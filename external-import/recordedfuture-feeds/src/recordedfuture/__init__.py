47a62363b85dd310d9e83e80f473dc816a4835f1
Helene Nguyen <helene.nguyen@filigran.io>
2024-02-20 15:09:20 +0100

    [Recorded Future Feeds] Remove Recorded Future Feeds connector as it is deprecated (#1844)

--- external-import/recordedfuture-feeds/src/recordedfuture/__init__.py
+++
@@ -1,7 +0,0 @@
-# recordedfuture/__init__.py
-
-# Version of the recorded_future module
-__version__ = "1.0.0"
-
-# Simplify imports for the end-users
-from .core.api import RecordedFutureClient  # noqa: F401
