0f60ca033f32911dfe8479faddfbcf58dc520c4a
Helene Nguyen <helene.nguyen@filigran.io>
2024-02-20 15:09:20 +0100

    [Recorded Future Feeds] Remove Recorded Future Feeds connector as it is deprecated (#1844)

--- external-import/recordedfuture-feeds/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Correct working directory
-cd /opt/rf-feed-connector
-
-# Start the connector
-python3 main.py
--- external-import/recordedfuture-feeds/src/recordedfuture/core/__init__.py
+++
@@ -1,3 +0,0 @@
-# recordedfuture/core/__init__.py
-
-from .api import RecordedFutureClient  # noqa: F401
