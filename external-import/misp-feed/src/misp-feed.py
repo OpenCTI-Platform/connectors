bbc5918e4b6d5b1a4cac5cafe3f4e4fbb3c559e8
Helene Nguyen <helene.nguyen@filigran.io>
2024-02-22 11:56:25 +0100

    [misp/misp-feed] Complete expected tag to handle for misp and misp-feed connectors (#1850)

--- external-import/misp-feed/src/misp-feed.py
+++ external-import/misp-feed/src/misp-feed.py
@@ -650,9 +650,11 @@ class MispFeed:
                         )
                         added_names.append(name)
             # Get the linked attack_patterns
-            if tag["name"].startswith("misp-galaxy:attack-pattern") or tag[
-                "name"
-            ].startswith("mitre-attack:attack-pattern"):
+            if (
+                tag["name"].startswith("misp-galaxy:mitre-attack-pattern")
+                or tag["name"].startswith("misp-galaxy:attack-pattern")
+                or tag["name"].startswith("mitre-attack:attack-pattern")
+            ):
                 tag_value_split = tag["name"].split('="')
                 if len(tag_value_split) > 1 and len(tag_value_split[1]) > 0:
                     tag_value = tag_value_split[1][:-1].strip()
