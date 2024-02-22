c972e97c87186fc8048fd728a33fa9f000ca9f56
Samuel Hassine <samuel.hassine@filigran.io>
2024-02-22 11:44:01 +0100

    [misp/misp-feed] Fix formatting

--- external-import/misp/src/misp.py
+++ external-import/misp/src/misp.py
@@ -904,7 +904,9 @@ class Misp:
                 # Report in STIX lib must have at least one object_refs
                 if len(object_refs) == 0:
                     # Put a fake ID in the report
-                    object_refs.append("intrusion-set--fc5ee88d-7987-4c00-991e-a863e9aa8a0e")
+                    object_refs.append(
+                        "intrusion-set--fc5ee88d-7987-4c00-991e-a863e9aa8a0e"
+                    )
                 attributes = filter_event_attributes(
                     event, **self.misp_report_description_attribute_filter
                 )
