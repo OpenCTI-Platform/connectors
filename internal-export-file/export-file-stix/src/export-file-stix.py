6ca5962ce3b67908946d8f2f38d85d0f5f11d523
Romy Alula <romy.alula@filigran.io>
2024-03-13 18:20:40 +0100

    [backend] Define file marking definitions (#issue/5797)

--- internal-export-file/export-file-stix/src/export-file-stix.py
+++ internal-export-file/export-file-stix/src/export-file-stix.py
@@ -22,7 +22,8 @@ class ExportFileStix:
         file_name = data["file_name"]
         export_scope = data["export_scope"]  # query or selection or single
         export_type = data["export_type"]  # Simple or Full
-        max_marking = data["max_marking"]
+        max_marking = data["content_max_markings"][0] # remove index on in chunk #2
+        file_markings = data["file_markings"]
 
         entity_id = data.get("entity_id")
         entity_type = data["entity_type"]
@@ -49,7 +50,7 @@ class ExportFileStix:
                 },
             )
             self.helper.api.stix_domain_object.push_entity_export(
-                entity_id, file_name, json_bundle
+                entity_id, file_name, json_bundle, file_markings
             )
             self.helper.connector_logger.info(
                 "Export done",
