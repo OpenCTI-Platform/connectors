6652d7ebc4f5820bcaecc376dc4c7f148bbab1ba
Moise-RF <moise.medici@recordedfuture.com>
2024-03-18 14:09:34 +0000

    Remove outputs folder from tests

--- external-import/recorded-future/tests/test_connector.py
+++ external-import/recorded-future/tests/test_connector.py
@@ -3,16 +3,11 @@ from src.rflib import StixNote
 
 import pytest
 import os
-import pathlib
 import json
-
-CWD = pathlib.Path(__file__).parent.resolve()
-OUPUT_DIR = os.path.join(CWD, 'outputs')
-
 
 class TestConnector:
     @pytest.mark.vcr()
-    def test_basic_pull_conversion(self, rf_client, opencti_helper):
+    def test_basic_pull_conversion(self, rf_client, opencti_helper, tmp_path):
         notes = rf_client.get_notes(10000, limit=20)
         tas = rf_client.get_threat_actors()
         for i, note in enumerate(notes):
@@ -20,5 +15,5 @@ class TestConnector:
             stixnote.from_json(note, stixnote.tlp.serialize())
             #            assert len(stixnote.objects) >= 1
             assert len(stixnote.to_stix_objects()) >= 1
-            with open(os.path.join(OUPUT_DIR, f'{i}.json'), 'w') as outfile:
+            with open(os.path.join(tmp_path, f'{i}.json'), 'w') as outfile:
                 json.dump(json.loads(stixnote.to_json_bundle()), outfile, indent=4)
--- external-import/recorded-future/tests/test_stixconverter.py
+++ external-import/recorded-future/tests/test_stixconverter.py
@@ -1,13 +1,12 @@
 # from src.rflib import IPAddress, Domain, URL, FileHash, TTP, Identity, Malware, StixNote
+from pathlib import Path
 from src.rflib import IPAddress, Domain, URL, FileHash, StixNote
 import stix2.v21
 import json
 import os
-import pathlib
 import pytest
 
-CWD = pathlib.Path(__file__).parent.resolve()
-
+CWD = Path(__file__).parent
 
 @pytest.fixture
 def rf_identity():
@@ -15,109 +14,107 @@ def rf_identity():
 
 
 class TestStixObjects:
-    def test_ip(self, rf_identity):
+    def test_ip(self, rf_identity, tmp_path):
         obj = IPAddress('8.8.8.8', 'IpAdress', rf_identity)
         js = json.loads(obj.to_json_bundle())
-        print(js)
         assert len(js['objects']) == 3
-        with open(os.path.join(CWD, 'outputs/iptest.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'iptest.json'), 'w') as file:
             json.dump(js, file, indent=4)
 
-    def test_domain(self, rf_identity):
+    def test_domain(self, rf_identity, tmp_path):
         obj = Domain('google.com', 'InternetDomainName', rf_identity)
         js = json.loads(obj.to_json_bundle())
         assert len(js['objects']) == 3
-        with open(os.path.join(CWD, 'outputs/domaintest.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'domaintest.json'), 'w') as file:
             json.dump(js, file, indent=4)
 
-    def test_url(self, rf_identity):
+    def test_url(self, rf_identity, tmp_path):
         obj = URL('https://google.com', 'URL', rf_identity)
         js = json.loads(obj.to_json_bundle())
-        print(js)
         assert len(js['objects']) == 3
-        with open(os.path.join(CWD, 'outputs/urltest.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'urltest.json'), 'w') as file:
             json.dump(js, file, indent=4)
 
-    def test_md5_hash(self, rf_identity):
+    def test_md5_hash(self, rf_identity, tmp_path):
         obj = FileHash('846e27a652a5e1bfbd0ddd38a16dc865', 'Hash', rf_identity)
         js = json.loads(obj.to_json_bundle())
         assert len(js['objects']) == 3
-        with open(os.path.join(CWD, 'outputs/hashtest.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'hashtest.json'), 'w') as file:
             json.dump(js, file, indent=4)
 
-    def test_note_creation(self, opencti_helper, tas, rf_client):
+    def test_note_creation(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client)
         with open(os.path.join(CWD, 'inputs/basicnote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 2
-        with open(os.path.join(CWD, 'outputs/basicnote.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'basicnote.json'), 'w') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_ta_note_creation(self, opencti_helper, tas, rf_client):
+    def test_ta_note_creation(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client)
         with open(os.path.join(CWD, 'inputs/tanote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 2
-        with open(os.path.join(CWD, 'outputs/tanote.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'tanote.json'), 'w') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_ta_org_note_creation(self, opencti_helper, tas, rf_client):
+    def test_ta_org_note_creation(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client)
         with open(os.path.join(CWD, 'inputs/taorgnote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 2
-        with open(os.path.join(CWD, 'outputs/taorgnote.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'taorgnote.json'), 'w') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_ta_org_note_creation_flag(self, opencti_helper, tas, rf_client):
+    def test_ta_org_note_creation_flag(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client, ta_to_intrusion_set=True)
         with open(os.path.join(CWD, 'inputs/taorgnote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 2
-        with open(os.path.join(CWD, 'outputs/taorgnoteflag.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'taorgnoteflag.json'), 'w') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_person_note_creation(self, opencti_helper, tas, rf_client):
+    def test_person_note_creation(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client)
         with open(os.path.join(CWD, 'inputs/personnote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 2
-        with open(os.path.join(CWD, 'outputs/personnoteflag.json'), 'w') as file:
+        with open(os.path.join(CWD, 'personnoteflag.json'), 'w') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_person_note_creation_flag(self, opencti_helper, tas, rf_client):
+    def test_person_note_creation_flag(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client, person_to_ta=True)
         with open(os.path.join(CWD, 'inputs/personnote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 2
-        with open(os.path.join(CWD, 'outputs/personnoteflag.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'personnoteflag.json'), 'w') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_vuln_note_creation(self, opencti_helper, tas, rf_client):
+    def test_vuln_note_creation(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client)
         with open(os.path.join(CWD, 'inputs/vulnnote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 1
-        with open(os.path.join(CWD, 'outputs/vulnnote.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'vulnnote.json'), 'w') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_yara_note_creation(self, opencti_helper, tas, rf_client):
+    def test_yara_note_creation(self, opencti_helper, tas, rf_client, tmp_path):
         note = StixNote(opencti_helper, tas, rf_client)
         with open(os.path.join(CWD, 'inputs/yaranote.json'), 'r') as file:
             raw = json.load(file)
         note.from_json(raw, note.tlp.serialize())
         assert len(note.objects) > 1
-        with open(os.path.join(CWD, 'outputs/yaranote.json'), 'w') as file:
+        with open(os.path.join(tmp_path, 'yaranote.json'), 'w+') as file:
             json.dump(json.loads(note.to_json_bundle()), file, indent=4)
 
-    def test_report_type_conversion(self, opencti_helper, tas, rf_client):
+    def test_report_type_conversion(self, opencti_helper, tas, rf_client, tmp_path):
         topics = [
             {
                 "id": "ZjnoP2",
