c3f8b8962fd8cc66a03c5fbc48a1dbe09587a63e
srobaux <160116607+srobaux@users.noreply.github.com>
2024-04-03 08:15:03 +0200

    [recordedfuture] Alert to incident (#1977)

--- external-import/recorded-future/src/main.py
+++ external-import/recorded-future/src/main.py
@@ -16,7 +16,14 @@ from datetime import datetime
 
 import yaml
 from pycti import OpenCTIConnectorHelper, get_config_variable
-from rflib import APP_VERSION, RFClient, RiskList, StixNote, ThreatMap
+from rflib import (
+    APP_VERSION,
+    RecordedFutureAlertConnector,
+    RFClient,
+    RiskList,
+    StixNote,
+    ThreatMap,
+)
 
 
 class RFNotes:
@@ -127,6 +134,10 @@ class RFNotes:
         else:
             self.risklist_related_entities = risklist_related_entities_list.split(",")
 
+        self.rf_alert_enable = get_config_variable(
+            "ALERT_ENABLE", ["alert", "enable"], config
+        )
+
     def get_interval(self):
         """Converts interval hours to seconds"""
         return int(self.rf_interval) * 3600
@@ -223,6 +234,10 @@ if __name__ == "__main__":
     try:
         RF = RFNotes()
 
+        # Start RF Alert Connector
+        if RF.rf_alert_enable:
+            RfCon = RecordedFutureAlertConnector(RF.helper)
+            RfCon.run()
         # Pull RF risk lists
         if RF.rf_pull_risk_list:
             RiskList = RiskList(
