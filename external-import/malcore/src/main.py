5425abfdaee4da38a6a41ada57df13c22063c7fa
Yassine Ouaamou <yassine.ouaamou@filigran.io>
2024-04-15 19:29:44 +0200

    [Malcore] fix File created_by_ref and add error catch (#2016)

--- external-import/malcore/src/main.py
+++ external-import/malcore/src/main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     try:
         connector = Malcore()
         connector.run()
-    except:
+    except Exception as e:
+        print(e)
         time.sleep(10)
         exit(0)
