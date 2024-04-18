00d03f4184358ff82677e1e6755f066f2cd9dc82
Ekultek <14183473+Ekultek@users.noreply.github.com>
2024-04-18 03:31:59 -0500

    [Malcore] Multiple updates (#2045)

--- external-import/malcore/src/main.py
+++ external-import/malcore/src/main.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     try:
         connector = Malcore()
         connector.run()
-    except Exception as e:
-        print(e)
+    except:
         time.sleep(10)
         exit(0)
