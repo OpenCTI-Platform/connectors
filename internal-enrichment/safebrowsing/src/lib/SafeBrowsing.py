c0a4ddcbe502edfc47f51c9434a9cfe2ffa2b7b9
sudesh <sudesh@sudeshs-MacBook-Pro.local>
2024-04-20 22:36:21 +0100

    sending the observable back as stix observable objects

--- internal-enrichment/safebrowsing/src/lib/SafeBrowsing.py
+++ internal-enrichment/safebrowsing/src/lib/SafeBrowsing.py
@@ -2,7 +2,8 @@ import os
 from typing import Dict
 
 import requests
-from pycti import OpenCTIConnectorHelper
+from pycti import CustomObservableHostname, OpenCTIConnectorHelper
+from stix2 import URL, DomainName
 
 
 class SafeBrowsingConnector:
@@ -29,15 +30,21 @@ class SafeBrowsingConnector:
             self.update_existing_data = "false"
 
     def google_safe_browsing(self, observable):
+
+        self.helper.log_info(
+            f"Checking domain {observable['value']} against Google Safe Browsing"
+        )
+
         """Checks a domain against the Google Safe Browsing API."""
 
         API_KEY = os.environ.get("GOOGLE_SAFE_BROWSING_API_KEY", "")
         domain = observable["value"]
+        stix_objects = []
         url = f"https://safebrowsing.googleapis.com/v4/threatMatches:find?key={API_KEY}"
         payload = {
             "client": {
-                "clientId": "OpenCTI_Connector",
-                "clientVersion": "1.0",
+                "clientId": "testing",  # A descriptive name for your client
+                "clientVersion": "1.0",  # Version of your application
             },
             "threatInfo": {
                 "threatTypes": [
@@ -62,34 +69,64 @@ class SafeBrowsingConnector:
         )
         if response.status_code == 200:
             if response.json():
-                # self.helper.log_info(f"Domain {domain} flagged by Safe Browsing")
-
-                # Get the existing description and add the new one
 
                 if observable.get("x_opencti_description") is None:
                     existing_description = ""
                 else:
                     existing_description = (
-                        observable.get("x_opencti_description") + "\n"
+                        observable.get("x_opencti_description") + "\\n"
                     )
 
                 safe_browsing_output = response.json()
 
                 first_output = safe_browsing_output.get("matches")[0]
 
-                observable_description = f"Domain flagged by Safe Browsing \n + Threat Type: {first_output.get('threatType')} \n + Platform Type: {first_output.get('platformType')} \n + Threat Entry Type: {first_output.get('threatEntryType')}"
+                observable_description = f"Domain flagged by Safe Browsing \n + Threat Type: {first_output.get('threatType')} \n + Platform Type: {first_output.get('platformType')} \n + Threat Entry Type: {first_output.get('threatEntryType')} \n \n"
+                if observable["entity_type"] == "Domain-Name":
 
-                self.helper.api.stix_cyber_observable.update_field(
-                    id=observable["id"],
-                    input={
-                        "key": "x_opencti_description",
-                        "value": existing_description + observable_description,
-                    },
-                )
+                    new_domain_object = DomainName(
+                        id=observable["standard_id"],
+                        value=observable["value"],
+                        custom_properties={
+                            "x_opencti_description": existing_description
+                            + observable_description,
+                            "x_opencti_score": 100,
+                        },
+                    )
+                    stix_objects.append(new_domain_object)
+                elif observable["entity_type"] == "Url":
+                    new_url_object = URL(
+                        id=observable["standard_id"],
+                        value=observable["value"],
+                        custom_properties={
+                            "x_opencti_description": existing_description
+                            + observable_description,
+                            "x_opencti_score": 100,
+                        },
+                    )
+                    stix_objects.append(new_url_object)
 
+                elif observable["entity_type"] == "Hostname":
+                    new_hostname_object = CustomObservableHostname(
+                        id=observable["standard_id"],
+                        value=observable["value"],
+                        custom_properties={
+                            "x_opencti_description": existing_description
+                            + observable_description,
+                            "x_opencti_score": 100,
+                        },
+                    )
+                    self.helper.log_info(f"Hostname flagged by Safe Browsing")
+                    stix_objects.append(new_hostname_object)
+
+                # creating a bundle
+                stix_bundle = self.helper.stix2_create_bundle(stix_objects)
+                bundles_sent = self.helper.send_stix2_bundle(stix_bundle)
                 self.helper.api.stix_cyber_observable.add_label(
                     id=observable["id"], label_name=first_output.get("threatType")
                 )
+                self.helper.log_info(f"Sent {bundles_sent} bundles to OpenCTI")
+                return f"Sent {bundles_sent} bundles to OpenCTI"
 
         else:
             self.helper.log_error(f"Error checking domain: {response.status_code}")
@@ -105,9 +142,12 @@ class SafeBrowsingConnector:
             data (dict): The data to process. The `enrichment_entity` attribute contains the object to enrich.
         """
         self.entity_id = data["entity_id"]
-        observable = data["stix_entity"]
+        observable = self.helper.api.stix_cyber_observable.read(id=self.entity_id)
 
-        if observable["entity_type"] == "DomainName":
+        if observable["entity_type"] == "Domain-Name":
+            self.helper.log_info(
+                f"Checking domain {observable} against Google Safe Browsing"
+            )
             return self.google_safe_browsing(observable)
         elif observable["entity_type"] == "Url":
             return self.google_safe_browsing(observable)
