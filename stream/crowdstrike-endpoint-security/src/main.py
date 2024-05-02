cb364be86782e029d1c8c14dd9dcd628ea7da967
Helene Nguyen <helene.nguyen@filigran.io>
2024-05-02 16:45:00 +0200

    [Crowdstrike Endpoint Security] Rework stream connector and allow update event (#1983)

---
+++ stream/crowdstrike-endpoint-security/src/main.py
@@ -0,0 +1,15 @@
+import time
+
+from connector import CrowdstrikeConnector
+
+if __name__ == "__main__":
+    """
+    Entry point of the script
+    """
+    try:
+        connector = CrowdstrikeConnector()
+        connector.start()
+    except Exception as err:
+        print(err)
+        time.sleep(10)
+        exit(0)
