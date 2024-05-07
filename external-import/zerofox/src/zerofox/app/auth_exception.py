f2d358a93a785c47a128f497d6bde068a664fe18
Diego Ramirez R <dnrr2808@gmail.com>
2024-05-07 16:23:15 -0400

    Add updated ingestion flow for open cti and botnet map (#11)
    * Add updated ingestion flow for open cti and botnet map
    
    update ingestion flow to match that of template ingestions proposed by open cti
    add botnet map into stix objects
    according to tasks ZFE-90122 and ZFE-90118

--- external-import/zerofox/src/zerofox/app/auth_exception.py
+++
@@ -1,4 +0,0 @@
-class AuthException(Exception):
-    def __init__(self) -> None:
-        msg = "The token you have provided is not allowed to access this integration"
-        super().__init__(msg)
