260790de58b6829f7de5e88d749ce05c777d986e
Diego Ramirez R <dnrr2808@gmail.com>
2024-05-09 10:15:19 -0400

    Add c2 domains domain map (#15)
    add c2 domains map and update stix map dictionary

--- external-import/zerofox/src/mappers/__init__.py
+++ external-import/zerofox/src/mappers/__init__.py
@@ -1,11 +1,12 @@
-from datetime import datetime
 from zerofox.app.endpoints import CTIEndpoint
 from typing import Any
 from mappers.malwareToMalware import malware_to_malware
+from mappers.c2DomainsToInfrastructure import c2_domains_to_infrastructure
 
 
 def threat_feed_to_stix(feed: Any):
     return {
+        CTIEndpoint.C2Domains: c2_domains_to_infrastructure,
         CTIEndpoint.Malware: malware_to_malware,
 
 
