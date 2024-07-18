47d41f8305ac3e8d0f9520ba29f9bc04b95105fc
Helene Nguyen <helene.nguyen@filigran.io>
2024-07-18 16:21:12 +0200

    [connector] Modification on connector to use the new pycti connector helper scheduler

--- external-import/recorded-future/src/rflib/risk_list.py
+++ external-import/recorded-future/src/rflib/risk_list.py
@@ -28,84 +28,79 @@ class RiskList(threading.Thread):
         self.risklist_related_entities = risklist_related_entities
 
     def run(self):
-        while True:
-            for key, risk_list_type in RISK_LIST_TYPE_MAPPER.items():
-                self.helper.log_info(f"[RISK LISTS] Pulling {key} risk lists")
+        for key, risk_list_type in RISK_LIST_TYPE_MAPPER.items():
+            self.helper.log_info(f"[RISK LISTS] Pulling {key} risk lists")
 
-                csv_file = self.rfapi.get_risk_list_CSV(risk_list_type["path"])
-                timestamp = int(time.time())
-                now = datetime.utcfromtimestamp(timestamp)
-                work_id = self.helper.api.work.initiate_work(
-                    self.helper.connect_id,
-                    "Recorded Future Risk List run @ "
-                    + now.strftime("%Y-%m-%d %H:%M:%S"),
+            csv_file = self.rfapi.get_risk_list_CSV(risk_list_type["path"])
+            timestamp = int(time.time())
+            now = datetime.utcfromtimestamp(timestamp)
+            work_id = self.helper.api.work.initiate_work(
+                self.helper.connect_id,
+                "Recorded Future Risk List run @ " + now.strftime("%Y-%m-%d %H:%M:%S"),
+            )
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                # Filtered by score with a threshold
+                if self.risk_list_threshold is not None:
+                    try:
+                        row_risk_score = int(row["Risk"])
+                    except ValueError:
+                        row_risk_score = 0
+
+                    row_name = row["Name"]
+                    if row_risk_score < self.risk_list_threshold:
+                        self.helper.log_info(
+                            f"[RISK LIST] Ignoring indicator '{row_name}' as its risk score ({row_risk_score}) is lower than the defined risk list threshold ({self.risk_list_threshold})"
+                        )
+                        continue
+                # Convert into stix object
+                indicator = risk_list_type["class"](row["Name"], key, tlp=self.tlp)
+
+                rule_criticality_list = row["RuleCriticality"].strip("][").split(",")
+                risk_rules_list_str = row["RiskRules"].strip("][")
+                risk_rules_list = re.sub(r"\"", "", risk_rules_list_str).split(",")
+                description = (
+                    "Triggered risk rules:"
+                    + "\n\n"
+                    + "|Rule|Risk Rule Severity|Risk Score Severity|"
+                    + "\n"
+                    + "|--|--|--|"
+                    + "\n"
                 )
-                reader = csv.DictReader(csv_file)
-                for row in reader:
-                    # Filtered by score with a threshold
-                    if self.risk_list_threshold is not None:
-                        try:
-                            row_risk_score = int(row["Risk"])
-                        except ValueError:
-                            row_risk_score = 0
 
-                        row_name = row["Name"]
-                        if row_risk_score < self.risk_list_threshold:
-                            self.helper.log_info(
-                                f"[RISK LIST] Ignoring indicator '{row_name}' as its risk score ({row_risk_score}) is lower than the defined risk list threshold ({self.risk_list_threshold})"
+                for index, criticality in enumerate(rule_criticality_list):
+                    # If criticality comes with empty string, replace value at 0
+                    if not criticality:
+                        criticality = 0
+
+                    criticality_score = int(criticality)
+
+                    for corresponding_rule in RISK_RULES_MAPPER:
+                        if criticality_score == corresponding_rule["rule_score"]:
+                            description += (
+                                "|"
+                                + risk_rules_list[index]
+                                + "|"
+                                + corresponding_rule["severity"]
+                                + "|"
+                                + corresponding_rule["risk_score"]
+                                + "|"
+                                + "\n"
                             )
-                            continue
-                    # Convert into stix object
-                    indicator = risk_list_type["class"](row["Name"], key, tlp=self.tlp)
 
-                    rule_criticality_list = (
-                        row["RuleCriticality"].strip("][").split(",")
-                    )
-                    risk_rules_list_str = row["RiskRules"].strip("][")
-                    risk_rules_list = re.sub(r"\"", "", risk_rules_list_str).split(",")
-                    description = (
-                        "Triggered risk rules:"
-                        + "\n\n"
-                        + "|Rule|Risk Rule Severity|Risk Score Severity|"
-                        + "\n"
-                        + "|--|--|--|"
-                        + "\n"
-                    )
-
-                    for index, criticality in enumerate(rule_criticality_list):
-                        # If criticality comes with empty string, replace value at 0
-                        if not criticality:
-                            criticality = 0
-
-                        criticality_score = int(criticality)
-
-                        for corresponding_rule in RISK_RULES_MAPPER:
-                            if criticality_score == corresponding_rule["rule_score"]:
-                                description += (
-                                    "|"
-                                    + risk_rules_list[index]
-                                    + "|"
-                                    + corresponding_rule["severity"]
-                                    + "|"
-                                    + corresponding_rule["risk_score"]
-                                    + "|"
-                                    + "\n"
-                                )
-
-                    indicator.add_description(description)
-                    indicator.map_data(row, self.tlp, self.risklist_related_entities)
-                    indicator.build_bundle(indicator)
-                    # Create bundle
-                    bundle = indicator.to_stix_bundle()
-                    self.helper.log_info(
-                        "[RISK LISTS] Sending Bundle to server with "
-                        + str(len(bundle.objects))
-                        + " objects"
-                    )
-                    self.helper.send_stix2_bundle(
-                        bundle.serialize(),
-                        update=self.update_existing_data,
-                        work_id=work_id,
-                    )
-            self.helper.set_state({"last_risk_list_run": timestamp})
-            time.sleep(int(self.interval) * 3600)
+                indicator.add_description(description)
+                indicator.map_data(row, self.tlp, self.risklist_related_entities)
+                indicator.build_bundle(indicator)
+                # Create bundle
+                bundle = indicator.to_stix_bundle()
+                self.helper.log_info(
+                    "[RISK LISTS] Sending Bundle to server with "
+                    + str(len(bundle.objects))
+                    + " objects"
+                )
+                self.helper.send_stix2_bundle(
+                    bundle.serialize(),
+                    update=self.update_existing_data,
+                    work_id=work_id,
+                )
+        self.helper.set_state({"last_risk_list_run": timestamp})
--- external-import/recorded-future/src/rflib/threat_map.py
+++ external-import/recorded-future/src/rflib/threat_map.py
@@ -19,82 +19,78 @@ class ThreatMap(threading.Thread):
         self.risk_list_threshold = risk_list_threshold
 
     def run(self):
-        while True:
-            threat_maps = self.rfapi.get_threat_maps()
+        threat_maps = self.rfapi.get_threat_maps()
 
-            timestamp = int(time.time())
-            now = datetime.utcfromtimestamp(timestamp)
+        timestamp = int(time.time())
+        now = datetime.utcfromtimestamp(timestamp)
 
-            work_id = self.helper.api.work.initiate_work(
-                self.helper.connect_id,
-                "Recorded Future Threat Map run @ " + now.strftime("%Y-%m-%d %H:%M:%S"),
+        work_id = self.helper.api.work.initiate_work(
+            self.helper.connect_id,
+            "Recorded Future Threat Map run @ " + now.strftime("%Y-%m-%d %H:%M:%S"),
+        )
+
+        self.helper.log_info("[THREAT MAPS] Pulling threat maps entities...")
+
+        for threat_map in threat_maps:
+            threat_map_url = parse.quote(threat_map["url"], safe="/")
+            entities_mapped = self.rfapi.get_entities_mapped(threat_map_url)
+            entities_mapped_ids = []
+
+            for entity_mapped in entities_mapped:
+                # Create a list of all entities in threat map
+                entities_mapped_ids.append(entity_mapped["id"])
+
+            info_msg = (
+                f"[THREAT MAPS] Getting {len(entities_mapped_ids)} entities for"
+                f" {threat_map['type']} threat map and links entities..."
+            )
+            self.helper.log_info(info_msg)
+
+            # Getting all entities (TA or Malware) with their links
+            entities_mapped_with_links = self.rfapi.get_entities_links(
+                entities_mapped_ids
             )
 
-            self.helper.log_info("[THREAT MAPS] Pulling threat maps entities...")
+            total_mapped_entities = len(entities_mapped_ids)
 
-            for threat_map in threat_maps:
-                threat_map_url = parse.quote(threat_map["url"], safe="/")
-                entities_mapped = self.rfapi.get_entities_mapped(threat_map_url)
-                entities_mapped_ids = []
+            for entity_with_links in entities_mapped_with_links:
+                for key, threat_map_type in THREAT_MAP_TYPE_MAPPER.items():
+                    if threat_map["type"] == key:
+                        # Convert into stix object
+                        _name = entity_with_links["entity"]["name"]
+                        _type = entity_with_links["entity"]["type"].replace("type:", "")
+                        entity_to_stix2 = threat_map_type["class"](
+                            _name, _type, tlp=self.tlp
+                        )
 
-                for entity_mapped in entities_mapped:
-                    # Create a list of all entities in threat map
-                    entities_mapped_ids.append(entity_mapped["id"])
+                        # Map data with related entities
+                        entity_to_stix2.map_data(entity_with_links, self.tlp)
 
-                info_msg = (
-                    f"[THREAT MAPS] Getting {len(entities_mapped_ids)} entities for"
-                    f" {threat_map['type']} threat map and links entities..."
-                )
-                self.helper.log_info(info_msg)
+                        # Create bundle
+                        entity_to_stix2.build_bundle(entity_to_stix2)
+                        bundle = entity_to_stix2.to_stix_bundle()
 
-                # Getting all entities (TA or Malware) with their links
-                entities_mapped_with_links = self.rfapi.get_entities_links(
-                    entities_mapped_ids
-                )
+                        self.helper.log_info(
+                            "[THREAT MAPS] Sending Bundle to server with "
+                            + str(len(bundle.objects))
+                            + " objects"
+                        )
 
-                total_mapped_entities = len(entities_mapped_ids)
+                        total_mapped_entities -= 1
 
-                for entity_with_links in entities_mapped_with_links:
-                    for key, threat_map_type in THREAT_MAP_TYPE_MAPPER.items():
-                        if threat_map["type"] == key:
-                            # Convert into stix object
-                            _name = entity_with_links["entity"]["name"]
-                            _type = entity_with_links["entity"]["type"].replace(
-                                "type:", ""
-                            )
-                            entity_to_stix2 = threat_map_type["class"](
-                                _name, _type, tlp=self.tlp
-                            )
+                        self.helper.log_info(
+                            "[THREAT MAPS] Remaining "
+                            + str(total_mapped_entities)
+                            + " entities with their links to import for "
+                            + threat_map["type"]
+                            + " threat map"
+                        )
 
-                            # Map data with related entities
-                            entity_to_stix2.map_data(entity_with_links, self.tlp)
+                        # Send stix bundle for ingestion
+                        self.helper.send_stix2_bundle(
+                            bundle.serialize(),
+                            update=self.update_existing_data,
+                            work_id=work_id,
+                        )
 
-                            # Create bundle
-                            entity_to_stix2.build_bundle(entity_to_stix2)
-                            bundle = entity_to_stix2.to_stix_bundle()
-
-                            self.helper.log_info(
-                                "[THREAT MAPS] Sending Bundle to server with "
-                                + str(len(bundle.objects))
-                                + " objects"
-                            )
-
-                            total_mapped_entities -= 1
-
-                            self.helper.log_info(
-                                "[THREAT MAPS] Remaining "
-                                + str(total_mapped_entities)
-                                + " entities with their links to import for "
-                                + threat_map["type"]
-                                + " threat map"
-                            )
-
-                            # Send stix bundle for ingestion
-                            self.helper.send_stix2_bundle(
-                                bundle.serialize(),
-                                update=self.update_existing_data,
-                                work_id=work_id,
-                            )
-
-            self.helper.set_state({"last_threat_map_run": timestamp})
-            time.sleep(int(self.interval) * 3600)
+        self.helper.set_state({"last_threat_map_run": timestamp})
