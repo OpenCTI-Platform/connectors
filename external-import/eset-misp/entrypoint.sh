ea28bbccac8a9e574b29dea29b36c4dccf67aae6
Samuel Hassine <samuel.hassine@filigran.io>
2024-07-30 23:39:31 +0200

    [eset] Remove useless and not built connectors

--- external-import/eset-misp/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Correct working directory
-cd /opt/opencti-eset-misp
-
-# Start the connector
-python3 misp.py
--- external-import/eset-taxii2/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Correct working directory
-cd /opt/opencti-eset-taxii2
-
-# Start the connector
-python taxii2.py
