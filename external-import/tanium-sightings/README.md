eed1e6561d0d994fee6e7cbd8c2a2d4af00a9f3f
PaulineEustachy <pauline.eustachy@filigran.io>
2024-09-12 15:07:15 +0200

    [connector] copy relevant files to new external import directory

---
+++ external-import/tanium-sightings/README.md
@@ -0,0 +1,39 @@
+# OpenCTI Tanium Connector
+
+This connector allows organizations to feed the **Tanium Intels** using OpenCTI knowledge.
+
+This connector leverages the OpenCTI *events stream*, so it consumes knowledge in real time and, depending on its settings, create detection and hunting intel pieces in the Tanium platform.
+
+## General overview
+
+OpenCTI data is coming from *import* connectors. Once this data is ingested in OpenCTI, it is pushed to a Redis event stream. This stream is consumed by the Tanium connector to insert intel in the Tanium platform.
+
+![Stream overview](doc/stream.png "Stream overview")
+
+## Installation
+
+### Requirements
+
+- OpenCTI Platform >= 5.0.0
+- Tanium Threat Response >= 3.X.X
+
+### Configuration
+
+| Parameter                                | Docker envvar                           | Mandatory | Description                                                                                                                |
+|------------------------------------------|-----------------------------------------|-----------|----------------------------------------------------------------------------------------------------------------------------|
+| `opencti_url`                            | `OPENCTI_URL`                           | Yes       | The URL of the OpenCTI platform.                                                                                           |
+| `opencti_token`                          | `OPENCTI_TOKEN`                         | Yes       | The token of the OpenCTI user (it's recommanded to create a dedicated user for the connector with the Administrator role). |
+| `connector_id`                           | `CONNECTOR_ID`                          | Yes       | A valid arbitrary `UUIDv4` that must be unique for this connector.                                                         |
+| `connector_live_stream_id`               | `CONNECTOR_LIVE_STREAM_ID`              | Yes       | The Live Stream ID of the stream created in the OpenCTI interface                                                          |
+| `connector_name`                         | `CONNECTOR_NAME`                        | Yes       | The name of the Tanium instance, to identify it if you have multiple Tanium connectors.                                    |
+| `connector_scope`                        | `CONNECTOR_SCOPE`                       | Yes       | Must be `tanium`, not used in this connector.                                                                              |
+| `connector_confidence_level`             | `CONNECTOR_CONFIDENCE_LEVEL`            | Yes       | The default confidence level for created sightings (a number between 1 and 4).                                             |
+| `connector_log_level`                    | `CONNECTOR_LOG_LEVEL`                   | Yes       | The log level for this connector, could be `debug`, `info`, `warn` or `error` (less verbose).                              |
+| `tanium_sightings__url`                  | `TANIUM_SIGHTINGS_URL`                  | Yes       | The Tanium instance API URL.                                                                                               |
+| `tanium_sightings__url_console`          | `TANIUM_SIGHTINGS_URL_CONSOLE`          | Yes       | The Tanium instance console URL.                                                                                           |
+| `tanium_sightings__ssl_verify`           | `TANIUM_SIGHTINGS_SSL_VERIFY`           | Yes       | Enable the SSL certificate check (default: `true`)                                                                         |
+| `tanium_sightings__token`                | `TANIUM_SIGHTINGS_TOKEN`                | Yes       | The Tanium login user.                                                                                                     |
+| `tanium_sightings__hashes_in_reputation` | `TANIUM_SIGHTINGS_HASHES_IN_REPUTATION` | Yes       | Put hashes in the Reputation shared service.                                                                               |
+| `tanium_sightings__no_hashes_in_intels`  | `TANIUM_SIGHTINGS_NO_HASHES_IN_INTELS`  | Yes       | Do not insert hashes in intel documents.                                                                                   |
+| `auto_ondemand_scan`                     | `TANIUM_SIGHTINGS_AUTO_ONDEMAND_SCAN`   | No        | Trigger a quickscan for each inserted intel document in Tanium.                                                            |
+| `tanium_sightings_computer_groups`       | `TANIUM_SIGHTINGS_COMPUTER_GROUPS  `    | No        | A list of computer groups separated by `,`, which will be the targets of the automatic quickscan the automatic quickscan   |
---
+++ external-import/tanium-sightings/src/tanium-sightings.py
@@ -0,0 +1,182 @@
+################################################
+# Tanium Connector for OpenCTI                 #
+################################################
+
+import json
+import os
+
+import yaml
+from import_manager import IntelManager
+from intel_cache import IntelCache
+from pycti import OpenCTIConnectorHelper, get_config_variable
+from sightings import Sightings
+from tanium_api_handler import TaniumApiHandler
+
+
+class TaniumConnector:
+    def __init__(self):
+        # Initialize parameters and OpenCTI helper
+        config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
+        config = (
+            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
+            if os.path.isfile(config_file_path)
+            else {}
+        )
+        self.helper = OpenCTIConnectorHelper(config)
+
+        # Initialize the Tanium API Handler
+        self.tanium_url = get_config_variable("TANIUM_URL", ["tanium", "url"], config)
+        self.tanium_url_console = get_config_variable(
+            "TANIUM_URL_CONSOLE", ["tanium", "url_console"], config
+        )
+        self.tanium_ssl_verify = get_config_variable(
+            "TANIUM_SSL_VERIFY", ["tanium", "ssl_verify"], config, False, True
+        )
+        self.tanium_token = get_config_variable(
+            "TANIUM_TOKEN", ["tanium", "token"], config
+        )
+        self.tanium_hashes_in_reputation = get_config_variable(
+            "TANIUM_HASHES_IN_REPUTATION",
+            ["tanium", "hashes_in_reputation"],
+            config,
+            False,
+            True,
+        )
+        self.tanium_no_hashes_in_intels = get_config_variable(
+            "TANIUM_NO_HASHES_IN_INTELS",
+            ["tanium", "no_hashes_in_intels"],
+            config,
+            False,
+            True,
+        )
+        self.tanium_auto_ondemand_scan = get_config_variable(
+            "TANIUM_AUTO_ONDEMAND_SCAN",
+            ["tanium", "ondemand_scan"],
+            config,
+            False,
+            True,
+        )
+        # Target computer group of the automatic quickscan (if enable)
+        self.tanium_computer_groups = get_config_variable(
+            "TANIUM_COMPUTER_GROUPS", ["tanium", "computer_groups"], config, False, "1"
+        ).split(",")
+        self.tanium_import_alerts = get_config_variable(
+            "TANIUM_IMPORT_ALERTS",
+            ["tanium", "import_alerts"],
+            config,
+            False,
+            True,
+        )
+
+        # Check Live Stream ID
+        if (
+            self.helper.connect_live_stream_id is None
+            or self.helper.connect_live_stream_id == "ChangeMe"
+        ):
+            raise ValueError("Missing Live Stream ID")
+
+        # Initialize Tanium API
+        self.tanium_api_handler = TaniumApiHandler(
+            self.helper,
+            self.tanium_url,
+            self.tanium_token,
+            self.tanium_ssl_verify,
+            self.tanium_auto_ondemand_scan,
+            self.tanium_computer_groups,
+        )
+
+        # Initialize managers
+        self.intel_cache = IntelCache(self.helper)
+        self.import_manager = IntelManager(
+            self.helper, self.tanium_api_handler, self.intel_cache
+        )
+
+    def _process_message(self, msg):
+        try:
+            data = json.loads(msg.data)["data"]
+        except:
+            raise ValueError("Cannot process the message")
+        # Handle creation
+        if msg.event == "create":
+            if data["type"] == "indicator":
+                self.helper.log_info(
+                    "[CREATE] Processing indicator {"
+                    + OpenCTIConnectorHelper.get_attribute_in_extension("id", data)
+                    + "}"
+                )
+                self.import_manager.import_intel_from_indicator(data)
+            elif data["type"] in [
+                "ipv4-addr",
+                "ipv6-addr",
+                "domain-name",
+                "hostname",
+                "process",
+            ]:
+                self.helper.log_info(
+                    "[CREATE] Processing observable {"
+                    + OpenCTIConnectorHelper.get_attribute_in_extension("id", data)
+                    + "}"
+                )
+                self.import_manager.import_intel_from_observable(data)
+            elif data["type"] in ["file", "artifact"]:
+                if self.tanium_hashes_in_reputation:
+                    self.import_manager.import_reputation(data)
+                if not self.tanium_no_hashes_in_intels:
+                    self.import_manager.import_intel_from_observable(data)
+            return
+        # Handle update
+        if msg.event == "update":
+            if data["type"] == "indicator":
+                self.helper.log_info(
+                    "[UPDATE] Processing indicator {"
+                    + OpenCTIConnectorHelper.get_attribute_in_extension("id", data)
+                    + "}"
+                )
+                self.import_manager.import_intel_from_indicator(data, True)
+            elif data["type"] in [
+                "ipv4-addr",
+                "ipv6-addr",
+                "domain-name",
+                "hostname",
+                "file",
+                "artifact",
+                "process",
+            ]:
+                self.helper.log_info(
+                    "[UPDATE] Processing observable {"
+                    + OpenCTIConnectorHelper.get_attribute_in_extension("id", data)
+                    + "}"
+                )
+                self.import_manager.import_intel_from_observable(data, True)
+            return
+        # Handle delete
+        elif msg.event == "delete":
+            if data["type"] == "indicator":
+                self.import_manager.delete_intel(data)
+            elif data["type"] in [
+                "ipv4-addr",
+                "ipv6-addr",
+                "domain-name",
+                "hostname",
+            ]:
+                self.import_manager.delete_intel(data)
+            elif data["type"] in ["file", "artifact"]:
+                self.import_manager.delete_intel(data)
+                self.import_manager.delete_reputation(data)
+            return
+        return
+
+    def start(self):
+        self.sightings = Sightings(
+            self.helper,
+            self.tanium_api_handler,
+            self.tanium_import_alerts,
+            self.tanium_url_console,
+        )
+        self.sightings.start()
+        self.helper.listen_stream(self._process_message)
+
+
+if __name__ == "__main__":
+    TaniumInstance = TaniumConnector()
+    TaniumInstance.start()
