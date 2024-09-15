09c18d88400d4aa5d37eba947f230816b90b0626
Samuel Hassine <samuel.hassine@filigran.io>
2024-09-15 10:15:42 +0200

    [misp] Align all connectors

--- external-import/misp-feed/src/misp-feed.py
+++ external-import/misp-feed/src/misp-feed.py
@@ -1033,7 +1033,15 @@ class MispFeed:
             observable_resolver = resolved_attribute["resolver"]
             observable_type = resolved_attribute["type"]
             observable_value = resolved_attribute["value"]
-            name = resolved_attribute["value"]
+            name = (
+                resolved_attribute["value"]
+                if len(resolved_attribute["value"]) > 2
+                else (
+                    attribute["comment"]
+                    if len(attribute["comment"]) > 2
+                    else observable_type
+                )
+            )
             pattern_type = "stix"
             pattern = None
             # observable type is yara or sigma for instance
@@ -1043,11 +1051,7 @@ class MispFeed:
                 name = (
                     attribute["comment"]
                     if len(attribute["comment"]) > 2
-                    else (
-                        observable_value
-                        if len(observable_value) > 2
-                        else observable_type
-                    )
+                    else observable_type
                 )
             # observable type is not in stix 2
             elif observable_resolver not in OPENCTISTIX2:
