46ed4a1de8b6c5d2ba410d5d89dc1d7b1d00ccec
PaulineEustachy <pauline.eustachy@filigran.io>
2024-09-23 16:50:32 +0200

    [connector] add docstrings

--- stream/tanium-stream/src/tanium_stream_connector/utils.py
+++ stream/tanium-stream/src/tanium_stream_connector/utils.py
@@ -1,8 +1,18 @@
 def is_indicator(stix_object) -> bool:
+    """
+    Check if a STIX 2.1 object is of type "indicator".
+    :param stix_object: STIX object to check
+    :return: True if STIX object is of type "indicator", otherwise False
+    """
     return stix_object["type"] == "indicator"
 
 
 def is_observable(stix_object) -> bool:
+    """
+    Check if a STIX 2.1 object is an Observable.
+    :param stix_object: STIX object to check
+    :return: True if STIX object is an Observable, otherwise False
+    """
     return stix_object["type"] in [
         "ipv4-addr",
         "ipv6-addr",
@@ -13,4 +23,9 @@ def is_observable(stix_object) -> bool:
 
 
 def is_file(stix_object) -> bool:
+    """
+    Check if a STIX 2.1 object is a File.
+    :param stix_object: STIX object to check
+    :return: True if STIX object is a File, otherwise False
+    """
     return stix_object["type"] in ["file", "artifact"]
