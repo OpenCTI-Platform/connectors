f23ccb8ee9d02d351abf1374adf9f681b9db327d
Axel Fahy <axel@fahy.net>
2024-10-01 08:48:23 +0200

    fix strtobool

--- external-import/stream-importer/src/importer/connector.py
+++ external-import/stream-importer/src/importer/connector.py
@@ -218,14 +218,14 @@ class StreamImporterConnector(ExternalImportConnector):
 
 
 def str_to_bool(val):
-    """Convert a string representation of truth to true (1) or false (0).
+    """Convert a string representation of truth to true or false.
     True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
     are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
     'val' is anything else.
     """
     val = val.lower()
     if val in ("y", "yes", "t", "true", "on", "1"):
-        return 1
+        return True
     if val in ("n", "no", "f", "false", "off", "0"):
-        return 0
+        return False
     raise ValueError(f"invalid truth value {val!r}")
