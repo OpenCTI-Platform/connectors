fff929452c69814362017765c6d4389967b0b462
Radargoger <burak.goger@socradar.io>
2024-10-10 13:20:51 +0300

    Fixed

--- external-import/socradar/socradar/connector.py
+++ external-import/socradar/socradar/connector.py
@@ -3,14 +3,18 @@ import re
 import time
 import urllib.parse
 from datetime import datetime
+
 import requests
 import yaml
 from pycti import OpenCTIApiClient
 from stix2 import URL, DomainName, File, IPv4Address, IPv6Address
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
+
 OPENCTI_API_URL = config['api_url']
 OPENCTI_API_TOKEN = config['api_token']
 THREAT_FEED_BASE_URL = config['base_feed_url']
@@ -19,7 +23,9 @@ SOCRADAR_KEY = config['socradar_key']
 COLLECTIONS_UUID = config['collections_uuid']
 collection_catalog = ["collection_1", "collection_2", "collection_3", "collection_4", "collection_5", "collection_6", "collection_7", "collection_8", "collection_9", "collection_10", "collection_11", "collection_12", "collection_13", "collection_14"]
 RUN_INTERVAL = config.get('run_interval', 86400)
+
 client = OpenCTIApiClient(OPENCTI_API_URL, OPENCTI_API_TOKEN)
+
 regex_patterns = {
     'md5': re.compile(r'^[a-fA-F\d]{32}$'),
     'sha1': re.compile(r'^[a-fA-F\d]{40}$'),
@@ -29,9 +35,11 @@ regex_patterns = {
     'domain': re.compile(r'^(?=.{1,255}$)(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)+[A-Za-z]{2,6}$'),
     'url': re.compile(r'^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$')
 }
+
 def format_date(date_string):
     dt = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
     return dt.strftime("%Y-%m-%dT%H:%M:%S+00:00")
+
 def process_feed(THREAT_FEED, COLLECTION_ID):
     logger.info("Starting feed processing")
     try:
@@ -40,8 +48,9 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
         response = requests.get(THREAT_FEED)
         response.raise_for_status()
         response_dict = response.json()
-        #response_dict = response_dict[:10]
+        #response_dict = response_dict[:10] 
         #hash_entries = [entry for entry in response_dict if entry['feed_type'] == 'hash']
+
         for item in response_dict:
             # Extract data
             value = item['feed']
@@ -49,10 +58,12 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
             latest_seen_date = format_date(item['latest_seen_date'])
             maintainer_name = item.get('maintainer_name', 'Unknown')
             feed_type = item.get('feed_type', '').lower()
+
             # Determine observable type
             observable_type = None
             pattern = None
             observableData = None
+
             # Use regex patterns to classify 'value'
             if feed_type == 'url' or regex_patterns['url'].match(value):
                 observable_type = 'Url'
@@ -87,6 +98,7 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
             else:
                 logger.warning(f"Unrecognized value format: {value}, skipping...")
                 continue
+
             # Create indicator and observable in OpenCTI
             if observable_type and pattern and observableData:
                 try:
@@ -100,11 +112,14 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
                     logger.info(f"Valid Until: {latest_seen_date}")
                     logger.info(f"Main Observable Type: {observable_type}")
                     logger.info(f"Labels: ['malicious-activity']")
+
+                    
                     maintainer_identity = client.identity.create(
                         name=maintainer_name,
                         type="Organization",
-                        update=True
+                        update=True  
                     )
+
                     indicator = client.indicator.create(
                         name=value,
                         description=f"Feed from {maintainer_name}",
@@ -117,10 +132,12 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
                         confidence=75,
                         x_opencti_score=75,
                         x_opencti_detection=True,
-                        createdBy=maintainer_identity['id'],
+                        createdBy=maintainer_identity['id'],  
                         update=True
                     )
+
                     logger.info(f"Created indicator: {indicator['id']}")
+
                     logger.info("Creating Observable")
                     observable = client.stix_cyber_observable.create(
                         observableData=observableData,
@@ -130,12 +147,15 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
                         logger.error("Observable creation returned None")
                     else:
                         logger.info(f"Created observable: {observable.get('id', 'No ID returned')}")
+
                 except Exception as e:
                     logger.error(f"Error creating indicator or observable: {str(e)}", exc_info=True)
             else:
                 logger.warning(f"Unrecognized type for value: {value}, skipping...")
+
     except Exception as e:
         logger.error(f"Error processing feed: {str(e)}", exc_info=True)
+
 def main():
     while True:
         for collection in collection_catalog:
@@ -148,5 +168,6 @@ def main():
             process_feed(THREAT_FEED, COLLECTION_ID)
         logger.info(f"Sleeping for {RUN_INTERVAL} seconds")
         time.sleep(RUN_INTERVAL)
+
 if __name__ == "__main__":
     main()
