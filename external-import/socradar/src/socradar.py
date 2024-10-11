8070e404491d7aaa2d24cbbabca71e0929bc7013
Radargoger <burak.goger@socradar.io>
2024-10-11 12:07:44 +0300

    Fixed 0013

--- external-import/socradar/src/socradar.py
+++ external-import/socradar/src/socradar.py
@@ -21,20 +21,10 @@ THREAT_FEED_FORMAT_TYPE = config["format_type"]
 SOCRADAR_KEY = config["socradar_key"]
 COLLECTIONS_UUID = config["collections_uuid"]
 collection_catalog = [
-    "collection_1",
-    "collection_2",
-    "collection_3",
-    "collection_4",
-    "collection_5",
-    "collection_6",
-    "collection_7",
-    "collection_8",
-    "collection_9",
-    "collection_10",
-    "collection_11",
-    "collection_12",
-    "collection_13",
-    "collection_14",
+    "collection_1", "collection_2", "collection_3", "collection_4",
+    "collection_5", "collection_6", "collection_7", "collection_8",
+    "collection_9", "collection_10", "collection_11", "collection_12",
+    "collection_13", "collection_14",
 ]
 RUN_INTERVAL = config.get("run_interval", 86400)
 
@@ -47,8 +37,8 @@ regex_patterns = {
     "ipv4": re.compile(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"),
     "ipv6": re.compile(r"^(?:[a-fA-F\d]{1,4}:){7}[a-fA-F\d]{1,4}$"),
     "domain": re.compile(
-        r"^(?=.{1,255}$)(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)+"
-        r"[A-Za-z]{2,6}$"
+        r"^(?=.{1,255}$)(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)"
+        r"+[A-Za-z]{2,6}$"
     ),
     "url": re.compile(r"^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$"),
 }
@@ -67,22 +57,18 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
         response = requests.get(THREAT_FEED)
         response.raise_for_status()
         response_dict = response.json()
-        # response_dict = response_dict[:10]
 
         for item in response_dict:
-            # Extract data
             value = item["feed"]
             first_seen_date = format_date(item["first_seen_date"])
             latest_seen_date = format_date(item["latest_seen_date"])
             maintainer_name = item.get("maintainer_name", "Unknown")
             feed_type = item.get("feed_type", "").lower()
 
-            # Determine observable type
             observable_type = None
             pattern = None
             observableData = None
 
-            # Use regex patterns to classify 'value'
             if feed_type == "url" or regex_patterns["url"].match(value):
                 observable_type = "Url"
                 pattern = f"[url:value = '{value}']"
@@ -100,7 +86,6 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
                 pattern = f"[ipv6-addr:value = '{value}']"
                 observableData = IPv6Address(value=value)
             elif feed_type == "hash":
-                # Determine hash type based on length
                 if regex_patterns["md5"].match(value):
                     hash_type = "MD5"
                 elif regex_patterns["sha1"].match(value):
@@ -117,7 +102,6 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
                 logger.warning(f"Unrecognized value format: {value}, skipping...")
                 continue
 
-            # Create indicator and observable in OpenCTI
             if observable_type and pattern and observableData:
                 try:
                     logger.info("*" * 50)
@@ -180,7 +164,7 @@ def process_feed(THREAT_FEED, COLLECTION_ID):
 def main():
     while True:
         for collection in collection_catalog:
-            try: 
+            try:
                 COLLECTION_ID = COLLECTIONS_UUID[collection]["id"][0]
                 THREAT_FEED = (
                     THREAT_FEED_BASE_URL
