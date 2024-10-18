c59ac5ac8adffecc302ad4640a2a4a703cadb2ee
PaulineEustachy <pauline.eustachy@filigran.io>
2024-10-18 23:36:49 +0200

    [connector] add harfanglab and opencti models

---
+++ external-import/harfanglab-incidents/src/harfanglab_incidents_connector/models/harfanglab.py
@@ -0,0 +1,213 @@
+from dateutil.parser import parse
+import ipaddress
+
+
+class Agent:
+    """
+    Represent an agent from a Harfanglab alert.
+    """
+
+    def __init__(self, data: dict):
+        self.hostname = data["hostname"]
+        self.agent_id = data["agentid"]
+        self.os_type = data["ostype"]
+        self.os_product_type = data["osproducttype"]
+
+
+class Alert:
+    """
+    Represent an Alert from Harfanglab API.
+    """
+
+    def __init__(self, data: dict):
+        self.id = data.get("alert_unique_id", None)
+        self.status = data.get("status", None)
+        self.message = data.get("msg", None)
+        self.rule_name = data.get("rule_name", None)
+        self.type = data.get("alert_type", None)
+        self.log_type = data.get("log_type", None)
+        self.level = data.get("level", None)
+        self.maturity = data.get("maturity", None)
+        self.process = Process(data["process"])
+        self.agent = Agent(data["agent"])
+        self.url_id = data["id"]
+        self.created_at = parse(data["alert_time"]) if "alert_time" in data else None
+        self.updated_at = parse(data["last_update"]) if "last_update" in data else None
+
+        self.name = f"{self.message} on {self.agent.hostname}"
+
+    def get_ioc_rule(self):
+        ioc_rule = None
+        split_message = self.message.split("=") or self.message.split(":")
+        if len(split_message) == 2:
+            ioc_rule = split_message[1]
+        return ioc_rule
+
+    def get_sigma_rule(self):
+        return self.rule_name
+
+    def get_yara_file(self):
+        yara_file = None
+        split_rule_name = self.rule_name.split(":").replace(" ", "")
+        if len(split_rule_name) == 2:
+            yara_file = split_rule_name[1]
+        return yara_file
+
+    def update_with_indicator_info(self, indicator):
+        if indicator.type.startswith("file:hashes."):
+            self.name = f"{indicator.value} on {self.agent.hostname}"
+        elif indicator.type == "file:name":
+            self.name = f"{self.process.hashes['sha256']} on {self.agent.hostname}"
+
+
+class Indicator:
+    def __init__(self, data: dict):
+        data_type = data.get("type", None)
+        data_value = data.get("value", None)
+
+        self.pattern_type = "stix"
+        self.value = data_value
+        self.name = data_value
+        self.description = data.get("description", None)
+        self.rule_name = data.get("rule_name", None)
+        self.created_at = (
+            parse(data["creation_date"]) if "creation_date" in data else None
+        )
+        self.updated_at = parse(data["last_update"]) if "last_update" in data else None
+
+        self.type = Indicator._convert_type(data_type, data_value)
+        self.pattern = f"[{self.type} = '{self.value}']"
+        self.hashes = None  # placeholder for update_with_alert_info()
+
+    def update_with_alert_info(self, alert):
+        if self.type.startswith("file:"):
+            self.name = alert.process.name
+        if self.type.startswith("file:hashes."):
+            self.hashes = alert.process.hashes
+            self.pattern = (
+                f"[file:hashes.'SHA-256' = '{alert.process.hashes['SHA-256']}' AND "
+                f"file:hashes.'MD5' = '{alert.process.hashes['MD5']}' AND "
+                f"file:hashes.'SHA-1' = '{alert.process.hashes['SHA-1']}']"
+            )
+
+    @staticmethod
+    def _convert_type(original_type: str, original_value: str) -> str:
+        indicator_types = {
+            "domain-name": "domain-name:value",
+            "filename": "file:name",
+            "filepath": "file:name",
+            "ip": {"ipv4": "ipv4-addr:value", "ipv6": "ipv6-addr:value"},
+            "url": "url:value",
+            "hash": {
+                "sha-512": "file:hashes.'SHA-512'",
+                "sha-256": "file:hashes.'SHA-256'",
+                "sha-1": "file:hashes.'SHA-1'",
+                "md5": "file:hashes.'MD5'",
+            },
+        }
+
+        indicator_type = None
+        if original_type in ["ip_src", "ip_dst", "ip_both"]:
+            if Indicator.is_ipv4(original_value):
+                indicator_type = indicator_types["ip"]["ipv4"]
+            if Indicator.is_ipv6(original_value):
+                indicator_type = indicator_types["ip"]["ipv6"]
+        elif original_type == "hash":
+            hash_algorithms_by_length = {
+                128: "sha-512",
+                64: "sha-256",
+                40: "sha-1",
+                32: "md5",
+            }
+            hash_algorithm = hash_algorithms_by_length[len(original_value)]
+            indicator_type = indicator_types["hash"][hash_algorithm]
+        else:
+            indicator_type = indicator_types[original_type]
+        return indicator_type
+
+    @staticmethod
+    def is_ipv4(value: str) -> bool:
+        """
+        Determine whether the provided IP string is IPv4
+        :param value: Value in string
+        :return: A boolean
+        """
+        try:
+            ipaddress.IPv4Address(value)
+            return True
+        except ipaddress.AddressValueError:
+            return False
+
+    @staticmethod
+    def is_ipv6(value: str) -> bool:
+        """
+        Determine whether the provided IP string is IPv6
+        :param value: Value in string
+        :return: A boolean
+        """
+        try:
+            ipaddress.IPv6Address(value)
+            return True
+        except ipaddress.AddressValueError:
+            return False
+
+
+class Process:
+    """
+    Represent a process from a Harfanglab alert.
+    """
+
+    def __init__(self, data: dict):
+        self.username = data["username"]
+        self.user_sid = data["usersid"]
+        self.current_directory = data["current_directory"]
+        self.name = data["process_name"]
+        self.hashes = {
+            # "SHA-512": data["hashes"]["sha512"],
+            "SHA-256": data["hashes"]["sha256"],
+            "SHA-1": data["hashes"]["sha1"],
+            "MD5": data["hashes"]["md5"],
+        }
+
+
+class SigmaIndicator:
+    def __init__(self, data: dict):
+        self.pattern_type = "sigma"
+        self.rule_names = data.get("rule_names", None)
+        self.content = data.get("content", None)
+        self.rule_technique_tags = data["rule_technique_tags"]
+
+
+class Threat:
+    """
+    Represent a Threat from Harfanglab.
+    """
+
+    def __init__(self, data: dict):
+        self.id = data.get("threat_unique_id", None)
+        self.status = data.get("status", None)
+        self.level = data.get("level", None)
+        # "process": {
+        #     "username": data["process"]["username"],
+        #     "user_sid": data["process"]["usersid"],
+        #     "process_name": data["process"]["process_name"],
+        #     "hashes": data["process"]["hashes"],
+        #     "current_directory": data["process"]["current_directory"],
+        # },
+        # "agent": {
+        #     "hostname": data["agent"]["hostname"],
+        #     "agent_id": data["agent"]["agentid"],
+        #     "os_type": data["agent"]["ostype"],
+        #     "os_product_type": data["agent"]["osproducttype"],
+        # },
+        self.url_id = data["id"]
+        self.first_seen = parse(data["first_seen"]) if "first_seen" in data else None
+        self.last_seen = parse(data["last_seen"]) if "last_seen" in data else None
+
+
+class YaraIndicator:
+    def __init__(self, data: dict):
+        self.pattern_type = "yara"
+        self.rule_names = data.get("rule_names", None)
+        self.content = data.get("content", None)
+        self.rule_technique_tags = data["rule_technique_tags"]
---
+++ external-import/harfanglab-incidents/src/harfanglab_incidents_connector/models/opencti.py
@@ -0,0 +1,391 @@
+import datetime
+from abc import abstractmethod
+from typing import Any
+
+import ipaddress
+
+import stix2
+from pycti import (
+    AttackPattern as PyCTIAttackPattern,
+    CustomObservableHostname as PyCTIObservableHostname,
+    Identity as PyCTIIdentity,
+    Incident as PyCTIIncident,
+    Indicator as PyCTIIndicator,
+    StixSightingRelationship as PyCTISighting,
+)
+
+
+class BaseModel:
+    """
+    Base class for OpenCTI models.
+    """
+
+    @abstractmethod
+    def to_stix2_object(self) -> Any:
+        """Construct STIX 2.1 object (usually from stix2 python lib objects)"""
+        ...
+
+
+class AttackPattern(BaseModel):
+    """
+    Represent an AttackPattern indicator in OpenCTI.
+    """
+
+    def __init__(
+        self, name=None, x_mitre_id=None, author=None, object_marking_refs=None
+    ):
+        self.name = name
+        self.x_mitre_id = x_mitre_id
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.AttackPattern:
+        return stix2.AttackPattern(
+            id=PyCTIAttackPattern.generate_id(self.name, self.name),
+            name=self.name,
+            custom_properties={
+                "x_mitre_id": self.x_mitre_id,
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class Author(BaseModel):
+    """
+    Represent an Author organization in OpenCTI.
+    """
+
+    def __init__(self, name: str = None, description: str = None):
+        identity_class = "organization"
+
+        self.id = PyCTIIdentity.generate_id(name, identity_class)
+        self.name = name
+        self.description = description
+        self.identity_class = identity_class
+
+    def to_stix2_object(self) -> stix2.Identity:
+        return stix2.Identity(
+            id=self.id,
+            name=self.name,
+            identity_class=self.identity_class,
+            description=self.description,
+        )
+
+
+class Directory(BaseModel):
+    """
+    Represent a Directory observable in OpenCTI.
+    """
+
+    def __init__(
+        self, path: str = None, author: Author = None, object_marking_refs=None
+    ):
+        self.id = None  # placeholder for stix2 ID generation
+        self.path = path
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.Directory:
+        return stix2.Directory(
+            path=self.path,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class DomainName(BaseModel):
+    """
+    Represent a DomainName observable in OpenCTI.
+    """
+
+    def __init__(self, value=None, author=None, object_marking_refs=None):
+        self.id = None  # placeholder for stix2 ID generation
+        self.value = value
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.DomainName:
+        return stix2.DomainName(
+            value=self.value,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class File(BaseModel):
+    """
+    Represent a File observable in OpenCTI.
+    """
+
+    def __init__(
+        self,
+        name: str = None,
+        hashes: dict = None,
+        author: Author = None,
+        object_marking_refs=None,
+    ):
+        self.id = None  # placeholer for stix2 ID generation
+        self.name = name
+        self.hashes = hashes
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.File:
+        return stix2.File(
+            name=self.name,
+            hashes=self.hashes,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class Hostname(BaseModel):
+    """
+    Represent a Hostname observable in OpenCTI.
+    """
+
+    def __init__(self, value=None, author=None, object_marking_refs=None):
+        self.id = None  # placeholder for stix2 ID generation
+        self.value = value
+        self.author = author
+
+    def to_stix2_object(self) -> PyCTIObservableHostname:
+        # PyCTIObservableHostname is an extension of the STIX2.1 as the spec doesn't implement Hostname observables
+        return PyCTIObservableHostname(
+            value=self.value,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class IPv4(BaseModel):
+    """
+    Represent a IPv4 observable in OpenCTI.
+    """
+
+    def __init__(self, value=None, author=None, object_marking_refs=None):
+        # TODO: validate IPv4 value
+        self.id = None  # placeholder for stix2 ID generation
+        self.value = value
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.IPv4Address:
+        return stix2.IPv4Address(
+            value=self.value,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class IPv6(BaseModel):
+    """
+    Represent a IPv6 observable in OpenCTI.
+    """
+
+    def __init__(self, value=None, author=None, object_marking_refs=None):
+        # TODO: validate IPv6 value
+        self.id = None  # placeholder for stix2 ID generation
+        self.value = value
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.IPv6Address:
+        return stix2.IPv6Address(
+            value=self.value,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class Incident(BaseModel):
+    """
+    Represent an Incident event in OpenCTI.
+    """
+
+    def __init__(
+        self,
+        name: str = None,
+        description: str = None,
+        source: str = None,
+        severity: str = None,
+        author: Author = None,
+        created_at: datetime.datetime = None,
+        updated_at: datetime.datetime = None,
+        object_marking_refs=None,
+        external_references=None,
+    ):
+        self.id = PyCTIIncident.generate_id(name, created_at)
+        self.name = name
+        self.description = description
+        self.source = source
+        self.severity = severity
+        self.author = author
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def to_stix2_object(self) -> stix2.Incident:
+        return stix2.Incident(
+            id=self.id,
+            created=self.created_at,
+            name=self.name,
+            description=self.description,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            created_by_ref=self.author.id,
+            # external_references=[
+            # {
+            #     "source_name": self.helper.connect_name,
+            #     "url": f"{self.config.harfanglab_api_base_url}/security-event/{alert.url_id}/summary",
+            #     "external_id": alert.url_id,
+            # }
+            # ],
+            custom_properties={
+                "source": self.source,
+                "severity": self.severity,
+                "incident_type": "alert",
+                "first_seen": self.created_at,
+                "last_seen": self.updated_at,
+            },
+        )
+
+
+class Indicator(BaseModel):
+    """
+    Represent an Indicator in OpenCTI.
+    """
+
+    def __init__(
+        self,
+        name: str = None,
+        description: str = None,
+        value: str = None,
+        pattern: str = None,
+        pattern_type: str = None,
+        x_opencti_score: int = None,
+        author: Author = None,
+        created_at: datetime.datetime = None,
+        updated_at: datetime.datetime = None,
+        object_marking_refs=None,
+        external_references=None,
+    ):
+        self.id = PyCTIIndicator.generate_id(value)
+        self.name = name
+        self.description = description
+        self.value = value
+        self.pattern = pattern
+        self.x_opencti_score = x_opencti_score
+        self.author = author
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def to_stix2_object(self) -> stix2.Indicator:
+        return stix2.Indicator(
+            id=PyCTIIndicator.generate_id(self.value),
+            created_by_ref=self.author.id,
+            created=self.created_at,
+            modified=self.updated_at,
+            name=self.name,
+            pattern=self.pattern,
+            description=self.description,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right marking
+            custom_properties={
+                "pattern_type": "stix",
+                "x_opencti_score": self.x_opencti_score,
+                "detection": True,
+            },
+        )
+
+
+class Sighting(BaseModel):
+    """
+    Represent a Sighting event in OpenCTI.
+    """
+
+    def __init__(
+        self,
+        source: Author = None,
+        target: Indicator = None,
+        first_seen_at: datetime.datetime = None,
+        last_seen_at: datetime.datetime = None,
+        x_opencti_negative: bool = False,
+        object_marking_refs=None,
+        external_references=None,
+    ):
+        self.id = PyCTISighting.generate_id(
+            source.id,
+            target.id,
+            first_seen_at,
+            first_seen_at,  # using same date twice avoids duplication
+        )
+        self.source = source
+        self.target = target
+        self.first_seen_at = first_seen_at
+        self.last_seen_at = last_seen_at
+        self.x_opencti_negative = x_opencti_negative
+
+    def to_stix2_object(self) -> stix2.Sighting:
+        return stix2.Sighting(
+            id=self.id,
+            first_seen=self.first_seen_at,
+            last_seen=self.last_seen_at,
+            sighting_of_ref=self.target.id,
+            where_sighted_refs=self.source.id,
+            count=1,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            # external_references=[
+            #     {
+            #         "source_name": "HarfangLab - Security Events",
+            #         "url": f"{self.harfanglab_url}/security-event/{new_alert_built['url_id']}/summary",
+            #         "external_id": new_alert_built["url_id"],
+            #     }
+            # ],
+            custom_properties={
+                "x_opencti_negative": self.x_opencti_negative,
+            },
+        )
+
+
+class Url(BaseModel):
+    """
+    Represent a URL observable in OpenCTI.
+    """
+
+    def __init__(self, value=None, author=None, object_marking_refs=None):
+        self.id = None  # placeholder for stix2 ID generation
+        self.value = value
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.URL:
+        return stix2.URL(
+            value=self.value,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
+
+
+class UserAccount(BaseModel):
+    """
+    Represent a UserAccount observable in OpenCTI.
+    """
+
+    def __init__(self, account_login=None, author=None, object_marking_refs=None):
+        self.id = None  # placeholder for stix2 ID generation
+        self.account_login = account_login
+        self.author = author
+
+    def to_stix2_object(self) -> stix2.UserAccount:
+        return stix2.UserAccount(
+            account_login=self.account_login,
+            object_marking_refs=[stix2.TLP_WHITE],  # TODO: set the right TLP
+            custom_properties={
+                "created_by_ref": self.author.id,
+            },
+        )
