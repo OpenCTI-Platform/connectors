aae258531f8575be404b11cfb3d313b737828a92
PaulineEustachy <pauline.eustachy@filigran.io>
2024-10-31 15:39:25 +0100

    [connector] update comparisons

--- external-import/harfanglab-incidents/src/harfanglab_incidents_connector/utils.py
+++ external-import/harfanglab-incidents/src/harfanglab_incidents_connector/utils.py
@@ -5,46 +5,46 @@ def is_ipv4(value: str) -> bool:
     """
     Return whether given value is a valid IPv4 address.
     """
-    return ip_address.ipv4(value) == True
+    return ip_address.ipv4(value) is True
 
 
 def is_ipv6(value: str) -> bool:
     """
     Return whether given value is a valid IPv6 address.
     """
-    return ip_address.ipv6(value) == True
+    return ip_address.ipv6(value) is True
 
 
 def is_domain(value: str) -> bool:
     """
     Return whether given value is a valid Domain name.
     """
-    return domain.domain(value) == True
+    return domain.domain(value) is True
 
 
 def is_md5(value: str) -> bool:
     """
     Return whether given value is a valid MD5 hash.
     """
-    return hashes.md5(value) == True
+    return hashes.md5(value) is True
 
 
 def is_sha1(value: str) -> bool:
     """
     Return whether given value is a valid SHA-1 hash.
     """
-    return hashes.sha1(value) == True
+    return hashes.sha1(value) is True
 
 
 def is_sha256(value: str) -> bool:
     """
     Return whether given value is a valid SHA-256 hash.
     """
-    return hashes.sha256(value) == True
+    return hashes.sha256(value) is True
 
 
 def is_sha512(value: str) -> bool:
     """
     Return whether given value is a valid SHA-512 hash.
     """
-    return hashes.sha512(value) == True
+    return hashes.sha512(value) is True
