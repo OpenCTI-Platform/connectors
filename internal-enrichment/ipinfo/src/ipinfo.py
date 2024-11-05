6671b9a9e0f3e6bfc3546bc92782870e90b3ec7e
annoyingapt <annoyingapt@protonmail.com>
2024-11-05 14:20:30 +1300

    rolling back isort changes

--- internal-enrichment/ipinfo/src/ipinfo.py
+++ internal-enrichment/ipinfo/src/ipinfo.py
@@ -6,8 +6,12 @@ import pycountry
 import requests
 import stix2
 import yaml
-from pycti import (Location, OpenCTIConnectorHelper, StixCoreRelationship,
-                   get_config_variable)
+from pycti import (
+    Location,
+    OpenCTIConnectorHelper,
+    StixCoreRelationship,
+    get_config_variable,
+)
 
 
 class IpInfoConnector:
