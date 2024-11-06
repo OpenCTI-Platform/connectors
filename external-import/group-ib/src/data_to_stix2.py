47252269149206edd06691e3f172a62429120acc
Helene Nguyen <helene.nguyen@filigran.io>
2024-11-06 14:48:08 +0100

    [connector] Re add missing generate_id arguments

--- external-import/group-ib/src/data_to_stix2.py
+++ external-import/group-ib/src/data_to_stix2.py
@@ -577,7 +577,7 @@ class ThreatActor(_BaseSDO):
 
     def _generate_sdo(self):
         self.stix_main_object = stix2.ThreatActor(
-            id=pycti.ThreatActor.generate_id(self.name),
+            id=pycti.ThreatActorGroup.generate_id(self.name),
             name=self.name,
             description=self.description,
             aliases=self.aliases,
