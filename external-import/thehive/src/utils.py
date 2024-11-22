ce2bfd069769489b5c7c4e6617cef90469df1348
Lucas Guiglionia <lucas.guiglionia@filigran.io>
2024-11-22 21:18:21 +0100

    Fix format

--- external-import/thehive/src/utils.py
+++ external-import/thehive/src/utils.py
@@ -6,6 +6,7 @@ def format_datetime(timestamp, time_format):
     """formatting the date based on the provided timestamp and time_format."""
     return datetime.fromtimestamp(int(timestamp), timezone.utc).strftime(time_format)
 
+
 def is_ipv6(ip_str):
     """Determine whether the provided string is an IPv6 address or valid IPv6 CIDR."""
     try:
