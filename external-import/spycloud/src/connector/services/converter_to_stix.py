9f3ae891ca64fb4e4c87289691649e2610d64131
PaulineEustachy <pauline.eustachy@filigran.io>
2024-12-18 18:08:38 +0100

    [connector] update converter_to_stix

--- external-import/spycloud/src/connector/services/converter_to_stix.py
+++ external-import/spycloud/src/connector/services/converter_to_stix.py
@@ -1,17 +1,15 @@
-import ipaddress
-
-import stix2
-import validators
-from pycti import OpenCTIConnectorHelper, Identity, StixCoreRelationship
+from typing import Literal
+from pycti import OpenCTIConnectorHelper
 from .config_loader import ConfigLoader
+from ..models import opencti, spycloud
+
+
+SEVERITY_LEVELS_BY_CODE = {2: "low", 5: "medium", 20: "high", 25: "critical"}
 
 
 class ConverterToStix:
     """
-    Provides methods for converting various types of input data into STIX 2.1 objects.
-
-    REQUIREMENTS:
-    - generate_id() for each entity from OpenCTI pycti library except observables to create
+    Provides methods for converting SpyCloud objects into OCTI objects following STIX 2.1 specification.
     """
 
     def __init__(
@@ -19,139 +17,53 @@ class ConverterToStix:
     ):
         self.helper = helper
         self.config = config
-        self.author = self.create_author()
-        self.external_reference = self.create_external_reference()
+        self.author = ConverterToStix._create_author(
+            name=self.helper.connect_name,
+            description="SpyCloud external import connector",
+            identity_class="organization",
+        )
+
+    def create_incident(
+        self,
+        breach_record: spycloud.BreachRecord = None,
+        breach_catalog: spycloud.BreachCatalog = None,
+    ) -> opencti.Incident:
+        """
+        Create an Incident from given breach record and its catalog.
+        :param breach_record: SpyCloud breach record
+        :param breach_catalog: SpyCloud breach record's catalog
+        :return: OpenCTI Incident
+        """
+        incident_source = breach_catalog.title or "Unknown"
+        incident_severity = SEVERITY_LEVELS_BY_CODE.get(breach_record.severity)
+        incident_name = f"Spycloud {incident_severity} alert on {breach_record.email or breach_record.username or breach_record.ip[0] or breach_record.document_id}"
+        incident_description = "a markdown table of breach record's keys/values"
+
+        incident = opencti.Incident(
+            name=incident_name,
+            description=incident_description,
+            source=incident_source,
+            severity=incident_severity,
+            incident_type="data-breach",
+            author=self.author,
+            created_at=breach_record.spycloud_publish_date,
+            updated_at=breach_record.spycloud_publish_date,
+            object_marking_refs=[],
+        )
+        return incident
 
     @staticmethod
-    def create_external_reference() -> list:
+    def _create_author(
+        name: str = None, description: str = None, identity_class: str = None
+    ) -> opencti.Author:
         """
-        Create external reference
-        :return: External reference STIX2 list
+        Create an Author.
+        :return: OpenCTI Author
         """
-        external_reference = stix2.ExternalReference(
-            source_name="External Source",
-            url="CHANGEME",
-            description="DESCRIPTION",
-        )
-        return [external_reference]
-
-    def create_author(self) -> dict:
-        """
-        Create Author
-        :return: Author in Stix2 object
-        """
-        identity_name = self.helper.connect_name
-        identity_class = "organization"
-
-        author = stix2.Identity(
-            id=Identity.generate_id(name=identity_name, identity_class=identity_class),
-            name=identity_name,
+        author = opencti.Author(
+            name=name,
             identity_class=identity_class,
-            description="DESCRIPTION",
+            description=description,
         )
         return author
-
-    def create_relationship(
-        self, source_id: str, relationship_type: str, target_id: str
-    ) -> dict:
-        """
-        Creates Relationship object
-        :param source_id: ID of source in string
-        :param relationship_type: Relationship type in string
-        :param target_id: ID of target in string
-        :return: Relationship STIX2 object
-        """
-        relationship = stix2.Relationship(
-            id=StixCoreRelationship.generate_id(
-                relationship_type, source_id, target_id
-            ),
-            relationship_type=relationship_type,
-            source_ref=source_id,
-            target_ref=target_id,
-            created_by_ref=self.author,
-            external_references=self.external_reference,
-        )
-        return relationship
-
-    # ===========================#
-    # Other Examples
-    # ===========================#
-
-    @staticmethod
-    def _is_ipv6(value: str) -> bool:
-        """
-        Determine whether the provided IP string is IPv6
-        :param value: Value in string
-        :return: A boolean
-        """
-        try:
-            ipaddress.IPv6Address(value)
-            return True
-        except ipaddress.AddressValueError:
-            return False
-
-    @staticmethod
-    def _is_ipv4(value: str) -> bool:
-        """
-        Determine whether the provided IP string is IPv4
-        :param value: Value in string
-        :return: A boolean
-        """
-        try:
-            ipaddress.IPv4Address(value)
-            return True
-        except ipaddress.AddressValueError:
-            return False
-
-    @staticmethod
-    def _is_domain(value: str) -> bool:
-        """
-        Valid domain name regex including internationalized domain name
-        :param value: Value in string
-        :return: A boolean
-        """
-        is_valid_domain = validators.domain(value)
-
-        if is_valid_domain:
-            return True
-        else:
-            return False
-
-    def create_obs(self, value: str) -> dict:
-        """
-        Create observable according to value given
-        :param value: Value in string
-        :return: Stix object for IPV4, IPV6 or Domain
-        """
-        if self._is_ipv6(value) is True:
-            stix_ipv6_address = stix2.IPv6Address(
-                value=value,
-                custom_properties={
-                    "x_opencti_created_by_ref": self.author["id"],
-                    "x_opencti_external_references": self.external_reference,
-                },
-            )
-            return stix_ipv6_address
-        elif self._is_ipv4(value) is True:
-            stix_ipv4_address = stix2.IPv4Address(
-                value=value,
-                custom_properties={
-                    "x_opencti_created_by_ref": self.author["id"],
-                    "x_opencti_external_references": self.external_reference,
-                },
-            )
-            return stix_ipv4_address
-        elif self._is_domain(value) is True:
-            stix_domain_name = stix2.DomainName(
-                value=value,
-                custom_properties={
-                    "x_opencti_created_by_ref": self.author["id"],
-                    "x_opencti_external_references": self.external_reference,
-                },
-            )
-            return stix_domain_name
-        else:
-            self.helper.connector_logger.error(
-                "This observable value is not a valid IPv4 or IPv6 address nor DomainName: ",
-                {"value": value},
-            )
+    
