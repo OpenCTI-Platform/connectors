eb9a62fd6ea2761addd6b88a34c4966933d696cc
FlavienSindou <flavien.sindou@filigran.io>
2025-01-17 15:08:00 +0100

    https://github.com/OpenCTI-Platform/connectors/pull/3262#discussion_r1915071234

--- external-import/proofpoint-tap/proofpoint_tap/client_api/v2/__init__.py
+++ external-import/proofpoint-tap/proofpoint_tap/client_api/v2/__init__.py
@@ -1,7 +1,6 @@
 """Offer python client and response models for the TAP v2 API."""
 
 from .campaign import CampaignClient
-from .compiled_campaign import CampaignCompiledInfo, TAPCompiledCampaignClient
 from .forensics import ForensicsClient
 from .threat import ThreatClient
 
