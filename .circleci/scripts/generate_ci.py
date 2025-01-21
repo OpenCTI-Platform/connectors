80891cb42315a7c960e724de116c0085a481fff9
Renizmy <remi.beltramini@gmail.com>
2025-01-21 20:20:30 +0100

    First shot

--- .circleci/scripts/generate_ci.py
+++ .circleci/scripts/generate_ci.py
@@ -56,6 +56,8 @@ def get_pycti() -> dict:
     else:
         pycti["version"] = get_latest_pycti_release()
         pycti["replace"] = True
+    print(pycti["version"])
+    print(pycti["CIRCLE_TAG"])
     return pycti
 
 # TODO: detect if rolling ==> Fetch last version of pycti // else get CIRCLE-CI-TAG = Version
