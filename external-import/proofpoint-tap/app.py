42e70fb848f516eaaabc736da5272e941b7a0704
FlavienSindou <flavien.sindou@filigran.io>
2025-01-30 21:44:20 +0100

    feat: add 2nd use case in app

--- external-import/proofpoint-tap/app.py
+++ external-import/proofpoint-tap/app.py
@@ -1,25 +1,29 @@
-# isort: skip_file
-# isort is removing the type ignore untyped import comment conflicting with mypy
+# isort:skip_file
 """Define the Connector class for the ProofPoint TAP module integration with OpenCTI.
 It handles the initialization, data retrieval, transformation to STIX format, and sending of data to OpenCTI.
 """
 
+import sys
 from datetime import datetime, timezone
 from typing import TYPE_CHECKING
-import sys
-
 
 from pycti import (  # type: ignore[import-untyped] # pycti does not provide stubs
     OpenCTIConnectorHelper,
 )
 
-
+from proofpoint_tap.adapters.events import EventsAPIV2
+from proofpoint_tap.domain.use_cases.ingest_incident import IncidentProcessor
 from proofpoint_tap.domain.use_cases.ingest_report import ReportProcessor
 from proofpoint_tap.errors import DataRetrievalError
+from proofpoint_tap.ports.event import EventPort
 
 if TYPE_CHECKING:
-    from proofpoint_tap.ports.config import ConfigLoaderPort
     from proofpoint_tap.ports.campaign import CampaignsPort
+    from proofpoint_tap.ports.config import ConfigLoaderPort
+    from proofpoint_tap.ports.event import EventsPort
+
+CAMPAIGN_STATE_KEY = "last_campaign_datetime"
+EVENT_STATE_KEY = "last_event_datetime"
 
 
 class Connector:
@@ -36,35 +40,54 @@ class Connector:
         self,
         config: "ConfigLoaderPort",
         campaigns: "CampaignsPort",
+        events: "EventsPort",
         helper: OpenCTIConnectorHelper,
     ):
         """Initialize the Connector with necessary configurations."""
         # Load configuration file and connection helper
-        self.config = config
-        self.helper = helper
-        self.logger = helper.connector_logger
-        self.campaigns = campaigns
-        self.report_processor = ReportProcessor(
-            tlp_marking_name=self.config.tap.marking_definition,
+        self._config = config
+        self._helper = helper
+        self._logger = helper.connector_logger
+        self._campaigns = campaigns
+        self._events = events
+        self._report_processor = ReportProcessor(
+            tlp_marking_name=self._config.tap.marking_definition,
         )
+        self._incident_processor = IncidentProcessor(
+            tlp_marking_name=self._config.tap.marking_definition,
+        )
+
+        # To be intialized during work
+        # keep track of current work
         self.work_id = None
-        self.campaign_since_datetime: datetime | None = None
+        # keep track of current work datetime to update state
+        # when finalizing work
+        self._work_start_datetime: datetime | None = None
+        # keep track of last datetime of campaign and event
+        # for ingestion driving
+        self._campaign_since_datetime: datetime | None = None
+        self._event_since_datetime: datetime | None = None
 
     def _log_error(self, error_message: str) -> None:
         # to connector logger
-        self.logger.error(message=error_message)
+        self._logger.error(message=error_message)
         # to OpenCTI
-        self.helper.api.work.report_expectation(
+        self._helper.api.work.report_expectation(
             work_id=self.work_id, error={"error": error_message, "source": "CONNECTOR"}
         )
 
     def _force_get_state(self) -> dict[str, str]:
-        self.helper.force_ping()
-        return self.helper.get_state() or {}
+        self._helper.force_ping()
+        return self._helper.get_state() or {}
 
     def _force_set_state(self, state: dict[str, str]) -> None:
-        self.helper.set_state(state=state)
-        self.helper.force_ping()
+        self._helper.set_state(state=state)
+        self._helper.force_ping()
+
+    def _force_update_state(self, key: str, value: "datetime") -> None:
+        state = self._force_get_state()
+        state[key] = value.isoformat()
+        self._force_set_state(state)
 
     def _initiate_work(self) -> None:
         """Initiate a new work process in the OpenCTI platform.
@@ -76,50 +99,57 @@ class Connector:
             4. set the work ID for future use.
         """
         state = self._force_get_state()
-        self.logger.debug("[CONNECTOR] Connector current state", {"state": state})
+        self._logger.debug("[CONNECTOR] Connector current state", {"state": state})
 
-        last_ingested_campaign_start_time_str = state.get(
-            "last_ingested_campaign_start_time"
-        )
+        def _set_acquisition_date(state_key: str) -> datetime:
 
-        # Update data retrieval start datetime
-        if last_ingested_campaign_start_time_str is not None:
-            self.logger.info(
-                "[CONNECTOR] Connector last ingested camapign start datetime",
-                {
-                    "last_ingested_campaign_start_time": last_ingested_campaign_start_time_str
-                },
-            )
+            if state_key not in [CAMPAIGN_STATE_KEY, EVENT_STATE_KEY]:
+                # check for the scatterbrained developer
+                raise ValueError(f"Invalid state key: {state_key}")
 
-            self.logger.warning(
-                "[CONNECTOR] Connector acquisition SINCE parameter overwritten",
-                {
-                    # "previous": str(self.config.tap.export_since),
-                    "current": last_ingested_campaign_start_time_str,
-                },
+            last_ingested_datetime_str = state.get(state_key)
+            if last_ingested_datetime_str is not None:
+                self._logger.info(
+                    f"[CONNECTOR] Connector last ingested {state_key} datetime",
+                    {state_key: last_ingested_datetime_str},
+                )
+                self._logger.warning(
+                    "[CONNECTOR] Connector acquisition SINCE parameter overwritten",
+                    {
+                        # "previous": str(self.config.tap.export_since),
+                        "current": last_ingested_datetime_str,
+                    },
+                )
+                return datetime.fromisoformat(last_ingested_datetime_str)
+
+            self._logger.info(
+                f"[CONNECTOR] Connector has never run successfully {state_key} ingestion..."
             )
-            self.campaign_since_datetime = datetime.fromisoformat(
-                last_ingested_campaign_start_time_str
-            )
-        else:
-            self.logger.info("[CONNECTOR] Connector has never run successfully...")
             # Today at midnight UTC
-            self.campaign_since_datetime = datetime.now(timezone.utc).replace(
+            midnight = datetime.now(timezone.utc).replace(
                 hour=0, minute=0, second=0, microsecond=0
             )
-            self.logger.info(
-                "[CONNECTOR] Connector campaign acquisition SINCE parameter set to",
-                {"current": self.campaign_since_datetime.isoformat()},
+            self._logger.info(
+                f"[CONNECTOR] Connector {state_key} acquisition SINCE parameter set to",
+                {"current": midnight.isoformat()},
             )
+            return midnight
+
+        if config.tap.export_campaigns:
+            self._campaign_since_datetime = _set_acquisition_date(CAMPAIGN_STATE_KEY)
+
+        if config.tap.export_events:
+            self._event_since_datetime = _set_acquisition_date(EVENT_STATE_KEY)
 
         # Initiate a new work
-        self.work_id = self.helper.api.work.initiate_work(
-            self.helper.connect_id, self.helper.connect_name
+        self.work_id = self._helper.api.work.initiate_work(
+            self._helper.connect_id, self._helper.connect_name
         )
+        self._work_start_datetime = datetime.now(timezone.utc)
 
-        self.logger.info(
+        self._logger.info(
             "[CONNECTOR] Running connector...",
-            {"connector_name": self.helper.connect_name},
+            {"connector_name": self._helper.connect_name},
         )
 
     def _finalize_work(self, error_flag: bool) -> None:
@@ -132,12 +162,29 @@ class Connector:
             None
 
         """
-        self.helper.api.work.to_processed(
+        # Update State
+        if config.tap.export_campaigns:
+            self._force_update_state(
+                key=CAMPAIGN_STATE_KEY,
+                value=self._work_start_datetime,  # type: ignore[arg-type] # should not be None
+            )
+        if config.tap.export_events:
+            self._force_update_state(
+                key=EVENT_STATE_KEY,
+                value=self._work_start_datetime,  # type: ignore[arg-type] # should not be None
+            )
+
+        self._helper.api.work.to_processed(
             work_id=self.work_id,
             message="Connector's work finished gracefully",
             in_error=error_flag,
         )
+
+        # reset
         self.work_id = None
+        self._work_start_datetime = None
+        self._campaign_since_datetime = None
+        self._event_since_datetime = None
 
     def _send_bundle(self, bundle_json: str) -> None:
         """Send the STIX bundle to the OpenCTI platform and update the total expectation.
@@ -149,12 +196,12 @@ class Connector:
            None
 
         """
-        bundles_sent = self.helper.send_stix2_bundle(
+        bundles_sent = self._helper.send_stix2_bundle(
             bundle=bundle_json,
             work_id=self.work_id,
             cleanup_inconsistent_bundle=True,
         )
-        self.logger.info(
+        self._logger.info(
             "STIX objects sent to OpenCTI.",
             {"bundles_sent": str(len(bundles_sent))},
         )
@@ -163,43 +210,84 @@ class Connector:
         """Fetch data, transform and send bundle. Return True if successful, False otherwise."""
         try:
             # Fetch data
-            if self.campaign_since_datetime is None:
+            if self._campaign_since_datetime is None:
+                # check for the scatterbrained developer
                 raise ValueError(
                     "No start datetime to fetch campaigns, call _initiate_work first."
                 )
-            campaign_ids = self.campaigns.list(
-                start_time=self.campaign_since_datetime,
+            campaign_ids = self._campaigns.list(
+                start_time=self._campaign_since_datetime,
                 stop_time=datetime.now(timezone.utc),
             )
-            self.logger.info(
+            self._logger.info(
                 "[CONNECTOR] Campaigns IDs fetched",
                 {"campaigns_count": len(campaign_ids)},
             )
             if len(campaign_ids) == 0:
-                self.logger.info("No campaign IDs fetched.")
+                self._logger.info("No campaign IDs fetched.")
                 return True
 
             # fetch details
             for campaign_id in campaign_ids:
-                campaign_info = self.campaigns.details(campaign_id)
-                self.logger.info(
+                campaign_info = self._campaigns.details(campaign_id)
+                self._logger.info(
                     "[CONNECTOR] Campaign details fetched",
                     {"campaign_id": campaign_id},
                 )
                 # process
-                entities = self.report_processor.run_on(campaign_info)
+                entities = self._report_processor.run_on(campaign_info)
                 if len(entities) == 0:
-                    self.logger.info("No entities to process.")
+                    self._logger.info("No entities to process.")
                     continue
                 stix_objects = [entity.to_stix2_object() for entity in entities]
                 # send
-                self._send_bundle(self.helper.stix2_create_bundle(stix_objects))
+                self._send_bundle(self._helper.stix2_create_bundle(stix_objects))
                 # update state
                 last_ingested_campaign_start_time = campaign_info.start_datetime
-                self.helper.set_state(
-                    {
-                        "last_ingested_campaign_start_time": last_ingested_campaign_start_time.isoformat(),
-                    }
+                self._force_update_state(
+                    key=CAMPAIGN_STATE_KEY, value=last_ingested_campaign_start_time
+                )
+            return True
+
+        except DataRetrievalError as e:
+            self._log_error(f"Data retrieval error: {str(e)}")
+            return False
+
+    def _process_events(self) -> bool:
+        """Fetch data, transform and send bundle. Return True if successful, False otherwise."""
+        try:
+            # Fetch data
+            if self._event_since_datetime is None:
+                # check for the scatterbrained developer
+                raise ValueError(
+                    "No start datetime to fetch events, call _initiate_work first."
+                )
+            events: list[EventPort] = self._events.fetch(
+                start_time=self._event_since_datetime,
+                stop_time=datetime.now(timezone.utc),
+                select=self._config.tap.events_type,
+            )
+            self._logger.info(
+                "[CONNECTOR] Events fetched",
+                {"events_count": len(events)},
+            )
+            if len(events) == 0:
+                self._logger.info("No events fetched.")
+                return True
+
+            # process
+            for event in events:
+                entities = self._incident_processor.run_on_event(event=event)
+                if len(entities) == 0:
+                    self._logger.info("No entities to process.")
+                    return True
+                stix_objects = [entity.to_stix2_object() for entity in entities]
+                # send
+                self._send_bundle(self._helper.stix2_create_bundle(stix_objects))
+                # update state
+                last_ingested_event_start_time = max([event.time for event in events])
+                self._force_update_state(
+                    key=EVENT_STATE_KEY, value=last_ingested_event_start_time
                 )
             return True
 
@@ -211,14 +299,23 @@ class Connector:
         """Define the main process of the connector."""
         error_flag = True
         try:
-            self.logger.info(
+            self._logger.info(
                 "[CONNECTOR] Starting connector work...",
-                {"connector_name": self.helper.connect_name},
+                {"connector_name": self._helper.connect_name},
             )
 
             self._initiate_work()
 
-            error_flag = not self._process_campaigns()
+            campaigns_error_flag = (
+                not self._process_campaigns()
+                if self._config.tap.export_campaigns
+                else False
+            )
+            events_error_flag = (
+                not self._process_events() if self._config.tap.export_events else False
+            )
+
+            error_flag = campaigns_error_flag or events_error_flag
 
         except (KeyboardInterrupt, SystemExit):
             error_message = "Connector stopped by user"
@@ -227,16 +324,11 @@ class Connector:
 
         except Exception as err:
             error_flag = True
-            self.logger.error("[CONNECTOR] Unexpected error.", {"error": str(err)})
+            self._logger.error("[CONNECTOR] Unexpected error.", {"error": str(err)})
             self._log_error("Unexpected error. See connector's log for more details.")
 
         finally:
-            self.helper.api.work.to_processed(
-                work_id=self.work_id,
-                message="Connector's work finished gracefully",
-                in_error=error_flag,
-            )
-            self.work_id = None
+            self._finalize_work(error_flag)
 
     def start(self) -> None:
         """Run the main process encapsulated in a scheduler.
@@ -249,9 +341,9 @@ class Connector:
         It requires the `duration_period` connector variable in ISO-8601 standard format
         Example: `CONNECTOR_DURATION_PERIOD=PT5M` => Will run the process every 5 minutes
         """
-        self.helper.schedule_iso(
+        self._helper.schedule_iso(
             message_callback=self.work,
-            duration_period=self.config.connector.duration_period,
+            duration_period=self._config.connector.duration_period,
         )
 
 
@@ -273,6 +365,14 @@ if __name__ == "__main__":
             retry=config.tap.api_retries,
             backoff=config.tap.api_backoff,
         )
+        events = EventsAPIV2(
+            base_url=config.tap.api_base_url,
+            principal=config.tap.api_principal_key,
+            secret=config.tap.api_secret_key,
+            timeout=config.tap.api_timeout,
+            retry=config.tap.api_retries,
+            backoff=config.tap.api_backoff,
+        )
     except (
         Exception
     ):  # Start up issue exception, Otherwise shoud be handle gracefully by the connector
@@ -281,5 +381,5 @@ if __name__ == "__main__":
 
     # Run the connector
     # Error will be handled in the connector
-    connector = Connector(config, campaigns, helper)
+    connector = Connector(config, campaigns, events, helper)
     connector.start()
