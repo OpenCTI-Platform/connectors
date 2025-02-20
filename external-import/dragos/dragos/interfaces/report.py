644e818bab7f7ab974a0d9a2220a7f424260141c
FlavienSindou <flavien.sindou@filigran.io>
2025-02-20 18:05:14 +0100

    simplify: rename Reports list method name

--- external-import/dragos/dragos/interfaces/report.py
+++ external-import/dragos/dragos/interfaces/report.py
@@ -159,6 +159,6 @@ class Reports(ABC):
     """Interface for Dragos Reports Retrieval."""
 
     @abstractmethod
-    def list_reports(self) -> Iterable[Report]:
+    def list(self) -> Iterable[Report]:
         """List all Dragos reports."""
         pass
