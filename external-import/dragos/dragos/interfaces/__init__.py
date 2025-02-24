ed891867f6951ec956822ae4ab5467a7cf3470ad
FlavienSindou <flavien.sindou@filigran.io>
2025-02-24 16:20:08 +0100

    feat: add sub package public element for error handling and typing purposes

---
+++ external-import/dragos/dragos/interfaces/__init__.py
@@ -0,0 +1,13 @@
+"""Provide the interface of the connector for configuration and dragos report."""
+
+from .config import ConfigLoaderOCTI, ConfigRetrievalError
+from .report import DataRetrievalError, Reports
+from .report import _Report as Report
+
+__all__ = [
+    "ConfigRetrievalError", # for error handling in application layer
+    "ConfigLoaderOCTI",  # for typing purposes in application layer
+    "DataRetrievalError", # for error handling in application layer
+    "Report",  # for typing purposes in application layer
+    "Reports",  # for typing purposes in application layer
+]
