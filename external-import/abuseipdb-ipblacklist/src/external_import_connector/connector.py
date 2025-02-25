affd9a7df646f705754d5b08a422cf4ec48e2900
Baptiste FOURMONT <baptiste_fourmont@tutanota.com>
2025-02-25 14:24:41 +0100

    Remove deprecated method

--- external-import/abuseipdb-ipblacklist/src/external_import_connector/connector.py
+++ external-import/abuseipdb-ipblacklist/src/external_import_connector/connector.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+from datetime import datetime, timezone
 
 from pycti import OpenCTIConnectorHelper
 
@@ -168,9 +168,9 @@ class ConnectorAbuseIPDB:
             )
             current_state = self.helper.get_state()
             current_state_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
-            last_run_datetime = datetime.utcfromtimestamp(current_timestamp).strftime(
-                "%Y-%m-%d %H:%M:%S"
-            )
+            last_run_datetime = datetime.fromtimestamp(
+                current_timestamp, tz=timezone.utc
+            ).strftime("%Y-%m-%d %H:%M:%S")
             if current_state:
                 current_state["last_run"] = current_state_datetime
             else:
