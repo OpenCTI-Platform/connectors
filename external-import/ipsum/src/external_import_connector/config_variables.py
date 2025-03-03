ef70afcfc08e4ad74eea0791438b0579d6f0f961
FlavienSindou <flavien.sindou@filigran.io>
2025-03-03 17:04:08 +0100

    fix: align naming

--- external-import/ipsum/src/external_import_connector/config_variables.py
+++ external-import/ipsum/src/external_import_connector/config_variables.py
@@ -61,7 +61,7 @@ class ConfigConnector:
             required=False,
         )
 
-        self.tlp_marking = get_config_variable(
+        self.tlp_level = get_config_variable(
             "CONNECTOR_IPSUM_TLP_LEVEL",
             ["connector_ipsum", "tlp_level"],
             self.load,
