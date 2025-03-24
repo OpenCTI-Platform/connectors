d1550fa72baa070e106013dc1d61637c830fa533
Philippe Damoune <philippe.damoune@filigran.io>
2025-03-24 15:25:29 +0100

    refactor(shodan-internetdb): Move stix objects creation to converter

--- internal-enrichment/shodan-internetdb/src/shodan_internetdb/connector.py
+++ internal-enrichment/shodan-internetdb/src/shodan_internetdb/connector.py
@@ -3,21 +3,15 @@
 from __future__ import annotations
 
 import logging
-from datetime import datetime, timedelta
-from typing import Any, Dict, List, Union
+from typing import Dict
 
 import stix2
 import validators
 from pycti import (
-    STIX_EXT_OCTI_SCO,
-    Note,
     OpenCTIConnectorHelper,
-    OpenCTIStix2,
-    StixCoreRelationship,
-    Vulnerability,
 )
 from requests.exceptions import RequestException
-from shodan_internetdb.client import ShodanInternetDbClient, ShodanResult
+from shodan_internetdb.client import ShodanInternetDbClient
 from shodan_internetdb.config import ConfigConnector
 from shodan_internetdb.converter_to_stix import ConverterToStix
 
@@ -78,139 +72,14 @@ class ShodanInternetDBConnector:
             return "Skipping observable (Shodan 404)"
 
         # Process the result
-        stix_objects = []
         log.debug("Processing %s", value)
-        stix_objects.extend(self._process_domains(stix_observable, result))
-        stix_objects.extend(self._process_vulns(stix_observable, result))
-        stix_objects.append(self._process_note(stix_observable, result))
-        stix_objects.append(self._process_tags(stix_observable, result))
+
+        stix_objects = self.converter.create_stix_objects(stix_observable, result)
 
         bundle = stix2.Bundle(objects=stix_objects, allow_custom=True).serialize()
         self.helper.log_info("Sending event STIX2 bundle")
         bundle_sent = self.helper.send_stix2_bundle(bundle)
         return f"Sent {len(bundle_sent)} stix bundle(s) for worker import"
-
-    def _process_note(
-        self,
-        observable: Dict[str, Any],
-        result: ShodanResult,
-    ) -> Note:
-        """
-        Add an enrichment note to the observable
-        :param observable: Observable data
-        :param result: Shodan data
-        :return: None
-        """
-
-        def format_list(alist: List[Union[str, int]]) -> str:
-            """Format a list of primitives into a Markdown list"""
-            return "".join(f"\n- {name}" for name in alist) or "n/a"
-
-        value = observable["value"]
-        abstract = f"Shodan InternetDB enrichment of {value}"
-        content = f"""```
-Shodan InternetDB:
-------------------
-Hostnames: {format_list(result.hostnames)}
-------------------
-Software: {format_list(result.cpes)}
-------------------
-Vulnerabilities: {format_list(result.vulns)}
-------------------
-Ports: {format_list(result.ports)}
-------------------
-```
-"""
-        note = stix2.Note(
-            id=Note.generate_id(datetime.now().isoformat(), content),
-            created_by_ref=self.converter.author.id,
-            object_marking_refs=[self.converter.tlp_marking.id],
-            abstract=abstract,
-            content=content,
-            object_refs=[observable["id"]],
-            allow_custom=True,
-        )
-        return note
-
-    def _process_domains(
-        self,
-        observable: Dict[str, Any],
-        result: ShodanResult,
-    ) -> list:
-        """
-        Add additional domains to the observable
-        :param observable: Observable data
-        :param result: Shodan data
-        :return: None
-        """
-        stix_objects = []
-        for name in result.hostnames:
-            log.debug("Adding domain %s", name)
-            stix_domain = stix2.DomainName(
-                value=name,
-                object_marking_refs=[self.converter.tlp_marking.id],
-                resolves_to_refs=[observable["id"]],
-                custom_properties={"created_by_ref": self.converter.author.id},
-            )
-            stix_objects.append(stix_domain)
-        return stix_objects
-
-    def _process_tags(
-        self,
-        observable: Dict[str, Any],
-        result: ShodanResult,
-    ) -> Dict:
-        """
-        Add additional tags to the observable
-        :param observable: Observable data
-        :param result: Shodan data
-        :return: None
-        """
-        for name in result.tags:
-            OpenCTIStix2.put_attribute_in_extension(
-                observable, STIX_EXT_OCTI_SCO, "labels", name, True
-            )
-        return observable
-
-    def _process_vulns(
-        self,
-        observable: Dict[str, Any],
-        result: ShodanResult,
-    ) -> list:
-        """
-        Add additional vulnerabilities to the observable
-        :param observable: Observable data
-        :param result: Shodan data
-        :return: None
-        """
-        stix_objects = []
-        now = datetime.utcnow()
-        vuln_eol = now + timedelta(days=60)
-
-        for name in result.vulns:
-            log.debug("Creating vulnerability %s", name)
-            stix_vuln = stix2.Vulnerability(
-                id=Vulnerability.generate_id(name),
-                name=f"{name}",
-                created_by_ref=self.converter.author.id,
-                object_marking_refs=[self.converter.tlp_marking.id],
-            )
-            relationship = stix2.Relationship(
-                id=StixCoreRelationship.generate_id(
-                    "related-to", observable["id"], stix_vuln.id
-                ),
-                relationship_type="related-to",
-                created_by_ref=self.converter.author.id,
-                source_ref=observable["id"],
-                target_ref=stix_vuln.id,
-                object_marking_refs=[self.converter.tlp_marking.id],
-                allow_custom=True,
-                start_time=now.strftime("%Y-%m-%dT%H:%M:%SZ"),
-                stop_time=vuln_eol.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            )
-            stix_objects.append(stix_vuln)
-            stix_objects.append(relationship)
-        return stix_objects
 
     def run(self) -> None:
         """
--- internal-enrichment/shodan-internetdb/src/shodan_internetdb/converter_to_stix.py
+++ internal-enrichment/shodan-internetdb/src/shodan_internetdb/converter_to_stix.py
@@ -1,7 +1,9 @@
-from typing import Literal
+import datetime
+from typing import Any, Literal
 
 import pycti
 import stix2
+from shodan_internetdb.client import ShodanResult
 
 
 class ConverterToStix:
@@ -58,3 +60,134 @@ class ConverterToStix:
                 return stix2.TLP_RED
             case _:  # default
                 raise ValueError(f"Invalid TLP level: {level}")
+
+    def _create_domain_names(
+        self,
+        observable_id: str,
+        hostnames: list[str],
+    ) -> list[stix2.DomainName]:
+        return [
+            stix2.DomainName(
+                value=hostname,
+                object_marking_refs=[self.tlp_marking.id],
+                resolves_to_refs=[observable_id],
+                custom_properties={"created_by_ref": self.author.id},
+            )
+            for hostname in hostnames
+        ]
+
+    def _create_vulnerabilities_and_relationships(
+        self,
+        observable_id: str,
+        vulnerability_names: list[str],
+    ) -> list[stix2.Vulnerability | stix2.Relationship]:
+        stix_objects = []
+        now = datetime.datetime.now(tz=datetime.UTC)
+        vuln_eol = now + datetime.timedelta(days=60)
+
+        for name in vulnerability_names:
+            self.helper.connector_logger.debug("Creating vulnerability %s", name)
+            stix_vuln = stix2.Vulnerability(
+                id=pycti.Vulnerability.generate_id(name),
+                name=f"{name}",
+                created_by_ref=self.author.id,
+                object_marking_refs=[self.tlp_marking.id],
+            )
+            relationship = stix2.Relationship(
+                id=pycti.StixCoreRelationship.generate_id(
+                    "related-to", observable_id, stix_vuln.id
+                ),
+                relationship_type="related-to",
+                created_by_ref=self.author.id,
+                source_ref=observable_id,
+                target_ref=stix_vuln.id,
+                object_marking_refs=[self.tlp_marking.id],
+                allow_custom=True,
+                start_time=now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                stop_time=vuln_eol.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            )
+            stix_objects.extend([stix_vuln, relationship])
+        return stix_objects
+
+    def _create_note(
+        self,
+        observable_id: str,
+        observable_value: str,
+        hostnames: list[str],
+        cpes: list[str],
+        vulns: list[str],
+        ports: list[int],
+    ) -> stix2.Note:
+
+        def format_list(alist: list[str] | list[int]) -> str:
+            """Format a list of primitives into a Markdown list"""
+            return "".join(f"\n- {name}" for name in alist) or "n/a"
+
+        abstract = f"Shodan InternetDB enrichment of {observable_value}"
+        content = (
+            "```\n"
+            "Shodan InternetDB:\n"
+            "------------------\n"
+            f"Hostnames: {format_list(hostnames)}\n"
+            "------------------\n"
+            f"Software: {format_list(cpes)}\n"
+            "------------------\n"
+            f"Vulnerabilities: {format_list(vulns)}\n"
+            "------------------\n"
+            f"Ports: {format_list(ports)}\n"
+            "------------------\n"
+            "```\n"
+        )
+        note = stix2.Note(
+            id=pycti.Note.generate_id(
+                datetime.datetime.now(tz=datetime.UTC).isoformat(), content
+            ),
+            created_by_ref=self.author.id,
+            object_marking_refs=[self.tlp_marking.id],
+            abstract=abstract,
+            content=content,
+            object_refs=[observable_id],
+            allow_custom=True,
+        )
+        return note
+
+    def _create_tags(
+        self, observable: dict[str, Any], tag_names: list[str]
+    ) -> list[dict[str, Any]]:
+        return [
+            pycti.OpenCTIStix2.put_attribute_in_extension(
+                observable, pycti.STIX_EXT_OCTI_SCO, "labels", name, True
+            )
+            for name in tag_names
+        ]
+
+    def create_stix_objects(
+        self, stix_observable: dict[str, Any], result: ShodanResult
+    ) -> list[
+        stix2.DomainName
+        | stix2.Vulnerability
+        | stix2.Relationship
+        | stix2.Note
+        | dict[str, Any]
+    ]:
+        return (
+            self._create_domain_names(
+                observable_id=stix_observable["id"],
+                hostnames=result.hostnames,
+            )
+            + self._create_vulnerabilities_and_relationships(
+                observable_id=stix_observable["id"],
+                vulnerability_names=result.vulns,
+            )
+            + self._create_tags(stix_observable, result.tags)
+            + [
+                self._create_note(
+                    observable_id=stix_observable["id"],
+                    observable_value=stix_observable["value"],
+                    hostnames=result.hostnames,
+                    cpes=result.cpes,
+                    vulns=result.vulns,
+                    ports=result.ports,
+                ),
+            ]
+        )
