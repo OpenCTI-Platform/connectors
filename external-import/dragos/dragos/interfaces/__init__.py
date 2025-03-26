ab254ad181493e37964c16a747ba147ab625c89e
FlavienSindou <flavien.sindou@filigran.io>
2025-03-26 17:49:20 +0100

    feat: add geocoding interface

--- external-import/dragos/dragos/interfaces/__init__.py
+++ external-import/dragos/dragos/interfaces/__init__.py
@@ -1,6 +1,7 @@
 """Provide the interface of the connector for configuration and dragos report."""
 
 from .config import ConfigLoader, ConfigRetrievalError
+from .geocoding import Geocoding, GeocodingRetrievalError
 from .report import ReportRetrievalError, Reports
 from .report import _Report as Report
 
@@ -10,4 +11,6 @@ __all__ = [
     "ReportRetrievalError",  # for error handling in application layer
     "Report",  # for typing purposes in application layer
     "Reports",  # for typing purposes in application layer
+    "GeocodingRetrievalError",  # for error handling in application or usecase layers
+    "Geocoding",  # for typing purposes in application layer
 ]
