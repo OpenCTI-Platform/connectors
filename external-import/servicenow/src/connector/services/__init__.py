b150c2e53ed922e7ad338451eae3400a25c1777a
Frederic Basler <frederic.basler@filigran.io>
2025-04-02 10:17:11 +0200

    [ServiceNow] Fix linter (import-unused)

--- external-import/servicenow/src/connector/services/__init__.py
+++ external-import/servicenow/src/connector/services/__init__.py
@@ -1,2 +1,9 @@
 from connector.services.client_api import ServiceNowClient
 from connector.services.config_loader import ServiceNowConfig
+from connector.services.converter_to_stix import ConverterToStix
+
+__all__ = [
+    "ServiceNowClient",
+    "ServiceNowConfig",
+    "ConverterToStix",
+]
