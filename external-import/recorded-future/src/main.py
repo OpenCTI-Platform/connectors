9561aefe131e63a6e608813be1cd9155bfa1c884
Lucas Guiglionia <lucas.guiglionia@filigran.io>
2025-04-05 15:19:36 +0200

    Fix format

--- external-import/recorded-future/src/main.py
+++ external-import/recorded-future/src/main.py
@@ -60,7 +60,10 @@ class BaseRFConnector:
             "RECORDED_FUTURE_PULL_RISK_LIST", ["rf", "pull_risk_list"], config
         )
         self.rf_riskrules_as_label = get_config_variable(
-            "RECORDED_FUTURE_RISKRULES_AS_LABEL", ["rf", "riskrules_as_label"], config, default=False
+            "RECORDED_FUTURE_RISKRULES_AS_LABEL",
+            ["rf", "riskrules_as_label"],
+            config,
+            default=False,
         )
         self.rf_insikt_only = get_config_variable(
             "RECORDED_FUTURE_INSIKT_ONLY", ["rf", "insikt_only"], config
