7375d151b998839cf3fad4edf4ac694a197b71de
Philippe Damoune <philippe.damoune@filigran.io>
2025-04-11 18:07:48 +0200

    test

--- run_test.sh
+++ run_test.sh
@@ -1,42 +1,23 @@
 #!/bin/bash
+set -e
 
-set -e  # exit on error 
-
+connector_dir=$1
 venv_name=".temp_venv"
 
-# virtual environment will not be cleaned up if script is interrupted
 if [ -d "$venv_name" ]; then
-  echo 'Removing virtual environment'
+  echo 'Removing existing virtual environment'
   rm -rf "$venv_name"
 fi
 
-test_requirements_files=$(find . -name "test-requirements.txt")
+echo "Creating virtual environment for $connector_dir"
+python3 -m venv "$venv_name"
+source "$venv_name/bin/activate"
 
-echo 'Found test-requirements.txt files:' "$test_requirements_files"
+pip install --upgrade pip
+pip install -r "$connector_dir/test-requirements.txt"
 
-for requirements_file in $test_requirements_files
-do
-  project=$(echo "$requirements_file" | cut -d'/' -f1-3)
-  echo 'Running tests pipeline for project' $project
+echo "Running tests for $connector_dir"
+pytest "$connector_dir/tests"
 
-  echo 'Creating isolated virtual environment'
-  python -m venv "$venv_name"
-  if [ -f "$venv_name/bin/activate" ]; then
-    source "$venv_name/bin/activate"  # Linux/MacOS
-  elif [ -f "$venv_name/Scripts/activate" ]; then
-    source "$venv_name/Scripts/activate"  # Windows
-  fi
-
-  echo 'Installing requirements'
-  python -m pip install -q -r "$requirements_file"
-
-  
-  echo 'Running tests'
-  python -m pytest "$project"  # exit non zero if no test run
-
-  echo 'Removing virtual environment'
-  deactivate
-  rm -rf "$venv_name"
-done
-
-
+deactivate
+rm -rf "$venv_name"
