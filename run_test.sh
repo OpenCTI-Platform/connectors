f4cbd85bb58d113fe7aac33b4d28f47a1dcb100e
Philippe Damoune <philippe.damoune@filigran.io>
2025-04-11 18:17:51 +0200

    test

--- run_test.sh
+++ run_test.sh
@@ -5,20 +5,29 @@ connector_dir=$1
 
 venv_name=".temp_venv"
 
-if [ -d "$venv_name" ]; then
-  echo 'Removing existing virtual environment'
+test_requirements_files=$(find $connector_dir -name "test-requirements.txt")
+
+echo 'Found test-requirements.txt files:' "$test_requirements_files"
+
+if [ -z "$test_requirements_files" ]; then
+  echo "No test-requirements.txt file found in $connector_dir"
+else
+  if [ -d "$venv_name" ]; then
+    echo 'Removing existing virtual environment'
+    rm -rf "$venv_name"
+  fi
+
+  echo "Creating virtual environment for $connector_dir"
+  python3 -m venv "$venv_name"
+  source "$venv_name/bin/activate"
+
+  pip install --upgrade pip
+  pip install -r "$connector_dir/test-requirements.txt"
+
+  echo "Running tests for $connector_dir"
+  pytest "$connector_dir/tests"
+
+  deactivate
   rm -rf "$venv_name"
 fi
 
-echo "Creating virtual environment for $connector_dir"
-python3 -m venv "$venv_name"
-source "$venv_name/bin/activate"
-
-pip install --upgrade pip
-pip install -r "$connector_dir/test-requirements.txt"
-
-echo "Running tests for $connector_dir"
-pytest "$connector_dir/tests"
-
-deactivate
-rm -rf "$venv_name"
