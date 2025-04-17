c9582467c2ea0a51aca199ad728b44720c56359e
Philippe Damoune <philippe.damoune@filigran.io>
2025-04-17 15:49:48 +0200

    [BaseConnector] Add tests

--- external-import/email-intel-imap/tests/base_connector/conftest.py
+++ external-import/email-intel-imap/tests/base_connector/conftest.py
@@ -1,7 +1,7 @@
 import os
 from copy import deepcopy
 from typing import Any
-from unittest.mock import Mock
+from unittest.mock import MagicMock, Mock
 
 import pytest
 from pytest_mock import MockerFixture
@@ -58,3 +58,14 @@ def fixture_mocked_config() -> Mock:
     config = Mock()
     config.tlp_level = "white"
     return config
+
+
+@pytest.fixture(name="mocked_helper")
+def fixture_mocked_helper(mocker: MockerFixture) -> MagicMock:
+    helper = mocker.patch("pycti.OpenCTIConnectorHelper", MagicMock())
+    helper.connect_id = "test-connector-id"
+    helper.connect_name = "Test Connector"
+    helper.api.work.initiate_work.return_value = "work-id"
+    helper.get_state.return_value = {"last_run": "2025-04-17T15:20:00Z"}
+    helper.stix2_create_bundle.return_value = "bundle"
+    return helper
---
+++ external-import/email-intel-imap/tests/base_connector/test_connector.py
@@ -0,0 +1,42 @@
+from unittest.mock import MagicMock, Mock
+
+import freezegun
+from base_connector.connector import BaseConnector
+from stix2.base import _DomainObject
+
+STIX_OBJECT = MagicMock()
+
+
+class TestConnector(BaseConnector):
+    def _collect_intelligence(self) -> list[_DomainObject]:
+        return [STIX_OBJECT]
+
+
+@freezegun.freeze_time("2025-04-17T15:24:00Z")
+def test_process(mocked_helper: MagicMock) -> None:
+    connector = TestConnector(
+        helper=mocked_helper, config=Mock(), client=Mock(), converter=Mock()
+    )
+    connector._process_message()
+
+    # Assert the work initiation and processing
+    mocked_helper.api.work.initiate_work.assert_called_once_with(
+        connector_id="test-connector-id", friendly_name="Test Connector"
+    )
+    mocked_helper.api.work.to_processed.assert_called_once_with(
+        message="Connector successfully run, storing last_run as 2025-04-17 15:24:00+00:00",
+        work_id="work-id",
+    )
+
+    # Assert the state management
+    mocked_helper.set_state.assert_called_once_with(
+        state={"last_run": "2025-04-17 15:24:00+00:00"}
+    )
+
+    # Assert the STIX object processing
+    mocked_helper.stix2_create_bundle.assert_called_once_with(
+        items=[STIX_OBJECT, connector.converter.author, connector.converter.tlp_marking]
+    )
+    mocked_helper.send_stix2_bundle.assert_called_once_with(
+        bundle="bundle", cleanup_inconsistent_bundle=True, work_id="work-id"
+    )
