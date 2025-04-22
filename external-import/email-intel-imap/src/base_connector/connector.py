6f278b640fbcc3e3cc10386046adb2a5d72fa500
Philippe Damoune <philippe.damoune@filigran.io>
2025-04-22 10:15:46 +0200

    [BaseConnector] Implement get_duration_period

--- external-import/email-intel-imap/src/base_connector/connector.py
+++ external-import/email-intel-imap/src/base_connector/connector.py
@@ -134,11 +134,14 @@ class BaseConnector(abc.ABC):
             return "Unexpected error. See connector logs for details."
         return None
 
+    def get_duration_period(self) -> float:
+        return self.config.connector.duration_period.total_seconds()
+
     def run(self) -> None:
         self.helper.connector_logger.info("Starting connector...")
         self.helper.schedule_process(
             message_callback=self.process,
-            duration_period=self.config.connector.duration_period.total_seconds(),
+            duration_period=self.get_duration_period(),
         )
 
     @abc.abstractmethod
