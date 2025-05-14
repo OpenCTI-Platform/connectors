33f0f25586af979037d9423a6a936f3c1ef3189c
Philippe Damoune <philippe.damoune@filigran.io>
2025-05-14 12:21:57 +0200

    refactor(ShadowServer): Inject helper instead of instanciate in class

--- external-import/shadowserver/src/main.py
+++ external-import/shadowserver/src/main.py
@@ -1,10 +1,12 @@
 import traceback
 
+from pycti import OpenCTIConnectorHelper
 from shadowserver.connector import CustomConnector
 
 if __name__ == "__main__":
     try:
-        connector = CustomConnector()
+        helper = OpenCTIConnectorHelper({})
+        connector = CustomConnector(helper=helper)
         connector.run()
     except Exception:
         traceback.print_exc()
