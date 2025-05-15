60890257573e8ce3afd23d46e6988124753e885a
Jorgeley <jorgeley@silentpush.com>
2025-05-15 17:59:39 +0100

    Silent Push enrichment connector ready for review

---
+++ internal-enrichment/silentpush-enrichment/src/settings.py
@@ -0,0 +1,32 @@
+import os
+
+import yaml
+from pycti import get_config_variable
+
+config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
+config = (
+    yaml.load(open(config_file_path), Loader=yaml.SafeLoader)
+    if os.path.isfile(config_file_path)
+    else {}
+)
+API_URI = get_config_variable(
+    "API_URI",
+    ["silentpush", "CONNECTOR_SILENTPUSH_API_BASE_URL"],
+    config,
+    False,
+    "https://app.silentpush.com/api/v1/",
+)
+API_VERIFY_CERT = get_config_variable(
+    "API_VERIFY_CERT",
+    ["silentpush", "CONNECTOR_SILENTPUSH_VERIFY_CERT"],
+    config,
+    False,
+    True,
+)
+API_KEY = get_config_variable("API_KEY", ["silentpush", "CONNECTOR_SILENTPUSH_API_KEY"], config)
+SILENTPUSH_SIGNATURE = "Silent Push"
+ip_diversity_uri = (
+    API_URI
+    + "merge-api/explore/padns/lookup/ipdiversity/a/{domain}/?format=json&timeline=1&window=1"
+)
+enrich_uri = API_URI + "merge-api/explore/enrich/{type}/{ioc}/?format=json&scan_data=1&explain=1"
