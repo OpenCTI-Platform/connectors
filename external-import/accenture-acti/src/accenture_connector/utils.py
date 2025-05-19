93b0bc1d1bc92779cfbeddc90dec5ddf929f52e9
Romain Guignard <romain.guignard@filigran.io>
2025-05-19 19:58:25 +0200

    remove useless code

--- external-import/accenture-acti/src/accenture_connector/utils.py
+++ external-import/accenture-acti/src/accenture_connector/utils.py
@@ -1,5 +1,1 @@
 #  Utilities: helper functions, classes, or modules that provide common, reusable functionality across a codebase
-def retrieve_all(bundle, key, value):
-    for item in bundle.get("objects"):
-        if item.get(key) == value:
-            yield item
