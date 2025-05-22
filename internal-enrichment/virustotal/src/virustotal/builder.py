2fa2f4401204036d41817687a2ab04760bee5214
Rajasekhar.Alkuri <rajasekhar.alkuri@kudelskisecurity.com>
2025-05-22 12:21:23 +0530

    [Virustotal] Adds Attributes for Virustotal Notes (#4024)

--- internal-enrichment/virustotal/src/virustotal/builder.py
+++ internal-enrichment/virustotal/src/virustotal/builder.py
@@ -32,6 +32,8 @@ class VirusTotalBuilder:
         stix_entity: dict,
         opencti_entity: dict,
         data: dict,
+        include_attributes_in_note: Optional[bool] = False,
+        url_related_object_data: dict = {},
     ) -> None:
         """Initialize Virustotal builder."""
         self.helper = helper
@@ -42,6 +44,8 @@ class VirusTotalBuilder:
         self.stix_entity = stix_entity
         self.attributes = data["attributes"]
         self.score = self._compute_score(self.attributes["last_analysis_stats"])
+        self.include_attributes_in_note = include_attributes_in_note
+        self.url_related_object_data = url_related_object_data
 
         # Update score of observable.
         OpenCTIStix2.put_attribute_in_extension(
@@ -366,6 +370,11 @@ class VirusTotalBuilder:
                     + (result["result"] if result["result"] is not None else "N/A")
                     + " | \n"
                 )
+            content += (
+                self.create_notes_attributes_content()
+                if self.include_attributes_in_note
+                else ""
+            )
             self.create_note(
                 "VirusTotal Results",
                 content,
@@ -378,6 +387,11 @@ class VirusTotalBuilder:
                 content += (
                     "| " + key + " | " + self.attributes["categories"][key] + " | \n"
                 )
+            content += (
+                self.create_notes_attributes_content()
+                if self.include_attributes_in_note
+                else ""
+            )
             self.create_note("VirusTotal Categories", content)
 
     def create_yara(
@@ -558,3 +572,47 @@ class VirusTotalBuilder:
             f'[VirusTotal] updating size with {self.attributes["size"]}'
         )
         self.stix_entity["size"] = self.attributes["size"]
+
+    def create_notes_attributes_content(self):
+        attributes_content = ""
+        attributes_scope = {
+            "StixFile": {
+                "File Type": self.attributes.get("type_description"),
+                "Type Tags": ", ".join(self.attributes.get("type_tags", [])),
+                "Last Submission": self.attributes.get("last_submission_date"),
+                "Popular Threat Label": self.attributes.get(
+                    "popular_threat_classification", {}
+                ).get("suggested_threat_label"),
+                "Signature Verification": self.attributes.get("signature_info", {}).get(
+                    "verified"
+                ),
+                "Names": ", ".join(self.attributes.get("names", [])),
+            },
+            "IPv4-Addr": {
+                "Country": self.attributes.get("country"),
+                "Autonomous System Label": self.attributes.get("as_owner"),
+            },
+            "Domain-Name": {
+                "Creation Date": self.attributes.get("creation_date"),
+                "Last Analysis Date": self.attributes.get("last_analysis_date"),
+            },
+            "Url": {
+                "Final URL": self.attributes.get("last_final_url"),
+                "Last Analysis Date": self.attributes.get("last_analysis_date"),
+                "Serving IP Address": self.url_related_object_data.get("id"),
+            },
+        }
+        attributes_scope = attributes_scope.get(self.opencti_entity.get("entity_type"))
+        if self.include_attributes_in_note and attributes_scope:
+            for attribute, value in attributes_scope.items():
+                attributes_content += (
+                    "| " + attribute + " | " + str(value or "N/A") + " | \n"
+                )
+            if attributes_content:
+                content = "## Attributes Info\n\n"
+                content += "Any falsy value will be replaced by ‘N/A’\n"
+                content += "| Attributes |      |\n"
+                content += "|--------|----------|\n"
+                content += attributes_content
+                return content
+        return ""
