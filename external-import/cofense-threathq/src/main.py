f29389f42071f1d8dab81bfd4363dee5e0b879b4
Frédéric BASLER <14902945+Megafredo@users.noreply.github.com>
2025-05-28 16:53:22 +0200

    [Cofense-ThreatHQ] Create new Cofense connector (#3933)

---
+++ external-import/cofense-threathq/src/main.py
@@ -0,0 +1,27 @@
+import traceback
+
+from connector import CofenseThreatHQ
+from connector.services.config_loader import CofenseThreatHQConfig
+from pycti import OpenCTIConnectorHelper
+
+if __name__ == "__main__":
+    """
+    Entry point of the script
+
+    - traceback.print_exc(): This function prints the traceback of the exception to the standard error (stderr).
+    The traceback includes information about the point in the program where the exception occurred,
+    which is very useful for debugging purposes.
+    - exit(1): effective way to terminate a Python program when an error is encountered.
+    It signals to the operating system and any calling processes that the program did not complete successfully.
+    """
+    try:
+        config = CofenseThreatHQConfig()
+        config_instance = config.load
+        # Convert the config into a dictionary, automatically excluding any parameters set to `None`.
+        config_dict = config_instance.model_dump(exclude_none=True)
+        helper = OpenCTIConnectorHelper(config=config_dict)
+        connector = CofenseThreatHQ(config_instance, helper)
+        connector.run()
+    except Exception:
+        traceback.print_exc()
+        exit(1)
