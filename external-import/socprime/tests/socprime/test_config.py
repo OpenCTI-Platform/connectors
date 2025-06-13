07a3183b3ca711b0748f1111474091a39294cb6b
Philippe Damoune <philippe.damoune@filigran.io>
2025-06-13 16:02:36 +0200

    clean(SocPrime): Change variables to new config & clean up

--- external-import/socprime/tests/socprime/test_config.py
+++ external-import/socprime/tests/socprime/test_config.py
@@ -10,7 +10,7 @@ pytestmark = pytest.mark.usefixtures(
 )
 
 
-def test_config() -> None:
+def test_config_helper() -> None:
     connector = SocprimeConnector()
 
     assert connector.helper.opencti_url == "http://test-opencti-url/"
@@ -21,13 +21,6 @@ def test_config() -> None:
     assert connector.helper.connect_type == "EXTERNAL_IMPORT"
     assert connector.helper.connect_scope == "socprime"
     assert connector.helper.log_level == "ERROR"
-
-    assert connector.tdm_api_client._api_key == "api-key"
-    assert connector._content_list_names == "name1,name2"
-    assert connector._job_ids == "job1,job2"
-    assert connector._siem_types_for_refs == "devo,snowflake"
-    assert connector._indicator_siem_type == "ChangeMe"
-    assert connector.interval_sec == 2000
 
 
 def test_config_settings() -> None:
