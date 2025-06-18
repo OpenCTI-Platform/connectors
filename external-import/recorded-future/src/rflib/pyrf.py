ffed0711a474307c500c93a2584c7a84f0815240
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-06-18 11:34:06 +0200

    [refactor] alerts pagination refactor

--- external-import/recorded-future/src/rflib/pyrf.py
+++ external-import/recorded-future/src/rflib/pyrf.py
@@ -1,7 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 import requests
 from pycti import OpenCTIConnectorHelper
+
+
+class RecordedFutureApiError(Exception):
+    """Wrap exceptions emitted in RecordedFutureApiClient"""
 
 
 class PlaybookAlertSummary:
@@ -139,6 +143,117 @@ class RecordedFutureApiClient:
                     "Process unsuccessful",
                     {"status_message": data["status"]["status_message"]},
                 )
+
+    def _raw_get_alerts(
+        self,
+        rule: PrioritiedRule,
+        triggered_since: datetime,
+        triggered_until: datetime | None = None,
+        limit: int = 100,
+        offset: int = 0,
+    ) -> dict:
+        """
+        Get search alerts raw data for a given rule and time range.
+        :param rule: The rule to get alerts for.
+        :param since: The date from which to get alerts.
+        :param until: The date until which to get alerts (optional).
+        :param limit: The maximum number of alerts to retrieve per request (default is 100).
+        :param offset: The offset for pagination (default is 0).
+        :return: Search alerts raw dictionary.
+        """
+        try:
+            triggered_since_iso = triggered_since.isoformat().replace("+00:00", "Z")
+            triggered_until_iso = (
+                triggered_until.isoformat().replace("+00:00", "Z")
+                if triggered_until
+                else ""
+            )
+
+            response = requests.get(
+                self.base_url + "v3/alerts",
+                headers={
+                    "X-RFToken": self.x_rf_token,
+                    "accept": "application/json",
+                },
+                params={
+                    "alertRule": rule.rule_id,
+                    "triggered": f"[{triggered_since_iso},{triggered_until_iso}]",
+                    "limit": limit,
+                    "from": offset,
+                },
+            )
+            response.raise_for_status()
+
+            data = response.json()
+
+            self.helper.connector_logger.debug(
+                "Alert(s) found on Recorded Future API",
+                {
+                    "rule_id": rule.rule_id,
+                    "rule_name": rule.rule_name,
+                    "alerts_total_count": data.get("counts", {}).get("total", 0),
+                },
+            )
+
+            return data
+        except requests.exceptions.RequestException as err:
+            raise RecordedFutureApiError(
+                f"Error while fetching RecordedFuture's API: {err}"
+            ) from err
+
+    def _get_paginated_alerts(
+        self,
+        rule: PrioritiedRule,
+        since: datetime,
+        until: datetime | None = None,
+        limit: int = 100,
+        offset: int = 0,
+    ) -> list[Alert]:
+        """
+        Paginate and get all alerts for a given rule and time range.
+        :param rule: The rule to get alerts for.
+        :param since: The date from which to get alerts.
+        :param until: The date until which to get alerts (optional).
+        :param limit: The maximum number of alerts to retrieve per request (default is 100).
+        :param offset: The offset for pagination (default is 0).
+        :return: A list of Alert objects.
+        """
+        alerts = []
+
+        data = self._raw_get_alerts(
+            rule=rule,
+            triggered_since=since,
+            triggered_until=until,
+            limit=limit,
+            offset=offset,
+        )
+        for alert in data.get("data", []):
+            alerts.append(
+                Alert(
+                    alert_id=alert["id"],
+                    alert_url=alert["url"]["portal"],
+                    alert_date=alert["log"]["triggered"],
+                    alert_title=alert["title"],
+                    alert_ai_insight=alert["ai_insights"]["comment"],
+                    alert_rf_rule=rule,
+                    alert_hits=alert["hits"],
+                )
+            )
+
+        offset += len(alerts)
+        alerts_count = data.get("counts", {}).get("total", 0)
+        if offset < alerts_count:
+            alerts.extend(
+                self._get_paginated_alerts(
+                    rule=rule,
+                    since=since,
+                    until=until,
+                    limit=limit,
+                    offset=offset,
+                )
+            )
+
+        return alerts
 
     def get_playbook_id(self, category, trigger_from, trigger_to, priority_threshold):
         try:
@@ -313,97 +428,35 @@ class RecordedFutureApiClient:
         except:
             return False, str("error.png"), str("error.png")
 
-    def get_alerts(
-        self,
-        rule: PrioritiedRule,
-        triggered_since: datetime,
-        triggered_until: datetime | None = None,
-    ) -> list[Alert]:
+    def get_alerts(self, rule: PrioritiedRule, since: datetime) -> list[Alert]:
         """
         Get alerts by rule and by trigger date.
         :param rule: The rule to get alerts for.
         :param triggered_since: The date from which to get alerts.
-        :param triggered_until: The date until which to get alerts. If None, it will get alerts until now.
+        :return: A list of Alert objects.
 
         See https://docs.recordedfuture.com/reference/alert-search
         """
-
         alerts = []
 
-        triggered_since_iso = triggered_since.strftime("%Y-%m-%d")
-        triggered_until_iso = (
-            triggered_until.strftime("%Y-%m-%d") if triggered_until else ""
-        )
+        # Get the first alert to check the total alerts count
+        data = self._raw_get_alerts(rule=rule, triggered_since=since, limit=1)
+        alerts_count = data.get("counts", {}).get("total", 0)
 
-        offset = 0
-        alerts_count = 0
-        while alerts_count < 1000 or offset < alerts_count:
-            try:
-                response = requests.get(
-                    self.base_url + "v3/alerts",
-                    headers={
-                        "X-RFToken": self.x_rf_token,
-                    },
-                    params={
-                        "alertRule": rule.rule_id,
-                        "triggered": f"[{triggered_since_iso},{triggered_until_iso}]",
-                        "from": offset,
-                        "limit": 100,
-                    },
-                )
-
-                # If there is an error during the request, the method raise the error
-                response.raise_for_status()
-                # Check if the response has correct content type
-                self._header_response_control(response)
-
-                data = response.json()
-
-                # If the response doesn't contain data, log the result
-                if not data.get("data"):
-                    self.helper.connector_logger.info(
-                        "No data returned from Recorded Future API",
-                        {
-                            "rule_id": rule.rule_id,
-                            "rule_name": rule.rule_name,
-                        },
+        # If more than 1000 alerts are found,
+        # recover alerts day by day to avoid API pagination limits
+        if alerts_count > 1000:
+            days_to_recover = (datetime.now(tz=timezone.utc) - since).days
+            for day_delta in range(0, days_to_recover + 1):  # exclusive range
+                alerts.extend(
+                    self._get_paginated_alerts(
+                        rule=rule,
+                        since=since + timedelta(days=day_delta),
+                        until=since + timedelta(days=day_delta + 1),
                     )
-                    break
-
-                # If the response contains data and contains the counts field, extract priority rules
-                if data.get("counts"):
-                    if data["counts"]["total"] == 0:
-                        break
-                    if data["counts"]["total"] > 1000:
-                        # TODO: replace by custom pagination to ingest more than 1000 alerts
-                        self.helper.connector_logger.warning(
-                            "More than 1000 alerts found for this rule, only the first 1000 will be returned.",
-                            {
-                                "rule_id": rule.rule_id,
-                                "rule_name": rule.rule_name,
-                            },
-                        )
-
-                    alerts_count = data["counts"]["total"]
-                    offset += data["counts"]["returned"]
-
-                    for alert in data["data"]:
-                        alerts.append(
-                            Alert(
-                                alert_id=alert["id"],
-                                alert_url=alert["url"]["portal"],
-                                alert_date=alert["log"]["triggered"],
-                                alert_title=alert["title"],
-                                alert_ai_insight=alert["ai_insights"]["comment"],
-                                alert_rf_rule=rule,
-                                alert_hits=alert["hits"],
-                            )
-                        )
-            except requests.exceptions.RequestException as e:
-                self.helper.connector_logger.error(
-                    "Exception occured when trying to reach RecordedFuture's API : ",
-                    {"error": str(e)},
                 )
+        else:
+            alerts = self._get_paginated_alerts(rule=rule, since=since)
 
         return alerts
 
