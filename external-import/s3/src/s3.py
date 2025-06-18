c8d9eccdf943d2a1df1e2ec6ccdda746529b79a2
Samuel Hassine <samuel.hassine@filigran.io>
2025-06-18 11:55:36 +0200

    [s3] Enhance mappings

--- external-import/s3/src/s3.py
+++ external-import/s3/src/s3.py
@@ -26,12 +26,12 @@ mapped_keys = [
     "x_zero_day",
     "x_notable_vuln",
     "x_name",
-]
-ignored_keys = [
-    "x_cvss_v2_vector",
     "x_cvss_v2",
+    "x_cvss_v2_vector",
     "x_cvss_v2_temporal_score",
     "x_cvss_v3_temporal_score",
+]
+ignored_keys = [
     "x_history",
     "x_first_seen_active",
     "x_acti_guid",
@@ -122,107 +122,8 @@ class S3Connector:
     def get_interval(self):
         return int(self.s3_interval) * 60
 
-    def cvss_score_to_severity(self, score):
-        if score == 0.0:
-            return "None"
-        elif 0.1 <= score <= 3.9:
-            return "Low"
-        elif 4.0 <= score <= 6.9:
-            return "Medium"
-        elif 7.0 <= score <= 8.9:
-            return "High"
-        elif 9.0 <= score <= 10.0:
-            return "Critical"
-
-    def parse_cvss3_vector(self, cvss_vector):
-        # Remove the initial "CVSS:3.1/" part
-        metrics_string = cvss_vector.split("/")[1:]
-        parsed_metrics = {}
-        for metric in metrics_string:
-            key, value = metric.split(":")
-            # Interpret each key according to CVSS3 standards
-            if key == "AV":
-                parsed_metrics["Attack Vector"] = {
-                    "N": "Network",
-                    "A": "Adjacent",
-                    "L": "Local",
-                    "P": "Physical",
-                }.get(value, "Unknown")
-
-            elif key == "AC":
-                parsed_metrics["Attack Complexity"] = {"L": "Low", "H": "High"}.get(
-                    value, "Unknown"
-                )
-
-            elif key == "PR":
-                parsed_metrics["Privileges Required"] = {
-                    "N": "None",
-                    "L": "Low",
-                    "H": "High",
-                }.get(value, "Unknown")
-
-            elif key == "UI":
-                parsed_metrics["User Interaction"] = {"N": "None", "R": "Required"}.get(
-                    value, "Unknown"
-                )
-
-            elif key == "S":
-                parsed_metrics["Scope"] = {"U": "Unchanged", "C": "Changed"}.get(
-                    value, "Unknown"
-                )
-
-            elif key == "C":
-                parsed_metrics["Confidentiality Impact"] = {
-                    "H": "High",
-                    "L": "Low",
-                    "N": "None",
-                }.get(value, "Unknown")
-
-            elif key == "I":
-                parsed_metrics["Integrity Impact"] = {
-                    "H": "High",
-                    "L": "Low",
-                    "N": "None",
-                }.get(value, "Unknown")
-
-            elif key == "A":
-                parsed_metrics["Availability Impact"] = {
-                    "H": "High",
-                    "L": "Low",
-                    "N": "None",
-                }.get(value, "Unknown")
-
-            elif key == "E":
-                parsed_metrics["Exploitability"] = {
-                    "X": "Not Defined",
-                    "H": "High",
-                    "F": "Functional",
-                    "P": "Proof-of-Concept",
-                    "U": "Unproven",
-                }.get(value, "Unknown")
-
-            elif key == "RL":
-                parsed_metrics["Remediation Level"] = {
-                    "X": "Not Defined",
-                    "U": "Unavailable",
-                    "W": "Workaround",
-                    "T": "Temporary Fix",
-                    "O": "Official Fix",
-                }.get(value, "Unknown")
-
-            elif key == "RC":
-                parsed_metrics["Report Confidence"] = {
-                    "X": "Not Defined",
-                    "C": "Confirmed",
-                    "R": "Reasonable",
-                    "U": "Unknown",
-                }.get(value, "Unknown")
-
-        return parsed_metrics
-
     def fix_bundle(self, bundle):
         included_entities = []
-        ignored_entities = []
         data = json.loads(bundle)
         new_bundle_objects = []
         new_bundle = []
@@ -235,11 +136,15 @@ class S3Connector:
                     and key not in mapped_keys
                     and key not in ignored_keys
                 ):
-                    self.helper.log_info("Found non-mapped custom key: " + key)
+                    self.helper.log_error("Found non-mapped custom key: " + key)
+
+            # Ensure author and marking
             if self.identity is not None and "created_by_ref" not in obj:
                 obj["created_by_ref"] = self.identity["standard_id"]
             if "object_marking_refs" not in obj:
                 obj["object_marking_refs"] = [self.s3_marking["id"]]
+
+            # TODO, TBD
             if "x_severity" in obj:
                 if obj["x_severity"] == "high":
                     obj["x_opencti_score"] = 90
@@ -247,25 +152,42 @@ class S3Connector:
                     obj["x_opencti_score"] = 60
                 elif obj["x_severity"] == "low":
                     obj["x_opencti_score"] = 30
+
+            # Aliases
             if "x_alias" in obj:
-                obj["x_opencti_aliases"] = obj["x_alias"]
+                obj["x_opencti_aliases"] = (
+                    obj["x_alias"]
+                    if isinstance(obj["x_alias"], list)
+                    else [obj["x_alias"]]
+                )
+
+            # CVSS 2
+            if "x_cvss_v2" in obj:
+                obj["x_opencti_cvss_v2_base_score"] = obj["x_cvss_v2"]
+            if "x_cvss_v2_temporal_score" in obj:
+                obj["x_opencti_cvss_v2_temporal_score"] = obj[
+                    "x_cvss_v2_temporal_score"
+                ]
+            if "x_cvss_v2_vector" in obj:
+                obj["x_opencti_cvss_v2_vector"] = obj["x_cvss_v2_vector"]
+
+            # CVSS3
             if "x_cvss_v3" in obj:
                 obj["x_opencti_cvss_base_score"] = obj["x_cvss_v3"]
-                obj["x_opencti_cvss_base_severity"] = self.cvss_score_to_severity(
-                    obj["x_cvss_v3"]
-                )
+            if "x_cvss_v3_temporal_score" in obj:
+                obj["x_opencti_cvss_temporal_score"] = obj["x_cvss_v3_temporal_score"]
             if "x_cvss_v3_vector" in obj:
-                parsed_metrics = self.parse_cvss3_vector(obj["x_cvss_v3_vector"])
-                obj["x_opencti_cvss_attack_vector"] = parsed_metrics["Attack Vector"]
-                obj["x_opencti_cvss_integrity_impact"] = parsed_metrics[
-                    "Integrity Impact"
-                ]
-                obj["x_opencti_cvss_availability_impact"] = parsed_metrics[
-                    "Availability Impact"
-                ]
-                obj["x_opencti_cvss_confidentiality_impact"] = parsed_metrics[
-                    "Confidentiality Impact"
-                ]
+                obj["x_opencti_cvss_vector"] = obj["x_cvss_v3_vector"]
+
+            # CWE
+            if "x_cwe" in obj:
+                obj["x_opencti_cwe"] = [obj["x_cwe"]]
+
+            # Ad-hoc desc
+            if "x_description" in obj:
+                obj["x_opencti_description"] = obj["x_description"]
+
+            # Note
             if "x_title" in obj and "x_analysis" in obj:
                 note = stix2.Note(
                     id=Note.generate_id(obj["created"], obj["x_analysis"]),
@@ -281,6 +203,8 @@ class S3Connector:
                     ),
                 )
                 new_bundle_objects.append(note)
+
+            # Labels
             if "x_wormable" in obj:
                 if "labels" in obj:
                     obj["labels"].append("wormable")
@@ -296,20 +220,15 @@ class S3Connector:
                     obj["labels"].append("notable-vuln")
                 else:
                     obj["labels"] = ["notable-vuln"]
+
+            # Software
             if obj["type"] == "software":
                 obj["name"] = obj["x_product"] + " " + obj["version"]
 
-            if "relationship_type" in obj and (
-                obj["relationship_type"] == "technology"
-                or obj["relationship_type"] == "technology-to"
-                or obj["relationship_type"] == "remediated-by"
-            ):
-                continue
-            if obj["type"] == "infrastructure" and obj["name"].startswith("cpe:"):
-                ignored_entities.append(obj["id"])
-                continue
+            # Relationships "has"
             if (
                 obj["type"] == "relationship"
+                and obj["relationship_type"] == "related-to"
                 and obj["source_ref"].startswith("vulnerability")
                 and obj["target_ref"].startswith("software")
             ):
@@ -317,11 +236,28 @@ class S3Connector:
                 original_source_ref = obj["source_ref"]
                 obj["source_ref"] = obj["target_ref"]
                 obj["target_ref"] = original_source_ref
+
+            # Relationship "remediates"
+            if (
+                obj["type"] == "relationship"
+                and obj["relationship_type"] == "remediated-by"
+                and obj["source_ref"].startswith("vulnerability")
+                and obj["target_ref"].startswith("software")
+            ):
+                obj["relationship_type"] = "remediates"
+                original_source_ref = obj["source_ref"]
+                obj["source_ref"] = obj["target_ref"]
+                obj["target_ref"] = original_source_ref
+
+            # Ignored technology / technology-to
+            # TODO: TBD
             if obj["type"] == "relationship" and (
-                obj["source_ref"] in ignored_entities
-                or obj["target_ref"] in ignored_entities
+                obj["relationship_type"] == "technology"
+                or obj["relationship_type"] == "technology-to"
             ):
                 continue
+
+            # Cleanup orphan relationships
             if (
                 obj["type"] == "relationship"
                 and obj["source_ref"] not in included_entities
