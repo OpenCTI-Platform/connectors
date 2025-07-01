dfa8bb77e6640cbeeb4e5733143894dcb309e402
Elodie Gameiro <elodie.gameiro@filigran.io>
2025-07-01 17:27:32 +0200

    [ransomware] First clean after review

--- external-import/ransomwarelive/src/ransomwarelive/config.py
+++ external-import/ransomwarelive/src/ransomwarelive/config.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 
-from lib.base_connector_config import (
+from base_connector_config import (
     BaseConnectorSettings,
     ConnectorConfig,
     ListFromString,
@@ -14,17 +14,31 @@ _FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 class _ConnectorConfig(ConnectorConfig):
-    name: str = Field(default="Ransomware Connector")
-    scope: ListFromString = Field(default=["stix2"])
-    log_level: LogLevelType = Field(default=LogLevelType.ERROR)
-    duration_period: datetime.timedelta = Field(default=datetime.timedelta(minutes=10))
+    name: str = Field(
+        description="Name of the connector",
+        min_length=1,
+    )
+    scope: ListFromString = Field(
+        description="The scope of the connector",
+        min_length=1,
+    )
+    log_level: LogLevelType = Field(
+        default=LogLevelType.ERROR, description="Determines the verbosity of the logs"
+    )
+    duration_period: datetime.timedelta = Field(
+        default=datetime.timedelta(minutes=10),
+        description="Duration between two scheduled runs of the connector (ISO 8601 format)",
+    )
 
 
 class _RansomwareConfig(BaseModel):
-    pull_history: bool = Field(default=False)
-    history_start_year: int = Field(default=2023)
-    create_threat_actor: bool = Field(default=False)
-    interval: str = Field(default=None)  # Warning Deprecated
+    pull_history: bool = Field(
+        default=False, description="Whether to pull historic data"
+    )
+    history_start_year: int = Field(default=2023, description="The year to start from")
+    create_threat_actor: bool = Field(
+        default=False, description="Whether to create a Threat Actor object"
+    )
 
 
 class ConnectorSettings(BaseConnectorSettings):
