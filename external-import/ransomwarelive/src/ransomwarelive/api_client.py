cf06d01e7b9d86115615978791f03f0aa24647f5
Elodie Gameiro <elodie.gameiro@filigran.io>
2025-07-02 10:27:54 +0200

    [ransomware] Clean after review

---
+++ external-import/ransomwarelive/src/ransomwarelive/api_client.py
@@ -0,0 +1,48 @@
+import requests
+
+
+class RansomwareAPIError(Exception):
+    """Custom wrapper for exceptions raised in RansomwareAPIClient"""
+
+
+class RansomwareAPIClient:
+    def __init__(
+        self,
+    ):
+        """
+        Initialize the client with necessary configurations
+        """
+        self.api_base_url = "https://api.ransomware.live/v2/"
+
+    def _send_request(self, url: str):
+        """
+        Send a request to SOCRansomware API.
+        :param method: Request HTTP method
+        :param url: Request URL
+        :param kwargs: Any arguments valid for session.request() method
+        :return: Any data returned by the API
+        """
+        try:
+            response = requests.get(
+                url, headers={"accept": "application/json", "User-Agent": "OpenCTI"}
+            )
+            response.raise_for_status()
+
+            if response.content:
+                return response.json()
+        except requests.RequestException as err:
+            raise RansomwareAPIError(
+                f"Error while fetching Ransomware API: {err}",
+                {"url": f"GET {url}", "error": err},
+            ) from err
+
+    def get_feed(self, path: str) -> list[dict]:
+        """
+        Get feed for given path.
+        :param path: path to get feed from.
+        :return: Collection's feed items
+        """
+        url = f"{self.api_base_url}{path}"
+        data = self._send_request(url)
+
+        return data
