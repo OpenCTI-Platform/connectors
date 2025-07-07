a700f887bacdbc51bf382f81110a93f8d6bd0b7b
Mohamed Merimi <merimimohamed@hotmail.com>
2025-07-07 12:47:07 +0200

    fix with isort

--- external-import/thehive/src/thehive.py
+++ external-import/thehive/src/thehive.py
@@ -4,17 +4,10 @@ import time
 import traceback
 import uuid
 from datetime import datetime
-import json
-
 
 import stix2
 import yaml
-from constants import DEFAULT_DATETIME, DEFAULT_UTC_DATETIME, PAP_MAPPINGS, TLP_MAPPINGS
 from dateutil.parser import parse
-from hive_observable_transform import (
-    HiveObservableTransform,
-    UnsupportedIndicatorTypeError,
-)
 from pycti import (
     CaseIncident,
     CustomObjectCaseIncident,
@@ -27,11 +20,17 @@ from pycti import (
     get_config_variable,
 )
 from thehive4py import TheHiveApi
-from thehive4py.query import Gt,Eq
+from thehive4py.query import Gt
 from thehive4py.query.page import Paginate
 from thehive4py.query.sort import Asc
 from thehive4py.types.alert import OutputAlert
 from thehive4py.types.case import OutputCase
+
+from constants import DEFAULT_DATETIME, DEFAULT_UTC_DATETIME, PAP_MAPPINGS, TLP_MAPPINGS
+from hive_observable_transform import (
+    HiveObservableTransform,
+    UnsupportedIndicatorTypeError,
+)
 
 from utils import format_datetime  # isort: skip
 
