e6f91f24653365c757a59d949e4f2bb6920f57d8
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-07-10 15:43:42 +0200

    [feat] split observables and vulnerability enrichment use cases

--- internal-enrichment/recordedfuture-enrichment/src/rflib/rf_connector.py
+++ internal-enrichment/recordedfuture-enrichment/src/rflib/rf_connector.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import yaml
 from pycti import OpenCTIConnectorHelper, get_config_variable
-from rflib import APP_VERSION, EnrichedIndicator, RFClient
+from rflib import APP_VERSION, EnrichedIndicator, EnrichedVulnerability, RFClient
 
 
 class RFEnrichmentConnector:
@@ -47,18 +47,18 @@ class RFEnrichmentConnector:
         self.work_id = None
 
     @staticmethod
-    def map_octi_type_to_rf_type(entity_type: str) -> str:
+    def to_rf_type(observable_type: str) -> str:
         """
-        Translates an OCTI entity type to its RF equivalent
+        Translates an OCTI observable type to its RF equivalent
 
         Args:
-            entity_type (str): An OCTI entity type
+            observable_type (str): An OCTI observable type
 
         Returns:
             Recorded Future object type as string
         """
 
-        match entity_type:
+        match observable_type:
             case "IPv4-Addr" | "IPv6-Addr":
                 return "ip"
             case "Domain-Name":
@@ -67,6 +67,8 @@ class RFEnrichmentConnector:
                 return "url"
             case "StixFile":
                 return "hash"
+            case _:
+                return None
 
     @staticmethod
     def generate_pattern(ioc, data_type, algorithm=None):
@@ -149,19 +151,10 @@ class RFEnrichmentConnector:
         in the OpenCTI platform
         """
 
-        observable = data["enrichment_entity"]
-        # Extract IOC from entity data
-        observable_value = observable["observable_value"]
-        observable_id = observable["standard_id"]
-        entity_type = observable["entity_type"]
-
-        friendly_name = f"Enrich: {observable_value}"
-        self.work_id = self.helper.api.work.initiate_work(
-            self.helper.connect_id, friendly_name
-        )
+        enrichment_entity = data["enrichment_entity"]
 
         tlp = "TLP:CLEAR"
-        for marking_definition in observable["objectMarking"]:
+        for marking_definition in enrichment_entity["objectMarking"]:
             if marking_definition["definition_type"] == "TLP":
                 tlp = marking_definition["definition"]
 
@@ -170,23 +163,29 @@ class RFEnrichmentConnector:
             self.helper.connector_logger.warning(msg)
             return msg
 
-        # Convert to RF types
-        rf_type = self.map_octi_type_to_rf_type(entity_type)
-        if rf_type is None:
+        entity_type = enrichment_entity["entity_type"]
+
+        enriched_object = None
+        if entity_type == "Vulnerability":
+            enriched_object = self.enrich_vulnerability(data=enrichment_entity)
+        elif rf_type := self.to_rf_type(entity_type):
+            enriched_object = self.enrich_observable(rf_type, data=enrichment_entity)
+        else:
             message = f"Recorded Future enrichment does not support type {entity_type}"
             self.helper.connector_logger.error(message)
             # Returned value should always be a string but a string is never displayed as an error in the UI.
             # But a list is displayed as en arror because it raises a BAD_USER_INPUT on OpenCTI... 🥲
             return [message]
 
-        enriched_object = self.enrich_observable(rf_type, data=observable)
-        if isinstance(enriched_object, EnrichedIndicator):
-            self.helper.connector_logger.info("Sending bundle...")
+        if isinstance(enriched_object, (EnrichedIndicator, EnrichedVulnerability)):
             bundle = enriched_object.to_json_bundle()
-            bundles_sent = self.helper.send_stix2_bundle(bundle)
-            return f"Sent {len(bundles_sent)} stix bundle(s) for worker import"
         else:
             return "No Stix bundle(s) imported."
+
+        self.helper.connector_logger.info("Sending bundle...")
+        bundles_sent = self.helper.send_stix2_bundle(bundle)
+
+        return f"Sent {len(bundles_sent)} stix bundle(s) for worker import"
 
     def start(self):
         """Start the main loop"""
