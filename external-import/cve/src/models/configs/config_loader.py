23cbee99cf229c83e5c537cded8f5b4e0d57c479
Helene Nguyen <helene.nguyen@filigran.io>
2025-07-22 16:52:42 +0200

    [connectors] Update shared + add config loader for CVE

---
+++ external-import/cve/src/models/configs/config_loader.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import (
+    BaseSettings,
+    DotEnvSettingsSource,
+    EnvSettingsSource,
+    PydanticBaseSettingsSource,
+    YamlConfigSettingsSource,
+)
+from src.models.configs import (
+    ConfigBaseSettings,
+    _ConfigLoaderConnector,
+    _ConfigLoaderCVE,
+    _ConfigLoaderOCTI,
+)
+
+
+class ConfigLoader(ConfigBaseSettings):
+    """Interface for loading global configuration settings."""
+
+    opencti: _ConfigLoaderOCTI = Field(
+        default_factory=_ConfigLoaderOCTI,
+        description="OpenCTI configurations.",
+    )
+    connector: _ConfigLoaderConnector = Field(
+        default_factory=lambda: _ConfigLoaderConnector(
+            id="cve--e0c380ad-6665-4f2e-8558-5d2610e5abcf",
+            name="Common Vulnerabilities and Exposures",
+            scope="cve",
+        ),
+        description="Connector configurations.",
+    )
+    cve: _ConfigLoaderCVE = Field(
+        default_factory=_ConfigLoaderCVE,
+        description="CVE configurations.",
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource]:
+        env_path = Path(__file__).parents[2] / ".env"
+        yaml_path = Path(__file__).parents[2] / "config.yml"
+
+        if env_path.exists():
+            return (
+                DotEnvSettingsSource(
+                    settings_cls,
+                    env_file=env_path,
+                    env_ignore_empty=True,
+                    env_file_encoding="utf-8",
+                ),
+            )
+        elif yaml_path.exists():
+            return (
+                YamlConfigSettingsSource(
+                    settings_cls,
+                    yaml_file=yaml_path,
+                    yaml_file_encoding="utf-8",
+                ),
+            )
+        else:
+            return (
+                EnvSettingsSource(
+                    settings_cls,
+                    env_ignore_empty=True,
+                ),
+            )
