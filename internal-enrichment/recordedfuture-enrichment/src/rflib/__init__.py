16c434c3829d0d1ce555b2b0dfcf7f87feb5532b
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-07-25 18:06:34 +0200

    [feat] add config runtime validation
    - install pydantic-settings
    - breaking change in config vars (dev mode only)

--- internal-enrichment/recordedfuture-enrichment/src/rflib/__init__.py
+++ internal-enrichment/recordedfuture-enrichment/src/rflib/__init__.py
@@ -11,31 +11,12 @@
 """
 
 from ._version import __version__ as APP_VERSION
-from .rf_client import RFClient, RFClientError
-from .rf_to_stix2 import (
-    TTP,
-    URL,
-    ConversionError,
-    Domain,
-    EnrichedIndicator,
-    EnrichedVulnerability,
-    FileHash,
-    Identity,
-    IPAddress,
-    Malware,
-)
+
+from .config_loader import ConnectorConfig, ConfigRetrievalError
+from .rf_connector import RFEnrichmentConnector
 
 __all__ = [
-    "RFClient",
-    "RFClientError",
-    "TTP",
-    "URL",
-    "ConversionError",
-    "Domain",
-    "EnrichedIndicator",
-    "EnrichedVulnerability",
-    "FileHash",
-    "Identity",
-    "IPAddress",
-    "Malware",
+    "ConnectorConfig",
+    "ConfigRetrievalError",
+    "RFEnrichmentConnector",
 ]
