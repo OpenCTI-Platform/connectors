649b0b851f56b2f13a3d3460c20be9353450bccc
Helene Nguyen <helene.nguyen@filigran.io>
2025-07-25 10:28:56 +0200

    [connectors] Parallelize generator + update manifest

--- shared/tools/composer/manifest_generator.sh
+++ shared/tools/composer/manifest_generator.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-set -e  # exit on error
+set -euo pipefail  # exit on error
 
 CONNECTOR_INFOS_DIRECTORY="__infos__"
 CONNECTOR_INFOS_FILENAME="connector_infos.json"
 MANAGER_SUPPORTED='"manager_supported": *true'
+VENV_NAME=".temp_venv"
 
 activate_venv() {
     # Method to activate isolate venv
@@ -13,15 +14,13 @@ activate_venv() {
     requirements_file=$(find "$1" -name "requirements.txt")
 
     # Create isolated virtual environment in connector path
-    venv_name=".temp_venv"
-    echo '> Creating isolated virtual environment...'
-    python -m venv "$1/$venv_name"
+    python -m venv "$1/$VENV_NAME"
 
     # Activate virtual environment according to OS
-    if [ -f "$1/$venv_name/bin/activate" ]; then
-      source "$1/$venv_name/bin/activate"  # Linux/MacOS
-    elif [ -f "$1/$venv_name/Scripts/activate" ]; then
-      source "$1/$venv_name/Scripts/activate"  # Windows
+    if [ -f "$1/$VENV_NAME/bin/activate" ]; then
+      source "$1/$VENV_NAME/bin/activate"  # Linux/MacOS
+    elif [ -f "$1/$VENV_NAME/Scripts/activate" ]; then
+      source "$1/$VENV_NAME/Scripts/activate"  # Windows
     fi
 
     echo '> Installing requirements in: ' "$1"
@@ -33,9 +32,9 @@ activate_venv() {
     venv_exists=$(find "$1" -name ".temp_venv")
 
     if [ -d "$venv_exists" ]; then
-      echo "✅- Requirements installed !"
+      echo "✅- Requirements installed for: " "$1"
     else
-      echo "❌- Requirements not installed..."
+      echo "❌- Requirements not installed for: " "$1"
     fi
 }
 
@@ -55,22 +54,18 @@ for connector_directory_path in $connector_directories_path
 do
   if [ -d "$connector_directory_path" ]; then
     if grep -q "$MANAGER_SUPPORTED" "$connector_directory_path/$CONNECTOR_INFOS_DIRECTORY/$CONNECTOR_INFOS_FILENAME"; then
-    # Activate isolated venv
-    activate_venv "$connector_directory_path"
-    # create tmp_schema_generator
-    generator_path=$(find . -name "generator.py.sample")
-    cp "$generator_path" "$connector_directory_path""/generator_tmp.py"
-    # apply generator
-    python "$connector_directory_path""/generator_tmp.py"
-
-    # Clean up
-    # Remove tmp_schema_generator
-    rm "$connector_directory_path/generator_tmp.py"
-
-    deactivate_venv "$connector_directory_path/$venv_name"
+      (
+        activate_venv "$connector_directory_path"
+        generator_path=$(find . -name "generator.py.sample")
+        cp "$generator_path" "$connector_directory_path/generator_tmp.py"
+        python "$connector_directory_path/generator_tmp.py"
+        rm "$connector_directory_path/generator_tmp.py"
+        deactivate_venv "$connector_directory_path/$VENV_NAME"
+      ) &
     fi
   fi
 done
+wait
 
 generate_manifest=$(find . -name "generate_manifest.py")
 echo -e "\nGenerating manifest file..."
