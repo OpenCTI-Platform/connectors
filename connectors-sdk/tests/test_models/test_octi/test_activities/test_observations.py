ab71b2f5da666829928242e73c5c9abb8655c98c
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-08-01 17:48:54 +0200

    [feat] add Software model and unit tests

--- connectors-sdk/tests/test_models/test_octi/test_activities/test_observations.py
+++ connectors-sdk/tests/test_models/test_octi/test_activities/test_observations.py
@@ -9,10 +9,12 @@ from connectors_sdk.models.octi.activities.observations import (
     Indicator,
     IPV4Address,
     Observable,
+    Software,
 )
 from pydantic import ValidationError
-from stix2.v21 import Indicator as Stix2Indicator
-from stix2.v21 import IPv4Address as Stix2IPv4Address
+from stix2.v21 import Indicator as stix2_Indicator
+from stix2.v21 import IPv4Address as stix2_IPv4Address
+from stix2.v21 import Software as stix2_Software
 
 ### OBSERVABLE BASE TYPE
 
@@ -181,3 +183,46 @@ def test_ip_v4_address_to_stix2_object_returns_valid_stix_object():
     stix2_obj = ipv4_address.to_stix2_object()
     # Then: A valid STIX2.1 IPV4Address is returned
     assert isinstance(stix2_obj, Stix2IPv4Address)
+
+
+### Software
+
+
+def test_software_class_should_not_accept_invalid_input():
+    """Test that Software class should not accept invalid input."""
+    # Given: An invalid input data for Software
+    input_data = {
+        "name": "Test software",
+        "invalid_key": "invalid_value",
+    }
+    # When validating the software
+    # Then: It should raise a ValidationError with the expected error field
+    with pytest.raises(ValidationError) as error:
+        Software.model_validate(input_data)
+        assert error.value.errors()[0]["loc"] == ("invalid_key",)
+
+
+def test_software_address_to_stix2_object_returns_valid_stix_object(
+    fake_valid_organization_author,
+    fake_valid_tlp_markings,
+    fake_valid_external_references,
+):
+    """Test that Software to_stix2_object method returns a valid STIX2.1 Software."""
+    # Given: A valid Software instance
+    ipv4_address = Software(
+        name="Test Software",
+        description="Test software description",
+        labels=["label_1", "label_2"],
+        version="1.0.0",
+        vendor="Test vendor",
+        swid="Test SWID",
+        cpe="cpe:/a:test:software:1.0.0",
+        languages=["python"],
+        author=fake_valid_organization_author,
+        markings=fake_valid_tlp_markings,
+        external_references=fake_valid_external_references,
+    )
+    # When: calling to_stix2_object method
+    stix2_obj = ipv4_address.to_stix2_object()
+    # Then: A valid STIX2.1 Software is returned
+    assert isinstance(stix2_obj, stix2_Software)
