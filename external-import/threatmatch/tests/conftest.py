f969a0daedeb08730c0e74c8f1cff2d9bcc6506d
Philippe Damoune <philippe.damoune@filigran.io>
2025-08-05 17:37:43 +0200

    refactor(ThreatMatch): Add pydantic settings config

--- external-import/threatmatch/tests/conftest.py
+++ external-import/threatmatch/tests/conftest.py
@@ -2,3 +2,8 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
+from threatmatch.config import ConnectorSettings
+
+# Ensure local config is not loaded
+ConnectorSettings.model_config["yaml_file"] = ""
