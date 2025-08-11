e7937d76a81bc7b3787a519c3d5e1fc408497fc1
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-08-11 19:19:14 +0200

    [fix] update ransomwarelive connector

--- external-import/ransomwarelive/src/main.py
+++ external-import/ransomwarelive/src/main.py
@@ -2,12 +2,12 @@ import sys
 import traceback
 
 from pycti import OpenCTIConnectorHelper
-from ransomwarelive.config import ConnectorSettings
+from ransomwarelive.config import ConnectorSettings as ConfigLoader
 from ransomwarelive.ransom_conn import RansomwareAPIConnector
 
 if __name__ == "__main__":
     try:
-        config = ConnectorSettings()
+        config = ConfigLoader()
         helper = OpenCTIConnectorHelper(config=config.model_dump_pycti())
         connector = RansomwareAPIConnector(helper=helper, config=config)
         connector.run()
