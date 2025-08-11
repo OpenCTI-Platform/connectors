09a40f9a03beb78963e8cb0dc3f3d4b26f36348a
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-08-11 20:08:39 +0200

    [feat] add ConfigLoader

---
+++ internal-enrichment/google-dns/src/connector/config_loader.py
@@ -0,0 +1,249 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+from typing import Annotated, Literal
+
+import __main__
+from pydantic import (
+    BaseModel,
+    BeforeValidator,
+    ConfigDict,
+    Field,
+    HttpUrl,
+    PlainSerializer,
+)
+from pydantic_core.core_schema import SerializationInfo
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+
+# TODO: type optional fields as Optional[type]
+
+# Get the path of the __main__ module file (the entry point of the connector)
+_MAIN_PATH = os.path.dirname(os.path.abspath(__main__.__file__))
+
+"""
+All the variables that have default values will override configuration from the OpenCTI helper.
+
+All the variables of this classes are customizable through:
+    - config.yml 
+    - .env
+    - environment variables.
+
+If a variable is set in 2 different places, the first one will be used in this order:
+    1. YAML file
+    2. .env file
+    3. Environment variables
+    4. Default value
+    
+WARNING:
+    The Environment variables in the .env or global environment must be set in the following format:
+    OPENCTI_<variable>
+    CONNECTOR_<variable>
+    
+    the split is made on the first occurrence of the "_" character.
+"""
+
+
+class ConfigRetrievalError(Exception):
+    """Known errors wrapper for config loaders."""
+
+
+def comma_separated_list_validator(value: str | list[str]) -> list[str]:
+    """
+    Convert comma-separated string into a list of values.
+
+    Example:
+        > values = pycti_list_validator("e1,e2,e3")
+        > print(values) # [ "e1", "e2", "e3" ]
+    """
+    if isinstance(value, str):
+        return [string.strip() for string in value.split(",")]
+    return value
+
+
+def pycti_list_serializer(value: list[str], info: SerializationInfo) -> str | list[str]:
+    """
+    Serialize list of values as comma-separated string.
+
+    Example:
+        > serialized_values = pycti_list_serializer([ "e1", "e2", "e3" ])
+        > print(serialized_values) # "e1,e2,e3"
+    """
+    if isinstance(value, list) and info.context and info.context.get("mode") == "pycti":
+        return ",".join(value)
+    return value
+
+
+ListFromString = Annotated[
+    list[str],
+    BeforeValidator(comma_separated_list_validator),
+    PlainSerializer(pycti_list_serializer, when_used="json"),
+]
+
+
+class ConfigBaseModel(BaseModel):
+    """
+    Base class for frozen config models, i.e. not alter-able after `model_post_init()`.
+    """
+
+    model_config = ConfigDict(
+        extra="ignore",
+        validate_default=True,
+        frozen=True,
+    )
+
+
+class OpenCTIConfig(ConfigBaseModel):
+    """
+    Define config specific to OpenCTI.
+    """
+
+    url: HttpUrl = Field(description="The base URL of the OpenCTI instance.")
+    token: str = Field(description="The API token to connect to OpenCTI.")
+    json_logging: bool = Field(
+        description="Whether to format logs as JSON or not.",
+        default=True,
+    )
+    ssl_verify: bool = Field(
+        description="Whether to check SSL certificate or not.",
+        default=False,
+    )
+
+
+class ConnectorConfig(ConfigBaseModel):
+    """
+    Define config specific to this type of connector, e.g. an `external-import`.
+    """
+
+    id: str = Field(description="A UUID v4 to identify the connector in OpenCTI.")
+    name: str = Field(description="The name of the connector.")
+    type: Literal["INTERNAL_ENRICHMENT"] = "INTERNAL_ENRICHMENT"
+    scope: ListFromString = Field(
+        description="The scope of the connector, e.g. 'googledns'.",
+        default=["googledns"],
+    )
+    duration_period: timedelta = Field(
+        description="The period of time to await between two runs of the connector.",
+        default=timedelta(hours=24),
+    )
+
+    log_level: Literal[
+        "debug",
+        "info",
+        "warn",
+        "error",
+    ] = Field(
+        description="The minimum level of logs to display.",
+        default="error",
+    )
+    expose_metrics: bool = Field(
+        description="Whether to expose metrics or not.",
+        default=False,
+    )
+    metrics_port: int = Field(
+        description="The port to expose metrics.",
+        default=9095,
+    )
+    only_contextual: bool = Field(
+        description="Whether to expose metrics or not.",
+        default=False,
+    )
+    run_and_terminate: bool = Field(
+        description="Connector run-and-terminate flag.",
+        default=False,
+    )
+    validate_before_import: bool = Field(
+        description="Whether to validate data before import or not.",
+        default=False,
+    )
+    queue_protocol: str = Field(
+        description="The queue protocol to use.",
+        default="amqp",
+    )
+    queue_threshold: int = Field(
+        description="Connector queue max size in Mbytes. Default to pycti value.",
+        default=500,
+    )
+    send_to_queue: bool = Field(
+        description="Connector send-to-queue flag. Default to True.",
+        default=True,
+    )
+    send_to_directory: bool = Field(
+        description="Connector send-to-directory flag.",
+        default=False,
+    )
+    send_to_directory_path: str | None = Field(
+        description="Connector send-to-directory path.",
+        default=None,
+    )
+    send_to_directory_retention: int = Field(
+        description="Connector send-to-directory retention.",
+        default=7,
+    )
+
+
+class ConfigLoader(BaseSettings):
+    """
+    Define a complete config for a connector with:
+        - opencti: the config specific to OpenCTI
+        - connector: the config specific to the `external-import` connectors
+    """
+
+    opencti: OpenCTIConfig
+    connector: ConnectorConfig
+
+    # Setup model config and env vars parsing
+    model_config = SettingsConfigDict(
+        extra="ignore",
+        frozen=True,
+        env_nested_delimiter="_",
+        env_nested_max_split=1,
+        enable_decoding=False,
+        yaml_file=f"{_MAIN_PATH}/config.yml",
+        env_file=f"{_MAIN_PATH}/../.env",
+    )
+
+    def __init__(self) -> None:
+        """
+        Wrap BaseConnectorConfig initialization to raise custom exception in case of error.
+        """
+        try:
+            super().__init__()
+        except Exception as e:
+            raise ConfigRetrievalError("Invalid connector configuration.", e) from e
+
+    def model_dump_pycti(self) -> dict:
+        """
+        Convert model into a valid dict for `pycti.OpenCTIConnectorHelper`.
+        """
+        return self.model_dump(mode="json", context={"mode": "pycti"})
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        """
+        Customise the sources of settings for the connector.
+
+        This method is called by the Pydantic BaseSettings class to determine the order of sources
+
+        The configuration come in this order either from:
+            1. YAML file
+            2. .env file
+            3. Environment variables
+            4. Default values
+        """
+        if Path(settings_cls.model_config["yaml_file"] or "").is_file():  # type: ignore
+            return (YamlConfigSettingsSource(settings_cls),)
+        if Path(settings_cls.model_config["env_file"] or "").is_file():  # type: ignore
+            return (dotenv_settings,)
+        return (env_settings,)
