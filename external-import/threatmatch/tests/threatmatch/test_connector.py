530ad1a5bef211c22c5bc44db0f0eefb45be5897
Philippe Damoune <philippe.damoune@filigran.io>
2025-08-11 08:45:52 +0200

    [ThreatMatch] Cleanup, tests and restructuration (https://github.com/OpenCTI-Platform/connectors/issues/3500)

--- external-import/threatmatch/tests/threatmatch/test_connector.py
+++ external-import/threatmatch/tests/threatmatch/test_connector.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest.mock import call
 
 import freezegun
@@ -14,7 +15,7 @@ def test_connector_run(mocked_helper: MockerFixture) -> None:
     connector.run()
     assert connector.helper.connector_logger.info.call_count == 1
     connector.helper.connector_logger.info.assert_has_calls(
-        [call("Fetching ThreatMatch...")]
+        [call("Connector starting...")]
     )
 
     assert mocked_helper.schedule_process.call_count == 1
@@ -31,12 +32,41 @@ def test_connector_process(mocked_helper: MockerFixture) -> None:
         "HTTPSConnectionPool(host='test-threatmatch-url', port=443): Max retries exceeded with url: /api/developers-platform/token"
         in connector.helper.connector_logger.error.call_args[0][0]
     )
-    assert connector.helper.connector_logger.info.call_count == 4
+    assert connector.helper.connector_logger.info.call_count == 3
     connector.helper.connector_logger.info.assert_has_calls(
         [
-            call("Connector has never run"),
-            call("Connector will run!"),
-            call("Connector successfully run, storing last_run as 1744903440"),
-            call("Last_run stored, next run in: 1440.0 minutes"),
+            call("Connector last run: never"),
+            call("Connector successfully ran"),
         ]
     )
+
+
+@freezegun.freeze_time("2025-04-17T15:24:00Z")
+@pytest.mark.usefixtures("mock_config", "mocked_helper")
+def test_connector_process_data_last_run(
+    mocker: MockerFixture, mocked_helper: MockerFixture
+) -> None:
+    now = datetime.fromisoformat("2025-04-17T15:24:00Z")
+    yesterday = datetime.fromisoformat("2025-04-16T15:24:00Z")
+
+    # Only test _process_data method
+    collect_intelligence = mocker.patch.object(Connector, "_collect_intelligence")
+
+    connector = Connector(helper=mocked_helper, config=ConnectorSettings())
+
+    # 1 No last_run in state
+    connector._process_data()
+    collect_intelligence.assert_called_once_with(None, "work-id")
+    mocked_helper.set_state.assert_called_once_with({"last_run": now.isoformat()})
+
+    # 2 last_run in state as timestamp (retro compatibility)
+    mocked_helper.get_state.return_value = {"last_run": yesterday.timestamp()}
+    connector._process_data()
+    collect_intelligence.assert_called_with(yesterday, "work-id")
+    mocked_helper.set_state.assert_called_with({"last_run": now.isoformat()})
+
+    # 3 last_run in state as ISO format
+    mocked_helper.get_state.return_value = {"last_run": yesterday.isoformat()}
+    connector._process_data()
+    collect_intelligence.assert_called_with(yesterday, "work-id")
+    mocked_helper.set_state.assert_called_with({"last_run": now.isoformat()})
