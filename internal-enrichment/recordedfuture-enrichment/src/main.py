83768e8c0c935487c4e3333ffcc4ff36ca2a3c43
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-08-20 15:59:07 +0200

    [refacto] add observable enrichment use case

--- internal-enrichment/recordedfuture-enrichment/src/main.py
+++ internal-enrichment/recordedfuture-enrichment/src/main.py
@@ -1,4 +1,13 @@
-"""Connector to enrich IOCs and Vulnerabilities with Recorded Future data"""
+"""
+############################## TERMS OF USE ####################################
+# The following code is provided for demonstration purposes only, and should   #
+# not be used without independent verification. Recorded Future makes no       #
+# representations or warranties, express, implied, statutory, or otherwise,    #
+# regarding this code, and provides it strictly "as-is".                       #
+# Recorded Future shall not be liable for, and you assume all risk of          #
+# using the foregoing.                                                         #
+################################################################################
+"""
 
 import traceback
 
