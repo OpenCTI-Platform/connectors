bc5a0d2a0033798d2d7f3eeaccd799a16f2d0d50
Helene Nguyen <helene.nguyen@filigran.io>
2025-08-23 06:51:29 +0200

    [fix] Flashpoit tests - CI comment condition

--- shared/tools/composer/generate_connectors_config_json_schemas.sh
+++ shared/tools/composer/generate_connectors_config_json_schemas.sh
@@ -69,23 +69,23 @@ for connector_directory_path in $connector_directories_path
 do
   if [ -d "$connector_directory_path" ]; then
     # Only generate schema for directory that changed
-    directory_has_changed=$(git diff "$connector_directory_path")
-    if [ -z "$directory_has_changed" ] ; then
-      echo "Nothing has changed."
-    else
-      echo "> Looking for a config loader in " "$connector_directory_path"
-      requirements_file=$(find_requirements_txt "$connector_directory_path")
-      echo "Found requirements.txt: " "$requirements_file"
-      if [ -f "$requirements_file" ] && grep -q "pydantic-settings" "$requirements_file"; then
-        (
-          activate_venv "$connector_directory_path"
-          generator_path=$(find . -name "generate_connectors_config_json_schemas.py.sample")
-          cp "$generator_path" "$connector_directory_path/generate_connectors_config_json_schemas_tmp.py"
-          python "$connector_directory_path/generate_connectors_config_json_schemas_tmp.py"
-          rm "$connector_directory_path/generate_connectors_config_json_schemas_tmp.py"
-          deactivate_venv "$connector_directory_path/$VENV_NAME"
-        ) &
-      fi
+#    directory_has_changed=$(git diff "$connector_directory_path")
+#    if [ -z "$directory_has_changed" ] ; then
+#      echo "Nothing has changed."
+#    else
+    echo "> Looking for a config loader in " "$connector_directory_path"
+    requirements_file=$(find_requirements_txt "$connector_directory_path")
+    echo "Found requirements.txt: " "$requirements_file"
+    if [ -f "$requirements_file" ] && grep -q "pydantic-settings" "$requirements_file"; then
+      (
+        activate_venv "$connector_directory_path"
+        generator_path=$(find . -name "generate_connectors_config_json_schemas.py.sample")
+        cp "$generator_path" "$connector_directory_path/generate_connectors_config_json_schemas_tmp.py"
+        python "$connector_directory_path/generate_connectors_config_json_schemas_tmp.py"
+        rm "$connector_directory_path/generate_connectors_config_json_schemas_tmp.py"
+        deactivate_venv "$connector_directory_path/$VENV_NAME"
+      ) &
+#      fi
     fi
   fi
 done
