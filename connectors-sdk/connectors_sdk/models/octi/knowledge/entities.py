07a6b9f8c6658a370c5582794d439e98f6a7915a
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-08-26 09:42:13 +0200

    [connectors-sdk] Add IPV6Address, DomainName, URL, File, Malware, Individual and ThreatActorGroup models (https://github.com/OpenCTI-Platform/connectors/issues/4261)
    Co-authored-by: Samuel Hassine <samuel.hassine@filigran.io>
    Co-authored-by: Romain Guignard <romain.guignard@filigran.io>
    Co-authored-by: Elodie Gameiro <elodie.gameiro@filigran.io>

--- connectors-sdk/connectors_sdk/models/octi/knowledge/entities.py
+++ connectors-sdk/connectors_sdk/models/octi/knowledge/entities.py
@@ -3,15 +3,79 @@
 from typing import Optional
 
 from connectors_sdk.models.octi._common import MODEL_REGISTRY, BaseIdentifiedEntity
-from connectors_sdk.models.octi.enums import IndustrySector, Reliability
+from connectors_sdk.models.octi.enums import (
+    IndustrySector,
+    OrganizationType,
+    Reliability,
+)
 from pycti import Identity as PyctiIdentity
 from pydantic import Field
 from stix2.v21 import Identity as Stix2Identity
 
 
 @MODEL_REGISTRY.register
+class Individual(BaseIdentifiedEntity):
+    """Define an OCTI organization.
+
+    Examples:
+        >>> individual = Individual(name="John Doe")
+        >>> entity = individual.to_stix2_object()
+    """
+
+    name: str = Field(
+        description="Name of the organization.",
+        min_length=1,
+    )
+    description: Optional[str] = Field(
+        description="Description of the organization.",
+        default=None,
+    )
+    contact_information: Optional[str] = Field(
+        description="Contact information for the organization.",
+        default=None,
+    )
+    reliability: Optional[Reliability] = Field(
+        description="OpenCTI Reliability of the organization.",
+        default=None,
+    )
+    aliases: Optional[list[str]] = Field(
+        description="Aliases of the organization.",
+        default=None,
+    )
+
+    def to_stix2_object(self) -> Stix2Identity:
+        """Make stix object.
+
+        Notes:
+            - OpenCTI maps STIX Identity SDO to OCTI Individual entity based on `identity_class`.
+            - To create an Individual entity on OpenCTI, `identity_class` MUST be 'individual'.
+        """
+        identity_class = "individual"
+
+        return Stix2Identity(
+            id=PyctiIdentity.generate_id(
+                identity_class=identity_class,
+                name=self.name,
+            ),
+            identity_class=identity_class,
+            name=self.name,
+            description=self.description,
+            contact_information=self.contact_information,
+            external_references=[
+                external_reference.to_stix2_object()
+                for external_reference in self.external_references or []
+            ],
+            object_marking_refs=[marking.id for marking in self.markings or []],
+            created_by_ref=self.author.id if self.author else None,
+            allow_custom=True,
+            x_opencti_reliability=self.reliability,
+            x_opencti_aliases=self.aliases,
+        )
+
+
+@MODEL_REGISTRY.register
 class Organization(BaseIdentifiedEntity):
-    """Defin an OCTI organization.
+    """Define an OCTI organization.
 
     Examples:
         >>> org = Organization(name="Example Corp")
@@ -19,38 +83,28 @@ class Organization(BaseIdentifiedEntity):
     """
 
     name: str = Field(
-        ...,
         description="Name of the organization.",
         min_length=1,
     )
     description: Optional[str] = Field(
-        None,
         description="Description of the organization.",
+        default=None,
     )
     contact_information: Optional[str] = Field(
-        None,
         description="Contact information for the organization.",
+        default=None,
     )
-    organization_type: Optional[str] = Field(
-        None,
-        description="OpenCTI Type of the organization. By default, OpenCTI handles: "
-        "'vendor', 'partner', 'constituent', 'csirt', 'other'."
-        "See https://github.com/OpenCTI-Platform/opencti/blob/master/opencti-platform/opencti-graphql/src/modules/vocabulary/vocabulary-utils.ts",
+    organization_type: Optional[OrganizationType] = Field(
+        description="OpenCTI Type of the organization.",
+        default=None,
     )
-    reliability: Optional[str] = Field(
-        None,
-        description="OpenCTI Reliability of the organization. By default, OpenCTI handles: "
-        "'A - Completely reliable', "
-        "'B - Usually reliable', "
-        "'C - Fairly reliable', "
-        "'D - Not usually reliable', "
-        "'E - Unreliable', "
-        "'F - Reliability cannot be judged'. "
-        "See https://github.com/OpenCTI-Platform/opencti/blob/master/opencti-platform/opencti-graphql/src/modules/vocabulary/vocabulary-utils.ts",
+    reliability: Optional[Reliability] = Field(
+        description="OpenCTI Reliability of the organization.",
+        default=None,
     )
     aliases: Optional[list[str]] = Field(
-        None,
         description="Aliases of the organization.",
+        default=None,
     )
 
     def to_stix2_object(self) -> Stix2Identity:
@@ -63,7 +117,10 @@ class Organization(BaseIdentifiedEntity):
         identity_class = "organization"
 
         return Stix2Identity(
-            id=PyctiIdentity.generate_id(identity_class=identity_class, name=self.name),
+            id=PyctiIdentity.generate_id(
+                identity_class=identity_class,
+                name=self.name,
+            ),
             identity_class=identity_class,
             name=self.name,
             description=self.description,
@@ -74,22 +131,10 @@ class Organization(BaseIdentifiedEntity):
             ],
             object_marking_refs=[marking.id for marking in self.markings or []],
             created_by_ref=self.author.id if self.author else None,
-            custom_properties=dict(  # noqa: C408  # No literal dict for maintainability
-                x_opencti_organization_type=self.organization_type,
-                x_opencti_reliability=self.reliability,
-                x_opencti_aliases=self.aliases,
-            ),
-            # unused
-            created=None,
-            modified=None,
-            roles=None,
-            sectors=None,
-            revoked=None,
-            labels=None,
-            confidence=None,
-            lang=None,
-            granular_markings=None,
-            extensions=None,
+            allow_custom=True,
+            x_opencti_organization_type=self.organization_type,
+            x_opencti_reliability=self.reliability,
+            x_opencti_aliases=self.aliases,
         )
 
 
@@ -110,14 +155,7 @@ class Sector(BaseIdentifiedEntity):
         default=None,
     )
     reliability: Optional[Reliability] = Field(
-        description="OpenCTI Reliability of the sector. By default, OpenCTI handles: "
-        "'A - Completely reliable', "
-        "'B - Usually reliable', "
-        "'C - Fairly reliable', "
-        "'D - Not usually reliable', "
-        "'E - Unreliable', "
-        "'F - Reliability cannot be judged'. "
-        "See https://github.com/OpenCTI-Platform/opencti/blob/master/opencti-platform/opencti-graphql/src/modules/vocabulary/vocabulary-utils.ts",
+        description="OpenCTI Reliability of the sector.",
         default=None,
     )
     aliases: Optional[list[str]] = Field(
