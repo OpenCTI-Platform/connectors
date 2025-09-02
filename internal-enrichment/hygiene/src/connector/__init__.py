6b87e4737af0b59b18bd9ae29e25aed6fe52883f
Frederic Basler <frederic.basler@filigran.io>
2025-09-02 09:21:32 +0200

    [Hygiene] Make connector compatible with connector composer

---
+++ internal-enrichment/hygiene/src/connector/__init__.py
@@ -0,0 +1,9 @@
+from src.connector.hygiene import HygieneConnector
+from src.connector.models import ConfigLoader
+from src.connector.services import HygieneConfig
+
+__all__ = [
+    "HygieneConnector",
+    "ConfigLoader",
+    "HygieneConfig",
+]
