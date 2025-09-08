7b438a92a6063dbf43901a3e7799bcfa5722a0dc
Frédéric BASLER <14902945+Megafredo@users.noreply.github.com>
2025-09-08 09:51:44 +0200

    [Hygiene] Make connector compatible with connector composer (#4563)

--- internal-enrichment/hygiene/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Go to the right directory
-cd /opt/opencti-connector-hygiene
-
-# Launch the worker
-python3 hygiene.py
---
+++ internal-enrichment/hygiene/src/connector/models/__init__.py
@@ -0,0 +1,11 @@
+from src.connector.models.configs import (
+    ConfigBaseSettings,
+    ConfigLoader,
+    ConfigLoaderConnector,
+)
+
+__all__ = [
+    "ConfigBaseSettings",
+    "ConfigLoader",
+    "ConfigLoaderConnector",
+]
