004da64fad9275b40e4e67676ee128e48065c68f
Philippe Damoune <philippe.damoune@filigran.io>
2025-09-11 11:55:04 +0200

    refactor(IpInfo): migrate scope configuration from str to ListFromString

--- internal-enrichment/ipinfo/src/models/configs/config_loader.py
+++ internal-enrichment/ipinfo/src/models/configs/config_loader.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Any
 
+from connectors_sdk.core.pydantic import ListFromString
 from pydantic import Field
 from pydantic_settings import (
     BaseSettings,
@@ -28,8 +29,8 @@ class ConfigLoaderConnector(_ConfigLoaderConnector):
         default="IPInfo",
         description="Name of the connector.",
     )
-    scope: str = Field(
-        default="IPv4-Addr,IPv6-Addr",
+    scope: ListFromString = Field(
+        default=["IPv4-Addr", "IPv6-Addr"],
         description="The scope or type of data the connector is importing, either a MIME type or Stix Object (for information only).",
     )
 
