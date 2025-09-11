d8c76d827b2926daf8826a064b97b76ffc10f5c3
Philippe Damoune <philippe.damoune@filigran.io>
2025-09-11 13:14:29 +0200

    refactor(Mitre): migrate scope configuration from str to ListFromString

--- external-import/mitre/src/models/configs/config_loader.py
+++ external-import/mitre/src/models/configs/config_loader.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Any
 
+from connectors_sdk.core.pydantic import ListFromString
 from pydantic import Field
 from pydantic_settings import (
     BaseSettings,
@@ -28,8 +29,22 @@ class ConfigLoaderConnector(_ConfigLoaderConnector):
         default="Mitre Att&ck",
         description="Name of the connector.",
     )
-    scope: str = Field(
-        default="tool,report,malware,identity,campaign,intrusion-set,attack-pattern,course-of-action,x-mitre-data-source,x-mitre-data-component,x-mitre-matrix,x-mitre-tactic,x-mitre-collection",
+    scope: ListFromString = Field(
+        default=[
+            "tool",
+            "report",
+            "malware",
+            "identity",
+            "campaign",
+            "intrusion-set",
+            "attack-pattern",
+            "course-of-action",
+            "x-mitre-data-source",
+            "x-mitre-data-component",
+            "x-mitre-matrix",
+            "x-mitre-tactic",
+            "x-mitre-collection",
+        ],
         description="The scope or type of data the connector is importing, either a MIME type or Stix Object (for information only).",
     )
 
