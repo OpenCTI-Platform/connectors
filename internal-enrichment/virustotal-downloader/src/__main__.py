656d93ec35c207f23ed9a77f507129c70776add5
Elodie <22365490+Ninoxe@users.noreply.github.com>
2025-09-12 12:10:11 +0200

    [VirusTotal Downloader] Make connector compatible with connector composer (https://github.com/OpenCTI-Platform/connectors/issues/4598)

---
+++ internal-enrichment/virustotal-downloader/src/__main__.py
@@ -0,0 +1,29 @@
+import traceback
+
+from pycti import OpenCTIConnectorHelper
+from src.connector import VirustotalDownloaderConnector
+from src.connector.services import VirusTotalDownloaderConfig
+
+if __name__ == "__main__":
+    """
+    Entry point of the script
+
+    - traceback.print_exc(): This function prints the traceback of the exception to the standard error (stderr).
+    The traceback includes information about the point in the program where the exception occurred,
+    which is very useful for debugging purposes.
+    - exit(1): effective way to terminate a Python program when an error is encountered.
+    It signals to the operating system and any calling processes that the program did not complete successfully.
+    """
+    try:
+        config = VirusTotalDownloaderConfig()
+        config_instance = config.load
+        helper = OpenCTIConnectorHelper(
+            config=config_instance.model_dump_pycti(),
+            playbook_compatible=True,
+        )
+
+        connector = VirustotalDownloaderConnector(config=config_instance, helper=helper)
+        connector.start()
+    except Exception:
+        traceback.print_exc()
+        exit(1)
---
+++ internal-enrichment/virustotal-downloader/src/connector/__init__.py
@@ -0,0 +1,9 @@
+from src.connector.connector import VirustotalDownloaderConnector
+from src.connector.models import ConfigLoader
+from src.connector.services import VirusTotalDownloaderConfig
+
+__all__ = [
+    "ConfigLoader",
+    "VirustotalDownloaderConnector",
+    "VirusTotalDownloaderConfig",
+]
