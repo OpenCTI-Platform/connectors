dd9cf535bc0d8b30f8efa17763a26ddbd3649e1e
Philippe Damoune <philippe.damoune@filigran.io>
2025-09-12 14:12:38 +0200

    refactor(Sekoia): Remove legacy SekoiaConfig class

--- external-import/sekoia/src/connector/__init__.py
+++ external-import/sekoia/src/connector/__init__.py
@@ -1,9 +1,7 @@
 from src.connector.models import ConfigLoader
 from src.connector.sekoia import SekoiaConnector
-from src.connector.services import SekoiaConfig
 
 __all__ = [
     "SekoiaConnector",
     "ConfigLoader",
-    "SekoiaConfig",
 ]
