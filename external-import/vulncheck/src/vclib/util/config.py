9168f95f8e7ecf84b48f22ecc1c54a24d7300642
maddawik <ereilly@vulncheck.com>
2025-09-22 10:55:14 -0400

    feat: add course-of-action, attack-patterns, labels and more

--- external-import/vulncheck/src/vclib/util/config.py
+++ external-import/vulncheck/src/vclib/util/config.py
@@ -9,6 +9,9 @@ SCOPE_LOCATION = "location"
 SCOPE_IP = "ip-addr"
 SCOPE_INDICATOR = "indicator"
 SCOPE_EXTERNAL_REF = "external-reference"
+SCOPE_ATTACK_PATTERN = "attack-pattern"
+SCOPE_COURSE_OF_ACTION = "course-of-action"
+SCOPE_DATA_SOURCE = "x-mitre-data-source"
 
 
 def compare_config_to_target_scope(
