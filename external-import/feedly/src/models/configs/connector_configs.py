34ecc675f198cf13337068d408d4a986ea7c04f6
Samuel Hassine <samuel.hassine@filigran.io>
2025-09-27 17:15:50 -0400

    [feedly] Compatibility with the connectors manager (#4834)

---
+++ external-import/feedly/src/models/configs/connector_configs.py
@@ -0,0 +1,53 @@
+from typing import Annotated, Literal
+
+from connectors_sdk.core.pydantic import ListFromString
+from models.configs import ConfigBaseSettings
+from pydantic import (
+    Field,
+    HttpUrl,
+    PlainSerializer,
+    field_validator,
+)
+
+LogLevelToLower = Annotated[
+    Literal["debug", "info", "warn", "warning", "error"],
+    PlainSerializer(lambda v: "".join(v), return_type=str),
+]
+
+HttpUrlToString = Annotated[HttpUrl, PlainSerializer(str, return_type=str)]
+
+
+class _ConfigLoaderOCTI(ConfigBaseSettings):
+    """Interface for loading OpenCTI dedicated configuration."""
+
+    # Config Loader OpenCTI
+    url: HttpUrlToString = Field(
+        alias="OPENCTI_URL",
+        description="The OpenCTI platform URL.",
+    )
+    token: str = Field(
+        alias="OPENCTI_TOKEN",
+        description="The token of the user who represents the connector in the OpenCTI platform.",
+    )
+
+
+class _ConfigLoaderConnector(ConfigBaseSettings):
+    """Interface for loading Connector dedicated configuration."""
+
+    # Config Loader Connector
+    id: str
+    name: str
+    scope: ListFromString
+
+    type: str = Field(
+        default="EXTERNAL_IMPORT",
+        description="Should always be set to EXTERNAL_IMPORT for this connector.",
+    )
+    log_level: LogLevelToLower = Field(
+        default="error",
+        description="Determines the verbosity of the logs.",
+    )
+
+    @field_validator("type")
+    def force_value_for_type_to_be_external_import(cls, value):
+        return "EXTERNAL_IMPORT"
