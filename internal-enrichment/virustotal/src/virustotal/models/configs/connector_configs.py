b60eedeb045f7163860d7180bd62bc40dfad0024
Frédéric BASLER <14902945+Megafredo@users.noreply.github.com>
2025-10-06 16:58:28 +0200

    [VirusTotal] Make connector compatible with connector composer (#4707)

---
+++ internal-enrichment/virustotal/src/virustotal/models/configs/connector_configs.py
@@ -0,0 +1,50 @@
+from typing import Annotated, Literal
+
+from connectors_sdk.core.pydantic import ListFromString
+from pydantic import Field, HttpUrl, PlainSerializer, field_validator
+from virustotal.models.configs.base_settings import ConfigBaseSettings
+
+LogLevelToLower = Annotated[
+    Literal["debug", "info", "warn", "warning", "error"],
+    PlainSerializer(lambda v: "".join(v), return_type=str),
+]
+
+HttpUrlToString = Annotated[HttpUrl, PlainSerializer(str, return_type=str)]
+
+
+class ConfigLoaderOCTI(ConfigBaseSettings):
+    """Interface for loading OpenCTI dedicated configuration."""
+
+    # Config Loader OpenCTI
+    url: HttpUrlToString = Field(
+        description="The OpenCTI platform URL.",
+    )
+    token: str = Field(
+        description="The token of the user who represents the connector in the OpenCTI platform.",
+    )
+
+
+class ConfigLoaderConnectorExtra(ConfigBaseSettings):
+    """Interface for loading Connector dedicated configuration."""
+
+    # Config Loader Connector
+    id: str
+    name: str
+    scope: ListFromString
+
+    type: str = Field(
+        default="INTERNAL_ENRICHMENT",
+        description="Should always be set to INTERNAL_ENRICHMENT for this connector.",
+    )
+    auto: bool = Field(
+        default=False,
+        description="Enables or disables automatic enrichment of observables for OpenCTI.",
+    )
+    log_level: LogLevelToLower = Field(
+        default="error",
+        description="Determines the verbosity of the logs.",
+    )
+
+    @field_validator("type")
+    def force_value_for_type_to_be_internal_enrichment(cls, value):
+        return "INTERNAL_ENRICHMENT"
