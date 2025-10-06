60cd520ca5fac329214bd7777705dec2d1d66a6b
Frédéric BASLER <14902945+Megafredo@users.noreply.github.com>
2025-10-06 16:58:28 +0200

    [VirusTotal] Make connector compatible with connector composer (#4707)

--- internal-enrichment/virustotal/src/main.py
+++ internal-enrichment/virustotal/src/main.py
@@ -1,16 +1,26 @@
-# -*- coding: utf-8 -*-
-"""VirusTotal connector main file."""
+import traceback
 
-import sys
-import time
-
+from pycti import OpenCTIConnectorHelper
 from virustotal import VirusTotalConnector
+from virustotal.models.configs.config_loader import ConfigLoader
 
 if __name__ == "__main__":
+    """
+    Entry point of the script
+
+    - traceback.print_exc(): This function prints the traceback of the exception to the standard error (stderr).
+    The traceback includes information about the point in the program where the exception occurred,
+    which is very useful for debugging purposes.
+    - exit(1): effective way to terminate a Python program when an error is encountered.
+    It signals to the operating system and any calling processes that the program did not complete successfully.
+    """
     try:
-        connector = VirusTotalConnector()
+        config = ConfigLoader()
+        helper = OpenCTIConnectorHelper(
+            config=config.model_dump_pycti(), playbook_compatible=True
+        )
+        connector = VirusTotalConnector(config, helper)
         connector.start()
-    except Exception as e:
-        print(e)
-        time.sleep(10)
-        sys.exit(0)
+    except Exception:
+        traceback.print_exc()
+        exit(1)
