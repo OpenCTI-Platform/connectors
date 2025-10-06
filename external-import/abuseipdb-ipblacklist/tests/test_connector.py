60cc51689e2445a1c6b19e71d24e138149a9e676
Mariot Tsitoara <mariot.tsitoara@filigran.io>
2025-10-06 14:38:23 +0200

    [Microsoft Sentinel Incident] move mocks to conftest.py

--- external-import/abuseipdb-ipblacklist/tests/test_connector.py
+++ external-import/abuseipdb-ipblacklist/tests/test_connector.py
@@ -1,67 +1,9 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
-from pycti import OpenCTIApiClient, OpenCTIApiConnector
-from src.external_import_connector.config_variables import ConfigConnector
 from src.external_import_connector.connector import ConnectorAbuseIPDB
 
 
-@patch.dict(
-    "os.environ",
-    {
-        "ABUSEIPDB_SCORE": "90",
-        "ABUSEIPDB_CREATE_INDICATOR": "true",
-    },
-)
-@patch.object(
-    ConfigConnector,
-    "_load_config",
-    return_value={
-        "opencti": {
-            "url": "http://localhost:8080",
-            "token": "changeme",
-            "ssl_verify": False,
-        },
-        "connector": {
-            "id": "CHANGEME",
-            "name": "AbuseIPDB Connector",
-            "type": "EXTERNAL_IMPORT",
-        },
-    },
-)
-@patch.object(
-    OpenCTIApiClient,
-    "health_check",
-    return_value=True,
-)
-@patch.object(
-    OpenCTIApiConnector,
-    "ping",
-    return_value={
-        "id": "CHANGEME",
-        "connector_user_id": "1",
-        "connector_state": "{}",
-    },
-)
-@patch.object(
-    OpenCTIApiConnector,
-    "register",
-    return_value={
-        "id": "CHANGEME",
-        "connector_user_id": "1",
-        "connector_state": "{}",
-        "config": {
-            "connection": {
-                "host": "rabbitmq",
-                "vhost": "/",
-                "use_ssl": False,
-                "port": 5672,
-                "user": "opencti",
-                "pass": "changeme",
-            }
-        },
-    },
-)
-def test_should_create_indicator_with_same_score(_, __, ___, ____):
+def test_should_create_indicator_with_same_score():
     connector = ConnectorAbuseIPDB()
 
     connector.helper.api.work.initiate_work = lambda x, y: "work_id_123"
@@ -77,16 +19,9 @@ def test_should_create_indicator_with_same_score(_, __, ___, ____):
         ]
     )
 
-    indictor_tag_exists = False
+    def mocked_stix2_create_bundle(stix_objects):
+        assert stix_objects[0].x_opencti_create_indicator
 
-    def check_if_indicator_tag_exists(stix_objects):
-        nonlocal indictor_tag_exists
-        indictor_tag_exists = stix_objects[0].x_opencti_create_indicator
-
-    connector.helper.stix2_create_bundle = Mock(
-        side_effect=check_if_indicator_tag_exists
-    )
+    connector.helper.stix2_create_bundle = mocked_stix2_create_bundle
 
     connector.process_message()
-
-    assert indictor_tag_exists
