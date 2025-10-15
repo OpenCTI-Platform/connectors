cd655d7b60adfde560a1ff3be306df4c66b52e5e
Mariot Tsitoara <mariot.tsitoara@filigran.io>
2025-10-15 13:13:17 +0200

    Update connectors-sdk/connectors_sdk/exceptions/__init__.py
    Co-authored-by: Philippe Damoune <philippe.damoune@filigran.io>

--- connectors-sdk/connectors_sdk/exceptions/__init__.py
+++ connectors-sdk/connectors_sdk/exceptions/__init__.py
@@ -3,13 +3,9 @@
 from .error import (
     ConfigError,
     ConfigValidationError,
-    DataRetrievalError,
-    UseCaseError,
 )
 
 __all__ = [
     "ConfigError",
     "ConfigValidationError",
-    "UseCaseError",
-    "DataRetrievalError",
 ]
