4bf9c04700596cc20d841ce012c1ba514205fb4f
Philippe Damoune <philippe.damoune@filigran.io>
2025-10-17 13:21:25 +0200

    refactor: Move Author

---
+++ connectors-sdk/connectors_sdk/models/_author.py
@@ -0,0 +1,29 @@
+"""Author."""
+
+from abc import ABC, abstractmethod
+
+import stix2.properties
+from connectors_sdk.models._base_identified_entity import _BaseIdentifiedEntity
+from connectors_sdk.models._model_registry import MODEL_REGISTRY
+
+
+@MODEL_REGISTRY.register
+class Author(ABC, _BaseIdentifiedEntity):
+    """Represent an author.
+
+    Author is an OpenCTI concept, a stix-like identity considered as the creator of a
+    report or an entity.
+
+    Warning:
+        This class cannot be used directly, it must be subclassed.
+
+    """
+
+    @abstractmethod
+    def to_stix2_object(self) -> stix2.v21._STIXBase21:
+        """Make stix object.
+
+        Returns:
+            (stix2.v21._STIXBase21): A stix object representing the author.
+
+        """
