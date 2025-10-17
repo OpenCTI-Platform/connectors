3bf1100d137b321e64a791a080ba275a7ec8d517
Elodie <22365490+Ninoxe@users.noreply.github.com>
2025-10-17 10:56:39 +0200

    [Connectors-sdk] Add BaseConnectorSettings (config validation) (#4821)
    Co-authored-by: Mariot Tsitoara <mariot.tsitoara@filigran.io>
    Co-authored-by: Philippe Damoune <philippe.damoune@filigran.io>
    Co-authored-by: Pauline Eustachy <pauline.eustachy@filigran.io>

--- connectors-sdk/connectors_sdk/exceptions/__init__.py
+++ connectors-sdk/connectors_sdk/exceptions/__init__.py
@@ -1,1 +1,11 @@
 """Offer Exception handling tools to develop connectors."""
+
+from .error import (
+    ConfigError,
+    ConfigValidationError,
+)
+
+__all__ = [
+    "ConfigError",
+    "ConfigValidationError",
+]
