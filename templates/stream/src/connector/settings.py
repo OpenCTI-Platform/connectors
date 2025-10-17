a7b3534d4df6e386f9511f0850239225c2906622
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-10-17 20:09:25 +0200

    feat: update template for connectors of type `STREAM`

---
+++ templates/stream/src/connector/settings.py
@@ -0,0 +1,40 @@
+from connectors_sdk import (
+    BaseConfigModel,
+    BaseConnectorSettings,
+    BaseStreamConnectorConfig,
+)
+from pydantic import Field
+
+
+class StreamConfig(BaseStreamConnectorConfig):
+    """
+    Override the `BaseStreamConnectorConfig` to add parameters and/or defaults
+    to the configuration for connectors of type `STREAM`.
+    """
+
+    name: str = Field(
+        description="The name of the connector.",
+        default="TemplateConnector",
+    )
+    live_stream_id: str = Field(
+        description="The ID of the live stream to connect to.",
+        default="live",  # listen the global stream (not filtered)
+    )
+
+
+class TemplateConfig(BaseConfigModel):
+    """
+    Define parameters and/or defaults for the configuration specific to the `TemplateConnector`.
+    """
+
+    api_base_url: str = Field(description="External API base URL.")
+    api_key: str = Field(description="API key for authentication.")
+
+
+class ConnectorSettings(BaseConnectorSettings):
+    """
+    Override `BaseConnectorSettings` to include `StreamConfig` and `TemplateConfig`.
+    """
+
+    connector: StreamConfig = Field(default_factory=StreamConfig)
+    template: TemplateConfig = Field(default_factory=TemplateConfig)
