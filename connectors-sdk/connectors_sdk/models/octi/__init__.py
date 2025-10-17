0526057788a39371901346e6b4c5c0ca2af27163
Philippe Damoune <philippe.damoune@filigran.io>
2025-10-17 16:36:03 +0200

    refactor: Add deprecation warning on import from models.octi

--- connectors-sdk/connectors_sdk/models/octi/__init__.py
+++ connectors-sdk/connectors_sdk/models/octi/__init__.py
@@ -1,22 +1,36 @@
 """Offer OpenCTI models."""
 
-from connectors_sdk.models.associated_file import AssociatedFile
-from connectors_sdk.models.attack_pattern import AttackPattern
-from connectors_sdk.models.base_entity import BaseEntity
-from connectors_sdk.models.base_identified_entity import BaseIdentifiedEntity
-from connectors_sdk.models.city import City
-from connectors_sdk.models.country import Country
-from connectors_sdk.models.domain_name import DomainName
-from connectors_sdk.models.external_reference import ExternalReference
-from connectors_sdk.models.file import File
-from connectors_sdk.models.indicator import Indicator
-from connectors_sdk.models.individual import Individual
-from connectors_sdk.models.intrusion_set import IntrusionSet
-from connectors_sdk.models.ipv4_address import IPV4Address
-from connectors_sdk.models.ipv6_address import IPV6Address
-from connectors_sdk.models.kill_chain_phase import KillChainPhase
-from connectors_sdk.models.malware import Malware
-from connectors_sdk.models.note import Note
+import warnings
+
+from connectors_sdk.models import (
+    URL,
+    AssociatedFile,
+    AttackPattern,
+    BaseEntity,
+    BaseIdentifiedEntity,
+    City,
+    Country,
+    DomainName,
+    ExternalReference,
+    File,
+    Indicator,
+    Individual,
+    IntrusionSet,
+    IPV4Address,
+    IPV6Address,
+    KillChainPhase,
+    Malware,
+    Note,
+    Organization,
+    OrganizationAuthor,
+    Relationship,
+    Report,
+    Sector,
+    Software,
+    ThreatActorGroup,
+    TLPMarking,
+    Vulnerability,
+)
 from connectors_sdk.models.octi.relationships import (
     based_on,
     derived_from,
@@ -26,16 +40,14 @@ from connectors_sdk.models.octi.relationships import (
     related_to,
     targets,
 )
-from connectors_sdk.models.organization import Organization
-from connectors_sdk.models.organization_author import OrganizationAuthor
-from connectors_sdk.models.relationship import Relationship
-from connectors_sdk.models.report import Report
-from connectors_sdk.models.sector import Sector
-from connectors_sdk.models.software import Software
-from connectors_sdk.models.threat_actor_group import ThreatActorGroup
-from connectors_sdk.models.tlp_marking import TLPMarking
-from connectors_sdk.models.url import URL
-from connectors_sdk.models.vulnerability import Vulnerability
+
+warnings.warn(
+    "The 'connectors_sdk.models.octi' module is deprecated and will be removed"
+    "in future versions. Please use 'connectors_sdk.models' instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 
 __all__ = [
     # Models flat list
