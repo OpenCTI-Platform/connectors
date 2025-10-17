41701c54a32ec9d88fdbb4d6e75b60b9eab0410b
Philippe Damoune <philippe.damoune@filigran.io>
2025-10-17 11:41:47 +0200

    refactor: Move File

--- connectors-sdk/connectors_sdk/models/octi/activities/observations.py
+++ connectors-sdk/connectors_sdk/models/octi/activities/observations.py
@@ -1,113 +1,14 @@
 """Offer observations OpenCTI entities."""
 
 import ipaddress
-from typing import Any
 
 from connectors_sdk.models._model_registry import MODEL_REGISTRY
 from connectors_sdk.models._observable import Observable
-from connectors_sdk.models.octi.enums import HashAlgorithm
-from pydantic import AwareDatetime, Field, PositiveInt, field_validator, model_validator
+from pydantic import Field, field_validator
 from stix2.v21 import URL as Stix2URL  # noqa: N811 # URL is not a constant but a class
-from stix2.v21 import File as Stix2File
 from stix2.v21 import IPv4Address as Stix2IPv4Address
 from stix2.v21 import IPv6Address as Stix2IPv6Address
 from stix2.v21 import Software as Stix2Software
-
-
-@MODEL_REGISTRY.register
-class File(Observable):
-    """Define a file observable on OpenCTI.
-
-    Notes:
-        - The `content_ref` field (from STIX2.1 spec) it not implemented on OpenCTI.
-          It must be replaced by explicit `____________________` relationships.
-        - The `parent_directory_ref` field (from STIX2.1 spec) is not implemented on OpenCTI.
-          It must be replaced by explicit `____________________` relationships.
-        - The `contains_refs` field (from STIX2.1 spec) is not implemented on OpenCTI.
-          It must be replaced by explicit `____________________` relationships.
-    """
-
-    hashes: dict[HashAlgorithm, str] | None = Field(
-        default=None,
-        description="A dictionary of hashes for the file.",
-        min_length=1,
-    )
-    size: PositiveInt | None = Field(
-        default=None,
-        description="The size of the file in bytes.",
-    )
-    name: str | None = Field(
-        default=None,
-        description="The name of the file.",
-    )
-    name_enc: str | None = Field(
-        default=None,
-        description="The observed encoding for the name of the file.",
-    )
-    magic_number_hex: str | None = Field(
-        default=None,
-        description="The hexadecimal constant ('magic number') associated with the file format.",
-    )
-    mime_type: str | None = Field(
-        default=None,
-        description="The MIME type name specified for the file, e.g., application/msword.",
-    )
-    ctime: AwareDatetime | None = Field(
-        default=None,
-        description="Date/time the directory was created.",
-    )
-    mtime: AwareDatetime | None = Field(
-        default=None,
-        description="Date/time the directory was last writtend to or modified.",
-    )
-    atime: AwareDatetime | None = Field(
-        default=None,
-        description="Date/time the directory was last accessed.",
-    )
-    additional_names: list[str] | None = Field(
-        default=None,
-        description="Additional names of the file.",
-    )
-
-    @model_validator(mode="before")
-    @classmethod
-    def _validate_data(cls, data: Any) -> Any:
-        """Pre validate data to avoid raising a `stix2.exceptions.AtLeastOnePropertyError` during `self.id` eval.
-
-        Notes:
-            The code to create a `File` instance is executed in this order:
-                1. Call "before" validators, here `File._validate_data`
-                2. Call `self.__init__()`
-                    2.1. During init, evaluate `self.id` (computed field from `BaseIdentifiedEntity` superclass)
-                        2.1.1. During `self.id` eval, call `self.to_stix2_object()`
-                3. Call `self._check_id()` "after" validator (from `BaseIdentifiedEntity` superclass)
-
-            This validator aims to replace the `stix2.exceptions.AtLeastOnePropertyError` that could be raised in
-            `self.to_stix2_object()` by a `pydantic.ValidationError`.
-        """
-        if isinstance(data, dict):
-            if not data.get("name") and not data.get("hashes"):
-                raise ValueError("Either 'name' or one of 'hashes' must be provided.")
-
-        return data
-
-    def to_stix2_object(self) -> Stix2File:
-        """Make stix object."""
-        return Stix2File(
-            hashes=self.hashes,
-            size=self.size,
-            name=self.name,
-            name_enc=self.name_enc,
-            magic_number_hex=self.magic_number_hex,
-            mime_type=self.mime_type,
-            ctime=self.ctime,
-            mtime=self.mtime,
-            atime=self.atime,
-            object_marking_refs=[marking.id for marking in self.markings or []],
-            allow_custom=True,
-            x_opencti_additional_names=self.additional_names,
-            **self._custom_properties_to_stix(),
-        )
 
 
 @MODEL_REGISTRY.register
@@ -264,10 +165,3 @@ class URL(Observable):
 
 
 MODEL_REGISTRY.rebuild_all()
-
-if __name__ == "__main__":  # pragma: no cover  # Do not run coverage on doctest
-    # import doctest
-
-    # doctest.testmod()
-
-    _ = File(mime_type="text/plain")
