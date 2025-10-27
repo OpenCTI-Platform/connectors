2a349d66657a65d4e9d4fc3bc42da0d64936236c
Christophe Melchior <christophe.melchior@filigran.io>
2025-10-27 18:10:21 +0100

    :green_heat: fix(test,ci): Fixes tests for the CI

--- external-import/crowdstrike/tests/configurable_report_relationships_guessing/test_conditional_relationship_building.py
+++ external-import/crowdstrike/tests/configurable_report_relationships_guessing/test_conditional_relationship_building.py
@@ -28,7 +28,7 @@ def fake_report_data() -> dict:
 @pytest.fixture
 def author_identity() -> Identity:
     """Fixture for author identity."""
-    return Identity(
+    return Identity(  # pylint: disable=W9101  # it's a test no real ingest
         name="CrowdStrike",
         identity_class="organization",
     )
