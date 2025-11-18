5d0e2813f7bfd9d6114476fca8afed070138e7cb
Mariot Tsitoara <mariot.tsitoara@filigran.io>
2025-11-18 11:03:38 +0100

    move config definition

--- external-import/hunt-io/src/main.py
+++ external-import/hunt-io/src/main.py
@@ -2,6 +2,8 @@ import sys
 import traceback
 
 from external_import_connector import ConnectorHuntIo
+from external_import_connector.settings import ConfigLoader
+from pycti import OpenCTIConnectorHelper
 
 if __name__ == "__main__":
     """
@@ -14,7 +16,9 @@ if __name__ == "__main__":
     It signals to the operating system and any calling processes that the program did not complete successfully.
     """
     try:
-        connector = ConnectorHuntIo()
+        config = ConfigLoader()
+        helper = OpenCTIConnectorHelper(config=config.to_helper_config())
+        connector = ConnectorHuntIo(config=config, helper=helper)
         connector.run()
     except Exception:
         traceback.print_exc()
