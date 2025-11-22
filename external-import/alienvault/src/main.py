14ea9bef072e30dae3067b4ca31198acd69161d4
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-11-22 16:19:34 +0100

    fix: fix entrypoint

--- external-import/alienvault/src/main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""OpenCTI AlienVault connector main module."""
-
-from alienvault import AlienVault
-
-if __name__ == "__main__":
-    connector = AlienVault()
-    connector.run()
