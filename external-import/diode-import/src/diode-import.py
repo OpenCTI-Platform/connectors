e88844becb8deec598a38de3b0157c367ace9241
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-11-27 16:21:35 +0100

    [Diode Import] Add missing required positional arguments (#5150)

--- external-import/diode-import/src/diode-import.py
+++ external-import/diode-import/src/diode-import.py
@@ -82,13 +82,15 @@ class DiodeImport:
             # region Register the connector in OpenCTI to simulate the real activity if not in cache
             if self.connectors_cache.get(connector_id) is None:
                 connector_registration = OpenCTIConnector(
-                    connector.get("id"),
-                    connector.get("name"),
-                    connector.get("type"),
-                    connector.get("scope"),
-                    connector.get("auto"),
-                    False,
-                    False,
+                    connector_id=connector.get("id"),
+                    connector_name=connector.get("name"),
+                    connector_type=connector.get("type"),
+                    scope=connector.get("scope"),
+                    auto=connector.get("auto"),
+                    only_contextual=False,  # default
+                    playbook_compatible=False,  # default
+                    auto_update=False,  # default
+                    enrichment_resolution="none",  # default
                 )
                 self.helper.api.connector.register(connector_registration)
                 self.connectors_cache[connector_id] = connector_id
