64f1d5d3af0a1e5514a50f7c6dfed02a40f132e4
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-12-03 17:15:31 +0100

    feat: automated migration

--- stream/microsoft-sentinel-intel/src/base_connector/config.py
+++ stream/microsoft-sentinel-intel/src/base_connector/config.py
@@ -21,27 +21,7 @@ from pydantic_settings import (
     YamlConfigSettingsSource,
 )
 
-"""
-All the variables that have default values will override configuration from the OpenCTI helper.
-
-All the variables of this classes are customizable through:
-    - config.yml 
-    - .env
-    - environment variables.
-
-If a variable is set in 2 different places, the first one will be used in this order:
-    1. YAML file
-    2. .env file
-    3. Environment variables
-    4. Default value
-    
-WARNING:
-    The Environment variables in the .env or global environment must be set in the following format:
-    OPENCTI_<variable>
-    CONNECTOR_<variable>
-    
-    the split is made on the first occurrence of the "_" character.
-"""
+'\nAll the variables that have default values will override configuration from the OpenCTI helper.\n\nAll the variables of this classes are customizable through:\n    - config.yml \n    - .env\n    - environment variables.\n\nIf a variable is set in 2 different places, the first one will be used in this order:\n    1. YAML file\n    2. .env file\n    3. Environment variables\n    4. Default value\n    \nWARNING:\n    The Environment variables in the .env or global environment must be set in the following format:\n    OPENCTI_<variable>\n    CONNECTOR_<variable>\n    \n    the split is made on the first occurrence of the "_" character.\n'
 
 
 def environ_list_validator(value: str | list[str]) -> list[str]:
@@ -51,13 +31,13 @@ def environ_list_validator(value: str | list[str]) -> list[str]:
 
 
 def pycti_list_serializer(v: list[str], info: SerializationInfo) -> str | list[str]:
-    if isinstance(v, list) and info.context and info.context.get("mode") == "pycti":
-        return ",".join(v)  # [ "e1", "e2", "e3" ] -> "e1,e2,e3"
+    if isinstance(v, list) and info.context and (info.context.get("mode") == "pycti"):
+        return ",".join(v)
     return v
 
 
 ListFromString = Annotated[
-    list[str],  # Final type
+    list[str],
     BeforeValidator(environ_list_validator),
     PlainSerializer(pycti_list_serializer, when_used="json"),
 ]
@@ -76,21 +56,17 @@ class StreamConnectorConfig(BaseModel):
     type: Literal["STREAM"] = Field(default="STREAM")
     scope: ListFromString
     log_level: LogLevelType
-
     live_stream_id: str
     live_stream_listen_delete: bool = Field(default=True)
     live_stream_no_dependencies: bool = Field(default=True)
     live_stream_with_inferences: bool = Field(default=False)
     live_stream_recover_iso_date: str | None = Field(default=None)
     live_stream_start_timestamp: str | None = Field(default=None)
-
     listen_protocol: str = Field(default="AMQP")
     listen_protocol_uri: str = Field(default="http://127.0.0.1:7070")
     listen_protocol_path: str = Field(default="/api/callback")
     listen_protocol_ssl: bool = Field(default=False)
     listen_protocol_port: int = Field(default=7070)
-
-    # TODO: See what is specific to stream / external
     auto: bool = Field(default=False)
     expose_metrics: bool = Field(default=False)
     metrics_port: int = Field(default=9095)
@@ -99,7 +75,6 @@ class StreamConnectorConfig(BaseModel):
     validate_before_import: bool = Field(default=False)
     queue_protocol: str = Field(default="amqp")
     queue_threshold: int = Field(default=500)
-
     send_to_queue: bool = Field(default=True)
     send_to_directory: bool = Field(default=False)
     send_to_directory_path: str | None = Field(default=None)
@@ -128,8 +103,6 @@ class StreamConnectorConfig(BaseModel):
 class BaseConnectorSettings(abc.ABC, BaseSettings):
     opencti: _OpenCTIConfig
     connector: StreamConnectorConfig
-
-    # files needs to be at the same level as the module
     model_config = SettingsConfigDict(
         env_nested_delimiter="_", env_nested_max_split=1, enable_decoding=False
     )
@@ -160,9 +133,9 @@ class BaseConnectorSettings(abc.ABC, BaseSettings):
             3. Environment variables
             4. Default values
         """
-        if Path(settings_cls.model_config["yaml_file"] or "").is_file():  # type: ignore
+        if Path(settings_cls.model_config["yaml_file"] or "").is_file():
             return (YamlConfigSettingsSource(settings_cls),)
-        if Path(settings_cls.model_config["env_file"] or "").is_file():  # type: ignore
+        if Path(settings_cls.model_config["env_file"] or "").is_file():
             return (dotenv_settings,)
         return (env_settings,)
 
