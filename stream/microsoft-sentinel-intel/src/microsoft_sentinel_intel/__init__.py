a068e17ded7a70006443e7fb9cb4c9dd3c586b4d
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-12-03 17:15:31 +0100

    feat: automated migration

--- stream/microsoft-sentinel-intel/src/microsoft_sentinel_intel/__init__.py
+++ stream/microsoft-sentinel-intel/src/microsoft_sentinel_intel/__init__.py
@@ -1,9 +1,5 @@
 from microsoft_sentinel_intel.client import ConnectorClient
-from microsoft_sentinel_intel.config import ConnectorSettings
 from microsoft_sentinel_intel.connector import Connector
+from microsoft_sentinel_intel.settings import ConnectorSettings
 
-__all__ = [
-    "Connector",
-    "ConnectorClient",
-    "ConnectorSettings",
-]
+__all__ = ["Connector", "ConnectorClient", "ConnectorSettings"]
