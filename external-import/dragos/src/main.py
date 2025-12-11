5a020223a92c4d9ab5d947bd6ed8a96df6606f9c
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-12-11 17:14:50 +0100

    [dragos] Update connector to be "manager_supported" (#5243)

--- external-import/dragos/src/main.py
+++ external-import/dragos/src/main.py
@@ -3,12 +3,11 @@
 import traceback
 from datetime import timedelta
 from logging import getLogger
-from pathlib import Path
 
-from dragos.adapters.config.env import ConfigLoaderEnv
 from dragos.adapters.geocoding.octi import OctiGeocoding
 from dragos.adapters.report.dragos_v1 import ReportsAPIV1
-from dragos.app import Connector
+from dragos.connector import Connector
+from dragos.settings import ConnectorSettings
 from limiter import Limiter  # type: ignore[import-untyped]  # Limiter is not typed
 from pycti import (  # type: ignore[import-untyped]  # PyCTI is not typed
     OpenCTIConnectorHelper,
@@ -19,25 +18,13 @@ logger = getLogger(__name__)
 
 if __name__ == "__main__":
     try:
-        dev_config = Path(__file__).parent / "config.yml"
-        if dev_config.exists():
-            from dragos.adapters.config.yaml import ConfigLoaderYAML
-
-            logger.warning(
-                "Using development config file. This should not be used for production."
-            )
-            config = ConfigLoaderYAML.from_yaml_path(config_path=dev_config)
-        else:
-            config = ConfigLoaderEnv()  # type: ignore[assignment]
-            # both config are ConfigLoader abstract classes
-
-        config_dict = config.to_dict(token_as_plaintext=True)
-        helper = OpenCTIConnectorHelper(config=config_dict)
+        settings = ConnectorSettings()
+        helper = OpenCTIConnectorHelper(config=settings.to_helper_config())
         geocoding = OctiGeocoding(api_client=helper.api)
         reports = ReportsAPIV1(
-            base_url=URL(str(config.dragos.api_base_url)),
-            token=config.dragos.api_token,
-            secret=config.dragos.api_secret,
+            base_url=URL(str(settings.dragos.api_base_url)),
+            token=settings.dragos.api_token,
+            secret=settings.dragos.api_secret,
             timeout=timedelta(seconds=30),
             retry=3,
             backoff=timedelta(seconds=1),
@@ -50,7 +37,7 @@ if __name__ == "__main__":
             ),
         )
         connector = Connector(
-            config=config,
+            config=settings,
             helper=helper,
             reports=reports,
             geocoding=geocoding,
