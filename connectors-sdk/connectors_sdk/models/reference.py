9866b50d6e2fe4bd917d064d0cef75fb9db47d10
Elodie Gameiro <elodie.gameiro@filigran.io>
2025-12-17 11:55:00 +0100

    fix: add Reference to AssociatedFile and BaseIdentifiedEntity models, add more tests

--- connectors-sdk/connectors_sdk/models/reference.py
+++ connectors-sdk/connectors_sdk/models/reference.py
@@ -4,7 +4,11 @@ from pydantic import BaseModel, Field
 
 
 class Reference(BaseModel):
-    """Represent a reference."""
+    """Reference was created to easily transform an ID into an object without having
+    to regenerate the object and therefore potentially a new ID.
+    It is used in fields such as object_ref, author, source etc.
+    Warning: This model must not be sent to OCTI. It is not a Stix2 model.
+    """
 
     id: str = Field(
         description="The id of the entity.",
