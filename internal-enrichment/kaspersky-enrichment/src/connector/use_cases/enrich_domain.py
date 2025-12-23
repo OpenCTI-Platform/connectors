dc9323fb7f2e747081b07631e2500417e39cee6c
Elodie Gameiro <elodie.gameiro@filigran.io>
2025-12-23 10:26:42 +0100

    feat: manage FilesDownloaded data (create file, url and relations)

--- internal-enrichment/kaspersky-enrichment/src/connector/use_cases/enrich_domain.py
+++ internal-enrichment/kaspersky-enrichment/src/connector/use_cases/enrich_domain.py
@@ -1,5 +1,12 @@
+from datetime import datetime, timedelta
+
+from connector.constants import DATETIME_FORMAT
 from connector.converter_to_stix import ConverterToStix
-from connector.utils import is_quota_exceeded
+from connector.utils import (
+    is_last_seen_equal_to_first_seen,
+    is_quota_exceeded,
+    string_to_datetime,
+)
 from kaspersky_client import KasperskyClient
 from pycti import STIX_EXT_OCTI_SCO, OpenCTIConnectorHelper, OpenCTIStix2
 
@@ -77,11 +84,80 @@ class DomainEnricher:
 
                 if obs_ipv4:
                     octi_objects.append(obs_ipv4.to_stix2_object())
-                    asn_relation = self.converter_to_stix.create_relationship(
+                    ipv4_relation = self.converter_to_stix.create_relationship(
                         source_obj=observable_to_ref,
                         relationship_type="resolves-to",
                         target_obj=obs_ipv4,
                     )
-                    octi_objects.append(asn_relation.to_stix2_object())
+                    octi_objects.append(ipv4_relation.to_stix2_object())
+
+        # Manage FilesDownloaded
+
+        if entity_data.get("FilesDownloaded"):
+            files_downloaded = entity_data["FilesDownloaded"]
+            for file_downloaded_entity in files_downloaded:
+                # Create File object and relation
+                obs_file = self.converter_to_stix.create_file(
+                    hashes={"MD5": file_downloaded_entity["Md5"]},
+                    score=self.zone_octi_score_mapping[
+                        file_downloaded_entity["Zone"].lower()
+                    ],
+                )
+
+                if obs_file:
+                    octi_objects.append(obs_file.to_stix2_object())
+                    file_first_seen_datetime, file_last_seen_datetime = (
+                        self.get_first_and_last_seen_datetime(
+                            file_downloaded_entity["FirstSeen"],
+                            file_downloaded_entity["LastSeen"],
+                        )
+                    )
+                    file_relation = self.converter_to_stix.create_relationship(
+                        source_obj=observable_to_ref,
+                        relationship_type="related-to",
+                        target_obj=obs_file,
+                        start_time=file_first_seen_datetime,
+                        stop_time=file_last_seen_datetime,
+                    )
+                    octi_objects.append(file_relation.to_stix2_object())
+
+                # Create Url object and relation
+                obs_url = self.converter_to_stix.create_url(
+                    url_info=file_downloaded_entity,
+                    obs_url_score=self.zone_octi_score_mapping[
+                        file_downloaded_entity["Zone"].lower()
+                    ],
+                )
+
+                if obs_url:
+                    octi_objects.append(obs_url.to_stix2_object())
+                    file_first_seen_datetime, file_last_seen_datetime = (
+                        self.get_first_and_last_seen_datetime(
+                            file_downloaded_entity["FirstSeen"],
+                            file_downloaded_entity["LastSeen"],
+                        )
+                    )
+                    file_relation = self.converter_to_stix.create_relationship(
+                        source_obj=observable_to_ref,
+                        relationship_type="related-to",
+                        target_obj=obs_url,
+                        start_time=file_first_seen_datetime,
+                        stop_time=file_last_seen_datetime,
+                    )
+                    octi_objects.append(file_relation.to_stix2_object())
 
         return octi_objects
+
+    def get_first_and_last_seen_datetime(
+        self, first_seen: str, last_seen: str
+    ) -> datetime:
+        """
+        Convert first and last seen string to datetime.
+        If last==first, add one minute to last seen value.
+        """
+        first_seen_datetime = string_to_datetime(first_seen, DATETIME_FORMAT)
+        last_seen_datetime = string_to_datetime(last_seen, DATETIME_FORMAT)
+        if is_last_seen_equal_to_first_seen(first_seen_datetime, last_seen_datetime):
+            last_seen_datetime = last_seen_datetime + timedelta(minutes=1)
+
+        return first_seen_datetime, last_seen_datetime
