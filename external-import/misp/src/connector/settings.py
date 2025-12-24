bbfe541ea73e491193f5556063b4f86fdd68c097
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-12-24 09:42:53 +0100

    [MISP] Update connector to be "manager_supported" (#4857)

--- external-import/misp/src/connector/settings.py
+++ external-import/misp/src/connector/settings.py
@@ -1,58 +1,27 @@
-import os
 import warnings
-from datetime import datetime, timedelta, timezone
-from pathlib import Path
-from typing import Annotated, Literal
+from datetime import timedelta
+from typing import Annotated, Any, Literal
 
-import __main__
+from connectors_sdk import (
+    BaseConfigModel,
+    BaseConnectorSettings,
+    BaseExternalImportConnectorConfig,
+    DatetimeFromIsoString,
+    ListFromString,
+)
 from pydantic import (
-    AwareDatetime,
-    BaseModel,
     BeforeValidator,
-    ConfigDict,
     Field,
     HttpUrl,
     PlainSerializer,
+    SecretStr,
+    SerializationInfo,
+    field_validator,
     model_validator,
 )
-from pydantic_core.core_schema import SerializationInfo
-from pydantic_settings import (
-    BaseSettings,
-    PydanticBaseSettingsSource,
-    SettingsConfigDict,
-    YamlConfigSettingsSource,
-)
-
-_MAIN_PATH_ = os.path.dirname(os.path.abspath(__main__.__file__))
-
-"""
-All the variables that have default values will override configuration from the OpenCTI helper.
-
-All the variables of this classes are customizable through:
-    - config.yml 
-    - .env
-    - environment variables.
-
-If a variable is set in 2 different places, the first one will be used in this order:
-    1. YAML file
-    2. .env file
-    3. Environment variables
-    4. Default value
-    
-WARNING:
-    The Environment variables in the .env or global environment must be set in the following format:
-    OPENCTI_<variable>
-    CONNECTOR_<variable>
-    
-    the split is made on the first occurrence of the "_" character.
-"""
 
 
-class ConfigRetrievalError(Exception):
-    """Known errors wrapper for config loaders."""
-
-
-def comma_separated_dict_validator(value: str | list[str]) -> dict[str, str]:
+def comma_separated_dict_validator(value: str | dict[str, str]) -> dict[str, str]:
     """
     Convert comma-separated string into a dict.
 
@@ -79,7 +48,11 @@ def pycti_dict_serializer(value: list[str], info: SerializationInfo) -> str | li
         > serialized_values = pycti_dict_serializer({ "key_1": "value_1", "key_2"="value_2" })
         > print(serialized_values) # "key_1=value_1,key_2=value_2"
     """
-    if isinstance(value, dict) and info.context and info.context.get("mode") == "pycti":
+    if (
+        isinstance(value, dict)
+        and info.context
+        and (info.context.get("mode") == "pycti")
+    ):
         entries = [
             f"{entry_key}={entry_value}"
             for entry_key, entry_value in list(value.items())
@@ -95,144 +68,68 @@ DictFromString = Annotated[
 ]
 
 
-def comma_separated_list_validator(value: str | list[str]) -> list[str]:
+class ExternalImportConnectorConfig(BaseExternalImportConnectorConfig):
     """
-    Convert comma-separated string into a list of values.
-
-    Example:
-        > values = comma_separated_list_validator("e1,e2,e3")
-        > print(values) # [ "e1", "e2", "e3" ]
-    """
-    if isinstance(value, str):
-        if len(value) == 0:
-            return []
-        else:
-            return [string.strip() for string in value.split(",")]
-    return value
-
-
-def pycti_list_serializer(value: list[str], info: SerializationInfo) -> str | list[str]:
-    """
-    Serialize list of values as comma-separated string.
-
-    Example:
-        > serialized_values = pycti_list_serializer([ "e1", "e2", "e3" ])
-        > print(serialized_values) # "e1,e2,e3"
-    """
-    if isinstance(value, list) and info.context and info.context.get("mode") == "pycti":
-        return ",".join(value)
-    return value
-
-
-ListFromString = Annotated[
-    list[str],
-    BeforeValidator(comma_separated_list_validator),
-    PlainSerializer(pycti_list_serializer, when_used="json"),
-]
-
-
-def safe_aware_datetime_validator(value: str | datetime) -> AwareDatetime:
-    """
-    Validate and convert a string or datetime into an aware datetime object.
-
-    Example:
-        > aware_datetime = safe_aware_datetime_validator("2023-10-01T12:00:00Z")
-        > print(aware_datetime) # 2023-10-01 12:00:00+00:00
-    """
-    # Parse ISO string first
-    if isinstance(value, str):
-        value = datetime.fromisoformat(value)
-
-    # Set timezone
-    if isinstance(value, datetime):
-        if value.tzinfo:
-            value = value.astimezone(tz=timezone.utc)
-        else:  # assumes ISO string represented UTC datetime
-            value = value.replace(tzinfo=timezone.utc)
-
-    return value
-
-
-SafeAwareDatetime = Annotated[
-    AwareDatetime,
-    BeforeValidator(safe_aware_datetime_validator),
-]
-
-
-class _ConfigBaseModel(BaseModel):
-    """
-    Base class for frozen config models, i.e. not alter-able after `model_post_init()`.
-    """
-
-    model_config = ConfigDict(frozen=True, extra="allow")
-
-
-class _OpenCTIConfig(_ConfigBaseModel):
-    """
-    Define config specific to OpenCTI.
-    """
-
-    url: HttpUrl
-    token: str
-
-
-class _ConnectorConfig(_ConfigBaseModel):
-    """
-    Define config specific to this type of connector, e.g. an `external-import`.
+    Override the `BaseExternalImportConnectorConfig` to add parameters and/or defaults
+    to the configuration for connectors of type `EXTERNAL_IMPORT`.
     """
 
     id: str = Field(
-        description="A UUID v4 to identify the connector in OpenCTI.",
+        description="The UUID of the connector.",
+        default="fae73097-aa2c-4460-96ee-1aa975ce1945",
     )
     name: str = Field(
         description="The name of the connector.",
+        default="MISP",
     )
-    type: Literal["EXTERNAL_IMPORT"] = "EXTERNAL_IMPORT"
     scope: ListFromString = Field(
-        description="The scope of the connector, e.g. 'misp'.",
+        description="The scope of the connector.",
         default=["misp"],
     )
     duration_period: timedelta = Field(
         description="The period of time to await between two runs of the connector.",
         default=timedelta(minutes=5),
     )
-    log_level: Literal[
-        "debug",
-        "info",
-        "warn",  # alias of warning
-        "warning",
-        "error",
-    ] = Field(default="error")
 
 
-class _MISPConfig(_ConfigBaseModel):
+class MispConfig(BaseConfigModel):
     """
     Define config specific to MISP connector.
     """
 
-    url: str = Field(description="MISP instance URL")
-    key: str = Field(
+    url: HttpUrl = Field(
+        description="MISP instance URL",
+    )
+    key: SecretStr = Field(
         description="MISP instance API key.",
     )
     ssl_verify: bool = Field(
         description="Whether to check if the SSL certificate is valid when using `HTTPS` protocol or not.",
+        default=False,
     )
     client_cert: str | None = Field(
         description="Filepath to the client certificate to use for MISP API calls. Required if `ssl_verify` is enabled.",
         default=None,
     )
-    reference_url: str | None = Field(
+    reference_url: HttpUrl | None = Field(
         description="MISP base URL used for External References",
         default=None,
     )
     create_reports: bool = Field(
         description="Whether to create reports for each imported MISP event or not.",
+        default=True,
     )
     create_indicators: bool = Field(
         description="Whether to create an indicator for each imported MISP attribute or not.",
+        default=True,
     )
     create_observables: bool = Field(
         description="Whether to create an observable for each imported MISP attribute or not.",
+        default=True,
+    )
+    create_object_observables: bool = Field(
+        description="Whether to create a text observable for each MISP Event's object or not.",
+        default=False,
     )
     datetime_attribute: Literal[
         "date",
@@ -243,7 +140,6 @@ class _MISPConfig(_ConfigBaseModel):
         description="The attribute to use as MISP events date.",
         default="timestamp",
     )
-    # TODO: check if Literal is correct
     date_filter_field: Literal[
         "date_from",
         "timestamp",
@@ -252,52 +148,41 @@ class _MISPConfig(_ConfigBaseModel):
         description="The attribute to use as filter to query new MISP events by date.",
         default="timestamp",
     )
-    # ? What does it means??
     report_description_attribute_filters: DictFromString = Field(
         description="Filter to use to find the attribute that will be used for report description (example: 'type=comment,category=Internal reference')",
         default={},
         alias="report_description_attribute_filter",  # backward compatibility with mispelled env var
     )
-    create_object_observables: bool = Field(
-        description="Whether to create a text observable for each MISP Event's object or not.",
-        default=False,
-    )
     create_tags_as_labels: bool = Field(
         description="Whether to create labels from MISP tags or not.",
         default=True,
     )
-    # ! Not documented in README
     guess_threats_from_tags: bool = Field(
         description="Whether to **guess** and create Threats from MISP tags or not.",
         default=False,
         alias="guess_threat_from_tags",  # backward compatibility with mispelled env var
     )
-    # ! Not documented in README
     author_from_tags: bool = Field(
         description="Whether to create Authors from MISP tags or not.",
         default=False,
     )
-    # ! Not documented in README
     markings_from_tags: bool = Field(
         description="Whether to create Markings from MISP tags or not.",
         default=False,
     )
-    # ! Not documented in README
     keep_original_tags_as_label: ListFromString = Field(
         description="List of original MISP tags to keep as labels.",
         default=[],
     )
-    # ! Not documented in README
     enforce_warning_list: bool = Field(
         description="Whether to enforce the warning list for MISP events or not.",
         default=False,
     )
-    # ! Documented as MISP_REPORT_CLASS in README
     report_type: str = Field(
         description="The type of report to create on OpenCTI from MISP events.",
         default="misp-event",
     )
-    import_from_date: SafeAwareDatetime | None = Field(
+    import_from_date: DatetimeFromIsoString | None = Field(
         description="A date (ISO-8601) from which to start importing MISP events (based on events creation date).",
         default=None,
     )
@@ -339,13 +224,15 @@ class _MISPConfig(_ConfigBaseModel):
     )
     import_only_published: bool = Field(
         description="Whether to only import published MISP events or not.",
+        default=False,
     )
     import_with_attachments: bool = Field(
         description="Whether to import attachment attribute content as a file (works only with PDF).",
         default=False,
     )
-    import_to_ids_no_score: int = Field(
+    import_to_ids_no_score: int | None = Field(
         description="A score value for the indicator/observable if the attribute `to_ids` value is no.",
+        default=None,
     )
     import_unsupported_observables_as_text: bool = Field(
         description="Whether to import unsupported observable as x_opencti_text or not.",
@@ -360,70 +247,24 @@ class _MISPConfig(_ConfigBaseModel):
         default=False,
     )
 
-
-class ConfigLoader(BaseSettings):
-    """
-    Define a complete config for a connector with:
-        - opencti: the config specific to OpenCTI
-        - connector: the config specific to the `external-import` connectors
-        - misp: the config specific to the MISP connector
-    """
-
-    opencti: _OpenCTIConfig
-    connector: _ConnectorConfig
-    misp: _MISPConfig
-
-    # Setup model config and env vars parsing
-    model_config = SettingsConfigDict(
-        frozen=True,
-        extra="allow",
-        env_nested_delimiter="_",
-        env_nested_max_split=1,
-        enable_decoding=False,
-        yaml_file=f"{_MAIN_PATH_}/config.yml",
-        env_file=f"{_MAIN_PATH_}/../.env",
-    )
-
-    def __init__(self) -> None:
-        """
-        Wrap BaseConnectorConfig initialization to raise custom exception in case of error.
-        """
-        try:
-            super().__init__()
-        except Exception as e:
-            raise ConfigRetrievalError("Invalid OpenCTI configuration.", e) from e
-
-    def model_dump_pycti(self) -> dict:
-        """
-        Convert model into a valid dict for `pycti.OpenCTIConnectorHelper`.
-        """
-        return self.model_dump(mode="json", context={"mode": "pycti"})
-
+    @field_validator("reference_url", mode="before")
     @classmethod
-    def settings_customise_sources(
-        cls,
-        settings_cls: type[BaseSettings],
-        init_settings: PydanticBaseSettingsSource,
-        env_settings: PydanticBaseSettingsSource,
-        dotenv_settings: PydanticBaseSettingsSource,
-        file_secret_settings: PydanticBaseSettingsSource,
-    ) -> tuple[PydanticBaseSettingsSource, ...]:
-        """
-        Customise the sources of settings for the connector.
+    def validate_reference_url(cls, value: Any) -> Any:
+        """Replace empty string with `None` before validation."""
+        if isinstance(value, str):
+            return value or None
+        return value
 
-        This method is called by the Pydantic BaseSettings class to determine the order of sources
 
-        The configuration come in this order either from:
-            1. YAML file
-            2. .env file
-            3. Environment variables
-            4. Default values
-        """
-        if Path(settings_cls.model_config["yaml_file"] or "").is_file():  # type: ignore
-            return (YamlConfigSettingsSource(settings_cls),)
-        if Path(settings_cls.model_config["env_file"] or "").is_file():  # type: ignore
-            return (dotenv_settings,)
-        return (env_settings,)
+class ConnectorSettings(BaseConnectorSettings):
+    """
+    Override `BaseConnectorSettings` to include `ExternalImportConnectorConfig` and `MispConfig`.
+    """
+
+    connector: ExternalImportConnectorConfig = Field(
+        default_factory=ExternalImportConnectorConfig
+    )
+    misp: MispConfig = Field(default_factory=MispConfig)
 
     @model_validator(mode="before")
     @classmethod
@@ -434,12 +275,9 @@ class ConfigLoader(BaseSettings):
         """
         connector_data: dict = data.get("connector", {})
         misp_data: dict = data.get("misp", {})
-
         if interval := misp_data.pop("interval", None):
             warnings.warn(
                 "Env var 'MISP_INTERVAL' is deprecated. Use 'CONNECTOR_DURATION_PERIOD' instead."
             )
-
             connector_data["duration_period"] = timedelta(minutes=int(interval))
-
         return data
