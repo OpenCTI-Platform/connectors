fcbca6146bf3f06c16e083a052dbc82bbca794de
Hugo DUPRAS <hugo.dupras@filigran.io>
2025-12-30 17:04:06 +0100

    fix: Place imports to the top of the file

--- stream/crowdstrike-endpoint-security/src/crowdstrike_connector/__init__.py
+++ stream/crowdstrike-endpoint-security/src/crowdstrike_connector/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["CrowdstrikeConnector", "ConnectorSettings"]
 from crowdstrike_connector.connector import CrowdstrikeConnector
 from crowdstrike_connector.settings import ConnectorSettings
+
+__all__ = ["CrowdstrikeConnector", "ConnectorSettings"]
