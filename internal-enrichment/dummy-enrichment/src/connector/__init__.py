6f56725282c6deef630574cc78a417c266b3ee16
Thibaut Rouxel <thibaut.rouxel@filigran.io>
2026-01-06 17:17:17 +0100

    Add dummy connector for demo

---
+++ internal-enrichment/dummy-enrichment/src/connector/__init__.py
@@ -0,0 +1,7 @@
+from connector.connector import DummyEnrichmentConnector
+from connector.settings import ConnectorSettings
+
+__all__ = [
+    "DummyEnrichmentConnector",
+    "ConnectorSettings",
+]
