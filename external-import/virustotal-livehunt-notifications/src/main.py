72c328516c0e80edadc7c6623cc583019b0384b6
Pauline Eustachy <pauline.eustachy@filigran.io>
2026-01-08 19:21:06 +0100

    feat: add config runtime validation
    + deprecate `interval_sec` config var

--- external-import/virustotal-livehunt-notifications/src/main.py
+++ external-import/virustotal-livehunt-notifications/src/main.py
@@ -1,11 +1,19 @@
 """Virustotal livehunt notifications main file."""
 
 import traceback
-from livehunt import VirustotalLivehuntNotifications
+
+from livehunt import ConnectorSettings, VirustotalLivehuntNotifications
+from pycti import OpenCTIConnectorHelper
 
 if __name__ == "__main__":
     try:
-        vt_livehunt_notifications = VirustotalLivehuntNotifications()
+        settings = ConnectorSettings()
+        helper = OpenCTIConnectorHelper(config=settings.to_helper_config())
+
+        vt_livehunt_notifications = VirustotalLivehuntNotifications(
+            config=settings,
+            helper=helper,
+        )
         vt_livehunt_notifications.run()
     except Exception:
         traceback.print_exc()
