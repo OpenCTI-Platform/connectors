deae8870ffb32dc97fe5beeaf47899adc62f212a
Pauline Eustachy <pauline.eustachy@filigran.io>
2026-01-09 15:10:10 +0100

    feat: implement scheduler

--- external-import/virustotal-livehunt-notifications/src/livehunt/livehunt.py
+++ external-import/virustotal-livehunt-notifications/src/livehunt/livehunt.py
@@ -17,9 +17,6 @@ class VirustotalLivehuntNotifications:
     """
 
     _DEFAULT_AUTHOR = "Virustotal Livehunt Notifications"
-
-    # Default run interval
-    _CONNECTOR_RUN_INTERVAL_SEC = 60
     _STATE_LATEST_RUN_TIMESTAMP = "latest_run_timestamp"
     # Number of days to load if no state
     _LAST_DAYS_TO_LOAD = 3
@@ -67,9 +64,6 @@ class VirustotalLivehuntNotifications:
     def _current_unix_timestamp() -> int:
         return int(time.time())
 
-    def _get_interval(self) -> int:
-        return int(self.interval_sec)
-
     @staticmethod
     def _get_state_value(
         state: Optional[Mapping[str, Any]], key: str, default: Optional[Any] = None
@@ -78,115 +72,84 @@ class VirustotalLivehuntNotifications:
             return state.get(key, default)
         return default
 
-    def _is_scheduled(self, last_run: Optional[int], current_time: int) -> bool:
-        if last_run is None:
-            self.helper.connector_logger.info(
-                "Virustotal Livehunt Notifications connector clean run"
-            )
-            return True
-
-        time_diff = current_time - last_run
-        return time_diff >= self._get_interval()
-
-    def _get_next_interval(
-        self, run_interval: int, timestamp: int, last_run: int
-    ) -> int:
-        """Get the delay for the next interval."""
-        next_run = self._get_interval() - (timestamp - last_run)
-        return min(run_interval, next_run)
-
     def _load_state(self) -> dict[str, Any]:
         current_state = self.helper.get_state()
         if not current_state:
             return {}
         return current_state
 
-    @classmethod
-    def _sleep(cls, delay_sec: Optional[int] = None) -> None:
-        sleep_delay = (
-            delay_sec if delay_sec is not None else cls._CONNECTOR_RUN_INTERVAL_SEC
-        )
-        time.sleep(sleep_delay)
+    def process(self):
+        """VirustotalLivehuntNotifications main process."""
 
-    def run(self):
-        """Run VirustotalLivehuntNotifications."""
+        self.helper.connector_logger.info(
+            "Running Virustotal Livehunt Notifications connector..."
+        )
+
+        try:
+            timestamp = self._current_unix_timestamp()
+            current_state = self._load_state()
+            self.helper.connector_logger.info(
+                f"[Virustotal Livehunt Notifications] loaded state: {current_state}"
+            )
+
+            last_run = self._get_state_value(
+                current_state,
+                self._STATE_LATEST_RUN_TIMESTAMP,
+                int(
+                    datetime.timestamp(
+                        datetime.fromtimestamp(timestamp)
+                        - timedelta(days=self._LAST_DAYS_TO_LOAD)
+                    )
+                ),
+            )
+
+            self.helper.metric.inc("run_count")
+            self.helper.metric.state("running")
+            self.helper.connector_logger.info(
+                f"[Virustotal Livehunt Notifications] starting run at: {current_state}"
+            )
+            new_state = current_state.copy()
+
+            self.builder.process(last_run, timestamp)
+
+            # Set the new state
+            new_state[self._STATE_LATEST_RUN_TIMESTAMP] = self._current_unix_timestamp()
+            self.helper.connector_logger.info(
+                f"[Virustotal Livehunt Notifications] Storing new state: {new_state}"
+            )
+            self.helper.set_state(new_state)
+
+            self.helper.connector_logger.info("No new Livehunt Notifications found...")
+            self.helper.metric.state("idle")
+
+        except (KeyboardInterrupt, SystemExit):
+            self.helper.connector_logger.info(
+                "[CONNECTOR] Connector stopped...",
+                {"connector_name": self.helper.connect_name},
+            )
+            sys.exit(0)
+        except Exception as e:
+            self.helper.metric.inc("error_count")
+            self.helper.connector_logger.error(str(e))
+
+    def run(self) -> None:
+        """
+        Start the connector, schedule its runs and trigger the first run.
+        It allows you to schedule the process to run at a certain interval.
+        This specific scheduler from the `OpenCTIConnectorHelper` will also check the queue size of a connector.
+        If `CONNECTOR_QUEUE_THRESHOLD` is set, and if the connector's queue size exceeds the queue threshold,
+        the connector's main process will not run until the queue is ingested and reduced sufficiently,
+        allowing it to restart during the next scheduler check. (default is 500MB)
+
+        Example:
+            - If `CONNECTOR_DURATION_PERIOD=PT5M`, then the connector is running every 5 minutes.
+        """
         self.helper.connector_logger.info(
             "Starting Virustotal Livehunt Notifications Connector..."
         )
         self.helper.metric.state("idle")
 
-        while True:
-            self.helper.connector_logger.info(
-                "Running Virustotal Livehunt Notifications connector..."
-            )
-            run_interval = self._CONNECTOR_RUN_INTERVAL_SEC
-
-            try:
-                self.helper.connector_logger.info(
-                    f"Connector interval sec: {run_interval}"
-                )
-                timestamp = self._current_unix_timestamp()
-                current_state = self._load_state()
-                self.helper.connector_logger.info(
-                    f"[Virustotal Livehunt Notifications] loaded state: {current_state}"
-                )
-
-                last_run = self._get_state_value(
-                    current_state,
-                    self._STATE_LATEST_RUN_TIMESTAMP,
-                    int(
-                        datetime.timestamp(
-                            datetime.fromtimestamp(timestamp)
-                            - timedelta(days=self._LAST_DAYS_TO_LOAD)
-                        )
-                    ),
-                )
-
-                if self._is_scheduled(last_run, timestamp):
-                    self.helper.metric.inc("run_count")
-                    self.helper.metric.state("running")
-                    self.helper.connector_logger.info(
-                        f"[Virustotal Livehunt Notifications] starting run at: {current_state}"
-                    )
-                    new_state = current_state.copy()
-
-                    self.builder.process(last_run, timestamp)
-
-                    # Set the new state
-                    new_state[self._STATE_LATEST_RUN_TIMESTAMP] = (
-                        self._current_unix_timestamp()
-                    )
-                    self.helper.connector_logger.info(
-                        f"[Virustotal Livehunt Notifications] Storing new state: {new_state}"
-                    )
-                    self.helper.set_state(new_state)
-
-                    self.helper.connector_logger.info(
-                        "No new Livehunt Notifications found..."
-                    )
-                    self.helper.metric.state("idle")
-                else:
-                    run_interval = self._get_next_interval(
-                        run_interval, timestamp, last_run
-                    )
-                    self.helper.connector_logger.info(
-                        f"[Virustotal Livehunt Notifications] Connector will not run, next run in {run_interval} seconds"
-                    )
-
-            except (KeyboardInterrupt, SystemExit):
-                self.helper.connector_logger.info(
-                    "Virustotal Livehunt Notifications connector stop"
-                )
-                sys.exit(0)
-
-            except Exception as e:
-                self.helper.metric.inc("error_count")
-                self.helper.connector_logger.error(str(e))
-
-            if self.helper.connect_run_and_terminate:
-                self.helper.metric.state("stopped")
-                self.helper.connector_logger.info("Connector stop")
-                self.helper.force_ping()
-                sys.exit(0)
-
-            self._sleep(delay_sec=run_interval)
+        self.helper.schedule_process(
+            message_callback=self.process,
+            duration_period=self.config.connector.duration_period.total_seconds(),
+        )
