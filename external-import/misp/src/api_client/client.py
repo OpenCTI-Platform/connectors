110c58dba5ace93a8a9efa70a998ddd90c4fef89
Hugo DUPRAS <hugo.dupras@filigran.io>
2026-01-13 11:44:17 +0100

    feat: Add parameter to sort MISP event by chosen attribute

--- external-import/misp/src/api_client/client.py
+++ external-import/misp/src/api_client/client.py
@@ -65,6 +65,7 @@ class MISPClient:
         self,
         date_field_filter: str,
         date_value_filter: datetime,
+        datetime_attribute: str,
         keyword: str,
         included_tags: list,
         excluded_tags: list,
@@ -92,9 +93,6 @@ class MISPClient:
                     not_parameters=excluded_org_creators,
                 )
 
-                # MISP API doesn't provide a way to sort the results.
-                # Events are always returned sorted by Event.id ASC,
-                # which is **NOT** equivalent to sorted by Event.date (creation date) ASC
                 results = self._client.search(
                     controller="events",
                     return_format="json",
@@ -106,6 +104,9 @@ class MISPClient:
                     with_attachments=with_attachments,
                     limit=limit,
                     page=current_page,
+                    # Undocumented parameter to sort the results by the given attribute
+                    # https://www.circl.lu/doc/misp/automation/#:~:text=Example-,Search,-Events%20management
+                    order=f"Event.{datetime_attribute} ASC",
                     **{date_field_filter: date_value_filter},
                 )
                 if isinstance(results, dict) and results.get("errors"):
