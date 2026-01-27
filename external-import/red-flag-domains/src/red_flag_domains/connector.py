6ee12a0418327c8c72f543e7a7ffcad1831f10b0
Hugo DUPRAS <hugo.dupras@filigran.io>
2026-01-27 14:39:39 +0100

    feat: Use `duration_period` to manage run interval

--- external-import/red-flag-domains/src/red_flag_domains/connector.py
+++ external-import/red-flag-domains/src/red_flag_domains/connector.py
@@ -1,4 +1,3 @@
-import time
 from datetime import datetime, timedelta, timezone
 
 import requests
@@ -21,47 +20,58 @@ class RedFlagDomainImportConnector:
         self.api_url = self.config.red_flag_domains.url
 
     def run(self):
-        while True:
-            try:
-                current_state = self.helper.get_state()
-                now = datetime.now(tz=timezone.utc)
-                friendly_name = "Red Flag Domains run @ " + now.strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                )
-                work_id = self.helper.api.work.initiate_work(
-                    self.helper.connect_id, friendly_name
-                )
-                if current_state is not None and "last_run" in current_state:
-                    last_seen = datetime.fromtimestamp(current_state["last_run"])
-                    self.helper.log_info(f"Connector last ran at: {last_seen} (UTC)")
-                else:
-                    self.helper.log_info("Connector has never run")
-                domain_list = self.get_domains(self.api_url)
-                observables = self.create_observables(domain_list)
-                indicators = self.create_indicators(observables)
-                relationships = self.create_relationships(observables, indicators)
-                bundle = self.create_bundle(observables, indicators, relationships)
-                self.send_bundle(bundle, work_id)
-                events_count = len(indicators) + len(observables) + len(relationships)
-                message = (
-                    f"Connector successfully run ({events_count} events have"
-                    f"been processed), storing last_run as {now}"
-                )
-                self.helper.log_info(message)
-                self.helper.set_state({"last_run": now.timestamp()})
-                time_now = datetime.now(timezone(timedelta(hours=2)))
-                time_until_2am = timedelta(
-                    days=1,
-                    hours=2 - time_now.hour,
-                    minutes=-time_now.minute,
-                    seconds=-time_now.second,
-                )
-                time.sleep(time_until_2am.total_seconds())
-            except (KeyboardInterrupt, SystemExit):
-                self.helper.log_info("Connector stop")
-                exit(0)
-            except Exception as exception:
-                self.helper.log_error(str(exception))
+        """
+        Run the main process encapsulated in a scheduler
+        It allows you to schedule the process to run at a certain intervals
+        This specific scheduler from the pycti connector helper will also check the queue size of a connector
+        If `CONNECTOR_QUEUE_THRESHOLD` is set, if the connector's queue size exceeds the queue threshold,
+        the connector's main process will not run until the queue is ingested and reduced sufficiently,
+        allowing it to restart during the next scheduler check. (default is 500MB)
+        It requires the `duration_period` connector variable in ISO-8601 standard format
+        Example: `CONNECTOR_DURATION_PERIOD=PT5M` => Will run the process every 5 minutes
+        :return: None
+        """
+        self.helper.schedule_iso(
+            message_callback=self.process_data,
+            duration_period=self.config.connector.duration_period,
+        )
+
+    def process_data(self) -> None:
+        """
+        Process the data
+        """
+        try:
+            current_state = self.helper.get_state()
+            now = datetime.now(tz=timezone.utc)
+            friendly_name = "Red Flag Domains run @ " + now.strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+            work_id = self.helper.api.work.initiate_work(
+                self.helper.connect_id, friendly_name
+            )
+            if current_state is not None and "last_run" in current_state:
+                last_seen = datetime.fromtimestamp(current_state["last_run"])
+                self.helper.log_info(f"Connector last ran at: {last_seen} (UTC)")
+            else:
+                self.helper.log_info("Connector has never run")
+            domain_list = self.get_domains(self.api_url)
+            observables = self.create_observables(domain_list)
+            indicators = self.create_indicators(observables)
+            relationships = self.create_relationships(observables, indicators)
+            bundle = self.create_bundle(observables, indicators, relationships)
+            self.send_bundle(bundle, work_id)
+            events_count = len(indicators) + len(observables) + len(relationships)
+            message = (
+                f"Connector successfully run ({events_count} events have"
+                f"been processed), storing last_run as {now}"
+            )
+            self.helper.log_info(message)
+            self.helper.set_state({"last_run": now.timestamp()})
+        except (KeyboardInterrupt, SystemExit):
+            self.helper.log_info("Connector stop")
+            exit(0)
+        except Exception as exception:
+            self.helper.log_error(str(exception))
 
     def get_domains(self, url):
         self.helper.log_info("Enumerating domains")
