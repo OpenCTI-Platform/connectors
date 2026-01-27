1aaf610625b1a3764716750fc6d66b3783c91637
Hugo DUPRAS <hugo.dupras@filigran.io>
2026-01-27 10:30:42 +0100

    fix: Move __all__ definition after imports

--- external-import/microsoft-sentinel-incidents/src/microsoft_sentinel_incidents_connector/__init__.py
+++ external-import/microsoft-sentinel-incidents/src/microsoft_sentinel_incidents_connector/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["MicrosoftSentinelIncidentsConnector", "ConnectorSettings"]
 from microsoft_sentinel_incidents_connector.connector import (
     MicrosoftSentinelIncidentsConnector,
 )
 from microsoft_sentinel_incidents_connector.settings import ConnectorSettings
+
+__all__ = ["MicrosoftSentinelIncidentsConnector", "ConnectorSettings"]
