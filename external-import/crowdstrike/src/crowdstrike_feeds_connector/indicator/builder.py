619768eae6f506703fb4e0553085eb7c34280a58
Christophe Melchior <christophe.melchior@filigran.io>
2026-01-28 11:10:32 +0100

    [crowdstrike] remove inferred relation created through indicator #5359

--- external-import/crowdstrike/src/crowdstrike_feeds_connector/indicator/builder.py
+++ external-import/crowdstrike/src/crowdstrike_feeds_connector/indicator/builder.py
@@ -485,43 +485,13 @@ class IndicatorBundleBuilder:
         malwares = self._create_malwares()
         bundle_objects.extend(malwares)
 
-        # Intrusion sets use malwares and add to bundle.
-        intrusion_sets_use_malwares = self._create_uses_relationships(
-            intrusion_sets, malwares
-        )
-        bundle_objects.extend(intrusion_sets_use_malwares)
-
         # Create target sectors and add to bundle.
         target_sectors = self._create_targeted_sectors()
         bundle_objects.extend(target_sectors)
 
-        # Intrusion sets target sectors and add to bundle.
-        intrusion_sets_target_sectors = self._create_targets_relationships(
-            intrusion_sets, target_sectors
-        )
-        bundle_objects.extend(intrusion_sets_target_sectors)
-
-        # Malwares target sectors and add to bundle.
-        malwares_target_sectors = self._create_targets_relationships(
-            malwares, target_sectors
-        )
-        bundle_objects.extend(malwares_target_sectors)
-
         # Create vulnerabilities and add to bundle.
         vulnerabilities = self._create_vulnerabilities()
         bundle_objects.extend(vulnerabilities)
-
-        # Intrusion sets target vulnerabilities and add to bundle.
-        intrusion_sets_target_vulnerabilities = self._create_targets_relationships(
-            intrusion_sets, vulnerabilities
-        )
-        bundle_objects.extend(intrusion_sets_target_vulnerabilities)
-
-        # Malwares target vulnerabilities and add to bundle.
-        malwares_target_vulnerabilities = self._create_targets_relationships(
-            malwares, vulnerabilities
-        )
-        bundle_objects.extend(malwares_target_vulnerabilities)
 
         # Create observations.
         observation = self._create_observation(kill_chain_phases)
@@ -565,13 +535,8 @@ class IndicatorBundleBuilder:
         object_refs = create_object_refs(
             cast(List[_DomainObject], intrusion_sets),
             cast(List[_DomainObject], malwares),
-            cast(List[_RelationshipObject], intrusion_sets_use_malwares),
             cast(List[_DomainObject], target_sectors),
-            cast(List[_RelationshipObject], intrusion_sets_target_sectors),
-            cast(List[_RelationshipObject], malwares_target_sectors),
             cast(List[_DomainObject], vulnerabilities),
-            cast(List[_RelationshipObject], intrusion_sets_target_vulnerabilities),
-            cast(List[_RelationshipObject], malwares_target_vulnerabilities),
             cast(List[_DomainObject], observables),
             cast(List[_DomainObject], indicators),
             cast(List[_RelationshipObject], indicators_based_on_observables),
