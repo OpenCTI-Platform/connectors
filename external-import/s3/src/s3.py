bcfb7b98c9e584f2310bc466c230b0ebca75033d
Samuel Hassine <samuel.hassine@filigran.io>
2026-01-29 15:27:01 +0100

    [s3] Fix relations

--- external-import/s3/src/s3.py
+++ external-import/s3/src/s3.py
@@ -575,32 +575,24 @@ class S3Connector:
                 new_bundle_objects.append(json.loads(individual_credit.serialize()))
                 new_bundle_objects.append(json.loads(credit_relationship.serialize()))
 
-            # Relationships "has" - convert from vulnerability -> software/infrastructure (related-to)
-            # to software/infrastructure -> vulnerability (has)
+            # Relationships "has"
             if (
                 obj["type"] == "relationship"
                 and obj["relationship_type"] == "related-to"
                 and obj["source_ref"].startswith("vulnerability")
-                and (
-                    obj["target_ref"].startswith("software")
-                    or obj["target_ref"].startswith("infrastructure")
-                )
+                and obj["target_ref"].startswith("software")
             ):
                 obj["relationship_type"] = "has"
                 original_source_ref = obj["source_ref"]
                 obj["source_ref"] = obj["target_ref"]
                 obj["target_ref"] = original_source_ref
 
-            # Relationship "remediates" - convert from vulnerability -> software/infrastructure (remediated-by)
-            # to software/infrastructure -> vulnerability (remediates)
+            # Relationship "remediates"
             if (
                 obj["type"] == "relationship"
                 and obj["relationship_type"] == "remediated-by"
                 and obj["source_ref"].startswith("vulnerability")
-                and (
-                    obj["target_ref"].startswith("software")
-                    or obj["target_ref"].startswith("infrastructure")
-                )
+                and obj["target_ref"].startswith("software")
             ):
                 obj["relationship_type"] = "remediates"
                 original_source_ref = obj["source_ref"]
