2db7a61bdfa4c121f7880356b1f2cf63a9cdf46b
Jason Maurath <jason.maurath@filigran.io>
2026-02-03 12:15:05 -0600

    [crowdstrike]: derive indicator confidence, types, and labels from CS data

--- external-import/crowdstrike/src/crowdstrike_feeds_services/utils/__init__.py
+++ external-import/crowdstrike/src/crowdstrike_feeds_services/utils/__init__.py
@@ -1008,6 +1008,7 @@ def create_indicator(
     object_markings: Optional[List[stix2.MarkingDefinition]] = None,
     x_opencti_main_observable_type: Optional[str] = None,
     x_opencti_score: Optional[int] = None,
+    indicator_types: Optional[List[str]] = None,
 ) -> stix2.Indicator:
     """Create an indicator."""
     custom_properties: Dict[str, Any] = {}
@@ -1035,4 +1036,5 @@ def create_indicator(
         confidence=confidence,
         object_marking_refs=object_markings,
         custom_properties=custom_properties,
+        indicator_types=indicator_types,
     )
