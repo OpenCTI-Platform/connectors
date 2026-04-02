34d91a4a346325f168ce5b0d60a151f9f6d35bbc
Hugo DUPRAS <hugo.dupras@filigran.io>
2026-04-02 18:56:26 +0200

    refactor(virustotal): extract entity processing into dedicated processor classes

---
+++ internal-enrichment/virustotal/src/virustotal/processors/__init__.py
@@ -0,0 +1,11 @@
+from virustotal.processors.file import FileProcessor
+from virustotal.processors.hostname import HostnameProcessor
+from virustotal.processors.ip_address import IPProcessor
+from virustotal.processors.url import URLProcessor
+
+__all__ = [
+    "FileProcessor",
+    "HostnameProcessor",
+    "URLProcessor",
+    "IPProcessor",
+]
