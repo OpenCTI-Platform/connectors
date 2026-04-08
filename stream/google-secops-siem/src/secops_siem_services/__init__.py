df548a959ca3939f8805b3a408e899ba9d9e97c2
Hugo DUPRAS <hugo.dupras@filigran.io>
2026-04-08 15:49:44 +0200

    chore: Remove deprecated config_loader.py

--- stream/google-secops-siem/src/secops_siem_services/__init__.py
+++ stream/google-secops-siem/src/secops_siem_services/__init__.py
@@ -1,5 +1,4 @@
 from .api_client import SecOpsEntitiesClient
-from .config_loader import ConfigConnector
 from .cti_converter import CTIConverter
 
-__all__ = ["SecOpsEntitiesClient", "ConfigConnector", "CTIConverter"]
+__all__ = ["SecOpsEntitiesClient", "CTIConverter"]
