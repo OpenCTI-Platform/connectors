56403a2999bf6ae7c91aecba960c673ca486b381
guzmud <ferdinand.bellissime@filigran.io>
2026-04-16 13:55:14 +0200

    [theHive] bug(connector): using pycti id generation for Note (#6240)

--- external-import/thehive/src/tests/test_thehive.py
+++ external-import/thehive/src/tests/test_thehive.py
@@ -12,10 +12,14 @@ import thehive as module
 class TheHiveTest(unittest.TestCase):
 
     def test_process_comments_simple(self, m_os, m_yaml, m_helper, m_thehiveapi):
-        """ testing the calls made to hive API by the process_comments function """
+        """testing the calls made to hive API by the process_comments function"""
         _now = int(time.time() * 1000)
         _case = MagicMock()
-        _case_values = {"_id": sentinel.case_id, "title": MagicMock(), "_createdAt": _now}
+        _case_values = {
+            "_id": sentinel.case_id,
+            "title": MagicMock(),
+            "_createdAt": _now,
+        }
         _case.get.side_effect = _case_values.get
 
         _stix_case = MagicMock()
@@ -43,10 +47,14 @@ class TheHiveTest(unittest.TestCase):
         self.assertEqual(processed_comments[0]["object_refs"], [sentinel.stix_case_id])
 
     def test_process_comments_duplicate(self, m_os, m_yaml, m_helper, m_thehiveapi):
-        """ testing process_comments proper handling of duplicates """
+        """testing process_comments proper handling of duplicates"""
         _now = int(time.time() * 1000)
         _case = MagicMock()
-        _case_values = {"_id": sentinel.case_id, "title": MagicMock(), "_createdAt": _now}
+        _case_values = {
+            "_id": sentinel.case_id,
+            "title": MagicMock(),
+            "_createdAt": _now,
+        }
         _case.get.side_effect = _case_values.get
 
         _stix_case = MagicMock()
@@ -56,7 +64,10 @@ class TheHiveTest(unittest.TestCase):
         _case_comment = MagicMock()
         _case_comment_values = {"message": _comment, "_createdAt": _now}
         _case_comment.get.side_effect = _case_comment_values.get
-        m_thehiveapi.return_value.case.find_comments.return_value = [_case_comment, _case_comment]
+        m_thehiveapi.return_value.case.find_comments.return_value = [
+            _case_comment,
+            _case_comment,
+        ]
 
         m_os.path.isfile.return_value = False
         _connector = module.TheHive()
@@ -72,8 +83,12 @@ class TheHiveTest(unittest.TestCase):
         self.assertEqual(processed_comments[0]["type"], "note")
         self.assertEqual(processed_comments[0]["type"], processed_comments[1]["type"])
         self.assertEqual(processed_comments[0]["content"], _comment)
-        self.assertEqual(processed_comments[0]["content"], processed_comments[1]["content"])
+        self.assertEqual(
+            processed_comments[0]["content"], processed_comments[1]["content"]
+        )
         self.assertEqual(processed_comments[0]["object_refs"], [sentinel.stix_case_id])
-        self.assertEqual(processed_comments[0]["object_refs"], processed_comments[1]["object_refs"])
+        self.assertEqual(
+            processed_comments[0]["object_refs"], processed_comments[1]["object_refs"]
+        )
 
         self.assertEqual(processed_comments[0]["id"], processed_comments[1]["id"])
