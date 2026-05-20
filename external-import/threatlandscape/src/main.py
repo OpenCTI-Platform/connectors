9e66ae2ba1de0cf6ffb9dc45b1250af112baa24c
Threat Landscape <contact@threatlandscape.io>
2026-05-20 23:12:54 +0200

    Implement Threat Landscape Connector

---
+++ external-import/threatlandscape/src/main.py
@@ -0,0 +1,16 @@
+import traceback
+
+from pycti import OpenCTIConnectorHelper
+
+from connector import ConnectorSettings, ThreatLandscapeConnector
+
+if __name__ == "__main__":
+    try:
+        settings = ConnectorSettings()
+        helper = OpenCTIConnectorHelper(config=settings.to_helper_config())
+
+        connector = ThreatLandscapeConnector(config=settings, helper=helper)
+        connector.run()
+    except Exception:
+        traceback.print_exc()
+        exit(1)
