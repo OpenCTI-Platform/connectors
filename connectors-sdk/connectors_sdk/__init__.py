58928225a690fa09efb58146f5de52cf159aa0d2
Powlinett <pauline.eustachy@filigran.io>
2026-05-27 17:19:08 +0200

    feat: create logging module

--- connectors-sdk/connectors_sdk/__init__.py
+++ connectors-sdk/connectors_sdk/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "0.1.0"
 
 from connectors_sdk.connectors.external_import._work_manager import WorkManager
+from connectors_sdk.logging.logger import Logger, logger
 from connectors_sdk.connectors.external_import.base_data_processor import (
     BaseDataProcessor,
 )
@@ -34,6 +35,9 @@ from connectors_sdk.settings.exceptions import (
 from connectors_sdk.states.states import ExternalImportConnectorState
 
 __all__ = [
+    # Logger
+    "Logger",  # mostly for typing purposes
+    "logger",
     # Base Settings
     "BaseConnectorSettings",
     # Base Configs
