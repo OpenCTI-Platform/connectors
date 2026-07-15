44c0bc8f07dfe3c5d344a7b8953689d3171c9f65
Powlinett <pauline.eustachy@filigran.io>
2026-07-15 14:59:55 +0200

    feat: automated migration

---
+++ external-import/citalid/src/connector/settings.py
@@ -0,0 +1,63 @@
+from datetime import timedelta
+
+from connectors_sdk import (
+    BaseConfigModel,
+    BaseConnectorSettings,
+    BaseExternalImportConnectorConfig,
+    ListFromString,
+)
+from pydantic import Field, HttpUrl, SecretStr
+
+
+class ExternalImportConnectorConfig(BaseExternalImportConnectorConfig):
+    """
+    Override the `BaseExternalImportConnectorConfig` to add parameters and/or defaults
+    to the configuration for connectors of type `EXTERNAL_IMPORT`.
+    """
+
+    id: str = Field(
+        description="A UUID v4 to identify the connector in OpenCTI.",
+        default="bfbc4966-88dd-4d2a-9ded-4c97e4bf90b5",
+    )
+    name: str = Field(
+        description="The name of the connector.",
+        default="Citalid",
+    )
+    scope: ListFromString = Field(
+        description="The scope of the connector.",
+        default=[],
+    )
+    duration_period: timedelta = Field(
+        description="The period of time to await between two runs of the connector.",
+        default=timedelta(hours=1),
+    )
+
+
+class CitalidConfig(BaseConfigModel):
+    """
+    Define parameters and/or defaults for the configuration specific to the `CitalidConnector`.
+    """
+
+    customer_sub_domain_url: str = Field(
+        description="URL of your Citalid instance (customer subdomain).",
+    )
+    user: str = Field(
+        description="Username with access to the Citalid instance.",
+    )
+    password: SecretStr = Field(
+        description="Password for the Citalid user.",
+    )
+    interval: int = Field(
+        description="Polling interval in hours between connector runs.",
+    )
+
+
+class ConnectorSettings(BaseConnectorSettings):
+    """
+    Override `BaseConnectorSettings` to include `ExternalImportConnectorConfig` and `CitalidConfig`.
+    """
+
+    connector: ExternalImportConnectorConfig = Field(
+        default_factory=ExternalImportConnectorConfig
+    )
+    citalid: CitalidConfig = Field(default_factory=CitalidConfig)
