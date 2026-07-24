87ece401c0ea1d2c2191cf9c6852b59adf3fe266
gkallenborn <g.kallenborn@protonmail.com>
2026-07-24 16:41:27 +0200

    fix main.py

--- external-import/withaname/src/main.py
+++ external-import/withaname/src/main.py
@@ -1,6 +1,6 @@
 import traceback
 
-from connector import ConnectorSettings, DdosiaConnector
+from connector import ConnectorSettings, WithanameConnector
 from pycti import OpenCTIConnectorHelper
 
 if __name__ == "__main__":
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         settings = ConnectorSettings()
         helper = OpenCTIConnectorHelper(config=settings.to_helper_config())
 
-        connector = DdosiaConnector(config=settings, helper=helper)
+        connector = WithanameConnector(config=settings, helper=helper)
         connector.run()
     except Exception:
         traceback.print_exc()
