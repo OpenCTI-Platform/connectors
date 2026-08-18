a17e03438886f2891410f2c32f38ebdc0c48651e
Helene Nguyen <helene.nguyen@filigran.io>
2026-08-18 22:34:31 +0200

    feat(export-file-stix): Update scope

--- internal-export-file/export-file-stix/src/settings.py
+++ internal-export-file/export-file-stix/src/settings.py
@@ -1,11 +1,23 @@
 from connectors_sdk import (
     BaseConnectorSettings,
     BaseInternalExportFileConnectorConfig,
+    ListFromString,
 )
 from pydantic import Field
 
 
+class InternalExportFileConnectorConfig(BaseInternalExportFileConnectorConfig):
+    """
+    Override the `BaseConnectorConfig` to add connector specific configuration parameters and/or defaults.
+    """
+
+    scope: ListFromString = Field(
+        default=["application/vnd.oasis.stix+json"],
+        description="The scope or type of data the connector is importing, either a MIME type or Stix Object (for information only).",
+    )
+
+
 class ConnectorSettings(BaseConnectorSettings):
-    connector: BaseInternalExportFileConnectorConfig = Field(
-        default_factory=BaseInternalExportFileConnectorConfig
+    connector: InternalExportFileConnectorConfig = Field(
+        default_factory=InternalExportFileConnectorConfig
     )
