b7df85615bac0bfbef07dc5af0c634400c7b352a
Helene Nguyen <helene.nguyen@filigran.io>
2026-08-19 12:02:09 +0200

    feat(export-file-stix): Update settings

--- internal-export-file/export-file-stix/src/settings.py
+++ internal-export-file/export-file-stix/src/settings.py
@@ -11,6 +11,14 @@ class InternalExportFileConnectorConfig(BaseInternalExportFileConnectorConfig):
     Override the `BaseConnectorConfig` to add connector specific configuration parameters and/or defaults.
     """
 
+    id: str = Field(
+        description="A UUID v4 to identify the connector in OpenCTI.",
+        default="9c06c7a0-96e3-4ca5-924c-5437587660f1",
+    )
+    name: str = Field(
+        description="The name of the connector.",
+        default="ExportFileStix2",
+    )
     scope: ListFromString = Field(
         default=["application/vnd.oasis.stix+json"],
         description="The scope or type of data the connector is importing, either a MIME type or Stix Object (for information only).",
