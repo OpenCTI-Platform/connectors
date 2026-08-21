51243579797ea16c6c848832f7035ae164f6cfa8
Hugo DUPRAS <hugo.dupras@filigran.io>
2026-08-21 09:52:07 +0200

    feat(export-file-csv): use pydantic settings in existing connector code (#7224)

--- internal-export-file/export-file-csv/src/export-file-csv.py
+++ internal-export-file/export-file-csv/src/export-file-csv.py
@@ -1,34 +1,23 @@
 import csv
 import io
 import json
-import os
 import sys
 import time
 
-import yaml
-from pycti import OpenCTIConnectorHelper, get_config_variable
+from pycti import OpenCTIConnectorHelper
+from settings import ConnectorSettings
 
 
 class ExportFileCsv:
     def __init__(self):
+        # Load and validate the configuration through the Pydantic settings
+        self.config = ConnectorSettings()
         # Instantiate the connector helper from config
-        config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
-        config = (
-            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
-            if os.path.isfile(config_file_path)
-            else {}
-        )
-        self.helper = OpenCTIConnectorHelper(config)
-        self.export_file_csv_delimiter = get_config_variable(
-            "EXPORT_FILE_CSV_DELIMITER",
-            ["export-file-csv", "delimiter"],
-            config,
-            False,
-            ";",
-        )
-        self.errors: list[Exception] = (
-            []
-        )  # error holder to be reset before each new process
+        self.helper = OpenCTIConnectorHelper(config=self.config.to_helper_config())
+        self.export_file_csv_delimiter = self.config.export_file_csv.delimiter
+        self.errors: list[
+            Exception
+        ] = []  # error holder to be reset before each new process
 
     # The frontend (opencti #15593) sends the visible DataTable column ids as
     # the ``visible_columns`` list. A few presentation column ids do not map 1:1
